1269d6b8afbdba7822153e03f48a600617d4ff28
Captain.B <bin@fit2cloud.com>
2020-02-17 11:33:48 +0800

    project backend

--- backend/src/main/java/io/metersphere/service/ProjectService.java
+++ backend/src/main/java/io/metersphere/service/ProjectService.java
@@ -7,12 +7,39 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class ProjectService {
     @Resource
     private ProjectMapper projectMapper;
+
+    public Project addProject(Project project) {
+        project.setId(UUID.randomUUID().toString());
+        long createTime = System.currentTimeMillis();
+        project.setCreateTime(createTime);
+        project.setUpdateTime(createTime);
+        // todo set workspace id
+//        project.setWorkspaceId();
+        projectMapper.insertSelective(project);
+        return project;
+    }
+
+    public List<Project> getProjectList() {
+        // todo 查询条件设置
+        return projectMapper.selectByExample(null);
+    }
+
+    public void deleteProject(String projectId) {
+        projectMapper.deleteByPrimaryKey(projectId);
+    }
+
+    public void updateProject(Project project) {
+        project.setCreateTime(null);// 创建时间禁止修改
+        project.setUpdateTime(System.currentTimeMillis());
+        projectMapper.updateByPrimaryKeySelective(project);
+    }
 
     public List<Project> listAll() {
         return projectMapper.selectByExample(null);
