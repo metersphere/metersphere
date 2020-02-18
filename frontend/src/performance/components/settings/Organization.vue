9a316738e3cab675815bd65ae5fbb0a89f8942d3
shiziyuan9527 <yuhao.li@fit2cloud.com>
2020-02-18 22:43:13 +0800

    用户列表分页和组织列表分页

--- frontend/src/performance/components/settings/Organization.vue
+++ frontend/src/performance/components/settings/Organization.vue
@@ -13,16 +13,35 @@
           </span>
         </el-row>
       </div>
-      <el-table :data="items" style="width: 100%">
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column type="selection" width="55"/>
         <el-table-column prop="name" label="名称"/>
         <el-table-column prop="description" label="描述"/>
-        <el-table-column>
+        <el-table-column label="操作">
           <template slot-scope="scope">
             <el-button @click="edit(scope.row)" type="primary" icon="el-icon-edit" size="mini" circle/>
             <el-button @click="del(scope.row)" type="danger" icon="el-icon-delete" size="mini" circle/>
           </template>
         </el-table-column>
       </el-table>
+
+      <div>
+        <el-row>
+          <el-col :span="22" :offset="1">
+            <div class="table-page">
+              <el-pagination
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+                :current-page.sync="currentPage"
+                :page-sizes="[5, 10, 20, 50, 100]"
+                :page-size="pageSize"
+                layout="total, sizes, prev, pager, next, jumper"
+                :total="total">
+              </el-pagination>
+            </div>
+          </el-col>
+        </el-row>
+      </div>
     </el-card>
 
     <el-dialog title="创建组织" :visible.sync="createVisible" width="30%" @closed="closeFunc" :destroy-on-close="true">
@@ -62,92 +81,22 @@
   export default {
     name: "MsOrganization",
     components: {MsCreateBox},
-    created() {
-      this.getOrganizationList();
-    },
-    methods: {
-      create() {
-        this.createVisible = true;
-      },
-      edit(row) {
-        window.console.log(row);
-        // this.loading = true;
-        this.updateVisible = true;
-        this.form = row;
-      },
-      del(row) {
-        window.console.log(row);
-        this.$confirm('此操作将永久删除该组织, 是否继续?', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(() => {
-          this.$get(`/organization/delete/${row.id}`).then(() => {
-            this.getOrganizationList()
-          });
-          this.$message({
-            type: 'success',
-            message: '删除成功!'
-          });
-        }).catch(() => {
-          this.$message({
-            type: 'info',
-            message: '已取消删除'
-          });
-        });
-      },
-      createOrganization(createOrganizationForm) {
-        this.$refs[createOrganizationForm].validate( valide => {
-          if (valide) {
-            this.$post("/organization/add", this.form)
-              .then(() => {
-                this.$message({
-                    type: 'success',
-                    message: '添加成功!'
-                  },
-                this.createVisible = false,
-                this.getOrganizationList())
-              });
-          } else {
-            return false;
-          }
-        })
-      },
-      updateOrganization(udpateOrganizationForm) {
-        this.$refs[udpateOrganizationForm].validate(valide => {
-          if (valide) {
-            this.$post("/organization/update", this.form)
-              .then(() => {
-                this.$message({
-                    type: 'success',
-                    message: '修改成功!'
-                  },
-                this.updateVisible = false,
-                this.getOrganizationList(),
-                self.loading = false)
-              });
-          } else {
-            return false;
-          }
-        })
-      },
-      getOrganizationList() {
-        this.$get("/organization/list").then(response => {
-          this.items = response.data.data;
-        })
-      },
-      closeFunc() {
-        this.form = {};
-      }
-    },
     data() {
       return {
+        queryPath: '/organization/list',
+        deletePath: '/organization/delete/',
+        createPath: '/organization/add',
+        updatePath: '/organization/update',
         loading: false,
         createVisible: false,
         updateVisible: false,
+        multipleSelection: [],
+        currentPage: 1,
+        pageSize: 5,
+        total: 0,
         btnTips: "添加组织",
         condition: "",
-        items: [],
+        tableData: [],
         form: {},
         rule: {
           name: [
@@ -165,12 +114,127 @@
           ]
         }
       }
+    },
+    created() {
+      this.initTableData();
+    },
+    methods: {
+      create() {
+        this.createVisible = true;
+      },
+      edit(row) {
+        // this.loading = true;
+        this.updateVisible = true;
+        this.form = row;
+      },
+      del(row) {
+        this.$confirm('是否删除组织' + row.name +' ？', '', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$get(this.deletePath + row.id).then(response => {
+            if (response.data.success) {
+              this.$message({
+                type: 'success',
+                message: '删除成功!'
+              });
+            } else {
+              this.$message.error(response.message);
+            }
+            this.initTableData()
+          });
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });
+        });
+      },
+      createOrganization(createOrganizationForm) {
+        this.$refs[createOrganizationForm].validate( valide => {
+          if (valide) {
+            this.$post(this.createPath, this.form)
+              .then(response => {
+                if (response.data.success) {
+                  this.$message({
+                    type: 'success',
+                    message: '添加成功!'
+                  });
+                  this.initTableData();
+                } else {
+                  this.$message.error(response.message);
+                }
+                this.createVisible = false;
+              });
+          } else {
+            return false;
+          }
+        })
+      },
+      updateOrganization(udpateOrganizationForm) {
+        this.$refs[udpateOrganizationForm].validate(valide => {
+          if (valide) {
+            this.$post(this.updatePath, this.form)
+              .then(response => {
+                if (response.data.success) {
+                  this.$message({
+                    type: 'success',
+                    message: '修改成功!'
+                  });
+                  this.updateVisible = false;
+                } else {
+                  this.$message.error(response.message);
+                }
+                this.initTableData();
+                self.loading = false;
+              });
+          } else {
+            return false;
+          }
+        })
+      },
+      initTableData() {
+        this.$post(this.buildPagePath(this.queryPath)).then(response => {
+          if (response.data.success) {
+            let data = response.data.data;
+            this.total = data.itemCount;
+            this.tableData = data.listObject;
+          } else {
+            this.$message.error(response.message);
+          }
+        })
+      },
+      closeFunc() {
+        this.form = {};
+      },
+      buildPagePath(path) {
+        return path + "/" + this.currentPage + "/" + this.pageSize;
+      },
+      handleSizeChange(size) {
+        this.pageSize = size;
+        this.initTableData();
+      },
+      handleCurrentChange(current) {
+        this.currentPage = current;
+        this.initTableData();
+      },
+      handleSelectionChange(val) {
+        this.multipleSelection = val;
+      }
     }
+
   }
 </script>
 
 <style scoped>
   .search {
     width: 240px;
+  }
+
+  .table-page {
+    padding-top: 20px;
+    margin-right: -9px;
+    float: right;
   }
 </style>
