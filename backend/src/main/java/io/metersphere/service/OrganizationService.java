369b0399cce2b611633ffe94cdcd3d7e5b9bd85b
shiziyuan9527 <yuhao.li@fit2cloud.com>
2020-03-02 18:20:14 +0800

    修改编辑成员角色时出现的问题

--- backend/src/main/java/io/metersphere/service/OrganizationService.java
+++ backend/src/main/java/io/metersphere/service/OrganizationService.java
@@ -84,21 +84,20 @@ public class OrganizationService {
         List<String> roles = memberDTO.getRoleIds();
         List<String> allRoleIds = memberRoles.stream().map(Role::getId).collect(Collectors.toList());
         // 更新用户时添加了角色
-        if (roles.size() > allRoleIds.size()) {
-            for (int i = 0; i < roles.size(); i++) {
-                if (checkSourceRole(orgId, userId, roles.get(i)) == 0) {
-                    UserRole userRole = new UserRole();
-                    userRole.setId(UUID.randomUUID().toString());
-                    userRole.setUserId(userId);
-                    userRole.setRoleId(roles.get(i));
-                    userRole.setSourceId(orgId);
-                    userRole.setCreateTime(System.currentTimeMillis());
-                    userRole.setUpdateTime(System.currentTimeMillis());
-                    userRoleMapper.insertSelective(userRole);
-                }
+        for (int i = 0; i < roles.size(); i++) {
+            if (checkSourceRole(orgId, userId, roles.get(i)) == 0) {
+                UserRole userRole = new UserRole();
+                userRole.setId(UUID.randomUUID().toString());
+                userRole.setUserId(userId);
+                userRole.setRoleId(roles.get(i));
+                userRole.setSourceId(orgId);
+                userRole.setCreateTime(System.currentTimeMillis());
+                userRole.setUpdateTime(System.currentTimeMillis());
+                userRoleMapper.insertSelective(userRole);
             }
-        } else if (roles.size() < allRoleIds.size()){
-            allRoleIds.removeAll(roles);
+        }
+        allRoleIds.removeAll(roles);
+        if (allRoleIds.size() > 0) {
             UserRoleExample userRoleExample = new UserRoleExample();
             userRoleExample.createCriteria().andUserIdEqualTo(userId)
                     .andSourceIdEqualTo(orgId)
