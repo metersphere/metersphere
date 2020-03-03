33bd1c55be8b86d2dbc0b86b2ad6ca36e6b4c4c3
Captain.B <bin@fit2cloud.com>
2020-03-03 14:01:50 +0800

    异常处理

---
+++ backend/src/main/java/io/metersphere/controller/handler/RestControllerExceptionHandler.java
@@ -0,0 +1,30 @@
+package io.metersphere.controller.handler;
+
+
+import io.metersphere.commons.exception.MSException;
+import io.metersphere.controller.ResultHolder;
+import org.apache.shiro.ShiroException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@RestControllerAdvice
+public class RestControllerExceptionHandler {
+    /*=========== Shiro 异常拦截==============*/
+    @ExceptionHandler(ShiroException.class)
+    public ResultHolder exceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception exception) {
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        return ResultHolder.error(exception.getMessage());
+    }
+
+
+    @ExceptionHandler(MSException.class)
+    public ResultHolder f2cExceptionHandler(HttpServletRequest request, HttpServletResponse response, MSException e) {
+        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return ResultHolder.error(e.getMessage());
+    }
+}
