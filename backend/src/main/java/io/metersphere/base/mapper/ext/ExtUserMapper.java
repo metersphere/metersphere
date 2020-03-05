42b4dfac7fa8f3847562e27e124fd673a68f98cc
shiziyuan9527 <yuhao.li@fit2cloud.com>
2020-03-05 13:26:54 +0800

    系统菜单下用户搜索

---
+++ backend/src/main/java/io/metersphere/base/mapper/ext/ExtUserMapper.java
@@ -0,0 +1,11 @@
+package io.metersphere.base.mapper.ext;
+
+import io.metersphere.base.domain.User;
+import io.metersphere.controller.request.UserRequest;
+import org.apache.ibatis.annotations.Param;
+import java.util.List;
+
+public interface ExtUserMapper {
+
+    List<User> getUserList(@Param("userRequest") UserRequest request);
+}
