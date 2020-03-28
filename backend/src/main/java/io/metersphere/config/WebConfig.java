408f3f0d4754d21d7a2d200db1c66aff55263e45
Captain.B <bin@fit2cloud.com>
2020-03-28 10:49:49 +0800

    删除不用的依赖

--- backend/src/main/java/io/metersphere/config/WebConfig.java
+++ backend/src/main/java/io/metersphere/config/WebConfig.java
@@ -1,7 +1,9 @@
 package io.metersphere.config;
 
 import io.metersphere.interceptor.TestInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new TestInterceptor());
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
