3f93d09a62192e77d1991778cfbacc118ac731e0
Captain.B <bin@fit2cloud.com>
2020-04-20 15:29:25 +0800

    testid

--- backend/src/main/java/io/metersphere/engine/docker/request/BaseRequest.java
+++ backend/src/main/java/io/metersphere/engine/docker/request/BaseRequest.java
@@ -1,5 +1,13 @@
 package io.metersphere.engine.docker.request;
 
 public class BaseRequest {
+    private String testId;
 
+    public String getTestId() {
+        return testId;
+    }
+
+    public void setTestId(String testId) {
+        this.testId = testId;
+    }
 }
