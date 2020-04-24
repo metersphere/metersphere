f3b9026421d0a91a923951b720f8e224fd98e048
shiziyuan9527 <yuhao.li@fit2cloud.com>
2020-04-24 18:04:45 +0800

    修改消息提示

--- frontend/src/business/components/performance/test/components/PerformanceBasicConfig.vue
+++ frontend/src/business/components/performance/test/components/PerformanceBasicConfig.vue
@@ -107,7 +107,7 @@
         }
 
         if (this.tableData.filter(f => f.name === file.name).length > 0) {
-          this.$message.error(this.$t('load_test.delete_file'));
+          this.$error(this.$t('load_test.delete_file'));
           return false;
         }
 
@@ -175,7 +175,7 @@
         }
       },
       handleExceed() {
-        this.$message.error(this.$t('load_test.delete_file'));
+        this.$error(this.$t('load_test.delete_file'));
       },
       fileValidator(file) {
         /// todo: 是否需要对文件内容和大小做限制
