81b888b2a2880263aa9a0e2d9adf7de8f0728bea
q4speed <kun@fit2cloud.com>
2020-05-10 11:54:35 +0800

    调整JMeter Home目录

--- backend/src/main/java/io/metersphere/api/jmeter/JMeterService.java
+++ backend/src/main/java/io/metersphere/api/jmeter/JMeterService.java
@@ -1,15 +1,16 @@
 package io.metersphere.api.jmeter;
 
 import io.metersphere.commons.exception.MSException;
-import io.metersphere.commons.utils.CommonBeanFactory;
+import io.metersphere.commons.utils.LogUtil;
 import io.metersphere.config.JmeterProperties;
 import io.metersphere.i18n.Translator;
 import org.apache.jmeter.save.SaveService;
 import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jorphan.collections.HashTree;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
 
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
 
@@ -24,8 +25,8 @@ public class JMeterService {
     public void run(InputStream is) {
         String JMETER_HOME = jmeterProperties.getHome();
         String JMETER_PROPERTIES = JMETER_HOME + "/bin/jmeter.properties";
-        JMeterUtils.loadJMeterProperties(JMETER_PROPERTIES);
-        JMeterUtils.setJMeterHome(JMETER_HOME);
+        JMeterUtils.loadJMeterProperties(getPath(JMETER_PROPERTIES));
+        JMeterUtils.setJMeterHome(getPath(JMETER_HOME));
         try {
             Object scriptWrapper = SaveService.loadElement(is);
             HashTree testPlan = getHashTree(scriptWrapper);
@@ -37,9 +38,19 @@ public class JMeterService {
         }
     }
 
-    public HashTree getHashTree(Object scriptWrapper) throws Exception {
+    private HashTree getHashTree(Object scriptWrapper) throws Exception {
         Field field = scriptWrapper.getClass().getDeclaredField("testPlan");
         field.setAccessible(true);
         return (HashTree) field.get(scriptWrapper);
+    }
+
+    private String getPath(String path) {
+        try {
+            return ResourceUtils.getURL(ResourceUtils.CLASSPATH_URL_PREFIX + path).getPath();
+        } catch (FileNotFoundException e) {
+            LogUtil.error("file not found: " + path, e);
+            MSException.throwException(Translator.get("api_file_not_found_error"));
+        }
+        return null;
     }
 }
