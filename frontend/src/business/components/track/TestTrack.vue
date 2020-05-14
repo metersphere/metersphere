a560fc8de4c9eabbcc55835eae70e1d01ac2086e
chenjianxing <jianxing.chen@fit2cloud.com>
2020-05-14 16:45:49 +0800

    修复样式

--- frontend/src/business/components/track/TestTrack.vue
+++ frontend/src/business/components/track/TestTrack.vue
@@ -33,7 +33,7 @@
     position: relative;
     border: 1px solid #EBEEF5;
     box-sizing: border-box;
-    min-height: calc(100vh - 80px);
+    height: calc(100vh - 80px);
     background: white;
   }
 
