78aaf1ebf3788712477a34909c63ed3116123096
q4speed <kun@fit2cloud.com>
2020-05-18 14:14:23 +0800

    跳转链接

--- frontend/src/business/components/api/home/ApiReportRecentList.vue
+++ frontend/src/business/components/api/home/ApiReportRecentList.vue
@@ -4,7 +4,11 @@
       <span class="title">{{$t('api_report.title')}}</span>
     </template>
     <el-table :data="tableData" class="table-content">
-      <el-table-column prop="name" :label="$t('commons.name')" width="150" show-overflow-tooltip/>
+      <el-table-column :label="$t('commons.name')" width="150" show-overflow-tooltip>
+        <template v-slot:default="scope">
+          <el-link type="info" @click="link(scope.row)">{{ scope.row.name }}</el-link>
+        </template>
+      </el-table-column>
       <el-table-column width="250" :label="$t('commons.create_time')">
         <template v-slot:default="scope">
           <span>{{ scope.row.createTime | timestampFormatDate }}</span>
@@ -46,6 +50,11 @@
           this.tableData = response.data;
         });
       },
+      link(row) {
+        this.$router.push({
+          path: '/api/report/view/' + row.id,
+        })
+      }
     },
 
     created() {
--- frontend/src/business/components/api/home/ApiTestRecentList.vue
+++ frontend/src/business/components/api/home/ApiTestRecentList.vue
@@ -4,7 +4,11 @@
       <span class="title">{{$t('api_test.title')}}</span>
     </template>
     <el-table :data="tableData" class="table-content">
-      <el-table-column prop="name" :label="$t('commons.name')" width="150" show-overflow-tooltip/>
+      <el-table-column :label="$t('commons.name')" width="150" show-overflow-tooltip>
+        <template v-slot:default="scope">
+          <el-link type="info" @click="link(scope.row)">{{ scope.row.name }}</el-link>
+        </template>
+      </el-table-column>
       <el-table-column prop="projectName" :label="$t('load_test.project_name')" width="150" show-overflow-tooltip/>
       <el-table-column width="250" :label="$t('commons.create_time')">
         <template v-slot:default="scope">
@@ -46,6 +50,11 @@
         this.result = this.$get("/api/recent/5", response => {
           this.tableData = response.data;
         });
+      },
+      link(row) {
+        this.$router.push({
+          path: '/api/test/edit?id=' + row.id,
+        })
       }
     },
 
