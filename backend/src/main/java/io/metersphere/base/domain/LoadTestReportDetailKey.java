b623a346311dde0d238daadc344e60253e8e372a
Captain.B <bin@fit2cloud.com>
2020-05-20 18:56:28 +0800

    修改保存metric

---
+++ backend/src/main/java/io/metersphere/base/domain/LoadTestReportDetailKey.java
@@ -0,0 +1,14 @@
+package io.metersphere.base.domain;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class LoadTestReportDetailKey implements Serializable {
+    private String reportId;
+
+    private Long part;
+
+    private static final long serialVersionUID = 1L;
+}
