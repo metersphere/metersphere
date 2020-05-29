94724302096d4a1c4274c921a17f6abe19b219e4
q4speed <kun@fit2cloud.com>
2020-05-29 15:28:12 +0800

    修改按钮禁用

--- frontend/src/business/components/api/test/ApiTestConfig.vue
+++ frontend/src/business/components/api/test/ApiTestConfig.vue
@@ -30,10 +30,10 @@
               <el-dropdown trigger="click" @command="handleCommand">
                 <el-button class="el-dropdown-link more" icon="el-icon-more" plain/>
                 <el-dropdown-menu slot="dropdown">
-                  <el-dropdown-item command="report" v-if="test.status === 'Completed'">
+                  <el-dropdown-item command="report" :disabled="test.status !== 'Completed'">
                     {{$t('api_report.title')}}
                   </el-dropdown-item>
-                  <el-dropdown-item command="performance">
+                  <el-dropdown-item command="performance" :disabled="create">
                     {{$t('api_test.create_performance_test')}}
                   </el-dropdown-item>
                 </el-dropdown-menu>
