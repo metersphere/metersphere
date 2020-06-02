ffaefb398592cf3af0d43df83eb39143764349f0
wenyann <64353056+wenyann@users.noreply.github.com>
2020-06-02 15:20:39 +0800

    修改API测试判断名称不能重复

--- backend/src/main/java/io/metersphere/api/service/APITestService.java
+++ backend/src/main/java/io/metersphere/api/service/APITestService.java
@@ -130,6 +130,7 @@ public class APITestService {
         test.setScenarioDefinition(request.getScenarioDefinition());
         test.setUpdateTime(System.currentTimeMillis());
         test.setStatus(APITestStatus.Saved.name());
+        checkApiTestPlanExist(test);
         apiTestMapper.updateByPrimaryKeySelective(test);
         return test;
     }
