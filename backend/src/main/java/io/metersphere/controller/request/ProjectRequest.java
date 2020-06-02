4f3aa4b8aefadbb52ae0e3a429ab1a175d165286
shiziyuan9527 <yuhao.li@fit2cloud.com>
2020-06-02 14:50:39 +0800

    项目排序

--- backend/src/main/java/io/metersphere/controller/request/ProjectRequest.java
+++ backend/src/main/java/io/metersphere/controller/request/ProjectRequest.java
@@ -3,9 +3,12 @@ package io.metersphere.controller.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class ProjectRequest {
     private String workspaceId;
     private String name;
+    private List<OrderRequest> orders;
 }
