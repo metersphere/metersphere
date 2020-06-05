5df638d51cbeeb5d9a7de2272bf8e474969c6f9f
chenjianxing <jianxing.chen@fit2cloud.com>
2020-06-05 14:49:47 +0800

    统一首页跳转样式

--- frontend/src/business/components/performance/home/PerformanceReportRecentList.vue
+++ frontend/src/business/components/performance/home/PerformanceReportRecentList.vue
@@ -3,12 +3,8 @@
     <template v-slot:header>
       <span class="title">{{$t('api_report.title')}}</span>
     </template>
-    <el-table :data="tableData" class="table-content">
-      <el-table-column :label="$t('commons.name')" width="150" show-overflow-tooltip>
-        <template v-slot:default="scope">
-          <el-link type="info" @click="link(scope.row)">{{ scope.row.name }}</el-link>
-        </template>
-      </el-table-column>
+    <el-table :data="tableData" class="table-content" @row-click="link">
+      <el-table-column prop="name" :label="$t('commons.name')" width="150" show-overflow-tooltip/>
       <el-table-column width="250" :label="$t('commons.create_time')">
         <template v-slot:default="scope">
           <span>{{ scope.row.createTime | timestampFormatDate }}</span>
@@ -62,5 +58,9 @@
 </script>
 
 <style scoped>
+
+  .el-table {
+    cursor:pointer;
+  }
 
 </style>
