66b60dbbc68993ad41bc36ec7bd78c0c41e482e4
Captain.B <bin@fit2cloud.com>
2020-06-08 11:19:45 +0800

    删除无用的代码

--- backend/src/main/java/io/metersphere/performance/engine/kubernetes/provider/KubernetesProvider.java
+++
@@ -1,7 +0,0 @@
-package io.metersphere.performance.engine.kubernetes.provider;
-
-public class KubernetesProvider extends AbstractClientProvider {
-    public KubernetesProvider(String credential) {
-        super(credential);
-    }
-}
