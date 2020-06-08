2181ef42ae5600c7938a4f6440b0d18d7d191662
wenyann <64353056+wenyann@users.noreply.github.com>
2020-06-08 17:45:32 +0800

    '去除弹出保存用户名密码弹框'

--- frontend/src/business/components/settings/system/SystemParameterSetting.vue
+++ frontend/src/business/components/settings/system/SystemParameterSetting.vue
@@ -36,7 +36,7 @@
             <el-form-item :label="$t('system_parameter_setting.SMTP_password')" prop="password">
             </el-form-item>
             <el-input v-model="formInline.password" :placeholder="$t('system_parameter_setting.SMTP_password')"
-                      show-password></el-input>
+                      autocomplete="new-password" type="text" @focus="changeType"></el-input>
           </el-col>
         </el-row>
 
@@ -79,6 +79,7 @@
           account: 'xjj0608@163.com',
           password: '2345678',*/
         },
+        readOnly: false,
         result: {},
         showEdit: true,
         showSave: false,
@@ -113,6 +114,9 @@
       this.query()
     },
     methods: {
+      changeType() {
+        this.type = 'password'
+      },
 
       query() {
         this.result = this.$get("/system/mail/info", response => {
