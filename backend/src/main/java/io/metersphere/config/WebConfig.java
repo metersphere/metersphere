18fcb24755e6293ca12b8d79177128d5f01d6bce
shiziyuan9527 <yuhao.li@fit2cloud.com>
2020-06-12 11:02:46 +0800

    restTemplate 超时限制

--- backend/src/main/java/io/metersphere/config/WebConfig.java
+++ backend/src/main/java/io/metersphere/config/WebConfig.java
@@ -3,6 +3,7 @@ package io.metersphere.config;
 import io.metersphere.interceptor.TestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -17,6 +18,12 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+        HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        httpRequestFactory.setConnectionRequestTimeout(4000);
+        httpRequestFactory.setConnectTimeout(4000);
+        httpRequestFactory.setReadTimeout(5000);
+        restTemplate.setRequestFactory(httpRequestFactory);
+        return restTemplate;
     }
 }
