12cc7da26bc45dbafea5ed0eba3c014022d756a1
chenjianxing <jianxing.chen@fit2cloud.com>
2020-06-16 19:49:00 +0800

    fix 测试执行人等过滤只读用户

--- frontend/src/business/components/track/plan/view/comonents/ExecutorEdit.vue
+++ frontend/src/business/components/track/plan/view/comonents/ExecutorEdit.vue
@@ -39,7 +39,7 @@
       methods: {
         setMaintainerOptions() {
           let workspaceId = localStorage.getItem(WORKSPACE_ID);
-          this.$post('/user/ws/member/list/all', {workspaceId:workspaceId}, response => {
+          this.$post('/user/ws/member/tester/list', {workspaceId:workspaceId}, response => {
             this.executorOptions = response.data;
           });
         },
