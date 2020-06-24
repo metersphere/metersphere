67e402afa3cdb3411fa9000501951ea107b2f1fb
shiziyuan9527 <yuhao.li@fit2cloud.com>
2020-06-24 17:40:49 +0800

    ldap

--- backend/src/main/java/io/metersphere/ldap/LdapService.java
+++ backend/src/main/java/io/metersphere/ldap/LdapService.java
@@ -2,56 +2,38 @@ package io.metersphere.ldap;
 
 import io.metersphere.commons.exception.MSException;
 import io.metersphere.controller.request.LoginRequest;
-import org.apache.shiro.realm.ldap.LdapUtils;
-import org.springframework.ldap.core.LdapTemplate;
+import io.metersphere.i18n.Translator;
+import org.springframework.ldap.CommunicationException;
 import org.springframework.stereotype.Service;
-
 import javax.annotation.Resource;
-import javax.naming.directory.DirContext;
-import javax.naming.ldap.LdapContext;
-
 import java.util.List;
-
-import static org.springframework.ldap.query.LdapQueryBuilder.query;
 
 @Service
 public class LdapService {
 
     @Resource
-    private LdapTemplate ldapTemplate;
-
-    @Resource
     private PersonRepoImpl personRepo;
 
     public boolean authenticate(LoginRequest request) {
-//        String userDn, String credentials
-        DirContext ctx = null;
         String dn = null;
         String username = request.getUsername();
         String credentials = request.getPassword();
 
-        List user = personRepo.findByName(username);
-
-        if (user.size() > 0) {
-            dn = personRepo.getDnForUser(username);
-        } else {
-            MSException.throwException("no such user");
-        }
         try {
-            ctx = ldapTemplate.getContextSource().getContext(dn, credentials);
-//            ldapTemplate.authenticate(dn, credentials);
-            // Take care here - if a base was specified on the ContextSource
-            // that needs to be removed from the user DN for the lookup to succeed.
-            // ctx.lookup(userDn);
-            return true;
-        } catch (Exception e) {
-            // Context creation failed - authentication did not succeed
-            System.out.println("Login failed: " + e);
-            MSException.throwException("login failed...");
-            return false;
-        } finally {
-            // It is imperative that the created DirContext instance is always closed
-            LdapUtils.closeContext((LdapContext) ctx);
+            // select user by sAMAccountName
+            List user = personRepo.findByName(username);
+
+            if (user.size() == 1) {
+                dn = personRepo.getDnForUser(username);
+            } else if (user.size() == 0){
+                MSException.throwException(Translator.get("user_not_exist") + username);
+            } else {
+                MSException.throwException("Found multiple users");
+            }
+        } catch (CommunicationException e) {
+            MSException.throwException("LDAP Server connection failed!");
         }
+
+        return personRepo.authenticate(dn, credentials);
     }
 }
