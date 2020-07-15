2a759ca848fe17e33bef7d68637e3cd6afc0fe91
Captain.B <bin@fit2cloud.com>
2020-07-15 17:54:46 +0800

    refactor: 去掉警告

--- backend/src/main/java/io/metersphere/track/request/testcase/DragNodeRequest.java
+++ backend/src/main/java/io/metersphere/track/request/testcase/DragNodeRequest.java
@@ -2,11 +2,13 @@ package io.metersphere.track.request.testcase;
 
 import io.metersphere.base.domain.TestCaseNode;
 import io.metersphere.track.dto.TestCaseNodeDTO;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 public class DragNodeRequest extends TestCaseNode {
 
     List<String> nodeIds;
