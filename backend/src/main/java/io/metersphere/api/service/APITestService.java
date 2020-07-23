e3e3a166e6f689c54adf21f1b53f3c87c4bb8fb4
chenjianxing <jianxing.chen@fit2cloud.com>
2020-07-23 09:57:44 +0800

    fix: 删除测试时删除定时任务

--- backend/src/main/java/io/metersphere/api/service/APITestService.java
+++ backend/src/main/java/io/metersphere/api/service/APITestService.java
@@ -140,6 +140,7 @@ public class APITestService {
         testCaseService.checkIsRelateTest(testId);
         deleteFileByTestId(testId);
         apiReportService.deleteByTestId(testId);
+        scheduleService.deleteByResourceId(testId);
         apiTestMapper.deleteByPrimaryKey(testId);
     }
 
