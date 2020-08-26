e8ee29da09f0ff2bba1f631c925bd3365b6d914c
chenjianxing <jianxing.chen@fit2cloud.com>
2020-08-26 15:46:47 +0800

    fix(接口测试): body 文件上传大小限制

--- frontend/src/business/components/api/test/components/body/ApiBodyFileUpload.vue
+++ frontend/src/business/components/api/test/components/body/ApiBodyFileUpload.vue
@@ -57,10 +57,10 @@
           this.parameter.files.push(file);
         },
         uploadValidate(file) {
-          // if (file.size / 1024 / 1024 > 20) {
-          //   this.$warning(this.$t('test_track.case.import.upload_limit_size'));
-          //   return false;
-          // }
+          if (file.size / 1024 / 1024 > 500) {
+            this.$warning(this.$t('api_test.request.body_upload_limit_size'));
+            return false;
+          }
           return true;
         },
       },
