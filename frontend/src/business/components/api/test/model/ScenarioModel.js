8cee5b816d120be7fe62ba0a8e0645da5670638a
q4speed <kun@fit2cloud.com>
2020-09-17 18:13:26 +0800

    fix: 删除重复代码

--- frontend/src/business/components/api/test/model/ScenarioModel.js
+++ frontend/src/business/components/api/test/model/ScenarioModel.js
@@ -9,8 +9,6 @@ import {
   HeaderManager,
   HTTPSamplerArguments,
   HTTPsamplerFiles,
-  HTTPSamplerProxy,
-  HTTPSamplerArguments, HTTPsamplerFiles,
   HTTPSamplerProxy, JDBCDataSource, JDBCSampler,
   JSONPathAssertion,
   JSONPostProcessor,
