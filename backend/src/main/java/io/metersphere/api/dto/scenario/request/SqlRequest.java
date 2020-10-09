831741c97d3d4ae1236955451db423094ae635a2
q4speed <kun@fit2cloud.com>
2020-10-09 13:50:50 +0800

    refactor: Request改为类，增加公共属性

--- backend/src/main/java/io/metersphere/api/dto/scenario/request/SqlRequest.java
+++ backend/src/main/java/io/metersphere/api/dto/scenario/request/SqlRequest.java
@@ -2,23 +2,15 @@ package io.metersphere.api.dto.scenario.request;
 
 import com.alibaba.fastjson.annotation.JSONField;
 import com.alibaba.fastjson.annotation.JSONType;
-import io.metersphere.api.dto.scenario.assertions.Assertions;
-import io.metersphere.api.dto.scenario.controller.IfController;
-import io.metersphere.api.dto.scenario.extract.Extract;
-import io.metersphere.api.dto.scenario.processor.JSR223PostProcessor;
-import io.metersphere.api.dto.scenario.processor.JSR223PreProcessor;
-import io.metersphere.api.dto.scenario.timer.ConstantTimer;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @JSONType(typeName = RequestType.SQL)
-public class SqlRequest implements Request {
+public class SqlRequest extends Request {
     // type 必须放最前面，以便能够转换正确的类
     private String type = RequestType.SQL;
-    @JSONField(ordinal = 1)
-    private String id;
-    @JSONField(ordinal = 2)
-    private String name;
     @JSONField(ordinal = 3)
     private String dataSource;
     @JSONField(ordinal = 4)
@@ -28,23 +20,9 @@ public class SqlRequest implements Request {
     @JSONField(ordinal = 6)
     private Boolean useEnvironment;
     @JSONField(ordinal = 7)
-    private Assertions assertions;
-    @JSONField(ordinal = 8)
-    private Extract extract;
-    @JSONField(ordinal = 9)
-    private Boolean enable;
-    @JSONField(ordinal = 10)
     private Boolean followRedirects;
-    @JSONField(ordinal = 11)
-    private JSR223PreProcessor jsr223PreProcessor;
-    @JSONField(ordinal = 12)
-    private JSR223PostProcessor jsr223PostProcessor;
     @JSONField(ordinal = 13)
     private String resultVariable;
     @JSONField(ordinal = 14)
     private String variableNames;
-    @JSONField(ordinal = 15)
-    private IfController controller;
-    @JSONField(ordinal = 16)
-    private ConstantTimer timer;
 }
