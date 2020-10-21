40d217b75bb498160f68f19714ebca75fed8bfc1
Captain.B <bin@fit2cloud.com>
2020-10-21 09:41:53 +0800

    refactor: 调整flyway

--- backend/src/main/resources/db/migration/V33__modify_message_task.sql
+++ backend/src/main/resources/db/migration/V33__modify_message_task.sql
@@ -1,5 +1,4 @@
-alter table message_task
-	add identification varchar(255) not null;
-alter table message_task
-    add result boolean not null;
-alter table message_task change result is_Set tinyint(1) not null;
+ALTER TABLE message_task
+    ADD identification varchar(255) NOT NULL;
+ALTER TABLE message_task
+    ADD result tinyint(1) NOT NULL;
