16e3ceb8ad4c11ea75b64de49eda3b15e7d67012
chenjianxing <jianxing.chen@fit2cloud.com>
2020-11-05 10:35:16 +0800

    fix(接口测试): body kv参数请求异常

--- frontend/src/business/components/api/test/model/ScenarioModel.js
+++ frontend/src/business/components/api/test/model/ScenarioModel.js
@@ -1397,11 +1397,13 @@ class JMXGenerator {
       body = this.filterKV(request.body.kvs);
       this.addRequestBodyFile(httpSamplerProxy, request, testId);
     } else {
-      body.push({name: '', value: request.body.raw, encode: false, enable: true});
+      if (request.body.raw) {
+        httpSamplerProxy.boolProp('HTTPSampler.postBodyRaw', true);
+        body.push({name: '', value: request.body.raw, encode: false, enable: true});
+      }
     }
 
     if (request.method !== 'GET') {
-      httpSamplerProxy.boolProp('HTTPSampler.postBodyRaw', true);
       httpSamplerProxy.add(new HTTPSamplerArguments(body));
     }
   }
