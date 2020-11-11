aee13a6df8cbbd26973c4c94d95956c0654922cd
Captain.B <bin@fit2cloud.com>
2020-11-11 18:00:39 +0800

    refactor(性能测试): 修改 dockerfile 保存jtl

--- docker/jmeter-master/run-test.sh
+++ docker/jmeter-master/run-test.sh
@@ -1,3 +1,3 @@
 for file in ${TESTS_DIR}/*.jmx; do
-  jmeter -n -t ${file} -Jserver.rmi.ssl.disable=${SSL_DISABLED}
+  jmeter -n -t ${file} -Jserver.rmi.ssl.disable=${SSL_DISABLED} -l ${TESTS_DIR}/${REPORT_ID}.jtl
 done
