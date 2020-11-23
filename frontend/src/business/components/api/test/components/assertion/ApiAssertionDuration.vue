1b1611514ca1bf619d2fea428b8fe656b114c4d5
chenjianxing <jianxing.chen@fit2cloud.com>
2020-11-23 11:28:26 +0800

    fix(接口测试): 响应时间断言格式校验

--- frontend/src/business/components/api/test/components/assertion/ApiAssertionDuration.vue
+++ frontend/src/business/components/api/test/components/assertion/ApiAssertionDuration.vue
@@ -2,7 +2,7 @@
   <div>
     <el-row :gutter="10" type="flex" justify="space-between" align="middle">
       <el-col>
-        <el-input :disabled="isReadOnly" :value="value" v-bind="$attrs" step="100" size="small" type="number" @change="change" @input="input"
+        <el-input :disabled="isReadOnly" :value="value" v-bind="$attrs" step="100" size="small" type="number" @change="change" @input="input" :min="0"
                   :placeholder="$t('api_test.request.assertions.response_in_time')"/>
       </el-col>
       <el-col class="assertion-btn">
@@ -35,17 +35,28 @@
 
     methods: {
       add() {
-        this.duration.value = this.value;
-        this.callback();
+        if (this.validate()) {
+          this.duration.value = this.value;
+          this.callback();
+        }
       },
       remove() {
         this.duration.value = undefined;
       },
       change(value) {
-        this.$emit('change', value);
+        if (this.validate()) {
+          this.$emit('change', value);
+        }
       },
       input(value) {
         this.$emit('input', value);
+      },
+      validate() {
+        if (Number(this.value) < 0 || this.value=='') {
+          this.$error(this.$t('commons.formatErr'));
+          return false;
+        }
+        return true;
       }
     }
   }
