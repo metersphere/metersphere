595c9572dec409751ba55e72daa8ec1850e23d2b
fit2-zhao <yong.zhao@fit2cloud.com>
2020-11-23 13:16:27 +0800

    feat(接口定义): 提取处理

--- backend/src/main/java/io/metersphere/api/dto/definition/request/MsTestElement.java
+++ backend/src/main/java/io/metersphere/api/dto/definition/request/MsTestElement.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.metersphere.api.dto.definition.request.assertions.MsAssertions;
 import io.metersphere.api.dto.definition.request.auth.MsAuthManager;
 import io.metersphere.api.dto.definition.request.configurations.MsHeaderManager;
+import io.metersphere.api.dto.definition.request.extract.MsExtract;
 import io.metersphere.api.dto.definition.request.processors.post.MsJSR223PostProcessor;
 import io.metersphere.api.dto.definition.request.processors.pre.MsJSR223PreProcessor;
 import io.metersphere.api.dto.definition.request.sampler.MsHTTPSamplerProxy;
@@ -31,9 +32,10 @@ import java.util.List;
         @JsonSubTypes.Type(value = MsThreadGroup.class, name = "ThreadGroup"),
         @JsonSubTypes.Type(value = MsAuthManager.class, name = "AuthManager"),
         @JsonSubTypes.Type(value = MsAssertions.class, name = "Assertions"),
+        @JsonSubTypes.Type(value = MsExtract.class, name = "Extract"),
 
 })
-@JSONType(seeAlso = {MsHTTPSamplerProxy.class, MsHeaderManager.class, MsJSR223PostProcessor.class, MsJSR223PreProcessor.class, MsTestPlan.class, MsThreadGroup.class, AuthManager.class, MsAssertions.class}, typeKey = "type")
+@JSONType(seeAlso = {MsHTTPSamplerProxy.class, MsHeaderManager.class, MsJSR223PostProcessor.class, MsJSR223PreProcessor.class, MsTestPlan.class, MsThreadGroup.class, AuthManager.class, MsAssertions.class, MsExtract.class}, typeKey = "type")
 @Data
 public abstract class MsTestElement {
     private String type;
---
+++ backend/src/main/java/io/metersphere/api/dto/definition/request/extract/MsExtract.java
@@ -0,0 +1,91 @@
+package io.metersphere.api.dto.definition.request.extract;
+
+import com.alibaba.fastjson.annotation.JSONType;
+import io.metersphere.api.dto.definition.request.MsTestElement;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.jmeter.extractor.RegexExtractor;
+import org.apache.jmeter.extractor.XPath2Extractor;
+import org.apache.jmeter.extractor.json.jsonpath.JSONPostProcessor;
+import org.apache.jmeter.save.SaveService;
+import org.apache.jmeter.testelement.TestElement;
+import org.apache.jorphan.collections.HashTree;
+
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@JSONType(typeName = "Extract")
+public class MsExtract extends MsTestElement {
+    private List<MsExtractRegex> regex;
+    private List<MsExtractJSONPath> json;
+    private List<MsExtractXPath> xpath;
+    private String type = "Extract";
+
+    public void toHashTree(HashTree tree, List<MsTestElement> hashTree) {
+        addRequestExtractors(tree);
+    }
+
+    private void addRequestExtractors(HashTree samplerHashTree) {
+        if (CollectionUtils.isNotEmpty(this.getRegex())) {
+            this.getRegex().stream().filter(MsExtractRegex::isValid).forEach(extractRegex ->
+                    samplerHashTree.add(regexExtractor(extractRegex))
+            );
+        }
+        if (CollectionUtils.isNotEmpty(this.getXpath())) {
+            this.getXpath().stream().filter(MsExtractCommon::isValid).forEach(extractXPath ->
+                    samplerHashTree.add(xPath2Extractor(extractXPath))
+            );
+        }
+        if (CollectionUtils.isNotEmpty(this.getJson())) {
+            this.getJson().stream().filter(MsExtractCommon::isValid).forEach(extractJSONPath ->
+                    samplerHashTree.add(jsonPostProcessor(extractJSONPath))
+            );
+        }
+    }
+
+    private RegexExtractor regexExtractor(MsExtractRegex extractRegex) {
+        RegexExtractor extractor = new RegexExtractor();
+        extractor.setEnabled(true);
+        extractor.setName(extractRegex.getVariable() + " RegexExtractor");
+        extractor.setProperty(TestElement.TEST_CLASS, RegexExtractor.class.getName());
+        extractor.setProperty(TestElement.GUI_CLASS, SaveService.aliasToClass("RegexExtractorGui"));
+        extractor.setRefName(extractRegex.getVariable());
+        extractor.setRegex(extractRegex.getExpression());
+        extractor.setUseField(extractRegex.getUseHeaders());
+        if (extractRegex.isMultipleMatching()) {
+            extractor.setMatchNumber(-1);
+        }
+        extractor.setTemplate("$1$");
+        return extractor;
+    }
+
+    private XPath2Extractor xPath2Extractor(MsExtractXPath extractXPath) {
+        XPath2Extractor extractor = new XPath2Extractor();
+        extractor.setEnabled(true);
+        extractor.setName(extractXPath.getVariable() + " XPath2Extractor");
+        extractor.setProperty(TestElement.TEST_CLASS, XPath2Extractor.class.getName());
+        extractor.setProperty(TestElement.GUI_CLASS, SaveService.aliasToClass("XPath2ExtractorGui"));
+        extractor.setRefName(extractXPath.getVariable());
+        extractor.setXPathQuery(extractXPath.getExpression());
+        if (extractXPath.isMultipleMatching()) {
+            extractor.setMatchNumber(-1);
+        }
+        return extractor;
+    }
+
+    private JSONPostProcessor jsonPostProcessor(MsExtractJSONPath extractJSONPath) {
+        JSONPostProcessor extractor = new JSONPostProcessor();
+        extractor.setEnabled(true);
+        extractor.setName(extractJSONPath.getVariable() + " JSONExtractor");
+        extractor.setProperty(TestElement.TEST_CLASS, JSONPostProcessor.class.getName());
+        extractor.setProperty(TestElement.GUI_CLASS, SaveService.aliasToClass("JSONPostProcessorGui"));
+        extractor.setRefNames(extractJSONPath.getVariable());
+        extractor.setJsonPathExpressions(extractJSONPath.getExpression());
+        if (extractJSONPath.isMultipleMatching()) {
+            extractor.setMatchNumbers("-1");
+        }
+        return extractor;
+    }
+}
