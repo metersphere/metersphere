4e607cfa7be2d15a97e6b0ec2d5f73a9861bdbb0
fit2-zhao <yong.zhao@fit2cloud.com>
2020-12-01 19:02:32 +0800

    fix(接口定义): 少文件提交

---
+++ backend/src/main/java/io/metersphere/base/domain/ApiTestCaseWithBLOBs.java
@@ -0,0 +1,17 @@
+package io.metersphere.base.domain;
+
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class ApiTestCaseWithBLOBs extends ApiTestCase implements Serializable {
+    private String request;
+
+    private String response;
+
+    private static final long serialVersionUID = 1L;
+}
