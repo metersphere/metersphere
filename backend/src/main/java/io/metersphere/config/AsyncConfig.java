bf13f0932261a34aca1e2235a4e3b6ad166d46b9
Captain.B <bin@fit2cloud.com>
2020-12-03 14:08:04 +0800

    fix: 修复异步方法启动的问题

--- backend/src/main/java/io/metersphere/config/AsyncConfig.java
+++ backend/src/main/java/io/metersphere/config/AsyncConfig.java
@@ -6,7 +6,7 @@ import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-@EnableAsync
+@EnableAsync(proxyTargetClass = true)
 @Configuration
 public class AsyncConfig {
 
