8696347f4ab1b2c77e590f2dcf500133d5107c27
chenjianxing <jianxing.chen@fit2cloud.com>
2020-12-28 12:49:40 +0800

    fix: 测试计划进度没有统计到接口测试用例

--- backend/src/main/java/io/metersphere/base/mapper/ext/ExtTestPlanScenarioCaseMapper.java
+++ backend/src/main/java/io/metersphere/base/mapper/ext/ExtTestPlanScenarioCaseMapper.java
@@ -18,4 +18,5 @@ public interface ExtTestPlanScenarioCaseMapper {
 
     List<ApiScenarioDTO> list(@Param("request") TestPlanScenarioRequest request);
 
+    List<String> getExecResultByPlanId(String planId);
 }
