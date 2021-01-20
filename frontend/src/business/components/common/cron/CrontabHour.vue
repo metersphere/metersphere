975ee4c20e9b9e19c13a1b45cce18425ac3bbc09
Coooder-X <1104143015@qq.com>
2021-01-20 15:52:13 +0800

    fix:解决 定时任务生成表达式 指定小时下拉框列表内容不正确

--- frontend/src/business/components/common/cron/CrontabHour.vue
+++ frontend/src/business/components/common/cron/CrontabHour.vue
@@ -9,16 +9,16 @@
 		<el-form-item>
 			<el-radio v-model='radioValue' :label="2">
         {{$t('schedule.cron.period')}} {{$t('schedule.cron.from')}}
-				<el-input-number v-model='cycle01' :min="0" :max="60" /> -
-				<el-input-number v-model='cycle02' :min="0" :max="60" /> 小时
+				<el-input-number v-model='cycle01' :min="0" :max="24" /> -
+				<el-input-number v-model='cycle02' :min="0" :max="24" /> 小时
 			</el-radio>
 		</el-form-item>
 
 		<el-form-item>
 			<el-radio v-model='radioValue' :label="3">
         {{$t('schedule.cron.from')}}
-				<el-input-number v-model='average01' :min="0" :max="60" /> {{$t('schedule.cron.hours')}}{{$t('schedule.cron.start')}}，{{$t('schedule.cron.every')}}
-				<el-input-number v-model='average02' :min="0" :max="60" /> {{$t('schedule.cron.hours')}}{{$t('schedule.cron.execute_once')}}
+				<el-input-number v-model='average01' :min="0" :max="24" /> {{$t('schedule.cron.hours')}}{{$t('schedule.cron.start')}}，{{$t('schedule.cron.every')}}
+				<el-input-number v-model='average02' :min="0" :max="24" /> {{$t('schedule.cron.hours')}}{{$t('schedule.cron.execute_once')}}
 			</el-radio>
 		</el-form-item>
 
@@ -26,7 +26,7 @@
 			<el-radio v-model='radioValue' :label="4">
         {{$t('schedule.cron.specify')}}
 				<el-select clearable v-model="checkboxList" :placeholder="$t('schedule.cron.multi_select')" multiple style="width:100%">
-					<el-option v-for="item in 60" :key="item" :value="item-1">{{item-1}}</el-option>
+					<el-option v-for="item in 24" :key="item" :value="item-1">{{item-1}}</el-option>
 				</el-select>
 			</el-radio>
 		</el-form-item>
