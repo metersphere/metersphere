efd01a25e2a24613323a4b7f43ac4e7b139c96c2
Coooder-X <1104143015@qq.com>
2021-01-24 13:34:41 +0800

    feat:实现 接口测试-接口定义界面，ctrl + s 保存快捷键

--- frontend/src/business/components/api/definition/ApiDefinition.vue
+++ frontend/src/business/components/api/definition/ApiDefinition.vue
@@ -198,6 +198,19 @@
           // 在 DOM 中添加 my-component 组件
           this.renderComponent = true;
         });
+      },
+      '$route'(to, from) {  //  路由改变时，把接口定义界面中的 ctrl s 保存快捷键监听移除
+        console.log(this.apiDefaultTab);
+        console.log(from.path);
+        console.log(to.path);
+        if (to.path.indexOf('/api/definition') == -1) {
+          console.log("out");
+          if (this.$refs) {
+            this.$refs.apiConfig.forEach(item => {
+              item.removeListener();
+            });
+          }
+        }
       }
     },
     methods: {
@@ -211,6 +224,13 @@
         if (tab.name === 'add') {
           this.handleTabsEdit(this.$t('api_test.definition.request.fast_debug'), "debug");
         }
+        this.$refs.apiConfig.forEach(item => {
+          console.log(item);
+          item.removeListener();
+        }); //  删除所有tab的 ctrl + s 监听
+        let tabs = this.apiTabs;
+        let index = tabs.findIndex(item => item.name === tab.name); //  找到当前选中tab的index
+        this.$refs.apiConfig[index-1].addListener(); //  为选中tab添加 ctrl + s 监听（index-1的原因是要除去第一个固有tab）
       },
       handleCommand(e) {
         switch (e) {
