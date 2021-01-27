25abe21823da39d9c3b92c3320a82d42441e842c
Captain.B <bin@fit2cloud.com>
2021-01-27 09:39:41 +0800

    refactor: 修改下载zip的接口，去掉threadnum

--- backend/src/main/java/io/metersphere/performance/service/JmeterFileService.java
+++ backend/src/main/java/io/metersphere/performance/service/JmeterFileService.java
@@ -2,9 +2,6 @@ package io.metersphere.performance.service;
 
 
 import com.alibaba.excel.util.CollectionUtils;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import io.metersphere.base.domain.LoadTestWithBLOBs;
 import io.metersphere.base.mapper.LoadTestMapper;
 import io.metersphere.commons.exception.MSException;
@@ -12,7 +9,6 @@ import io.metersphere.commons.utils.LogUtil;
 import io.metersphere.performance.engine.EngineContext;
 import io.metersphere.performance.engine.EngineFactory;
 import org.apache.commons.lang3.SerializationUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -20,7 +16,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -31,12 +26,11 @@ public class JmeterFileService {
     @Resource
     private LoadTestMapper loadTestMapper;
 
-    public byte[] downloadZip(String testId, String resourceId, double ratio, long startTime, String reportId, int resourceIndex, int threadNum) {
+    public byte[] downloadZip(String testId, String resourceId, double ratio, long startTime, String reportId, int resourceIndex) {
         try {
             LoadTestWithBLOBs loadTest = loadTestMapper.selectByPrimaryKey(testId);
             // deep copy
             LoadTestWithBLOBs subTest = SerializationUtils.clone(loadTest);
-            setThreadNum(subTest, threadNum);
             EngineContext context = EngineFactory.createContext(subTest, resourceId, ratio, startTime, reportId, resourceIndex);
             return zipFilesToByteArray(context);
         } catch (MSException e) {
@@ -47,36 +41,6 @@ public class JmeterFileService {
             MSException.throwException(e);
         }
         return null;
-    }
-
-    private void setThreadNum(LoadTestWithBLOBs t, Integer limit) {
-        // 传入limit才去改这个值
-        if (limit <= 0) {
-            return;
-        }
-        String loadConfiguration = t.getLoadConfiguration();
-        JSONArray jsonArray = JSON.parseArray(loadConfiguration);
-        for (int i = 0; i < jsonArray.size(); i++) {
-            if (jsonArray.get(i) instanceof Map) {
-                JSONObject o = jsonArray.getJSONObject(i);
-                if (StringUtils.equals(o.getString("key"), "TargetLevel")) {
-                    o.put("value", limit);
-                    break;
-                }
-            }
-            if (jsonArray.get(i) instanceof List) {
-                JSONArray o = jsonArray.getJSONArray(i);
-                for (int j = 0; j < o.size(); j++) {
-                    JSONObject b = o.getJSONObject(j);
-                    if (StringUtils.equals(b.getString("key"), "TargetLevel")) {
-                        b.put("value", limit);
-                        break;
-                    }
-                }
-            }
-        }
-        // 设置线程数
-        t.setLoadConfiguration(jsonArray.toJSONString());
     }
 
     private byte[] zipFilesToByteArray(EngineContext context) throws IOException {
