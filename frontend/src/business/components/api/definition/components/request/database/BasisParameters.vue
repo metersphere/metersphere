267dd14a934a2d7b2cf0db4358cabb5976195f74
fit2-zhao <yong.zhao@fit2cloud.com>
2021-01-28 09:56:55 +0800

    fix(接口定义): 修复数据源删除后页面显示问题

--- frontend/src/business/components/api/definition/components/request/database/BasisParameters.vue
+++ frontend/src/business/components/api/definition/components/request/database/BasisParameters.vue
@@ -64,22 +64,22 @@
           </el-form>
         </div>
         <!--<div v-if="showScript">-->
-          <!--<div v-for="row in request.hashTree" :key="row.id" v-loading="isReloadData" style="margin-left: 20px;width: 100%">-->
-            <!--&lt;!&ndash; 前置脚本 &ndash;&gt;-->
-            <!--<ms-jsr233-processor v-if="row.label ==='JSR223 PreProcessor'" @copyRow="copyRow" @remove="remove" :is-read-only="false" :title="$t('api_test.definition.request.pre_script')" style-type="color: #B8741A;background-color: #F9F1EA"-->
-                                 <!--:jsr223-processor="row"/>-->
-            <!--&lt;!&ndash;后置脚本&ndash;&gt;-->
-            <!--<ms-jsr233-processor v-if="row.label ==='JSR223 PostProcessor'" @copyRow="copyRow" @remove="remove" :is-read-only="false" :title="$t('api_test.definition.request.post_script')" style-type="color: #783887;background-color: #F2ECF3"-->
-                                 <!--:jsr223-processor="row"/>-->
-            <!--&lt;!&ndash;断言规则&ndash;&gt;-->
-            <!--<div style="margin-top: 10px">-->
-              <!--<ms-api-assertions v-if="row.type==='Assertions'" @copyRow="copyRow" @remove="remove" :is-read-only="isReadOnly" :assertions="row"/>-->
-            <!--</div>-->
-            <!--&lt;!&ndash;提取规则&ndash;&gt;-->
-            <!--<div style="margin-top: 10px">-->
-              <!--<ms-api-extract :is-read-only="isReadOnly" @copyRow="copyRow" @remove="remove" v-if="row.type==='Extract'" :extract="row"/>-->
-            <!--</div>-->
-          <!--</div>-->
+        <!--<div v-for="row in request.hashTree" :key="row.id" v-loading="isReloadData" style="margin-left: 20px;width: 100%">-->
+        <!--&lt;!&ndash; 前置脚本 &ndash;&gt;-->
+        <!--<ms-jsr233-processor v-if="row.label ==='JSR223 PreProcessor'" @copyRow="copyRow" @remove="remove" :is-read-only="false" :title="$t('api_test.definition.request.pre_script')" style-type="color: #B8741A;background-color: #F9F1EA"-->
+        <!--:jsr223-processor="row"/>-->
+        <!--&lt;!&ndash;后置脚本&ndash;&gt;-->
+        <!--<ms-jsr233-processor v-if="row.label ==='JSR223 PostProcessor'" @copyRow="copyRow" @remove="remove" :is-read-only="false" :title="$t('api_test.definition.request.post_script')" style-type="color: #783887;background-color: #F2ECF3"-->
+        <!--:jsr223-processor="row"/>-->
+        <!--&lt;!&ndash;断言规则&ndash;&gt;-->
+        <!--<div style="margin-top: 10px">-->
+        <!--<ms-api-assertions v-if="row.type==='Assertions'" @copyRow="copyRow" @remove="remove" :is-read-only="isReadOnly" :assertions="row"/>-->
+        <!--</div>-->
+        <!--&lt;!&ndash;提取规则&ndash;&gt;-->
+        <!--<div style="margin-top: 10px">-->
+        <!--<ms-api-extract :is-read-only="isReadOnly" @copyRow="copyRow" @remove="remove" v-if="row.type==='Extract'" :extract="row"/>-->
+        <!--</div>-->
+        <!--</div>-->
         <!--</div>-->
       </el-col>
       <el-col :span="3" class="ms-left-cell" v-if="showScript">
@@ -224,6 +224,9 @@
           }
           if (!hasEnvironment) {
             this.request.environmentId = undefined;
+          }
+          if (!this.request.environmentId) {
+            this.request.dataSourceId = undefined;
           }
           this.initDataSource();
         });
