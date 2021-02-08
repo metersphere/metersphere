51ca71531eb489f84494b8b5267bcb4851f86fdb
Captain.B <bin@fit2cloud.com>
2021-02-08 14:28:24 +0800

    fix(性能测试): 修复请求很多的时候，图标鼠标悬停显示不全

--- frontend/src/business/components/performance/report/components/TestOverview.vue
+++ frontend/src/business/components/performance/report/components/TestOverview.vue
@@ -148,7 +148,9 @@ export default {
           },
           tooltip: {
             show: true,
-            trigger: 'axis'
+            trigger: 'axis',
+            // extraCssText: 'z-index: 999;',
+            confine: true,
           },
           legend: {},
           xAxis: {},
@@ -226,7 +228,8 @@ export default {
           tooltip: {
             show: true,
             trigger: 'axis',
-            extraCssText: 'z-index: 999;',
+            // extraCssText: 'z-index: 999;',
+            confine: true,
             formatter: function (params, ticket, callback) {
               let result = "";
               let name = params[0].name;
@@ -306,7 +309,8 @@ export default {
           tooltip: {
             show: true,
             trigger: 'axis',
-            extraCssText: 'z-index: 999;',
+            // extraCssText: 'z-index: 999;',
+            confine: true,
             formatter: function (params, ticket, callback) {
               let result = "";
               let name = params[0].name;
@@ -376,7 +380,8 @@ export default {
           tooltip: {
             show: true,
             trigger: 'axis',
-            extraCssText: 'z-index: 999;',
+            // extraCssText: 'z-index: 999;',
+            confine: true,
             formatter: function (params, ticket, callback) {
               let result = "";
               let name = params[0].name;
