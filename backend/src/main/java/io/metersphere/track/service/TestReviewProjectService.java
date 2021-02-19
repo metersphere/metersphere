eb5b2d3de156fbf18df3063f8b7011e400c83438
shiziyuan9527 <yuhao.li@fit2cloud.com>
2021-02-19 16:44:51 +0800

    feat(测试跟踪): 用例评审默认关联工作空间下所有项目

--- backend/src/main/java/io/metersphere/track/service/TestReviewProjectService.java
+++ backend/src/main/java/io/metersphere/track/service/TestReviewProjectService.java
@@ -2,8 +2,7 @@ package io.metersphere.track.service;
 
 import io.metersphere.base.domain.*;
 import io.metersphere.base.mapper.ProjectMapper;
-import io.metersphere.base.mapper.TestCaseReviewMapper;
-import io.metersphere.base.mapper.TestCaseReviewProjectMapper;
+import io.metersphere.commons.utils.SessionUtils;
 import io.metersphere.track.request.testreview.TestReviewRelevanceRequest;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
@@ -19,31 +18,13 @@ public class TestReviewProjectService {
 
     @Resource
     private ProjectMapper projectMapper;
-    @Resource
-    private TestCaseReviewProjectMapper testCaseReviewProjectMapper;
-    @Resource
-    private TestCaseReviewMapper testCaseReviewMapper;
 
-    public List<String> getProjectIdsByReviewId(String reviewId) {
-        TestCaseReviewProjectExample example = new TestCaseReviewProjectExample();
-        example.createCriteria().andReviewIdEqualTo(reviewId);
-        List<String> projectIds = testCaseReviewProjectMapper.selectByExample(example)
-                .stream()
-                .map(TestCaseReviewProject::getProjectId)
-                .collect(Collectors.toList());
-        TestCaseReview caseReview = testCaseReviewMapper.selectByPrimaryKey(reviewId);
-        if (caseReview != null && StringUtils.isNotBlank(caseReview.getProjectId())) {
-            if (!projectIds.contains(caseReview.getProjectId())) {
-                projectIds.add(caseReview.getProjectId());
-            }
-        }
-        if (projectIds.isEmpty()) {
-            return null;
-        }
-
-        return projectIds;
+    public List<String> getProjectIdsByReviewId() {
+        ProjectExample example = new ProjectExample();
+        example.createCriteria().andWorkspaceIdEqualTo(SessionUtils.getCurrentWorkspaceId());
+        List<Project> projects = projectMapper.selectByExample(example);
+        return projects.stream().map(Project::getId).collect(Collectors.toList());
     }
-
 
     public List<Project> getProject(TestReviewRelevanceRequest request) {
         ProjectExample projectExample = new ProjectExample();
