812ea192852292ddf5e3901b4a56fec3dad1666b
song.tianyang <tianyang.song@fit2cloud.com>
2021-02-25 15:51:04 +0800

    fix: 修改sql
    修改sql

--- backend/src/main/resources/db/migration/V79__api_document_share.sql
+++ backend/src/main/resources/db/migration/V79__api_document_share.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `metersphere_dev`.`api_document_share`  (
+CREATE TABLE IF NOT EXISTS `api_document_share`  (
  `id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'Api Document Share Info ID',
  `create_time` BIGINT ( 13 ) NOT NULL COMMENT 'Create timestamp',
  `create_user_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
