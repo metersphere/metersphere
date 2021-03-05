595a594d508834261ad31bfd2951a13bc20f465b
Coooder-X <55648333+Coooder-X@users.noreply.github.com>
2021-03-05 17:31:05 +0800

    feat(场景步骤、环境配置): 场景变量、环境配置 支持mock参数 (#1566)
    * feat(测试跟踪): 测试用例下载模版增加标签列
    
    * fix(接口定义): 扩大请求头键长度
    
    * feat(场景步骤、环境配置): 场景变量、环境配置支持mock参数

--- backend/src/main/java/io/metersphere/api/dto/definition/request/ParameterConfig.java
+++ backend/src/main/java/io/metersphere/api/dto/definition/request/ParameterConfig.java
@@ -2,7 +2,9 @@ package io.metersphere.api.dto.definition.request;
 
 import io.metersphere.api.dto.definition.request.variable.ScenarioVariable;
 import io.metersphere.api.dto.scenario.environment.EnvironmentConfig;
+import io.metersphere.commons.utils.ScriptEngineUtils;
 import lombok.Data;
+import org.apache.jmeter.config.Arguments;
 
 import java.util.List;
 import java.util.Map;
@@ -37,4 +39,13 @@ public class ParameterConfig {
         }
         return false;
     }
+
+    static public Arguments valueSupposeMock(Arguments arguments) {
+        for(int i = 0; i < arguments.getArguments().size(); ++i) {
+            String argValue = arguments.getArgument(i).getValue();
+            arguments.getArgument(i).setValue(ScriptEngineUtils.calculate(argValue));
+        }
+        return arguments;
+    }
+
 }
