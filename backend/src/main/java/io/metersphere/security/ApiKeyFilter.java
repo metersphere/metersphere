aeb3af81fa992a98a328b77980a2bc45d5619c72
song.tianyang <tianyang.song@fit2cloud.com>
2021-03-05 18:58:26 +0800

    fix: 登陆验证失效，改过的代码暂时回退
    登陆验证失效，改过的代码暂时回退

--- backend/src/main/java/io/metersphere/security/ApiKeyFilter.java
+++ backend/src/main/java/io/metersphere/security/ApiKeyFilter.java
@@ -16,14 +16,14 @@ public class ApiKeyFilter extends AnonymousFilter {
     @Override
     protected boolean onPreHandle(ServletRequest request, ServletResponse response, Object mappedValue) {
         try {
-            try{
-                //这部分代码是为了测试打包完成之后能否访问文档相关路径，封版之前要删除
-                HttpServletRequest httpServlet = WebUtils.toHttp(request);
-                String url = httpServlet.getRequestURL().toString();
-                LogUtil.info("Url message : "+url);
-            }catch (Exception e){
-
-            }
+//            try{
+//                //这部分代码是为了测试打包完成之后能否访问文档相关路径，封版之前要删除
+//                HttpServletRequest httpServlet = WebUtils.toHttp(request);
+//                String url = httpServlet.getRequestURL().toString();
+//                LogUtil.info("Url message : "+url);
+//            }catch (Exception e){
+//
+//            }
 
             if (!SecurityUtils.getSubject().isAuthenticated()) {
                 String userId = ApiKeyHandler.getUser(WebUtils.toHttp(request));
