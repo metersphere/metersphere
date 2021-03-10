076baff3212f9ce04477c3e299c00924435d3606
Captain.B <bin@fit2cloud.com>
2021-03-10 15:24:54 +0800

    refactor: 使用csrf拦截器过滤请求, 如果配置了 referer.urls 则同时校验 referer
    Closes #1088

--- backend/src/main/java/io/metersphere/security/CsrfFilter.java
+++ backend/src/main/java/io/metersphere/security/CsrfFilter.java
@@ -4,11 +4,13 @@ import io.metersphere.commons.user.SessionUser;
 import io.metersphere.commons.utils.CodingUtil;
 import io.metersphere.commons.utils.CommonBeanFactory;
 import io.metersphere.commons.utils.SessionUtils;
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.web.filter.authc.AnonymousFilter;
 import org.apache.shiro.web.util.WebUtils;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -31,12 +33,31 @@ public class CsrfFilter extends AnonymousFilter {
         }
         // 请求头取出的token value
         String csrfToken = httpServletRequest.getHeader(TOKEN_NAME);
-        // 校验
-        validate(csrfToken);
+        // 校验 token
+        validateToken(csrfToken);
+        // 校验 referer
+        validateReferer(httpServletRequest);
         return true;
     }
 
-    private void validate(String csrfToken) {
+    private void validateReferer(HttpServletRequest request) {
+        Environment env = CommonBeanFactory.getBean(Environment.class);
+        String domains = env.getProperty("referer.urls");
+        if (StringUtils.isBlank(domains)) {
+            // 没有配置不校验
+            return;
+        }
+
+        String[] split = StringUtils.split(domains, ",");
+        String referer = request.getHeader(HttpHeaders.REFERER);
+        if (split != null) {
+            if (!ArrayUtils.contains(split, referer)) {
+                throw new RuntimeException("csrf error");
+            }
+        }
+    }
+
+    private void validateToken(String csrfToken) {
         csrfToken = CodingUtil.aesDecrypt(csrfToken, SessionUser.secret, SessionUser.iv);
 
         String[] signatureArray = StringUtils.split(StringUtils.trimToNull(csrfToken), "|");
@@ -51,7 +72,6 @@ public class CsrfFilter extends AnonymousFilter {
             throw new RuntimeException(e);
         }
         Environment env = CommonBeanFactory.getBean(Environment.class);
-        assert env != null;
         long timeout = env.getProperty("session.timeout", Long.class, 43200L);
         if (Math.abs(System.currentTimeMillis() - signatureTime) > timeout * 1000) {
             throw new RuntimeException("expired token");
