613555101ea8aac84f4607ea4b665f3c3c4189eb
shiziyuan9527 <yuhao.li@fit2cloud.com>
2021-03-18 16:55:54 +0800

    fix(LDAP): 修复LDAP登陆失败的问题

--- backend/src/main/java/io/metersphere/ldap/controller/LdapController.java
+++ backend/src/main/java/io/metersphere/ldap/controller/LdapController.java
@@ -4,6 +4,9 @@ import io.metersphere.base.domain.User;
 import io.metersphere.commons.constants.ParamConstants;
 import io.metersphere.commons.constants.UserSource;
 import io.metersphere.commons.exception.MSException;
+import io.metersphere.commons.utils.CommonBeanFactory;
+import io.metersphere.commons.utils.RsaKey;
+import io.metersphere.commons.utils.RsaUtil;
 import io.metersphere.controller.ResultHolder;
 import io.metersphere.controller.request.LoginRequest;
 import io.metersphere.i18n.Translator;
@@ -16,6 +19,7 @@ import org.springframework.ldap.core.DirContextOperations;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.security.NoSuchAlgorithmException;
 
 @RestController
 @RequestMapping("/ldap")
@@ -29,7 +33,7 @@ public class LdapController {
     private SystemParameterService systemParameterService;
 
     @PostMapping(value = "/signin")
-    public ResultHolder login(@RequestBody LoginRequest request) {
+    public ResultHolder login(@RequestBody LoginRequest request) throws NoSuchAlgorithmException {
 
         String isOpen = systemParameterService.getValue(ParamConstants.LDAP.OPEN.getValue());
         if (StringUtils.isBlank(isOpen) || StringUtils.equals(Boolean.FALSE.toString(), isOpen)) {
@@ -71,7 +75,8 @@ public class LdapController {
 
         // 执行 ShiroDBRealm 中 LDAP 登录逻辑
         LoginRequest loginRequest = new LoginRequest();
-        loginRequest.setUsername(userId);
+        RsaKey rsaKey = CommonBeanFactory.getBean(RsaKey.class);
+        loginRequest.setUsername(RsaUtil.publicEncrypt(userId,rsaKey.getPublicKey()));
         return userService.login(loginRequest);
     }
 
