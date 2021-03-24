11a69bd2b0a858778d2133edbea188ce6f69099f
fit2-zhao <yong.zhao@fit2cloud.com>
2021-03-24 20:11:18 +0800

    fix(接口自动化): 去掉脚本成功后缓存

--- backend/src/main/java/io/metersphere/api/dto/definition/request/controller/MsLoopController.java
+++ backend/src/main/java/io/metersphere/api/dto/definition/request/controller/MsLoopController.java
@@ -201,7 +201,7 @@ public class MsLoopController extends MsTestElement {
             jsr223PreProcessor.setName("循环超时处理");
             jsr223PreProcessor.setProperty(TestElement.TEST_CLASS, JSR223Sampler.class.getName());
             jsr223PreProcessor.setProperty(TestElement.GUI_CLASS, SaveService.aliasToClass("TestBeanGUI"));
-            jsr223PreProcessor.setProperty("cacheKey", "true");
+            /*jsr223PreProcessor.setProperty("cacheKey", "true");*/
             jsr223PreProcessor.setProperty("scriptLanguage", "beanshell");
             jsr223PreProcessor.setProperty("script", script());
             hashTree.add(jsr223PreProcessor);
