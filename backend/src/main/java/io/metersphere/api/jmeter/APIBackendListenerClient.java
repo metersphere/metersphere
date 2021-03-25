049a006e97248a15f33ff8f0c0706bc7a89e0929
wenyann <64353056+wenyann@users.noreply.github.com>
2021-03-25 12:14:12 +0800

    fix: 接口定义执行结果修改

--- backend/src/main/java/io/metersphere/api/jmeter/APIBackendListenerClient.java
+++ backend/src/main/java/io/metersphere/api/jmeter/APIBackendListenerClient.java
@@ -185,10 +185,7 @@ public class APIBackendListenerClient extends AbstractBackendListenerClient impl
             }
         } else if (StringUtils.equals(this.runMode, ApiRunMode.JENKINS.name())) {
             apiDefinitionService.addResult(testResult);
-            apiDefinitionExecResultService.saveApiResult(testResult, ApiRunMode.API.name());
-            ApiDefinitionExecResult api=apiTestCaseService.getInfo(testId);
-            report.setStatus(api.getStatus());
-
+            apiDefinitionExecResultService.saveApiResult(testResult, ApiRunMode.DEFINITION.name());
 
         } else if (StringUtils.equals(this.runMode, ApiRunMode.JENKINS_API_PLAN.name())) {
             apiDefinitionService.addResult(testResult);
