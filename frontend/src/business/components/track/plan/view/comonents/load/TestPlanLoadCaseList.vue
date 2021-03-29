7502bdcc7afd1c1e00ddf46f65395091d43c55c9
wenyann <64353056+wenyann@users.noreply.github.com>
2021-03-29 15:29:10 +0800

    fix:删除无用代码

--- frontend/src/business/components/track/plan/view/comonents/load/TestPlanLoadCaseList.vue
+++ frontend/src/business/components/track/plan/view/comonents/load/TestPlanLoadCaseList.vue
@@ -131,7 +131,7 @@ import MsTablePagination from "@/business/components/common/pagination/TablePagi
 import MsPerformanceTestStatus from "@/business/components/performance/test/PerformanceTestStatus";
 import MsTableOperatorButton from "@/business/components/common/components/MsTableOperatorButton";
 import LoadCaseReport from "@/business/components/track/plan/view/comonents/load/LoadCaseReport";
-import {_filter, _sort, getLabel, getSystemLabel} from "@/common/js/tableUtils";
+import {_filter, _sort, getLabel} from "@/common/js/tableUtils";
 import HeaderCustom from "@/business/components/common/head/HeaderCustom";
 import {TEST_CASE_LIST, TEST_PLAN_LOAD_CASE} from "@/common/js/constants";
 import {Test_Plan_Load_Case, Track_Test_Case} from "@/business/components/common/model/JsonData";
@@ -198,7 +198,7 @@ export default {
   created() {
     this.initTable();
     this.refreshStatus();
-    getSystemLabel(this, this.type)
+
 
   },
   watch: {
