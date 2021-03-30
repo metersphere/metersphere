2a70396bf80679497966dbdbcad409c4caeb4bca
fit2-zhao <yong.zhao@fit2cloud.com>
2021-03-30 15:25:23 +0800

    fix: 恢复被覆盖的sql

--- backend/src/main/resources/db/migration/V79__v1.8.1_release.sql
+++ backend/src/main/resources/db/migration/V79__v1.8.1_release.sql
@@ -1,2 +1,4 @@
 ALTER TABLE file_metadata
     MODIFY name VARCHAR(250) NOT NULL COMMENT 'File name';
+-- api_scenario_report modify column length
+ALTER TABLE api_scenario_report MODIFY COLUMN name VARCHAR(300);
