e738102fe1037946af79527572a1f3c7af0eadbe
chenjianxing <jianxing.chen@fit2cloud.com>
2021-04-02 14:26:29 +0800

    refactor: 全选按钮颜色高亮

--- frontend/src/business/components/api/definition/components/list/ApiCaseSimpleList.vue
+++ frontend/src/business/components/api/definition/components/list/ApiCaseSimpleList.vue
@@ -23,6 +23,7 @@
         <ms-table-header-select-popover v-show="total>0"
                                         :page-size="pageSize>total?total:pageSize"
                                         :total="total"
+                                        :select-data-counts="selectDataCounts"
                                         @selectPageAll="isSelectDataAll(false)"
                                         @selectAll="isSelectDataAll(true)"/>
 
@@ -411,7 +412,7 @@ export default {
       this.initTable();
     },
     handleSelectAll(selection) {
-      _handleSelectAll(this, selection, this.tableData, this.selectRows);
+      _handleSelectAll(this, selection, this.tableData, this.selectRows, this.condition);
       this.selectRowsCount(this.selectRows)
     },
     search() {
