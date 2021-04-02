c2b8e391be3029e820194eb45afdaa08371c8a61
Captain.B <bin@fit2cloud.com>
2021-04-02 16:20:37 +0800

    fix(性能测试): 压力配置上可以选择场景是否顺序执行

--- frontend/src/business/components/performance/test/EditPerformanceTest.vue
+++ frontend/src/business/components/performance/test/EditPerformanceTest.vue
@@ -229,30 +229,6 @@ export default {
         }
       };
     },
-    stringToByte(str) {
-      var bytes = new Array();
-      var len, c;
-      len = str.length;
-      for (var i = 0; i < len; i++) {
-        c = str.charCodeAt(i);
-        if (c >= 0x010000 && c <= 0x10FFFF) {
-          bytes.push(((c >> 18) & 0x07) | 0xF0);
-          bytes.push(((c >> 12) & 0x3F) | 0x80);
-          bytes.push(((c >> 6) & 0x3F) | 0x80);
-          bytes.push((c & 0x3F) | 0x80);
-        } else if (c >= 0x000800 && c <= 0x00FFFF) {
-          bytes.push(((c >> 12) & 0x0F) | 0xE0);
-          bytes.push(((c >> 6) & 0x3F) | 0x80);
-          bytes.push((c & 0x3F) | 0x80);
-        } else if (c >= 0x000080 && c <= 0x0007FF) {
-          bytes.push(((c >> 6) & 0x1F) | 0xC0);
-          bytes.push((c & 0x3F) | 0x80);
-        } else {
-          bytes.push(c & 0xFF);
-        }
-      }
-      return bytes;
-    },
     cancel() {
       this.$router.push({path: '/performance/test/all'})
     },
