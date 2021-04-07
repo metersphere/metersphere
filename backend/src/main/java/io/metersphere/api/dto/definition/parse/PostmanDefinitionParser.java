f91013d56bdf14c1e4c0ebda1ff10d50c586cdd2
chenjianxing <jianxing.chen@fit2cloud.com>
2021-04-07 12:00:09 +0800

    fix: postman导入缺失

--- backend/src/main/java/io/metersphere/api/dto/definition/parse/PostmanDefinitionParser.java
+++ backend/src/main/java/io/metersphere/api/dto/definition/parse/PostmanDefinitionParser.java
@@ -9,10 +9,14 @@ import io.metersphere.api.dto.parse.postman.PostmanKeyValue;
 import io.metersphere.api.parse.PostmanAbstractParserParser;
 import io.metersphere.base.domain.ApiDefinitionWithBLOBs;
 import io.metersphere.base.domain.ApiModule;
+import io.metersphere.base.domain.ApiTestCaseWithBLOBs;
+import io.metersphere.base.domain.Project;
+import io.metersphere.base.mapper.ProjectMapper;
+import io.metersphere.commons.utils.BeanUtils;
+import io.metersphere.commons.utils.CommonBeanFactory;
 
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class PostmanDefinitionParser extends PostmanAbstractParserParser<ApiDefinitionImport> {
 
@@ -24,33 +28,47 @@ public class PostmanDefinitionParser extends PostmanAbstractParserParser<ApiDefi
         List<PostmanKeyValue> variables = postmanCollection.getVariable();
         ApiDefinitionImport apiImport = new ApiDefinitionImport();
         List<ApiDefinitionWithBLOBs> results = new ArrayList<>();
+        List<ApiTestCaseWithBLOBs> cases = new ArrayList<>();
+        Map<String, String> repeatMap = new HashMap();
+        ProjectMapper projectMapper = CommonBeanFactory.getBean(ProjectMapper.class);
+        Project project = projectMapper.selectByPrimaryKey(request.getProjectId());
         parseItem(postmanCollection.getItem(), variables, results,
-                ApiDefinitionImportUtil.buildModule(ApiDefinitionImportUtil.getSelectModule(request.getModuleId()), postmanCollection.getInfo().getName(), this.projectId), true);
+                ApiDefinitionImportUtil.buildModule(ApiDefinitionImportUtil.getSelectModule(request.getModuleId()),
+                        postmanCollection.getInfo().getName(), this.projectId), cases, repeatMap, project.getRepeatable());
         apiImport.setData(results);
+        apiImport.setCases(cases);
         return apiImport;
     }
 
-    protected void parseItem(List<PostmanItem> items, List<PostmanKeyValue> variables, List<ApiDefinitionWithBLOBs> results, ApiModule parentModule, Boolean isCreateModule) {
+    protected void parseItem(List<PostmanItem> items, List<PostmanKeyValue> variables, List<ApiDefinitionWithBLOBs> results,
+                             ApiModule parentModule, List<ApiTestCaseWithBLOBs> cases, Map<String, String> repeatMap, Boolean repeatable) {
         for (PostmanItem item : items) {
             List<PostmanItem> childItems = item.getItem();
             if (childItems != null) {
                 ApiModule module = null;
-                if (isCreateModule) {
-                    module = ApiDefinitionImportUtil.buildModule(parentModule, item.getName(), this.projectId);
-                }
-                parseItem(childItems, variables, results, module, isCreateModule);
+                module = ApiDefinitionImportUtil.buildModule(parentModule, item.getName(), this.projectId);
+                parseItem(childItems, variables, results, module, cases, repeatMap, repeatable);
             } else {
                 MsHTTPSamplerProxy msHTTPSamplerProxy = parsePostman(item);
                 ApiDefinitionWithBLOBs request = buildApiDefinition(msHTTPSamplerProxy.getId(), msHTTPSamplerProxy.getName(),
                         msHTTPSamplerProxy.getPath(), msHTTPSamplerProxy.getMethod(), new ApiTestImportRequest());
                 request.setPath(msHTTPSamplerProxy.getPath());
                 request.setRequest(JSON.toJSONString(msHTTPSamplerProxy));
-
-                if (request != null) {
-                    results.add(request);
-                }
                 if (parentModule != null) {
                     request.setModuleId(parentModule.getId());
+                }
+                if (request != null) {
+                    if (repeatMap.keySet().contains(request.getMethod() + request.getPath())
+                            && (repeatable == null || repeatable == false)) {
+                        ApiTestCaseWithBLOBs apiTestCase =  new ApiTestCaseWithBLOBs();
+                        BeanUtils.copyBean(apiTestCase, request);
+                        apiTestCase.setApiDefinitionId(repeatMap.get(request.getMethod() + request.getPath()));
+                        apiTestCase.setPriority("P0");
+                        cases.add(apiTestCase);
+                    } else {
+                        repeatMap.put(request.getMethod() + request.getPath(), request.getId());
+                        results.add(request);
+                    }
                 }
             }
         }
