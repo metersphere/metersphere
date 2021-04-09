778a646699779f6c134df81b000c0358cce371c3
Captain.B <bin@fit2cloud.com>
2021-04-09 11:37:40 +0800

    feat(性能测试): 性能测试报告页面数据自动更新时间可动态调整

--- backend/src/main/java/io/metersphere/websocket/ReportWebSocket.java
+++ backend/src/main/java/io/metersphere/websocket/ReportWebSocket.java
@@ -13,6 +13,7 @@ import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
 
 @ServerEndpoint("/performance/report/{reportId}")
 @Component
@@ -20,6 +21,7 @@ public class ReportWebSocket {
 
     private static ReportService reportService;
     private static PerformanceTestService performanceTestService;
+    private static ConcurrentHashMap<Session, Integer> refreshTimes = new ConcurrentHashMap<>();
 
     @Resource
     public void setReportService(ReportService reportService) {
@@ -38,6 +40,7 @@ public class ReportWebSocket {
     public void onOpen(@PathParam("reportId") String reportId, Session session) throws IOException {
         //开启一个线程对数据库中的数据进行轮询
         ReportThread reportThread = new ReportThread(session, reportId);
+        refreshTimes.put(session, 20);
         Thread thread = new Thread(reportThread);
         thread.start();
     }
@@ -47,7 +50,7 @@ public class ReportWebSocket {
      */
     @OnClose
     public void onClose(Session session) {
-
+        refreshTimes.remove(session);
     }
 
     /**
@@ -55,6 +58,17 @@ public class ReportWebSocket {
      */
     @OnMessage
     public void onMessage(Session session, String message) {
+        int refreshTime = 20;
+        try {
+            refreshTime = Integer.parseInt(message);
+        } catch (Exception e) {
+        }
+        refreshTimes.put(session, refreshTime);
+        try {
+            session.getBasicRemote().sendText("refresh-" + 0);
+        } catch (IOException e) {
+            LogUtil.error(e.getMessage(), e);
+        }
     }
 
     /**
@@ -104,7 +118,7 @@ public class ReportWebSocket {
                     if (StringUtils.equalsAny(report.getStatus(), PerformanceTestStatus.Running.name(), PerformanceTestStatus.Reporting.name())) {
                         session.getBasicRemote().sendText("refresh-" + this.refresh++);
                     }
-                    Thread.sleep(20 * 1000L);
+                    Thread.sleep(refreshTimes.get(session) * 1000L);
                 } catch (Exception e) {
                     LogUtil.error(e.getMessage(), e);
                 }
