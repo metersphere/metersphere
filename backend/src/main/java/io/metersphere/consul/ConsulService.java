7a3dafe272bf962aaffcae515ba2d77633b220f7
Captain.B <bin@fit2cloud.com>
2021-04-14 16:54:38 +0800

    refactor(性能测试): 提供类consul的接口

--- backend/src/main/java/io/metersphere/consul/ConsulService.java
+++ backend/src/main/java/io/metersphere/consul/ConsulService.java
@@ -1,28 +1,51 @@
 package io.metersphere.consul;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import io.metersphere.base.domain.TestResource;
+import io.metersphere.commons.constants.PerformanceTestStatus;
 import io.metersphere.commons.constants.ResourcePoolTypeEnum;
+import io.metersphere.dto.LoadTestDTO;
 import io.metersphere.dto.NodeDTO;
 import io.metersphere.dto.TestResourcePoolDTO;
+import io.metersphere.performance.dto.Monitor;
+import io.metersphere.performance.request.QueryTestPlanRequest;
+import io.metersphere.performance.service.PerformanceTestService;
 import io.metersphere.service.TestResourcePoolService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class ConsulService {
     @Resource
     private TestResourcePoolService testResourcePoolService;
+    @Resource
+    private PerformanceTestService performanceTestService;
 
     public Map<String, List<String>> getActiveNodes() {
-        List<TestResourcePoolDTO> testResourcePoolDTOS = testResourcePoolService.listValidResourcePools();
         Map<String, List<String>> result = new HashMap<>();
+
+        List<TestResourcePoolDTO> testResourcePoolDTOS = testResourcePoolService.listValidResourcePools();
+        QueryTestPlanRequest request = new QueryTestPlanRequest();
+        request.setFilters(new HashMap<String, List<String>>() {{
+            put("status", Arrays.asList(PerformanceTestStatus.Starting.name(), PerformanceTestStatus.Running.name()));
+        }});
+        List<LoadTestDTO> list = performanceTestService.list(request);
+        for (LoadTestDTO loadTestDTO : list) {
+            String advancedConfiguration = performanceTestService.getAdvancedConfiguration(loadTestDTO.getId());
+            JSONObject adv = JSON.parseObject(advancedConfiguration);
+            Object monitorParams = adv.get("monitorParams");
+            if (monitorParams == null) {
+                continue;
+            }
+            List<Monitor> monitors = JSON.parseArray(monitorParams.toString(), Monitor.class);
+            for (Monitor monitor : monitors) {
+                result.put(monitor.getIp() + "-" + monitor.getPort(), Collections.singletonList("metersphere"));
+            }
+        }
         for (TestResourcePoolDTO pool : testResourcePoolDTOS) {
             if (!StringUtils.equals(pool.getType(), ResourcePoolTypeEnum.NODE.name())) {
                 continue;
@@ -30,6 +53,7 @@ public class ConsulService {
             List<TestResource> resources = pool.getResources();
             for (TestResource resource : resources) {
                 NodeDTO node = JSON.parseObject(resource.getConfiguration(), NodeDTO.class);
+                // 资源池默认9100
                 result.put(node.getIp() + "-9100", Collections.singletonList("metersphere"));
             }
         }
