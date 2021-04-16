477ec7e1694398082fcd5642dc189ab95618e0a8
chenjianxing <jianxing.chen@fit2cloud.com>
2021-04-16 10:10:14 +0800

    fix: sql 文件冲突

--- backend/src/main/resources/db/migration/V80__v1.9_release.sql
+++
@@ -1,5 +0,0 @@
-
--- api_scenario_report modify column length
-ALTER TABLE api_scenario_report MODIFY COLUMN name VARCHAR(3000);
--- api_scenario_report modify column length
-ALTER TABLE api_scenario_report MODIFY COLUMN scenario_id VARCHAR(3000);
