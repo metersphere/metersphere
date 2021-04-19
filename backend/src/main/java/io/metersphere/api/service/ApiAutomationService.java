2effb8b78a525652313a2f0d164c18460444a6de
fit2-zhao <yong.zhao@fit2cloud.com>
2021-04-19 10:28:47 +0800

    fix(接口自动化)复制场景创建人错误修复

--- backend/src/main/java/io/metersphere/api/service/ApiAutomationService.java
+++ backend/src/main/java/io/metersphere/api/service/ApiAutomationService.java
@@ -262,11 +262,7 @@ public class ApiAutomationService {
         } else {
             scenario.setStatus(ScenarioStatus.Underway.name());
         }
-        if (request.getUserId() == null) {
-            scenario.setUserId(SessionUtils.getUserId());
-        } else {
-            scenario.setUserId(request.getUserId());
-        }
+        scenario.setUserId(SessionUtils.getUserId());
         if (StringUtils.isEmpty(request.getApiScenarioModuleId()) || "default-module".equals(request.getApiScenarioModuleId())) {
             ApiScenarioModuleExample example = new ApiScenarioModuleExample();
             example.createCriteria().andProjectIdEqualTo(request.getProjectId()).andNameEqualTo("默认模块");
