7746975d0711571f7f7f41c76883b38e55964557
fit2-zhao <yong.zhao@fit2cloud.com>
2021-04-20 19:05:45 +0800

    fix(接口自动化): 修复执行部分缺陷

--- frontend/src/business/components/api/automation/scenario/EditApiScenario.vue
+++ frontend/src/business/components/api/automation/scenario/EditApiScenario.vue
@@ -239,7 +239,7 @@
   import {ELEMENT_TYPE, ELEMENTS} from "./Setting";
   import MsApiCustomize from "./ApiCustomize";
   import {getUUID, objToStrMap, strMapToObj, handleCtrlSEvent} from "@/common/js/utils";
-  import ApiEnvironmentConfig from "../../definition/components/environment/ApiEnvironmentConfig";
+  import ApiEnvironmentConfig from "@/business/components/api/test/components/ApiEnvironmentConfig";
   import MsInputTag from "./MsInputTag";
   import MsRun from "./DebugRun";
   import MsApiReportDetail from "../report/ApiReportDetail";
