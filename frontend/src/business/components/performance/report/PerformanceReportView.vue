f9326c7b4383b0604c7dda4dc2ad3f73d1adad62
Captain.B <bin@fit2cloud.com>
2021-04-20 12:02:10 +0800

    fix(性能测试): 测试报告上的定时刷新修改

--- frontend/src/business/components/performance/report/PerformanceReportView.vue
+++ frontend/src/business/components/performance/report/PerformanceReportView.vue
@@ -266,8 +266,6 @@ export default {
         this.result = this.$post('/performance/run', {id: testId, triggerMode: 'MANUAL'}, (response) => {
           this.reportId = response.data;
           this.$router.push({path: '/performance/report/view/' + this.reportId});
-          // 注册 socket
-          this.initWebSocket();
         });
       }).catch(() => {
       });
@@ -375,7 +373,7 @@ export default {
       });
     },
     refresh() {
-      if (this.status === 'Running') {
+      if (this.status === 'Running' || this.status === 'Starting') {
         if (this.websocket && this.websocket.readyState === 1) {
           this.websocket.send(this.refreshTime);
         }
@@ -403,7 +401,6 @@ export default {
         this.initBreadcrumb((response) => {
           this.initReportTimeInfo();
         });
-        this.initWebSocket();
       } else {
         // console.log("close socket.");
         this.websocket.close(); //离开路由之后断开websocket连接
