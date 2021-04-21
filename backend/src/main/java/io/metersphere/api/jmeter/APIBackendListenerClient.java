627653bbfd495894e27ff292631b1e7201199d47
fit2-zhao <yong.zhao@fit2cloud.com>
2021-04-21 18:43:03 +0800

    fix(接口自动化): 修复循环无法展开，环境 类型为无只能添加一条 控制。

--- backend/src/main/java/io/metersphere/api/jmeter/APIBackendListenerClient.java
+++ backend/src/main/java/io/metersphere/api/jmeter/APIBackendListenerClient.java
@@ -144,6 +144,7 @@ public class APIBackendListenerClient extends AbstractBackendListenerClient impl
         testResult.setTestId(testId);
         testResult.setTotal(queue.size());
         testResult.setReportName(this.reportName);
+
         // 一个脚本里可能包含多个场景(ThreadGroup)，所以要区分开，key: 场景Id
         final Map<String, ScenarioResult> scenarios = new LinkedHashMap<>();
         queue.forEach(result -> {
