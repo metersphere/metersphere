d2fd6fae1f585a916aad65e1cf3d7ffd0cb7c4de
Captain.B <bin@fit2cloud.com>
2021-04-27 16:03:11 +0800

    refactor: 提供输入csrf-token的输入框

--- backend/src/main/java/io/metersphere/config/OpenApiConfig.java
+++ backend/src/main/java/io/metersphere/config/OpenApiConfig.java
@@ -3,6 +3,9 @@ package io.metersphere.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import org.springdoc.core.customizers.OperationCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
@@ -14,4 +17,13 @@ import org.springframework.context.annotation.Configuration;
 )
 @Configuration
 public class OpenApiConfig {
+    @Bean
+    public OperationCustomizer customize() {
+        return (operation, handlerMethod) -> {
+            if (!"login".equals(handlerMethod.getMethod().getName())) {
+                return operation.addParametersItem(new Parameter().in("header").required(true).name("CSRF-TOKEN"));
+            }
+            return operation;
+        };
+    }
 }
