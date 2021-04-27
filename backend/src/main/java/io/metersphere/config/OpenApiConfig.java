03b2ee75373e105b4ceab5765afa5509687bd1eb
Captain.B <bin@fit2cloud.com>
2021-04-27 14:32:15 +0800

    fix: 修复swagger-ui不能生成https的url

--- backend/src/main/java/io/metersphere/config/OpenApiConfig.java
+++ backend/src/main/java/io/metersphere/config/OpenApiConfig.java
@@ -2,13 +2,15 @@ package io.metersphere.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
         info = @Info(
                 title = "MeterSphere",
                 version = "1.0"
-        )
+        ),
+        servers = @Server(url = "/")
 )
 @Configuration
 public class OpenApiConfig {
