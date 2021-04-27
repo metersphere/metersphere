aaad4877fd77c88f2b4de3314e34c72167115c95
wenyann <wenyan.yang@fit2cloud.com>
2021-04-27 15:22:45 +0800

    fix: 测试计划-自定义表头，筛选功能会有问题

--- frontend/src/common/js/tableUtils.js
+++ frontend/src/common/js/tableUtils.js
@@ -176,4 +176,19 @@ export function buildBatchParam(vueObj) {
   return param;
 }
 
+// 深拷贝
+export function deepClone(source) {
+  if (!source && typeof source !== 'object') {
+    throw new Error('error arguments', 'deepClone');
+  }
+  const targetObj = source.constructor === Array ? [] : {};
+  Object.keys(source).forEach(keys => {
+    if (source[keys] && typeof source[keys] === 'object') {
+      targetObj[keys] = deepClone(source[keys]);
+    } else {
+      targetObj[keys] = source[keys];
+    }
+  });
+  return targetObj;
+}
 
