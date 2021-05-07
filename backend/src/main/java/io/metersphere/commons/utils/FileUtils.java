ed898fe71ed0bec3a7de983eb0b96ff7d448e64e
chenjianxing <jianxing.chen@fit2cloud.com>
2021-05-07 14:59:01 +0800

    refator: 代码规范

--- backend/src/main/java/io/metersphere/commons/utils/FileUtils.java
+++ backend/src/main/java/io/metersphere/commons/utils/FileUtils.java
@@ -4,17 +4,9 @@ import io.metersphere.commons.exception.MSException;
 import io.metersphere.i18n.Translator;
 import org.apache.commons.collections.CollectionUtils;
 import org.aspectj.util.FileUtil;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.net.URLEncoder;
-import java.util.Date;
 import java.util.List;
 
 public class FileUtils {
