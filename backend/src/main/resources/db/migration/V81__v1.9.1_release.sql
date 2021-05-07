4afbd0fb42acaa92ab7862181f8e9d11afd89dae
shiziyuan9527 <yuhao.li@fit2cloud.com>
2021-05-07 16:42:42 +0800

    fix: flyway

--- backend/src/main/resources/db/migration/V81__v1.9.1_release.sql
+++ backend/src/main/resources/db/migration/V81__v1.9.1_release.sql
@@ -1,4 +1,1 @@
 ALTER TABLE api_scenario_report MODIFY COLUMN scenario_name VARCHAR(3000);
-
-alter table project add scenario_custom_num tinyint(1) default 0 null comment '是否开启场景自定义ID(默认关闭)';
-alter table api_scenario add custom_num varchar(64) null comment 'custom num';
