a99bb0172f3d4ff1128b7b4b46aa6ab864b340e3
shiziyuan9527 <yuhao.li@fit2cloud.com>
2021-05-07 16:42:42 +0800

    fix: flyway

--- backend/src/main/resources/db/migration/V82__v1.9.2_release.sql
+++ backend/src/main/resources/db/migration/V82__v1.9.2_release.sql
@@ -4,3 +4,6 @@ ALTER TABLE issues ADD creator varchar(50) NULL COMMENT 'Creator';
 -- 删除创建人的自定义字段
 delete cft,cf from custom_field_template cft,custom_field cf
     where cft.field_id = cf.id and cf.name = '创建人' and cf.scene = 'ISSUE';
+
+alter table project add scenario_custom_num tinyint(1) default 0 null comment '是否开启场景自定义ID(默认关闭)';
+alter table api_scenario add custom_num varchar(64) null comment 'custom num';
