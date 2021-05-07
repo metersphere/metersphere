00d8a48f18f978ba4bf0f28a8c8eb35912f1dc52
chenjianxing <jianxing.chen@fit2cloud.com>
2021-05-07 11:35:09 +0800

    reafactor: markdown 替换富文本框

--- backend/src/main/java/io/metersphere/commons/utils/FileUtils.java
+++ backend/src/main/java/io/metersphere/commons/utils/FileUtils.java
@@ -4,9 +4,17 @@ import io.metersphere.commons.exception.MSException;
 import io.metersphere.i18n.Translator;
 import org.apache.commons.collections.CollectionUtils;
 import org.aspectj.util.FileUtil;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.net.URLEncoder;
+import java.util.Date;
 import java.util.List;
 
 public class FileUtils {
@@ -31,4 +39,36 @@ public class FileUtils {
             }
         }
     }
+
+    public static String uploadFile(MultipartFile uploadFile, String path, String name) {
+        if (uploadFile == null) {
+            return null;
+        }
+        File testDir = new File(path);
+        if (!testDir.exists()) {
+            testDir.mkdirs();
+        }
+        String filePath = testDir + "/" + name;
+        File file = new File(filePath);
+        try (InputStream in = uploadFile.getInputStream(); OutputStream out = new FileOutputStream(file)) {
+            file.createNewFile();
+            FileUtil.copyStream(in, out);
+        } catch (IOException e) {
+            LogUtil.error(e.getMessage(), e);
+            MSException.throwException(Translator.get("upload_fail"));
+        }
+        return filePath;
+    }
+
+    public static String uploadFile(MultipartFile uploadFile, String path) {
+       return uploadFile(uploadFile, path, uploadFile.getOriginalFilename());
+    }
+
+    public static void deleteFile(String path) {
+        File file = new File(path);
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+
 }
