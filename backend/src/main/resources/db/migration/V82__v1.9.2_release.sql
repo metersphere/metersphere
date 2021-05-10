ed9859c80fe3c22fb3d142424f060bcb2ce51da3
wenyann <wenyan.yang@fit2cloud.com>
2021-05-10 15:37:43 +0800

    fix: 接口定义列表自定义展示字段增加接口状态，并默认显示

--- backend/src/main/resources/db/migration/V82__v1.9.2_release.sql
+++ backend/src/main/resources/db/migration/V82__v1.9.2_release.sql
@@ -18,3 +18,6 @@ alter table api_scenario
 update system_header
 set props='[{"id":"num","label":"ID"},{"id":"name","label":"场景名称"},{"id":"level","label":"用例等级"},{"id":"status","label":"当前状态"},{"id":"tags","label":"标签"},{"id":"principal","label":"负责人"},{"id":"updateTime","label":"最后更新时间"},{"id":"stepTotal","label":"步骤数"},{"id":"lastResult","label":"最后结果"},{"id":"passRate","label":"通过率"}]'
 where type = 'api_scenario_list';
+update system_header
+set props= '[{"id":"num","label":"ID"},{"id":"status","label":"接口状态"},{"id":"name","label":"接口名称"},{"id":"method","label":"请求类型"},{"id":"userName","label":"负责人"},{"id":"path","label":"路径"},{"id":"tags","label":"标签"},{"id":"updateTime","label":"最后更新时间"},{"id":"caseTotal","label":"用例数"},{"id":"caseStatus","label":"用例状态"},{"id":"casePassingRate","label":"用例通过率"}]'
+where type = 'api_list';
