dcbbf5440cc5c8b6c4368b31b13546b6d7f1e965
shiziyuan9527 <yuhao.li@fit2cloud.com>
2021-05-13 12:16:28 +0800

    refactor: 自定义ID显示问题

--- frontend/src/business/components/track/plan/view/comonents/api/RelevanceScenarioList.vue
+++ frontend/src/business/components/track/plan/view/comonents/api/RelevanceScenarioList.vue
@@ -16,7 +16,12 @@
 
       <el-table ref="scenarioTable" border :data="tableData" class="adjust-table" @select-all="handleSelectAll" @select="handleSelect">
         <el-table-column type="selection"/>
-
+        <el-table-column v-if="!customNum" prop="num" label="ID"
+                         show-overflow-tooltip>
+        </el-table-column>
+        <el-table-column v-if="customNum" prop="customNum" label="ID"
+                         show-overflow-tooltip>
+        </el-table-column>
         <el-table-column prop="name" :label="$t('api_test.automation.scenario_name')"
                          show-overflow-tooltip/>
         <el-table-column prop="level" :label="$t('api_test.automation.case_level')"
@@ -102,7 +107,8 @@
         projectEnvMap: new Map(),
         projectList: [],
         projectIds: new Set(),
-        map: new Map()
+        map: new Map(),
+        customNum: false
       }
     },
     watch: {
@@ -123,6 +129,7 @@
         if (!this.projectId) {
           return;
         }
+        this.getProject(this.projectId);
         this.selectRows = new Set();
         this.loading = true;
 
@@ -165,6 +172,16 @@
         this.$get("/project/listAll", res => {
           this.projectList = res.data;
         })
+      },
+      getProject(projectId) {
+        if (projectId) {
+          this.$get("/project/get/" + projectId, result => {
+            let data = result.data;
+            if (data) {
+              this.customNum = data.scenarioCustomNum;
+            }
+          });
+        }
       },
       initProjectIds() {
         this.projectIds.clear();
