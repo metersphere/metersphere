49e410f66fa982fa0c9939546b31071c075dee40
shiziyuan9527 <yuhao.li@fit2cloud.com>
2021-05-17 18:05:32 +0800

    refactor(用户组和权限): 优化

--- backend/src/main/java/io/metersphere/commons/constants/UserGroupType.java
+++ backend/src/main/java/io/metersphere/commons/constants/UserGroupType.java
@@ -1,8 +1,8 @@
 package io.metersphere.commons.constants;
 
 public class UserGroupType {
-    public static final String SYSTEM = "system";
-    public static final String ORGANIZATION = "organization";
-    public static final String WORKSPACE = "workspace";
-    public static final String PROJECT = "project";
+    public static final String SYSTEM = "SYSTEM";
+    public static final String ORGANIZATION = "ORGANIZATION";
+    public static final String WORKSPACE = "WORKSPACE";
+    public static final String PROJECT = "PROJECT";
 }
--- backend/src/main/java/io/metersphere/dto/GroupPermissionDTO.java
+++ backend/src/main/java/io/metersphere/dto/GroupPermissionDTO.java
@@ -2,12 +2,10 @@ package io.metersphere.dto;
 
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class GroupPermissionDTO {
-    private List<GroupResourceDTO> system;
-    private List<GroupResourceDTO> organization;
-    private List<GroupResourceDTO> workspace;
-    private List<GroupResourceDTO> project;
+    private List<GroupResourceDTO> permissions = new ArrayList<>();
 }
