115287ac7280ff8e814345210dfc8614938cb165
Captain.B <bin@fit2cloud.com>
2021-05-18 14:33:43 +0800

    refactor(性能测试): 性能测试报告开始前不显示startTime

--- frontend/src/business/components/performance/report/PerformanceReportView.vue
+++ frontend/src/business/components/performance/report/PerformanceReportView.vue
@@ -38,10 +38,13 @@
             <span class="ms-report-time-desc">
               {{ $t('report.test_duration', [this.minutes, this.seconds]) }}
             </span>
-            <span class="ms-report-time-desc">
+            <span class="ms-report-time-desc" v-if="startTime !== '0'">
               {{ $t('report.test_start_time') }}：{{ startTime }}
             </span>
-            <span class="ms-report-time-desc" v-if="report.status === 'Completed'">
+            <span class="ms-report-time-desc" v-else>
+              {{ $t('report.test_start_time') }}：-
+            </span>
+            <span class="ms-report-time-desc" v-if="report.status === 'Completed' && endTime !== '0'">
               {{ $t('report.test_end_time') }}：{{ endTime }}
             </span>
             <span class="ms-report-time-desc" v-else>
