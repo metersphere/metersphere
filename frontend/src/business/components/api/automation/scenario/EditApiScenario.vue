497331f73a699e85e5c086626f1a077412852b4c
chenjianxing <jianxing.chen@fit2cloud.com>
2021-05-19 10:51:58 +0800

    refactor: 场景默认加载当前项目的环境配置

--- frontend/src/business/components/api/automation/scenario/EditApiScenario.vue
+++ frontend/src/business/components/api/automation/scenario/EditApiScenario.vue
@@ -1019,6 +1019,7 @@
           this.$post("/api/automation/getApiScenarioEnv", {definition: definition}, res => {
             if (res.data) {
               this.projectIds = new Set(res.data.projectIds);
+              this.projectIds.add(this.projectId);
               this.isFullUrl = res.data.fullUrl;
             }
             resolve();
