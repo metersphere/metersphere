6035c6fdbe39b85026f267e6aaff0b76c1dedbf3
Captain.B <bin@fit2cloud.com>
2021-05-24 15:22:10 +0800

    refactor: 合并sql

--- backend/src/main/resources/db/migration/V84__v1.10_release.sql
+++ backend/src/main/resources/db/migration/V84__v1.10_release.sql
@@ -63,13 +63,70 @@ ALTER TABLE user  ADD create_user VARCHAR(100) NULL;
 ALTER TABLE organization  ADD create_user VARCHAR(100) NULL;
 ALTER TABLE workspace  ADD create_user VARCHAR(100) NULL;
 ALTER TABLE test_resource_pool  ADD create_user VARCHAR(100) NULL;
-ALTER TABLE custom_field  ADD create_user VARCHAR(100) NULL;
-ALTER TABLE test_case_template  ADD create_user VARCHAR(100) NULL;
-ALTER TABLE issue_template  ADD create_user VARCHAR(100) NULL;
-ALTER TABLE test_case_report_template  ADD create_user VARCHAR(100) NULL;
-ALTER TABLE api_test_environment  ADD create_user VARCHAR(100) NULL;
+ALTER TABLE custom_field
+    ADD create_user VARCHAR(100) NULL;
+ALTER TABLE test_case_template
+    ADD create_user VARCHAR(100) NULL;
+ALTER TABLE issue_template
+    ADD create_user VARCHAR(100) NULL;
+ALTER TABLE test_case_report_template
+    ADD create_user VARCHAR(100) NULL;
+ALTER TABLE api_test_environment
+    ADD create_user VARCHAR(100) NULL;
 
 
 -- 缺陷来源
-ALTER TABLE issues ADD resource_id varchar(50) DEFAULT NULL COMMENT 'Issue resource id';
+ALTER TABLE issues
+    ADD resource_id varchar(50) DEFAULT NULL COMMENT 'Issue resource id';
 
+alter table issues
+    add num int null;
+
+DROP PROCEDURE IF EXISTS test_cursor;
+DELIMITER //
+CREATE PROCEDURE test_cursor()
+BEGIN
+    DECLARE projectId VARCHAR(64);
+    DECLARE issueId VARCHAR(64);
+    DECLARE num INT;
+    DECLARE done INT DEFAULT 0;
+    DECLARE cursor1 CURSOR FOR (SELECT DISTINCT project_id
+                                FROM issues
+                                WHERE num IS NULL);
+    DECLARE cursor2 CURSOR FOR (SELECT id
+                                FROM issues
+                                WHERE project_id = projectId
+                                ORDER BY create_time);
+
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    OPEN cursor1;
+    outer_loop:
+    LOOP
+        FETCH cursor1 INTO projectId;
+        IF done
+        THEN
+            LEAVE outer_loop;
+        END IF;
+        SET num = 100001;
+        OPEN cursor2;
+        inner_loop:
+        LOOP
+            FETCH cursor2 INTO issueId;
+            IF done
+            THEN
+                LEAVE inner_loop;
+            END IF;
+            UPDATE issues
+            SET num = num
+            WHERE id = issueId;
+            SET num = num + 1;
+        END LOOP;
+        SET done = 0;
+        CLOSE cursor2;
+    END LOOP;
+    CLOSE cursor1;
+END //
+DELIMITER ;
+
+CALL test_cursor();
+DROP PROCEDURE IF EXISTS test_cursor;
