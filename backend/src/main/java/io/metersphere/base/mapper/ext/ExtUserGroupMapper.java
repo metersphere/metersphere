a2c6c35afb466074587f631dcd3d5633a502af0e
Captain.B <bin@fit2cloud.com>
2021-05-24 21:19:01 +0800

    fix: 代码格式

--- backend/src/main/java/io/metersphere/base/mapper/ext/ExtUserGroupMapper.java
+++ backend/src/main/java/io/metersphere/base/mapper/ext/ExtUserGroupMapper.java
@@ -1,7 +1,6 @@
 package io.metersphere.base.mapper.ext;
 
 import io.metersphere.base.domain.Group;
-import io.metersphere.base.domain.Role;
 import io.metersphere.base.domain.User;
 import io.metersphere.controller.request.member.QueryMemberRequest;
 import io.metersphere.controller.request.organization.QueryOrgMemberRequest;
