0bc98d28c7dbba1ea700b83a1e2812feb69e774c
Captain.B <bin@fit2cloud.com>
2021-05-24 21:46:37 +0800

    fix: 处理冲突

--- backend/src/main/java/io/metersphere/controller/UserController.java
+++ backend/src/main/java/io/metersphere/controller/UserController.java
@@ -31,6 +31,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
