f9647a00fadb8eeb57d8e7f213021607c08cd94d
Captain.B <bin@fit2cloud.com>
2021-05-26 17:41:52 +0800

    fix(测试跟踪): 修复表哥高度和全选checkbox

--- frontend/src/business/components/common/components/table/MsTable.vue
+++ frontend/src/business/components/common/components/table/MsTable.vue
@@ -82,8 +82,8 @@ export default {
   },
   props: {
     screenHeight: {
-      type: Number,
-      default: 400,
+      type: String,
+      default: '400',
     },
     selectNodeIds: {
       type: Array,
