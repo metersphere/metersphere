87bd515c9d7037608e0db6be92ae5069efbc8047
fit2-zhao <yong.zhao@fit2cloud.com>
2021-06-04 18:13:33 +0800

    fix(接口自动化): 场景批量执行-并行 增加集合报告选项。

---
+++ backend/src/main/java/io/metersphere/api/jmeter/MessageCache.java
@@ -0,0 +1,8 @@
+package io.metersphere.api.jmeter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MessageCache {
+    public static Map<String, ReportCounter> cache = new HashMap<>();
+}
