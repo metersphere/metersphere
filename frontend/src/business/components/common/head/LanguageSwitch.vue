a6e5bedc681641e5f27f94e123ea0aed49323ce1
Captain.B <bin@fit2cloud.com>
2021-06-05 18:59:03 +0800

    refactor: 切换语言时不刷新页面

--- frontend/src/business/components/common/head/LanguageSwitch.vue
+++ frontend/src/business/components/common/head/LanguageSwitch.vue
@@ -3,101 +3,104 @@
            mode="horizontal"
            :background-color="color"
            text-color="#fff"
-           active-text-color="#fff"
-  >
+           active-text-color="#fff">
     <el-submenu index="1">
       <template slot="title">
         <font-awesome-icon class="icon global" :icon="['fas', 'globe']"/>
-        <span>{{language}}</span>
+        <span>{{ language }}</span>
       </template>
       <el-menu-item v-for="(value, key) in languageMap" :key="key" @click="changeLanguage(key)">
-        {{value}} <i class="el-icon-check" v-if="language === value"/>
+        {{ value }} <i class="el-icon-check" v-if="language === value"/>
       </el-menu-item>
     </el-submenu>
   </el-menu>
 </template>
 
 <script>
-import {DEFAULT_LANGUAGE, EN_US, TokenKey, ZH_CN, ZH_TW} from '../../../../common/js/constants';
-import {getCurrentUser} from "../../../../common/js/utils";
+import {DEFAULT_LANGUAGE, EN_US, TokenKey, ZH_CN, ZH_TW} from '@/common/js/constants';
+import {getCurrentUser} from "@/common/js/utils";
 
-  export default {
-    name: "MsLanguageSwitch",
-    data() {
-      return {
-        currentUserInfo: {},
-        language: '',
-        languageMap: {
-          [ZH_CN]: '简体中文',
-          [EN_US]: 'English',
-          [ZH_TW]: '繁體中文',
-        }
+export default {
+  name: "MsLanguageSwitch",
+  inject: [
+    'reload'
+  ],
+  data() {
+    return {
+      currentUserInfo: {},
+      language: '',
+      languageMap: {
+        [ZH_CN]: '简体中文',
+        [EN_US]: 'English',
+        [ZH_TW]: '繁體中文',
+      }
+    };
+  },
+  props: {
+    color: String
+  },
+  created() {
+    let lang = this.currentUser().language;
+    this.currentUserInfo = this.currentUser();
+    if (!lang) {
+      lang = localStorage.getItem(DEFAULT_LANGUAGE);
+    }
+    this.checkLanguage(lang)
+  },
+  methods: {
+    checkLanguage(lang) {
+      if (!lang) return;
+      this.$setLang(lang);
+      switch (lang) {
+        case ZH_CN:
+          this.language = this.languageMap[ZH_CN];
+          break;
+        case ZH_TW:
+          this.language = this.languageMap[ZH_TW];
+          break;
+        case EN_US:
+          this.language = this.languageMap[EN_US];
+          break;
+        default:
+          this.language = this.languageMap[ZH_CN];
+          break;
+      }
+    },
+    currentUser: () => {
+      return getCurrentUser();
+    },
+    changeLanguage(language) {
+      let user = {
+        id: this.currentUser().id,
+        language: language
       };
-    },
-    props: {
-      color: String
-    },
-    created() {
-      let lang = this.currentUser().language;
-      this.currentUserInfo = this.currentUser();
-      if (!lang) {
-        lang = localStorage.getItem(DEFAULT_LANGUAGE);
-      }
-      this.checkLanguage(lang)
-    },
-    methods: {
-      checkLanguage(lang) {
-        if (!lang) return;
-        this.$setLang(lang);
-        switch (lang) {
-          case ZH_CN:
-            this.language = this.languageMap[ZH_CN];
-            break;
-          case ZH_TW:
-            this.language = this.languageMap[ZH_TW];
-            break;
-          case EN_US:
-            this.language = this.languageMap[EN_US];
-            break;
-          default:
-            this.language = this.languageMap[ZH_CN];
-            break;
-        }
-      },
-      currentUser: () => {
-        return getCurrentUser();
-      },
-      changeLanguage(language) {
-        let user = {
-          id: this.currentUser().id,
-          language: language
-        };
-        this.checkLanguage(language);
-        this.result = this.$post("/user/update/current", user, response => {
-          localStorage.setItem(TokenKey, JSON.stringify(response.data));
-          window.location.reload();
-        });
-      }
+      this.checkLanguage(language);
+      this.result = this.$post("/user/update/current", user, response => {
+        localStorage.setItem(TokenKey, JSON.stringify(response.data));
+        // 刷新路由
+        this.reload();
+      });
     }
   }
+}
 </script>
 
 <style scoped>
 
-  .el-icon-check {
-    color: #44b349;
-    margin-left: 10px;
-  }
+.el-icon-check {
+  color: #44b349;
+  margin-left: 10px;
+}
 
-  .align-right {
-    float: right;
-  }
+.align-right {
+  float: right;
+}
 
-  .icon {
-    width: 24px;
-  }
+.icon {
+  width: 24px;
+}
 
-  .global {
-    color: #fff;
-  }
+.global {
+  color: #fff;
+}
 </style>
