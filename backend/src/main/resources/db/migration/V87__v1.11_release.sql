63d3167ed6a5a139d57f8ad0d431cb499fac3f28
fit2-zhao <yong.zhao@fit2cloud.com>
2021-06-09 19:03:46 +0800

    fix(任务中心): 合并sql脚本

--- backend/src/main/resources/db/migration/V87__v1.11_release.sql
+++
@@ -1,3 +0,0 @@
-ALTER table api_scenario_report add actuator varchar(100) null;
-ALTER table api_definition_exec_result add actuator varchar(100) null;
-ALTER table api_definition_exec_result add trigger_mode varchar(50) null;
