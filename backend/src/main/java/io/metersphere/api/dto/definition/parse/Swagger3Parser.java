534a0e016e05773b636a537b841bb67fb92c3b25
chenjianxing <jianxing.chen@fit2cloud.com>
2021-06-10 16:31:31 +0800

    fix: 接口定义导出swagger报错

--- backend/src/main/java/io/metersphere/api/dto/definition/parse/Swagger3Parser.java
+++ backend/src/main/java/io/metersphere/api/dto/definition/parse/Swagger3Parser.java
@@ -736,7 +736,9 @@ public class Swagger3Parser extends SwaggerAbstractParser {
                 } catch (Exception e1) {    //  若请求体 json 不合法，则忽略错误，原样字符串导出/导入
                     bodyInfo = new JSONObject();
                     ((JSONObject) bodyInfo).put("type", "string");
-                    ((JSONObject) bodyInfo).put("example", body.get("raw").toString());
+                    if (body != null && body.get("raw") != null) {
+                        ((JSONObject) bodyInfo).put("example", body.get("raw").toString());
+                    }
                 }
             } else if(bodyType.equals("XML")) {
                 String xmlText = body.getString("raw");
