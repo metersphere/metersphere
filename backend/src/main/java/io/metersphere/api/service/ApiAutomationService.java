2bdd448a045feb472499b0b9a542f49dc520da5b
wenyann <wenyan.yang@fit2cloud.com>
2021-06-14 16:27:00 +0800

    fix: 修改场景自动化 失败继续

--- backend/src/main/java/io/metersphere/api/service/ApiAutomationService.java
+++ backend/src/main/java/io/metersphere/api/service/ApiAutomationService.java
@@ -1070,6 +1070,7 @@ public class ApiAutomationService {
                     scenario.setVariables(variables);
                 }
                 group.setEnableCookieShare(scenario.isEnableCookieShare());
+                group.setOnSampleError(scenario.getOnSampleError());
                 LinkedList<MsTestElement> scenarios = new LinkedList<>();
                 scenarios.add(scenario);
                 // 创建场景报告
