12ecadf4b921dc51bf91b12e8bf5c3e5886d6022
chenjianxing <jianxing.chen@fit2cloud.com>
2021-06-17 19:33:10 +0800

    fix: 登出清空了localStorage

--- frontend/src/business/components/common/head/HeaderUser.vue
+++ frontend/src/business/components/common/head/HeaderUser.vue
@@ -28,7 +28,7 @@ import {getCurrentUser} from "@/common/js/utils";
 import AboutUs from "./AboutUs";
 import axios from "axios";
 import {mapGetters} from "vuex";
-import {ORGANIZATION_ID, PROJECT_ID, WORKSPACE_ID} from "@/common/js/constants";
+import {ORGANIZATION_ID, PROJECT_ID, TokenKey, WORKSPACE_ID} from "@/common/js/constants";
 
 const requireComponent = require.context('@/business/components/xpack/', true, /\.vue$/);
 const auth = requireComponent.keys().length > 0 ? requireComponent("./auth/Auth.vue") : {};
@@ -53,7 +53,7 @@ export default {
     logout: function () {
       axios.get("/signout").then(response => {
         if (response.data.success) {
-          localStorage.clear();
+          localStorage.removeItem(TokenKey);
 
           sessionStorage.removeItem(ORGANIZATION_ID);
           sessionStorage.removeItem(WORKSPACE_ID);
@@ -61,7 +61,7 @@ export default {
           window.location.href = "/login";
         }
       }).catch(error => {
-        localStorage.clear();
+        localStorage.removeItem(TokenKey);
 
         sessionStorage.removeItem(ORGANIZATION_ID);
         sessionStorage.removeItem(WORKSPACE_ID);
