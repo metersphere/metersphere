c1cf34ad859da41efbcc72ba0dce83ef73c1c643
fit2-zhao <yong.zhao@fit2cloud.com>
2021-06-17 16:09:44 +0800

    fix(接口自动化): 修复由 失败继续 这个功能引起不能执行的问题。

--- backend/src/main/java/io/metersphere/api/service/ApiAutomationService.java
+++ backend/src/main/java/io/metersphere/api/service/ApiAutomationService.java
@@ -780,6 +780,7 @@ public class ApiAutomationService {
             group.setLabel(item.getName());
             group.setName(reportId);
             MsScenario scenario = JSONObject.parseObject(item.getScenarioDefinition(), MsScenario.class);
+            group.setOnSampleError(scenario.isOnSampleError());
             this.preduceMsScenario(scenario);
             if (planEnvMap.size() > 0) {
                 scenario.setEnvironmentMap(planEnvMap);
@@ -823,6 +824,7 @@ public class ApiAutomationService {
                 group.setLabel(apiScenario.getName());
                 group.setName(apiScenario.getName());
                 group.setEnableCookieShare(scenario.isEnableCookieShare());
+                group.setOnSampleError(scenario.isOnSampleError());
                 group.setHashTree(new LinkedList<MsTestElement>() {{
                     this.add(scenario);
                 }});
@@ -1055,6 +1057,7 @@ public class ApiAutomationService {
                 mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
                 JSONObject element = JSON.parseObject(item.getScenarioDefinition());
                 MsScenario scenario = JSONObject.parseObject(item.getScenarioDefinition(), MsScenario.class);
+                group.setOnSampleError(scenario.isOnSampleError());
                 this.preduceMsScenario(scenario);
                 // 多态JSON普通转换会丢失内容，需要通过 ObjectMapper 获取
                 if (element != null && StringUtils.isNotEmpty(element.getString("hashTree"))) {
