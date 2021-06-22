1360462f61f2b52f04aa0cc1fd9f7865c7447584
Captain.B <bin@fit2cloud.com>
2021-06-22 13:11:05 +0800

    refactor(性能测试): 请求统计支持排序

--- frontend/src/business/components/performance/report/components/RequestStatistics.vue
+++ frontend/src/business/components/performance/report/components/RequestStatistics.vue
@@ -39,6 +39,7 @@
         <el-table-column
           prop="fail"
           label="FAIL"
+          sortable
           align="center"
           min-width="60"
         />
@@ -46,6 +47,7 @@
         <el-table-column
           prop="error"
           label="Error%"
+          sortable
           align="center"
         />
       </el-table-column>
@@ -54,36 +56,43 @@
         <el-table-column
           prop="average"
           label="Avg"
+          sortable
           min-width="60"
         />
         <el-table-column
           prop="min"
           label="Min"
+          sortable
           min-width="60"
         />
         <el-table-column
           prop="max"
           label="Max"
+          sortable
           min-width="60"
         />
         <el-table-column
           prop="median"
           label="Med"
+          sortable
           min-width="60"
         />
         <el-table-column
           prop="tp90"
           label="90%"
+          sortable
           min-width="60"
         />
         <el-table-column
           prop="tp95"
           label="95%"
+          sortable
           min-width="60"
         />
         <el-table-column
           prop="tp99"
           label="99%"
+          sortable
           min-width="60"
         />
       </el-table-column>
@@ -92,6 +101,7 @@
         <el-table-column
           prop="transactions"
           label="Trans/s"
+          sortable
           width="100"
         />
       </el-table-column>
@@ -99,13 +109,15 @@
       <el-table-column label="NetWork(KB/sec)" align="center">
         <el-table-column
           prop="received"
-          label="Received"
+          label="Recd"
+          sortable
           align="center"
           width="100"
         />
         <el-table-column
           prop="sent"
           label="Sent"
+          sortable
           align="center"
           width="100"
         />
