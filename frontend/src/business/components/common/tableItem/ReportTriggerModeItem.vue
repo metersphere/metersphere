47a4cdc6b35e779832743e18b62edfffa9d3a604
wenyann <wenyan.yang@fit2cloud.com>
2021-06-24 11:36:06 +0800

    fix:jenkins调用测试报告不更新

--- frontend/src/business/components/common/tableItem/ReportTriggerModeItem.vue
+++ frontend/src/business/components/common/tableItem/ReportTriggerModeItem.vue
@@ -1,5 +1,6 @@
 <template>
   <span>
+    <span v-if="triggerMode === 'TEST_PLAN_API'">{{ $t('commons.trigger_mode.api') }}</span>
     <span v-if="triggerMode === 'MANUAL'">{{$t('commons.trigger_mode.manual')}}</span>
     <span v-if="triggerMode === 'SCHEDULE'">{{$t('commons.trigger_mode.schedule')}}</span>
     <span v-if="triggerMode === 'TEST_PLAN_SCHEDULE'">{{$t('commons.trigger_mode.schedule')}}</span>
