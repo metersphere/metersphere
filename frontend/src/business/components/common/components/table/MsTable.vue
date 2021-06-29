60b6a723a6df7e12060a3f8a9ff39fe07a96f3f2
wenyann <wenyan.yang@fit2cloud.com>
2021-06-29 18:35:22 +0800

    fix:全选自动提示

--- frontend/src/business/components/common/components/table/MsTable.vue
+++ frontend/src/business/components/common/components/table/MsTable.vue
@@ -29,7 +29,8 @@
                        :resizable="false" align="center">
         <template v-slot:default="scope">
           <!-- 选中记录后浮现的按钮，提供对记录的批量操作 -->
-          <show-more-btn :is-show="scope.row.showMore" :buttons="batchOperators" :size="selectDataCounts"/>
+          <show-more-btn :is-show-tool="scope.row.showTool" :is-show="scope.row.showMore" :buttons="batchOperators"
+                         :size="selectDataCounts"/>
         </template>
       </el-table-column>
 
