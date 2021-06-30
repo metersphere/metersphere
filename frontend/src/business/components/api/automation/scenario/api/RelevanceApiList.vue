d197cdc3acef261f78b9251917d6027588880e2b
song-tianyang <tianyang.song@fit2cloud.com>
2021-06-30 11:35:23 +0800

    fix(场景测试报告，功能案例导入，场景测试-导入街口): 修复场景测试报告在多条名称相同的SQL请求下步骤统计错误的问题，修复测试用例同一模块下用例可以重复导入的问题，修复场景导入接口时鼠标悬停在ID上弹出编辑气泡的问题
    修复场景测试报告在多条名称相同的SQL请求下步骤统计错误的问题，修复测试用例同一模块下用例可以重复导入的问题，修复场景导入接口时鼠标悬停在ID上弹出编辑气泡的问题

--- frontend/src/business/components/api/automation/scenario/api/RelevanceApiList.vue
+++ frontend/src/business/components/api/automation/scenario/api/RelevanceApiList.vue
@@ -22,13 +22,13 @@
           label="ID"
           min-width="80px"
           sortable>
-          <template slot-scope="scope">
-            <!-- 判断为只读用户的话不可点击ID进行编辑操作 -->
-            <span style="cursor:pointer" v-if="isReadOnly"> {{ scope.row.num }} </span>
-            <el-tooltip v-else content="编辑">
-              <a style="cursor:pointer" @click="editApi(scope.row)"> {{ scope.row.num }} </a>
-            </el-tooltip>
-          </template>
+<!--          <template slot-scope="scope">-->
+<!--            &lt;!&ndash; 判断为只读用户的话不可点击ID进行编辑操作 &ndash;&gt;-->
+<!--            <span style="cursor:pointer" v-if="isReadOnly"> {{ scope.row.num }} </span>-->
+<!--            <el-tooltip v-else content="编辑">-->
+<!--              <a style="cursor:pointer" @click="editApi(scope.row)"> {{ scope.row.num }} </a>-->
+<!--            </el-tooltip>-->
+<!--          </template>-->
         </ms-table-column>
         <ms-table-column
           prop="name"
