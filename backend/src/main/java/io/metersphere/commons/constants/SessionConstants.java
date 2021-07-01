0b21fc8e6ae386c6b86c44d11b552ea0dc0882d1
Captain.B <bin@fit2cloud.com>
2021-07-01 11:43:42 +0800

    refactor: 后台重启过后，前端操作页面弹框提示重新登录

--- backend/src/main/java/io/metersphere/commons/constants/SessionConstants.java
+++ backend/src/main/java/io/metersphere/commons/constants/SessionConstants.java
@@ -2,4 +2,6 @@ package io.metersphere.commons.constants;
 
 public class SessionConstants {
     public static final String ATTR_USER = "user";
+    public static final String AUTHENTICATION_STATUS = "Authentication-Status";
+    public static final String AUTHENTICATION_INVALID = "invalid";
 }
