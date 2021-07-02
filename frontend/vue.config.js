c7cb74be3ac6a94fbdcd46a9435b2fb52c483f61
Captain.B <bin@fit2cloud.com>
2021-07-02 14:01:53 +0800

    refactor: 前端分开构建

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,8 +1,40 @@
-const path = require('path')
+const path = require('path');
 
 function resolve(dir) {
-  return path.join(__dirname, dir)
+  return path.join(__dirname, dir);
 }
+
+let projectName = process.argv[3];
+
+const projects = {
+  business: {
+    entry: "src/business/main.js",
+    template: "src/business/index.html",
+    filename: "index.html"
+  },
+  login: {
+    entry: "src/login/login.js",
+    template: "src/login/login.html",
+    filename: "login.html"
+  },
+  document: {
+    entry: "src/document/document.js",
+    template: "src/document/document.html",
+    filename: "document.html"
+  }
+};
+
+const getPage = function (projectName) {
+  if (!projectName) {
+    return projects;
+  }
+  return {
+    [projectName]: projects[projectName]
+  };
+};
+
+
+let pages = getPage(projectName);
 
 module.exports = {
   productionSourceMap: false,
@@ -17,23 +49,7 @@ module.exports = {
       },
     }
   },
-  pages: {
-    business: {
-      entry: "src/business/main.js",
-      template: "src/business/index.html",
-      filename: "index.html"
-    },
-    login: {
-      entry: "src/login/login.js",
-      template: "src/login/login.html",
-      filename: "login.html"
-    },
-    document: {
-      entry: "src/document/document.js",
-      template: "src/document/document.html",
-      filename: "document.html"
-    }
-  },
+  pages: pages,
   configureWebpack: {
     devtool: 'source-map',
     resolve: {
@@ -43,6 +59,6 @@ module.exports = {
     }
   },
   chainWebpack(config) {
-    config.plugins.delete('prefetch')
+    config.plugins.delete('prefetch');
   }
 };
