034fe38cead9da134bd897dbde613c01eab8e0c8
Captain.B <bin@fit2cloud.com>
2021-07-02 14:22:11 +0800

    revert: 先回退前端分开构建

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,38 +4,6 @@ function resolve(dir) {
   return path.join(__dirname, dir);
 }
 
-let projectName = process.argv[3];
-
-const projects = {
-  business: {
-    entry: "src/business/main.js",
-    template: "src/business/index.html",
-    filename: "index.html"
-  },
-  login: {
-    entry: "src/login/login.js",
-    template: "src/login/login.html",
-    filename: "login.html"
-  },
-  document: {
-    entry: "src/document/document.js",
-    template: "src/document/document.html",
-    filename: "document.html"
-  }
-};
-
-const getPage = function (projectName) {
-  if (!projectName) {
-    return projects;
-  }
-  return {
-    [projectName]: projects[projectName]
-  };
-};
-
-
-let pages = getPage(projectName);
-
 module.exports = {
   productionSourceMap: false,
   devServer: {
@@ -49,7 +17,23 @@ module.exports = {
       },
     }
   },
-  pages: pages,
+  pages: {
+    business: {
+      entry: "src/business/main.js",
+      template: "src/business/index.html",
+      filename: "index.html"
+    },
+    login: {
+      entry: "src/login/login.js",
+      template: "src/login/login.html",
+      filename: "login.html"
+    },
+    document: {
+      entry: "src/document/document.js",
+      template: "src/document/document.html",
+      filename: "document.html"
+    }
+  },
   configureWebpack: {
     devtool: 'source-map',
     resolve: {
