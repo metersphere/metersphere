11f91a58626bf6559a038466b19d801eac043929
chenjianxing <jianxing.chen@fit2cloud.com>
2021-07-02 14:07:00 +0800

    refactor: 执行按钮样式优化

--- frontend/src/business/components/api/automation/scenario/ApiScenarioList.vue
+++ frontend/src/business/components/api/automation/scenario/ApiScenarioList.vue
@@ -332,6 +332,7 @@ export default {
           tip: this.$t('api_test.automation.execute'),
           icon: "el-icon-video-play",
           exec: this.execute,
+          class: "run-button",
           permissions: ['PROJECT_API_SCENARIO:READ+RUN']
         },
         {
