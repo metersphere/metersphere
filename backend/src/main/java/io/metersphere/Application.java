0ee090acb8cb2dff5a7d5c7e854b57d8c83a4646
Inverseli <1920727772@qq.com>
2021-07-03 17:07:48 +0800

    build: 本地运行

--- backend/src/main/java/io/metersphere/Application.java
+++ backend/src/main/java/io/metersphere/Application.java
@@ -23,8 +23,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
         JmeterProperties.class
 })
 @EnableScheduling
-//@PropertySource(value = {"file:c:\\opt\\metersphere\\conf\\metersphere.properties"}, encoding = "UTF-8", ignoreResourceNotFound = true)
-@PropertySource(value = {"file:/opt/metersphere/conf/metersphere.properties"}, encoding = "UTF-8", ignoreResourceNotFound = true)
+@PropertySource(value = {"file:c:\\opt\\metersphere\\conf\\metersphere.properties"}, encoding = "UTF-8", ignoreResourceNotFound = true)
+//@PropertySource(value = {"file:/opt/metersphere/conf/metersphere.properties"}, encoding = "UTF-8", ignoreResourceNotFound = true)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
