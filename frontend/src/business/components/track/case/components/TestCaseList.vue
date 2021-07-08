613dbcb50c65109aad07cbd247271816462db06a
chenjianxing <jianxing.chen@fit2cloud.com>
2021-07-08 21:11:49 +0800

    refactor: 测试用例页面创建人显示名称

--- frontend/src/business/components/track/case/components/TestCaseList.vue
+++ frontend/src/business/components/track/case/components/TestCaseList.vue
@@ -56,7 +56,11 @@
           :field="item"
           :fields-width="fieldsWidth"
           :label="$t('commons.create_user')"
-          min-width="120"/>
+          min-width="120">
+          <template v-slot:default="scope">
+            {{memberMap.get(scope.row.createUser)}}
+          </template>
+        </ms-table-column>
 
         <ms-table-column
           prop="reviewStatus"
@@ -291,6 +295,7 @@ export default {
       page: getPageInfo(),
       fields: [],
       fieldsWidth: getCustomTableWidth('TRACK_TEST_CASE'),
+      memberMap: new Map()
     };
   },
   props: {
@@ -351,6 +356,9 @@ export default {
       this.page.result.loading = true;
       let p1 = getProjectMember((data) => {
         this.members = data;
+        this.members.forEach(item => {
+          this.memberMap.set(item.id, item.name);
+        });
       });
       let p2 = getTestTemplate();
       Promise.all([p1, p2]).then((data) => {
