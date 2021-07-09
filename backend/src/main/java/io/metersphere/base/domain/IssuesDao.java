01e6bc1b1e607691fc22d8acfdc95a403274fd4a
chenjianxing <jianxing.chen@fit2cloud.com>
2021-07-09 14:52:49 +0800

    refactor: 缺陷列表点击关联用例跳转到用例列表过滤

--- backend/src/main/java/io/metersphere/base/domain/IssuesDao.java
+++ backend/src/main/java/io/metersphere/base/domain/IssuesDao.java
@@ -3,6 +3,8 @@ package io.metersphere.base.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class IssuesDao extends IssuesWithBLOBs {
@@ -11,4 +13,5 @@ public class IssuesDao extends IssuesWithBLOBs {
     private String creatorName;
     private String resourceName;
     private long caseCount;
+    private List<String> caseIds;
 }
