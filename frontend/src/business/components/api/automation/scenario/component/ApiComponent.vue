e30fbd3b5c5e39340f66f5c18941c6fe392265a9
fit2-zhao <yong.zhao@fit2cloud.com>
2021-07-09 16:12:32 +0800

    fix(接口自动化): 修复场景引用步骤导出再导入缺陷

--- frontend/src/business/components/api/automation/scenario/component/ApiComponent.vue
+++ frontend/src/business/components/api/automation/scenario/component/ApiComponent.vue
@@ -18,7 +18,7 @@
         <el-tag size="mini" class="ms-tag" v-if="request.referenced==='Deleted'" type="danger">{{ $t('api_test.automation.reference_deleted') }}</el-tag>
         <el-tag size="mini" class="ms-tag" v-if="request.referenced==='Copy'">{{ $t('commons.copy') }}</el-tag>
         <el-tag size="mini" class="ms-tag" v-if="request.referenced ==='REF'">{{ $t('api_test.scenario.reference') }}</el-tag>
-        <span class="ms-tag">{{ getProjectName(request.projectId) }}</span>
+        <span class="ms-tag ms-step-name-api">{{ getProjectName(request.projectId) }}</span>
       </template>
       <template v-slot:debugStepCode>
          <span class="ms-step-debug-code" :class="request.requestResult.success?'ms-req-success':'ms-req-error'" v-if="request.debug && request.requestResult && request.requestResult.responseResult">
@@ -457,7 +457,16 @@ export default {
   margin: 20px;
   float: right;
 }
-
+.ms-step-name-api {
+  display: inline-block;
+  margin: 0 5px;
+  overflow-x: hidden;
+  padding-bottom: 0;
+  text-overflow: ellipsis;
+  vertical-align: middle;
+  white-space: nowrap;
+  width: 60px;
+}
 .ms-tag {
   margin-left: 10px;
 }
