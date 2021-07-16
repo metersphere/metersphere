c16c1bc7ac5fa9fd5ebc54e6e1f994980d26c70f
song-tianyang <tianyang.song@fit2cloud.com>
2021-07-16 17:14:51 +0800

    feat(接口测试,接口自动化,测试案例):
    临时提交

--- backend/src/main/java/io/metersphere/api/service/ApiTestCaseService.java
+++ backend/src/main/java/io/metersphere/api/service/ApiTestCaseService.java
@@ -93,7 +93,7 @@ public class ApiTestCaseService {
 
     public List<ApiTestCaseResult> list(ApiTestCaseRequest request) {
         request.setOrders(ServiceUtils.getDefaultOrder(request.getOrders()));
-        if (request.getModuleIds() == null) {
+        if (request.getModuleIds() == null && request.getModuleId() != null) {
             List<String> moduleIds = new ArrayList<>();
             moduleIds.add(request.getModuleId());
             request.setModuleIds(moduleIds);
