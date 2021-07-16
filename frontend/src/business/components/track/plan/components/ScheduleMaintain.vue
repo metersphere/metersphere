c22da6be6e151eae8d359ce2f5a23c850750de79
Captain.B <bin@fit2cloud.com>
2021-07-16 17:06:25 +0800

    fix: 修复测试计划定时任务不能选资源池的问题 --bug=1005180 --user=刘瑞斌 【测试计划】定时任务... https://www.tapd.cn/55049933/s/1025630

--- frontend/src/business/components/track/plan/components/ScheduleMaintain.vue
+++ frontend/src/business/components/track/plan/components/ScheduleMaintain.vue
@@ -78,6 +78,30 @@
                 </el-col>
               </el-row>
             </div>
+            <div class="ms-mode-div" v-if="runConfig.mode === 'parallel'">
+              <el-row>
+                <el-col :span="3">
+                  <span class="ms-mode-span">{{ $t("run_mode.other_config") }}:</span>
+                </el-col>
+                <el-col :span="18">
+                  <div v-if="scheduleTaskType === 'TEST_PLAN_TEST'" style="padding-top: 10px">
+                    <el-checkbox v-model="runConfig.runWithinResourcePool" style="padding-right: 10px;">
+                      {{ $t('run_mode.run_with_resource_pool') }}
+                    </el-checkbox>
+                    <el-select :disabled="!runConfig.runWithinResourcePool" v-model="runConfig.resourcePoolId"
+                               size="mini">
+                      <el-option
+                        v-for="item in resourcePools"
+                        :key="item.id"
+                        :label="item.name"
+                        :disabled="!item.api"
+                        :value="item.id">
+                      </el-option>
+                    </el-select>
+                  </div>
+                </el-col>
+              </el-row>
+            </div>
             <el-dialog width="60%" :title="$t('schedule.generate_expression')" :visible.sync="showCron"
                        :modal="false">
               <crontab @hide="showCron=false" @fill="crontabFill" :expression="schedule.value"
