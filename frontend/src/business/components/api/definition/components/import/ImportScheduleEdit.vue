b4e4903c9486d4305cbdb9d86c155647b078eb4b
Captain.B <bin@fit2cloud.com>
2021-07-16 16:29:22 +0800

    fix: 修复创建定时任务保存workspaceId和projectId的问题

--- frontend/src/business/components/api/definition/components/import/ImportScheduleEdit.vue
+++ frontend/src/business/components/api/definition/components/import/ImportScheduleEdit.vue
@@ -40,7 +40,7 @@
 <script>
 import {
   getCurrentProjectID,
-  getCurrentUser,
+  getCurrentUser, getCurrentWorkspaceId,
   listenGoBack,
   removeGoBackListener
 } from "@/common/js/utils";
@@ -175,6 +175,7 @@ export default {
       param = this.schedule;
       param.resourceId = this.swaggerUrl;
       param.projectId = getCurrentProjectID();
+      param.workspaceId = getCurrentWorkspaceId();
       param.moduleId = this.moduleId;
       param.modulePath = this.modulePath;
       param.modeId = this.modeId;
