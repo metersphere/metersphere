aa9ca2481a2f877f8fc5e6c0656ac364994c9d09
fit2-zhao <yong.zhao@fit2cloud.com>
2021-07-23 16:46:43 +0800

    fix(接口自动化): 修复调试执行结果丢包问题，日志排查DEBUG

--- backend/src/main/java/io/metersphere/api/service/MsResultService.java
+++ backend/src/main/java/io/metersphere/api/service/MsResultService.java
@@ -13,15 +13,14 @@ import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import sun.security.util.Cache;
 
-import java.util.Comparator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
 public class MsResultService {
     // 零时存放实时结果
     private Cache cache = Cache.newHardMemoryCache(0, 3600 * 2);
+    private Map<String, List<SampleResult>> processCache = new HashMap<>();
 
     private final static String THREAD_SPLIT = " ";
 
@@ -34,29 +33,56 @@ public class MsResultService {
         return null;
     }
 
+    public List<SampleResult> procResult(String key) {
+        if (this.processCache.get(key) != null) {
+            return this.processCache.get(key);
+        }
+        return new LinkedList<>();
+    }
+
     public void setCache(String key, SampleResult result) {
         if (key.startsWith("[") && key.endsWith("]")) {
             key = JSON.parseArray(key).get(0).toString();
         }
-        TestResult testResult = this.getResult(key);
+        List<SampleResult> testResult = this.procResult(key);
+        testResult.add(result);
+        this.processCache.put(key, testResult);
+    }
+
+    public TestResult sysnSampleResult(String key) {
+        if (key.startsWith("[") && key.endsWith("]")) {
+            key = JSON.parseArray(key).get(0).toString();
+        }
+        List<SampleResult> results = this.processCache.get(key);
+        boolean isRemove = false;
+        TestResult testResult = (TestResult) cache.get(key);
         if (testResult == null) {
             testResult = new TestResult();
         }
-        if (result.getResponseCode().equals(MsResultCollector.TEST_END)) {
-            testResult.setEnd(true);
+        if (CollectionUtils.isNotEmpty(results)) {
+            final Map<String, ScenarioResult> scenarios = new LinkedHashMap<>();
+            for (SampleResult result : results) {
+                if (result.getResponseCode().equals(MsResultCollector.TEST_END)) {
+                    testResult.setEnd(true);
+                    this.cache.put(key, testResult);
+                    isRemove = true;
+                    break;
+                }
+                testResult.setTestId(key);
+                testResult.setConsole(getJmeterLogger(key, false));
+                testResult.setTotal(0);
+                this.formatTestResult(testResult, scenarios, result);
+            }
+            testResult.getScenarios().clear();
+            testResult.getScenarios().addAll(scenarios.values());
+            testResult.getScenarios().sort(Comparator.comparing(ScenarioResult::getId));
             this.cache.put(key, testResult);
-            return;
+
+            if (isRemove) {
+                this.processCache.remove(key);
+            }
         }
-        testResult.setTestId(key);
-        testResult.setConsole(getJmeterLogger(key, false));
-        testResult.setTotal(0);
-        final Map<String, ScenarioResult> scenarios = new LinkedHashMap<>();
-
-        this.formatTestResult(testResult, scenarios, result);
-
-        testResult.getScenarios().addAll(scenarios.values());
-        testResult.getScenarios().sort(Comparator.comparing(ScenarioResult::getId));
-        this.cache.put(key, testResult);
+        return (TestResult) cache.get(key);
     }
 
     public void delete(String testId) {
@@ -88,7 +114,6 @@ public class MsResultService {
             scenarioResult.addError(result.getErrorCount());
             testResult.addError(result.getErrorCount());
         }
-
         RequestResult requestResult = this.getRequestResult(result);
         scenarioResult.getRequestResults().add(requestResult);
         scenarioResult.addResponseTime(result.getTime());
