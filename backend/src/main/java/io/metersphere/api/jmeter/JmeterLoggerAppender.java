fdaf2a3e48fba8c54ded72a0377b56d09f0767b9
fit2-zhao <yong.zhao@fit2cloud.com>
2021-07-23 16:46:43 +0800

    fix(接口自动化): 修复调试执行结果丢包问题，日志排查DEBUG

--- backend/src/main/java/io/metersphere/api/jmeter/JmeterLoggerAppender.java
+++ backend/src/main/java/io/metersphere/api/jmeter/JmeterLoggerAppender.java
@@ -17,15 +17,17 @@ public class JmeterLoggerAppender extends UnsynchronizedAppenderBase<ILoggingEve
             if (logger == null) {
                 logger = new LinkedHashMap<>();
             }
-            StringBuffer message = new StringBuffer();
-            message.append(DateUtils.getTimeStr(event.getTimeStamp())).append(" ")
-                    .append(event.getLevel()).append(" ")
-                    .append(event.getThreadName()).append(" ")
-                    .append(event.getFormattedMessage()).append("\n");
-            if (logger.containsKey(event.getTimeStamp())) {
-                logger.get(event.getTimeStamp()).append(message);
-            } else {
-                logger.put(event.getTimeStamp(), message);
+            if (!event.getLevel().levelStr.equals(LogUtil.DEBUG)) {
+                StringBuffer message = new StringBuffer();
+                message.append(DateUtils.getTimeStr(event.getTimeStamp())).append(" ")
+                        .append(event.getLevel()).append(" ")
+                        .append(event.getThreadName()).append(" ")
+                        .append(event.getFormattedMessage()).append("\n");
+                if (logger.containsKey(event.getTimeStamp())) {
+                    logger.get(event.getTimeStamp()).append(message);
+                } else {
+                    logger.put(event.getTimeStamp(), message);
+                }
             }
         } catch (Exception e) {
             LogUtil.error(e);
