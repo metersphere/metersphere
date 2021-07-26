2781edbd55964b1236c41b4d84d52672eb3a1cd4
Captain.B <bin@fit2cloud.com>
2021-07-26 13:41:48 +0800

    refactor: 缓存监控节点，避免 prometheus 查询频繁导致数据库链接超时

---
+++ backend/src/main/java/io/metersphere/consul/CacheNodeAspect.java
@@ -0,0 +1,33 @@
+package io.metersphere.consul;
+
+import io.metersphere.commons.utils.LogUtil;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Aspect
+@Component
+public class CacheNodeAspect {
+    @Resource
+    private ConsulService consulService;
+
+    /**
+     * 定义切点 @Pointcut 在注解的位置切入代码
+     */
+    @Pointcut("@annotation(io.metersphere.consul.CacheNode)")
+    public void cacheNodes() {
+    }
+
+    @After("cacheNodes()")
+    public void after() {
+        try {
+            consulService.updateCache();
+        } catch (Exception e) {
+            LogUtil.error(e.getMessage());
+        }
+    }
+
+}
