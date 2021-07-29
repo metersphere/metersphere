4bd97aeeacdf2a863fa316197bcec5f3f07bde97
song-tianyang <tianyang.song@fit2cloud.com>
2021-07-29 14:58:01 +0800

    fix(接口定义、接口自动化、功能案例): 修复串行执行场景，环境参数没引用到的缺陷，功能案例导出模板的责任人提示优化，修复功能案例导出时自定义字段缺失的缺陷
    --bug=1005315 --user=宋天阳 【环境参数】串行执行场景，环境参数没引用到
    https://www.tapd.cn/55049933/s/1030553；--bug=1005444 --user=宋天阳
    【github#4701】项目关联的是自定义用例模板，导入用例后，选择用例进行导出，导出的用例中字段还是老的模板
    https://www.tapd.cn/55049933/s/1030569；修复接口定义和接口自动化编辑页面中责任人的展现形式：名称（ID）；功能案例导出模板的责任人提示进行优化

--- frontend/src/business/components/api/definition/components/complete/EditCompleteHTTPApi.vue
+++ frontend/src/business/components/api/definition/components/complete/EditCompleteHTTPApi.vue
@@ -43,7 +43,7 @@
                   <el-option
                     v-for="item in maintainerOptions"
                     :key="item.id"
-                    :label="item.id + ' (' + item.name + ')'"
+                    :label="item.name + ' (' + item.id + ')'"
                     :value="item.id">
                   </el-option>
                 </el-select>
