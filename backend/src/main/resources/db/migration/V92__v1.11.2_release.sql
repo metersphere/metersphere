fe484a425d91c0bf6474f31d21e2ad1e87c648d8
Captain.B <bin@fit2cloud.com>
2021-07-29 15:53:30 +0800

    refactor: 重建中间表，保证联合主键的自增问题

--- backend/src/main/resources/db/migration/V92__v1.11.2_release.sql
+++ backend/src/main/resources/db/migration/V92__v1.11.2_release.sql
@@ -13,3 +13,17 @@ values (UUID(), 'project_member', 'PROJECT_REPORT_ANALYSIS:READ+EXPORT', 'PROJEC
 -- 只读
 insert into user_group_permission (id, group_id, permission_id, module_id)
 values (UUID(), 'read_only', 'PROJECT_REPORT_ANALYSIS:READ', 'PROJECT_REPORT_ANALYSIS');
+
+
+-- 重建中间表
+DROP TABLE load_test_report_detail;
+CREATE TABLE `load_test_report_detail`
+(
+    `report_id` VARCHAR(50)           NOT NULL,
+    `part`      BIGINT AUTO_INCREMENT NOT NULL,
+    `content`   LONGTEXT,
+    PRIMARY KEY (`report_id`, `part`)
+)
+    ENGINE = MyISAM
+    DEFAULT CHARSET = utf8mb4
+    COLLATE utf8mb4_general_ci;
