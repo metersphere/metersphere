6f27c43a241a42189d3d560266b394b6a23968ac
fit2-zhao <yong.zhao@fit2cloud.com>
2021-08-02 15:32:03 +0800

    refactor(接口测试): 执行相关websocket 处理优化防止数据堆积情况

--- backend/src/main/java/io/metersphere/api/jmeter/MessageCache.java
+++ backend/src/main/java/io/metersphere/api/jmeter/MessageCache.java
@@ -1,8 +1,13 @@
 package io.metersphere.api.jmeter;
 
+import javax.websocket.Session;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class MessageCache {
     public static Map<String, ReportCounter> cache = new HashMap<>();
+
+    public static ConcurrentHashMap<String, Session> reportCache = new ConcurrentHashMap<>();
+
 }
