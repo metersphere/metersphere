18d12e5c9fce886561d3222e1919693b7ae51acb
shiziyuan9527 <yuhao.li@fit2cloud.com>
2021-08-04 11:10:52 +0800

    fix: 接口定义JMeter导入

--- frontend/src/i18n/zh-TW.js
+++ frontend/src/i18n/zh-TW.js
@@ -160,6 +160,7 @@ export default {
     node_name_tip: "名稱不能包含'\\'",
     more_operator: "更多操作",
     mock: "Mock 設置",
+    trash: "回收站",
     all_module_title: "全部模塊",
     create_user: "創建人",
     table: {
