6597b676a81b381bb448cdf5c15f0920de4c10e1
Captain.B <bin@fit2cloud.com>
2021-08-18 17:00:42 +0800

    refactor: 显示消息中心

---
+++ backend/src/main/java/io/metersphere/websocket/NotificationWebSocket.java
@@ -0,0 +1,107 @@
+package io.metersphere.websocket;
+
+import io.metersphere.base.domain.Notification;
+import io.metersphere.commons.constants.NotificationConstants;
+import io.metersphere.commons.utils.LogUtil;
+import io.metersphere.notice.service.NotificationService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentHashMap;
+
+@ServerEndpoint("/notification/count/{userId}")
+@Component
+public class NotificationWebSocket {
+    private static NotificationService notificationService;
+    private static ConcurrentHashMap<Session, Timer> refreshTasks = new ConcurrentHashMap<>();
+
+    @Resource
+    public void setNotificationService(NotificationService notificationService) {
+        NotificationWebSocket.notificationService = notificationService;
+    }
+
+    /**
+     * 开启连接的操作
+     */
+    @OnOpen
+    public void onOpen(@PathParam("userId") String userId, Session session) {
+        Timer timer = new Timer(true);
+        NotificationCenter task = new NotificationCenter(session, userId);
+        timer.schedule(task, 0, 10 * 1000);
+        refreshTasks.putIfAbsent(session, timer);
+    }
+
+    /**
+     * 连接关闭的操作
+     */
+    @OnClose
+    public void onClose(Session session) {
+        Timer timer = refreshTasks.get(session);
+        if (timer != null) {
+            timer.cancel();
+            refreshTasks.remove(session);
+        }
+    }
+
+    /**
+     * 推送消息
+     */
+    @OnMessage
+    public void onMessage(@PathParam("userId") String userId, Session session, String message) {
+        int refreshTime = 10;
+        try {
+            refreshTime = Integer.parseInt(message);
+        } catch (Exception e) {
+        }
+        try {
+            Timer timer = refreshTasks.get(session);
+            timer.cancel();
+
+            Timer newTimer = new Timer(true);
+            newTimer.schedule(new NotificationCenter(session, userId), 0, refreshTime * 1000L);
+            refreshTasks.put(session, newTimer);
+        } catch (Exception e) {
+            LogUtil.error(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * 出错的操作
+     */
+    @OnError
+    public void onError(Throwable error) {
+        System.out.println(error);
+        error.printStackTrace();
+    }
+
+    public static class NotificationCenter extends TimerTask {
+        private Session session;
+        private String userId;
+
+        NotificationCenter(Session session, String userId) {
+            this.session = session;
+            this.userId = userId;
+        }
+
+        @Override
+        public void run() {
+            try {
+                if (!session.isOpen()) {
+                    return;
+                }
+                Notification notification = new Notification();
+                notification.setReceiver(userId);
+                notification.setStatus(NotificationConstants.Status.UNREAD.name());
+                int count = notificationService.countNotification(notification);
+                session.getBasicRemote().sendText(count + "");
+            } catch (Exception e) {
+                LogUtil.error(e.getMessage(), e);
+            }
+        }
+    }
+}
