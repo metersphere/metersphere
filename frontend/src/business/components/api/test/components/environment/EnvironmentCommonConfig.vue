a17e722b5c454680ff099bc866194b7a2d894ff5
song-tianyang <tianyang.song@fit2cloud.com>
2021-08-18 17:23:15 +0800

    feat(环境配置): 环境配置通用配置增加超时时间配置 #1002297
    https://www.tapd.cn/55049933/prong/stories/view/1155049933001002297

--- frontend/src/business/components/api/test/components/environment/EnvironmentCommonConfig.vue
+++ frontend/src/business/components/api/test/components/environment/EnvironmentCommonConfig.vue
@@ -1,65 +1,89 @@
 <template>
-    <div>
-      <el-form :model="commonConfig" :rules="rules" ref="commonConfig" :disabled="isReadOnly">
+  <div>
+    <el-form :model="commonConfig" :rules="rules" ref="commonConfig" :disabled="isReadOnly">
 
-        <span>{{$t('api_test.environment.globalVariable')}}</span>
-        <ms-api-scenario-variables :show-copy="false" :items="commonConfig.variables"/>
+      <span>{{$t('api_test.environment.globalVariable')}}</span>
+      <ms-api-scenario-variables :show-copy="false" :items="commonConfig.variables"/>
 
-        <el-form-item>
-          <el-switch v-model="commonConfig.enableHost" active-text="Hosts"/>
-        </el-form-item>
-        <ms-api-host-table v-if="commonConfig.enableHost" :hostTable="commonConfig.hosts" ref="refHostTable"/>
-      </el-form>
-    </div>
+      <el-form-item>
+        <el-switch v-model="commonConfig.enableHost" active-text="Hosts"/>
+      </el-form-item>
+      <el-form-item>
+        <span>{{$t('api_test.environment.request_timeout')}}:</span>
+        <el-input-number style="margin-left: 20px" v-model="commonConfig.requestTimeout">{{$t('api_test.environment.globalVariable')}}</el-input-number>
+        <span style="margin-left: 30px">{{$t('api_test.environment.response_timeout')}}:</span>
+        <el-input-number style="margin-left: 20px" v-model="commonConfig.responseTimeout">{{$t('api_test.environment.globalVariable')}}</el-input-number>
+      </el-form-item>
+      <ms-api-host-table v-if="commonConfig.enableHost" :hostTable="commonConfig.hosts" ref="refHostTable"/>
+    </el-form>
+  </div>
 </template>
 
 <script>
-  import {CommonConfig, Environment} from "../../model/EnvironmentModel";
-  import MsApiScenarioVariables from "../ApiScenarioVariables";
-  import MsApiHostTable from "../ApiHostTable";
+import {CommonConfig, Environment} from "../../model/EnvironmentModel";
+import MsApiScenarioVariables from "../ApiScenarioVariables";
+import MsApiHostTable from "../ApiHostTable";
 
-    export default {
-      name: "MsEnvironmentCommonConfig",
-      components: {MsApiHostTable, MsApiScenarioVariables},
-      props: {
-        commonConfig: new CommonConfig(),
-        isReadOnly: {
-          type: Boolean,
-          default: false
-        },
-      },
-      data() {
-        return {
-          rules: {
+export default {
+  name: "MsEnvironmentCommonConfig",
+  components: {MsApiHostTable, MsApiScenarioVariables},
+  props: {
+    commonConfig: new CommonConfig(),
+    isReadOnly: {
+      type: Boolean,
+      default: false
+    },
+  },
+  data() {
+    return {
+      rules: {
 
-          },
-        }
       },
-      methods: {
-        validate() {
-          let isValidate = false;
-          this.$refs['commonConfig'].validate((valid) => {
-            if (valid) {
-              // 校验host列表
-              let valHost = true;
-              if (this.commonConfig.enableHost) {
-                for (let i = 0; i < this.commonConfig.hosts.length; i++) {
-                  valHost = this.$refs['refHostTable'].confirm(this.commonConfig.hosts[i]);
-                }
-              }
-              if (valHost) {
-                isValidate = true;
-              } else {
-                isValidate = false;
-              }
-            } else {
-              isValidate = false;
-            }
-          });
-          return isValidate;
-        }
+    }
+  },
+  created() {
+    if(!this.commonConfig.requestTimeout){
+      this.commonConfig.requestTimeout = 60000;
+    }
+    if(!this.commonConfig.responseTimeout){
+      this.commonConfig.responseTimeout = 60000;
+    }
+  },
+  watch: {
+    commonConfig(){
+      if(!this.commonConfig.requestTimeout){
+        this.commonConfig.requestTimeout = 60000;
+      }
+      if(!this.commonConfig.responseTimeout){
+        this.commonConfig.responseTimeout = 60000;
       }
     }
+  },
+  methods: {
+    validate() {
+      let isValidate = false;
+      this.$refs['commonConfig'].validate((valid) => {
+        if (valid) {
+          // 校验host列表
+          let valHost = true;
+          if (this.commonConfig.enableHost) {
+            for (let i = 0; i < this.commonConfig.hosts.length; i++) {
+              valHost = this.$refs['refHostTable'].confirm(this.commonConfig.hosts[i]);
+            }
+          }
+          if (valHost) {
+            isValidate = true;
+          } else {
+            isValidate = false;
+          }
+        } else {
+          isValidate = false;
+        }
+      });
+      return isValidate;
+    }
+  }
+}
 </script>
 
 <style scoped>
