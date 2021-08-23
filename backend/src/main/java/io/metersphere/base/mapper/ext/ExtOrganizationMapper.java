9c430f4ee5ca6429b5719caf98c5930a76dee695
Captain.B <bin@fit2cloud.com>
2021-08-23 17:12:12 +0800

    refactor: 组织默认配置通知

--- backend/src/main/java/io/metersphere/base/mapper/ext/ExtOrganizationMapper.java
+++ backend/src/main/java/io/metersphere/base/mapper/ext/ExtOrganizationMapper.java
@@ -1,6 +1,5 @@
 package io.metersphere.base.mapper.ext;
 
-import io.metersphere.base.domain.Organization;
 import io.metersphere.dto.OrganizationMemberDTO;
 import org.apache.ibatis.annotations.Param;
 
@@ -11,4 +10,6 @@ public interface ExtOrganizationMapper {
     int checkSourceRole(@Param("sourceId") String sourceId,@Param("userId") String userId,@Param("groupId") String groupId);
 
     List<OrganizationMemberDTO> findIdAndNameByOrganizationId(@Param("organizationId")String organizationID);
+
+    void setDefaultMessageTask(@Param("orgId") String orgId);
 }
