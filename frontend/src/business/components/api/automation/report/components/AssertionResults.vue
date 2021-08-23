962af43c56ca02d592f8835dbe84cbc3f4300289
fit2-zhao <yong.zhao@fit2cloud.com>
2021-08-23 14:20:33 +0800

    fix (接口定义): 用例通过率统计问题修复 #1006031 --bug=1006031 --user=赵勇 【接口测试】用例列表... https://www.tapd.cn/55049933/s/1037723

--- frontend/src/business/components/api/automation/report/components/AssertionResults.vue
+++ frontend/src/business/components/api/automation/report/components/AssertionResults.vue
@@ -1,14 +1,19 @@
 <template>
   <el-table :data="assertions" :row-style="getRowStyle" :header-cell-style="getRowStyle">
-    <el-table-column prop="name" :label="$t('api_report.assertions_name')" width="300"/>
+    <el-table-column prop="name" :label="$t('api_report.assertions_name')" width="150" show-overflow-tooltip>
+      <template v-slot:default="scope">
+        <span>{{ !scope.row.name || scope.row.name === 'null' ? "" : scope.row.name }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column prop="content" label="内容" width="200" show-overflow-tooltip/>
     <el-table-column prop="message" :label="$t('api_report.assertions_error_message')"/>
     <el-table-column prop="pass" :label="$t('api_report.assertions_is_success')" width="180">
       <template v-slot:default="{row}">
         <el-tag size="mini" type="success" v-if="row.pass">
-          {{$t('api_report.success')}}
+          {{ $t('api_report.success') }}
         </el-tag>
         <el-tag size="mini" type="danger" v-else>
-          {{$t('api_report.fail')}}
+          {{ $t('api_report.fail') }}
         </el-tag>
       </template>
     </el-table-column>
@@ -16,19 +21,19 @@
 </template>
 
 <script>
-  export default {
-    name: "MsAssertionResults",
+export default {
+  name: "MsAssertionResults",
 
-    props: {
-      assertions: Array
-    },
+  props: {
+    assertions: Array
+  },
 
-    methods: {
-      getRowStyle() {
-        return {backgroundColor: "#F5F5F5"};
-      }
+  methods: {
+    getRowStyle() {
+      return {backgroundColor: "#F5F5F5"};
     }
   }
+}
 </script>
 
 <style scoped>
