dd4f3b75bfe2c5557e29b18ac0ea8a92572a74b0
Captain.B <bin@fit2cloud.com>
2021-08-26 15:07:44 +0800

    refactor: 发送通知查询手机邮箱优化

--- backend/src/main/java/io/metersphere/notice/sender/impl/LarkNoticeSender.java
+++ backend/src/main/java/io/metersphere/notice/sender/impl/LarkNoticeSender.java
@@ -23,7 +23,7 @@ public class LarkNoticeSender extends AbstractNoticeSender {
                 .map(Receiver::getUserId)
                 .distinct()
                 .collect(Collectors.toList());
-        List<UserDetail> userDetails = super.getUserDetails(noticeModel, userIds);
+        List<UserDetail> userDetails = super.getUserDetails(userIds);
         List<String> collect = userDetails.stream()
                 .map(ud -> "<at email=\"" + ud.getEmail() + "\">" + ud.getName() + "</at>")
                 .collect(Collectors.toList());
