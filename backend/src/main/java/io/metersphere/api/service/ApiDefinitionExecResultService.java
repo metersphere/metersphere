3dcc927a3805c2dcb05165aeb978c419bdb9ee9a
Captain.B <bin@fit2cloud.com>
2021-08-27 16:13:01 +0800

    refactor: jenkins 通知发送

--- backend/src/main/java/io/metersphere/api/service/ApiDefinitionExecResultService.java
+++ backend/src/main/java/io/metersphere/api/service/ApiDefinitionExecResultService.java
@@ -160,10 +160,15 @@ public class ApiDefinitionExecResultService {
                 .failedMailTemplate("api/CaseResult")
                 .paramMap(paramMap)
                 .event(event)
+                .excludeSelf(true)
                 .build();
 
         String taskType = NoticeConstants.TaskType.API_DEFINITION_TASK;
-        noticeSendService.send(taskType, noticeModel);
+        if (StringUtils.equals(ReportTriggerMode.API.name(), result.getTriggerMode())) {
+            noticeSendService.send(ReportTriggerMode.API.name(), taskType, noticeModel);
+        } else {
+            noticeSendService.send(taskType, noticeModel);
+        }
     }
 
     private String getName(String type, String id, String status, Long time, String resourceId) {
@@ -225,7 +230,7 @@ public class ApiDefinitionExecResultService {
         String finalSaveResultType = saveResultType;
 
         Map<String, String> apiIdResultMap = new HashMap<>();
-        Map<String,ApiDefinitionExecResult> caseReportMap = new HashMap<>();
+        Map<String, ApiDefinitionExecResult> caseReportMap = new HashMap<>();
 
         if (CollectionUtils.isNotEmpty(result.getScenarios())) {
             result.getScenarios().forEach(scenarioResult -> {
@@ -235,7 +240,7 @@ public class ApiDefinitionExecResultService {
                         ApiDefinitionExecResult saveResult = new ApiDefinitionExecResult();
                         saveResult.setId(UUID.randomUUID().toString());
                         saveResult.setCreateTime(System.currentTimeMillis());
-                        saveResult.setName(getName(type, item.getName(), status, saveResult.getCreateTime(),saveResult.getId()));
+                        saveResult.setName(getName(type, item.getName(), status, saveResult.getCreateTime(), saveResult.getId()));
                         ApiDefinitionWithBLOBs apiDefinitionWithBLOBs = apiDefinitionMapper.selectByPrimaryKey(item.getName());
                         String caseId = null;
                         if (apiDefinitionWithBLOBs != null) {
@@ -254,7 +259,7 @@ public class ApiDefinitionExecResultService {
                                 }
                             }
                         }
-                        if(StringUtils.isNotEmpty(caseId)){
+                        if (StringUtils.isNotEmpty(caseId)) {
                             apiIdResultMap.put(caseId, item.isSuccess() ? TestPlanApiExecuteStatus.SUCCESS.name() : TestPlanApiExecuteStatus.FAILD.name());
                         }
 
@@ -301,14 +306,14 @@ public class ApiDefinitionExecResultService {
                             apiDefinitionExecResultMapper.updateByPrimaryKeyWithBLOBs(prevResult);
                         }
                         apiDefinitionExecResultMapper.insert(saveResult);
-                        caseReportMap.put(caseId,saveResult);
+                        caseReportMap.put(caseId, saveResult);
                     });
                 }
             });
         }
         testPlanLog.info("TestPlanReportId[" + testPlanReportId + "] APICASE OVER. API CASE STATUS:" + JSONObject.toJSONString(apiIdResultMap));
         TestPlanReportExecuteCatch.updateApiTestPlanExecuteInfo(testPlanReportId, apiIdResultMap, null, null);
-        TestPlanReportExecuteCatch.updateTestPlanExecuteResultInfo(testPlanReportId,caseReportMap,null,null);
+        TestPlanReportExecuteCatch.updateTestPlanExecuteResultInfo(testPlanReportId, caseReportMap, null, null);
     }
 
     public void deleteByResourceId(String resourceId) {
