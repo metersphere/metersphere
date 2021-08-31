22abd0493f9253e42e9110aaf0bfa927657ec755
fit2-zhao <yong.zhao@fit2cloud.com>
2021-08-31 18:42:15 +0800

    fix (接口自动化): 批量并发执行优化增加节点守护防止持续running状态·

--- backend/src/main/java/io/metersphere/api/jmeter/ReportCounter.java
+++ backend/src/main/java/io/metersphere/api/jmeter/ReportCounter.java
@@ -1,5 +1,6 @@
 package io.metersphere.api.jmeter;
 
+import io.metersphere.api.dto.JvmInfoDTO;
 import lombok.Data;
 
 import java.util.List;
@@ -7,5 +8,6 @@ import java.util.List;
 @Data
 public class ReportCounter {
     private int number;
+    private List<JvmInfoDTO> poolUrls;
     private List<String> reportIds;
 }
