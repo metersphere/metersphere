c4288bf01ba9fddc46ba3aa145ed332a65cc369a
fit2-zhao <yong.zhao@fit2cloud.com>
2021-08-31 18:42:15 +0800

    fix (接口自动化): 批量并发执行优化增加节点守护防止持续running状态·

--- backend/src/main/java/io/metersphere/api/jmeter/FixedTask.java
+++ backend/src/main/java/io/metersphere/api/jmeter/FixedTask.java
@@ -2,6 +2,8 @@ package io.metersphere.api.jmeter;
 
 import io.metersphere.api.service.ApiScenarioReportService;
 import io.metersphere.commons.utils.CommonBeanFactory;
+import io.metersphere.commons.utils.LogUtil;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -10,21 +12,43 @@ import java.util.Map;
 
 @Component
 public class FixedTask {
-    public static Map<String,Long> tasks = new HashMap<>();
+    public static Map<String, Long> tasks = new HashMap<>();
+    public static Map<String, Integer> guardTask = new HashMap<>();
+    private ApiScenarioReportService scenarioReportService;
 
-    @Scheduled(cron = "*/3 * * * * ?")
+    @Scheduled(cron = "*/6 * * * * ?")
     public void execute() {
-        ApiScenarioReportService scenarioReportService = CommonBeanFactory.getBean(ApiScenarioReportService.class);
+        if (scenarioReportService == null) {
+            scenarioReportService = CommonBeanFactory.getBean(ApiScenarioReportService.class);
+        }
         if (MessageCache.cache != null && MessageCache.cache.size() > 0) {
             for (String key : MessageCache.cache.keySet()) {
                 ReportCounter counter = MessageCache.cache.get(key);
                 // 合并
                 if (counter.getNumber() == counter.getReportIds().size()) {
                     scenarioReportService.margeReport(key, counter.getReportIds());
+                    guardTask.remove(key);
                     MessageCache.cache.remove(key);
+                } else {
+                    int number = 0;
+                    if (guardTask.containsKey(key)) {
+                        number = guardTask.get(key);
+                        guardTask.put(key, number++);
+                    } else {
+                        guardTask.put(key, number);
+                    }
+                    if (CollectionUtils.isNotEmpty(counter.getPoolUrls()) && counter.getNumber() > 0 && guardTask.get(key) > 200) {
+                        // 资源池中已经没有执行的请求了
+                        int runningCount = scenarioReportService.get(key, counter);
+                        if (runningCount == 0) {
+                            LogUtil.info("发生未知异常，进行资源合并，请检查资源池是否正常运行");
+                            scenarioReportService.margeReport(key, counter.getReportIds());
+                            guardTask.remove(key);
+                            MessageCache.cache.remove(key);
+                        }
+                    }
                 }
             }
         }
     }
-
 }
