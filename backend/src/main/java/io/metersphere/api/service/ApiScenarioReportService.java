b95aa50b5e18835262673742f92ecfda73b17d15
Captain.B <bin@fit2cloud.com>
2021-09-04 15:53:12 +0800

    fix: 接口自动化执行通知排除操作人

--- backend/src/main/java/io/metersphere/api/service/ApiScenarioReportService.java
+++ backend/src/main/java/io/metersphere/api/service/ApiScenarioReportService.java
@@ -643,6 +643,7 @@ public class ApiScenarioReportService {
                 .failedMailTemplate("api/ScenarioResult")
                 .paramMap(paramMap)
                 .event(event)
+                .excludeSelf(true)
                 .build();
 
         String taskType = NoticeConstants.TaskType.API_AUTOMATION_TASK;
