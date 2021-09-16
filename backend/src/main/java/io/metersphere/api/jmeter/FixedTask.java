305db60aeac74f08bd8b80aed8672bdc6fc763ba
fit2-zhao <yong.zhao@fit2cloud.com>
2021-09-16 15:59:44 +0800

    fix(场景自动化) 增加部分输出便与判断集成进度

--- backend/src/main/java/io/metersphere/api/jmeter/FixedTask.java
+++ backend/src/main/java/io/metersphere/api/jmeter/FixedTask.java
@@ -24,28 +24,33 @@ public class FixedTask {
         if (MessageCache.cache != null && MessageCache.cache.size() > 0) {
             for (String key : MessageCache.cache.keySet()) {
                 ReportCounter counter = MessageCache.cache.get(key);
+                LogUtil.info("集成报告：【" + key + "】总执行场景：【" + counter.getReportIds().size() + "】已经执行完成场景：【" + counter.getNumber() + "】");
                 // 合并
                 if (counter.getNumber() == counter.getReportIds().size()) {
                     scenarioReportService.margeReport(key, counter.getReportIds());
                     guardTask.remove(key);
                     MessageCache.cache.remove(key);
                 } else {
-                    int number = 0;
-                    if (guardTask.containsKey(key)) {
-                        number = guardTask.get(key);
-                        guardTask.put(key, number++);
-                    } else {
-                        guardTask.put(key, number);
-                    }
-                    if (CollectionUtils.isNotEmpty(counter.getPoolUrls()) && counter.getNumber() > 0 && guardTask.get(key) > 200) {
-                        // 资源池中已经没有执行的请求了
-                        int runningCount = scenarioReportService.get(key, counter);
-                        if (runningCount == 0) {
-                            LogUtil.info("发生未知异常，进行资源合并，请检查资源池是否正常运行");
-                            scenarioReportService.margeReport(key, counter.getReportIds());
-                            guardTask.remove(key);
-                            MessageCache.cache.remove(key);
+                    try {
+                        int number = 0;
+                        if (guardTask.containsKey(key)) {
+                            number = guardTask.get(key);
+                            guardTask.put(key, number++);
+                        } else {
+                            guardTask.put(key, number);
                         }
+                        if (CollectionUtils.isNotEmpty(counter.getPoolUrls()) && counter.getNumber() > 0 && guardTask.get(key) > 200) {
+                            // 资源池中已经没有执行的请求了
+                            int runningCount = scenarioReportService.get(key, counter);
+                            if (runningCount == 0) {
+                                LogUtil.info("发生未知异常，进行资源合并，请检查资源池是否正常运行");
+                                scenarioReportService.margeReport(key, counter.getReportIds());
+                                guardTask.remove(key);
+                                MessageCache.cache.remove(key);
+                            }
+                        }
+                    } catch (Exception ex) {
+                        LogUtil.error(ex.getMessage());
                     }
                 }
             }
--- backend/src/main/java/io/metersphere/api/service/ApiAutomationService.java
+++ backend/src/main/java/io/metersphere/api/service/ApiAutomationService.java
@@ -1098,6 +1098,7 @@ public class ApiAutomationService {
                     counter.setPoolUrls(request.getConfig().getTestResources());
                 }
                 counter.setReportIds(reportIds);
+                request.getConfig().setAmassReport(serialReportId);
                 MessageCache.cache.put(serialReportId, counter);
             }
         }
