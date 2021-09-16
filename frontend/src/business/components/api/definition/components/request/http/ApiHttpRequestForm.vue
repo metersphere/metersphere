08003ba4aba5b90bd91ac76b0785acfbdf192cd9
fit2-zhao <yong.zhao@fit2cloud.com>
2021-09-16 18:11:46 +0800

    fix(场景自动化) 增加数组数据处理

--- frontend/src/business/components/api/definition/components/request/http/ApiHttpRequestForm.vue
+++ frontend/src/business/components/api/definition/components/request/http/ApiHttpRequestForm.vue
@@ -69,7 +69,7 @@
             </el-tab-pane>
             <el-tab-pane name="create" v-if="hasPermission('PROJECT_API_DEFINITION:READ+CREATE_API') && hasLicense() && definitionTest">
               <template v-slot:label>
-                <el-button size="mini" type="primary" @click.stop @click="generate">{{$t('commons.generate_test_data')}}</el-button>
+                <el-button size="mini" type="primary" @click.stop @click="generate">{{ $t('commons.generate_test_data') }}</el-button>
               </template>
             </el-tab-pane>
           </el-tabs>
@@ -83,246 +83,257 @@
 </template>
 
 <script>
-  import MsApiKeyValue from "../../ApiKeyValue";
-  import MsApiBody from "../../body/ApiBody";
-  import MsApiAuthConfig from "../../auth/ApiAuthConfig";
-  import ApiRequestMethodSelect from "../../collapse/ApiRequestMethodSelect";
-  import {REQUEST_HEADERS} from "@/common/js/constants";
-  import MsApiVariable from "../../ApiVariable";
-  import MsApiAssertions from "../../assertion/ApiAssertions";
-  import MsApiExtract from "../../extract/ApiExtract";
-  import {Body, KeyValue} from "../../../model/ApiTestModel";
-  import {hasLicense, getUUID} from "@/common/js/utils";
-  import BatchAddParameter from "../../basis/BatchAddParameter";
-  import MsApiAdvancedConfig from "./ApiAdvancedConfig";
-  import MsJsr233Processor from "../../../../automation/scenario/component/Jsr233Processor";
-  import ApiDefinitionStepButton from "../components/ApiDefinitionStepButton";
-  import {hasPermission} from '@/common/js/utils';
-  import Convert from "@/business/components/common/json-schema/convert/convert";
-  export default {
-    name: "MsApiHttpRequestForm",
-    components: {
-      ApiDefinitionStepButton,
-      MsJsr233Processor,
-      MsApiAdvancedConfig,
-      BatchAddParameter,
-      MsApiVariable,
-      ApiRequestMethodSelect,
-      MsApiExtract,
-      MsApiAuthConfig,
-      MsApiBody,
-      MsApiKeyValue,
-      MsApiAssertions
-    },
-    props: {
-      method: String,
-      request: {},
-      response: {},
-      definitionTest:{
-        type:Boolean,
-        default() {
-          return false;
-        }
-      },
-      showScript: {
-        type: Boolean,
-        default: true,
-      },
-      headers: {
-        type: Array,
-        default() {
-          return [];
-        }
-      },
-      referenced: {
-        type: Boolean,
-        default: false,
-      },
-      isShowEnable: Boolean,
-      jsonPathList: Array,
-      isReadOnly: {
-        type: Boolean,
-        default: false
-      },
-      type: String,
-    },
-    data() {
-      let validateURL = (rule, value, callback) => {
-        try {
-          new URL(this.addProtocol(this.request.url));
-        } catch (e) {
-          callback(this.$t('api_test.request.url_invalid'));
-        }
-      };
-      return {
-        activeName: this.request.method === "POST" ? "body" : "parameters",
-        rules: {
-          name: [
-            {max: 300, message: this.$t('commons.input_limit', [1, 300]), trigger: 'blur'}
-          ],
-          url: [
-            {max: 500, required: true, message: this.$t('commons.input_limit', [1, 500]), trigger: 'blur'},
-            {validator: validateURL, trigger: 'blur'}
-          ],
-          path: [
-            {max: 500, message: this.$t('commons.input_limit', [0, 500]), trigger: 'blur'},
-          ]
-        },
-        spanCount: 21,
-        headerSuggestions: REQUEST_HEADERS,
-        isReloadData: false,
-        isBodyShow: true,
-        dialogVisible: false
-      }
-    },
-    created() {
-      if (!this.referenced && this.showScript) {
-        this.spanCount = 21;
-      } else {
-        this.spanCount = 24;
-      }
-      this.init();
-    },
-    methods: {
-      hasPermission,
-      hasLicense,
-      generate() {
-        if (this.request.body && (this.request.body.jsonSchema || this.request.body.raw)) {
-          if (!this.request.body.jsonSchema) {
-            const MsConvert = new Convert();
-            this.request.body.jsonSchema = MsConvert.format(JSON.parse(this.request.body.raw));
-          }
-          this.$post('/api/test/data/generator', this.request.body.jsonSchema, response => {
-            if (response.data) {
-              if (this.request.body.format !== 'JSON-SCHEMA') {
-                this.request.body.raw = response.data;
-              } else {
-                const MsConvert = new Convert();
-                this.request.body.jsonSchema = MsConvert.format(JSON.parse(response.data));
-              }
-              this.reloadBody();
-            }
-          });
-        }
-      },
-      remove(row) {
-        let index = this.request.hashTree.indexOf(row);
-        this.request.hashTree.splice(index, 1);
-        this.reload();
-      },
-      copyRow(row) {
-        let obj = JSON.parse(JSON.stringify(row));
-        obj.id = getUUID();
-        this.request.hashTree.push(obj);
-        this.reload();
-      },
-      reload() {
-        this.isReloadData = true
-        this.$nextTick(() => {
-          this.isReloadData = false
-        })
-      },
-      init() {
-        if (!this.request.body) {
-          this.request.body = new Body();
-        }
-        if (!this.request.body.kvs) {
-          this.request.body.kvs = [];
-        }
-        if (!this.request.rest) {
-          this.request.rest = [];
-        }
-        if (!this.request.arguments) {
-          this.request.arguments = [];
-        }
-      },
-      // 解决修改请求头后 body 显示错位
-      reloadBody() {
-        this.isBodyShow = false;
-        this.$nextTick(() => {
-          this.isBodyShow = true;
-        });
-      },
-      batchAdd() {
-        this.$refs.batchAddParameter.open();
-      },
-      format(array, obj) {
-        if (array) {
-          let isAdd = true;
-          for (let i in array) {
-            let item = array[i];
-            if (item.name === obj.name) {
-              item.value = obj.value;
-              isAdd = false;
-            }
-          }
-          if (isAdd) {
-            this.request.arguments.unshift(obj);
-          }
-        }
-      },
-      batchSave(data) {
-        if (data) {
-          let params = data.split("\n");
-          let keyValues = [];
-          params.forEach(item => {
-            let line = item.split(/：|:/);
-            let required = false;
-            keyValues.unshift(new KeyValue({
-              name: line[0],
-              required: required,
-              value: line[1],
-              description: line[2],
-              type: "text",
-              valid: false,
-              file: false,
-              encode: true,
-              enable: true,
-              contentType: "text/plain"
-            }));
-          })
+import MsApiKeyValue from "../../ApiKeyValue";
+import MsApiBody from "../../body/ApiBody";
+import MsApiAuthConfig from "../../auth/ApiAuthConfig";
+import ApiRequestMethodSelect from "../../collapse/ApiRequestMethodSelect";
+import {REQUEST_HEADERS} from "@/common/js/constants";
+import MsApiVariable from "../../ApiVariable";
+import MsApiAssertions from "../../assertion/ApiAssertions";
+import MsApiExtract from "../../extract/ApiExtract";
+import {Body, KeyValue} from "../../../model/ApiTestModel";
+import {hasLicense, getUUID} from "@/common/js/utils";
+import BatchAddParameter from "../../basis/BatchAddParameter";
+import MsApiAdvancedConfig from "./ApiAdvancedConfig";
+import MsJsr233Processor from "../../../../automation/scenario/component/Jsr233Processor";
+import ApiDefinitionStepButton from "../components/ApiDefinitionStepButton";
+import {hasPermission} from '@/common/js/utils';
+import Convert from "@/business/components/common/json-schema/convert/convert";
 
-          keyValues.forEach(item => {
-            switch (this.activeName) {
-              case "parameters":
-                this.format(this.request.arguments,item);
-                break;
-              case "rest":
-                this.format(this.request.rest,item);
-                break;
-              case "headers":
-                this.format(this.request.headers,item);
-                break;
-              default:
-                break;
-            }
-          })
-        }
+export default {
+  name: "MsApiHttpRequestForm",
+  components: {
+    ApiDefinitionStepButton,
+    MsJsr233Processor,
+    MsApiAdvancedConfig,
+    BatchAddParameter,
+    MsApiVariable,
+    ApiRequestMethodSelect,
+    MsApiExtract,
+    MsApiAuthConfig,
+    MsApiBody,
+    MsApiKeyValue,
+    MsApiAssertions
+  },
+  props: {
+    method: String,
+    request: {},
+    response: {},
+    definitionTest: {
+      type: Boolean,
+      default() {
+        return false;
       }
-
+    },
+    showScript: {
+      type: Boolean,
+      default: true,
+    },
+    headers: {
+      type: Array,
+      default() {
+        return [];
+      }
+    },
+    referenced: {
+      type: Boolean,
+      default: false,
+    },
+    isShowEnable: Boolean,
+    jsonPathList: Array,
+    isReadOnly: {
+      type: Boolean,
+      default: false
+    },
+    type: String,
+  },
+  data() {
+    let validateURL = (rule, value, callback) => {
+      try {
+        new URL(this.addProtocol(this.request.url));
+      } catch (e) {
+        callback(this.$t('api_test.request.url_invalid'));
+      }
+    };
+    return {
+      activeName: this.request.method === "POST" ? "body" : "parameters",
+      rules: {
+        name: [
+          {max: 300, message: this.$t('commons.input_limit', [1, 300]), trigger: 'blur'}
+        ],
+        url: [
+          {max: 500, required: true, message: this.$t('commons.input_limit', [1, 500]), trigger: 'blur'},
+          {validator: validateURL, trigger: 'blur'}
+        ],
+        path: [
+          {max: 500, message: this.$t('commons.input_limit', [0, 500]), trigger: 'blur'},
+        ]
+      },
+      spanCount: 21,
+      headerSuggestions: REQUEST_HEADERS,
+      isReloadData: false,
+      isBodyShow: true,
+      dialogVisible: false
     }
+  },
+  created() {
+    if (!this.referenced && this.showScript) {
+      this.spanCount = 21;
+    } else {
+      this.spanCount = 24;
+    }
+    this.init();
+  },
+  methods: {
+    hasPermission,
+    hasLicense,
+    generate() {
+      if (this.request.body && (this.request.body.jsonSchema || this.request.body.raw)) {
+        if (!this.request.body.jsonSchema) {
+          const MsConvert = new Convert();
+          this.request.body.jsonSchema = MsConvert.format(JSON.parse(this.request.body.raw));
+        }
+        this.$post('/api/test/data/generator', this.request.body.jsonSchema, response => {
+          if (response.data) {
+            if (this.request.body.format !== 'JSON-SCHEMA') {
+              this.request.body.raw = response.data;
+            } else {
+              const MsConvert = new Convert();
+              this.request.body.jsonSchema = MsConvert.format(JSON.parse(response.data));
+            }
+            this.reloadBody();
+          }
+        });
+      }
+    },
+    remove(row) {
+      let index = this.request.hashTree.indexOf(row);
+      this.request.hashTree.splice(index, 1);
+      this.reload();
+    }
+    ,
+    copyRow(row) {
+      let obj = JSON.parse(JSON.stringify(row));
+      obj.id = getUUID();
+      this.request.hashTree.push(obj);
+      this.reload();
+    }
+    ,
+    reload() {
+      this.isReloadData = true
+      this.$nextTick(() => {
+        this.isReloadData = false
+      })
+    }
+    ,
+    init() {
+      if (!this.request.body) {
+        this.request.body = new Body();
+      }
+      if (!this.request.body.kvs) {
+        this.request.body.kvs = [];
+      }
+      if (!this.request.rest) {
+        this.request.rest = [];
+      }
+      if (!this.request.arguments) {
+        this.request.arguments = [];
+      }
+    }
+    ,
+// 解决修改请求头后 body 显示错位
+    reloadBody() {
+      this.isBodyShow = false;
+      this.$nextTick(() => {
+        this.isBodyShow = true;
+      });
+    }
+    ,
+    batchAdd() {
+      this.$refs.batchAddParameter.open();
+    }
+    ,
+    format(array, obj) {
+      if (array) {
+        let isAdd = true;
+        for (let i in array) {
+          let item = array[i];
+          if (item.name === obj.name) {
+            item.value = obj.value;
+            isAdd = false;
+          }
+        }
+        if (isAdd) {
+          this.request.arguments.unshift(obj);
+        }
+      }
+    }
+    ,
+    batchSave(data) {
+      if (data) {
+        let params = data.split("\n");
+        let keyValues = [];
+        params.forEach(item => {
+          let line = item.split(/：|:/);
+          let required = false;
+          keyValues.unshift(new KeyValue({
+            name: line[0],
+            required: required,
+            value: line[1],
+            description: line[2],
+            type: "text",
+            valid: false,
+            file: false,
+            encode: true,
+            enable: true,
+            contentType: "text/plain"
+          }));
+        })
+
+        keyValues.forEach(item => {
+          switch (this.activeName) {
+            case "parameters":
+              this.format(this.request.arguments, item);
+              break;
+            case "rest":
+              this.format(this.request.rest, item);
+              break;
+            case "headers":
+              this.format(this.request.headers, item);
+              break;
+            default:
+              break;
+          }
+        })
+      }
+    }
+
   }
+}
 </script>
 
 <style scoped>
-  .ms-query {
-    background: #783887;
-    color: white;
-    height: 18px;
-    border-radius: 42%;
-  }
-  .ms-header {
-    background: #783887;
-    color: white;
-    height: 18px;
-    border-radius: 42%;
-  }
-  .request-tabs {
-    margin: 20px;
-    min-height: 200px;
-  }
-  .ms-el-link {
-    float: right;
-    margin-right: 45px;
-  }
+.ms-query {
+  background: #783887;
+  color: white;
+  height: 18px;
+  border-radius: 42%;
+}
+
+.ms-header {
+  background: #783887;
+  color: white;
+  height: 18px;
+  border-radius: 42%;
+}
+
+.request-tabs {
+  margin: 20px;
+  min-height: 200px;
+}
+
+.ms-el-link {
+  float: right;
+  margin-right: 45px;
+}
 </style>
