2c1fe258fa4c5e1a61ec57ed9ce395261340f1d8
Captain.B <bin@fit2cloud.com>
2021-09-16 17:15:10 +0800

    feat: 添加自定监控

--- backend/src/main/java/io/metersphere/performance/dto/Monitor.java
+++ backend/src/main/java/io/metersphere/performance/dto/Monitor.java
@@ -3,14 +3,14 @@ package io.metersphere.performance.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class Monitor {
     private String name;
-    private String environmentId;
-    private String environmentName;
     private String ip;
     private Integer port;
     private String description;
-    private String monitorStatus;
+    private List<MonitorItem> monitorConfig;
 }
