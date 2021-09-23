f317892b8294b3782e32faf1d48073daca33b2c7
fit2-zhao <yong.zhao@fit2cloud.com>
2021-09-23 15:20:03 +0800

    fix(接口自动化) 增加单个导出jmx 文件方法

--- backend/src/main/java/io/metersphere/config/KafkaConfig.java
+++ backend/src/main/java/io/metersphere/config/KafkaConfig.java
@@ -7,9 +7,9 @@ import org.springframework.kafka.config.TopicBuilder;
 @Configuration
 public class KafkaConfig {
     // 执行内容监听
-    public final static String EXEC_TOPIC = "ms-automation-exec-topic";
+    public final static String EXEC_TOPIC = "ms-automation-exec-topic99";
     //执行结果回传
-    public static final String TOPICS = "ms-api-exec-topic";
+    public static final String TOPICS = "ms-api-exec-topic99";
 
     @Bean
     public NewTopic apiExecTopic() {
