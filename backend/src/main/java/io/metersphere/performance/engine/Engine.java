4a9698419212b4e05ca9239cbac45ba106ad92a5
Captain.B <bin@fit2cloud.com>
2021-09-23 16:54:43 +0800

    refactor: 性能测试重构

--- backend/src/main/java/io/metersphere/performance/engine/Engine.java
+++ backend/src/main/java/io/metersphere/performance/engine/Engine.java
@@ -1,10 +1,6 @@
 package io.metersphere.performance.engine;
 
 public interface Engine {
-    Long getStartTime();
-
-    String getReportId();
-
     void start();
 
     void stop();
---
+++ backend/src/main/resources/db/migration/V96__v1.13.1__release.sql
@@ -0,0 +1,2 @@
+CREATE INDEX load_test_report_test_resource_pool_id_index
+    ON load_test_report (test_resource_pool_id);
