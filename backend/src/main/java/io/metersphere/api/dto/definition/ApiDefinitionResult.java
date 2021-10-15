df0d443a808f1125a5ccf1776b5cdab938aaf4e7
wxg0103 <727495428@qq.com>
2021-10-15 11:46:27 +0800

    feat_接口定义_首页卡片可配置 --story=1003343 --user=王孝刚 首页展示卡片可配置 https://www.tapd.cn/55049933/s/1057199

--- backend/src/main/java/io/metersphere/api/dto/definition/ApiDefinitionResult.java
+++ backend/src/main/java/io/metersphere/api/dto/definition/ApiDefinitionResult.java
@@ -16,7 +16,15 @@ public class ApiDefinitionResult extends ApiDefinitionWithBLOBs {
 
     private String caseStatus;
 
+    private String scenarioTotal;
+
     private String casePassingRate;
 
     private String deleteUser;
+
+    private String[] ids;
+
+    private String caseType;
+
+    private String scenarioType;
 }
