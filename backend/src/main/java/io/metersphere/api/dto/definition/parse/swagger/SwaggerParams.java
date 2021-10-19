90bfde8136f7b75a7944b2deb1f6beed836256b3
fit2-zhao <yong.zhao@fit2cloud.com>
2021-10-19 17:51:56 +0800

    fix (接口定义):  修复导入导出Swagger问题 --bug=1007229 --user=赵勇 [github#6835]接口导出后导入，响应内容中的描述信息没有了，同时请求参数中的值没有了 https://www.tapd.cn/55049933/s/1056772

--- backend/src/main/java/io/metersphere/api/dto/definition/parse/swagger/SwaggerParams.java
+++ backend/src/main/java/io/metersphere/api/dto/definition/parse/swagger/SwaggerParams.java
@@ -7,6 +7,7 @@ import lombok.*;
 @Setter
 public class SwaggerParams {
     private String name;    //  对应 API 请求参数名
+    private String example;    //  参数值
     private String in;  //  参数类型，可选值为 path,header,query 等
     private String description;
     private boolean required;   //  是否是必填参数
