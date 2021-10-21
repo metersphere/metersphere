fa2942542757ba3ff52e3001c85f563a059b8b42
CaptainB <bin@fit2cloud.com>
2021-10-21 17:19:44 +0800

    refactor: 优化代码import

--- backend/src/main/java/io/metersphere/controller/WorkstationController.java
+++ backend/src/main/java/io/metersphere/controller/WorkstationController.java
@@ -1,18 +1,11 @@
 package io.metersphere.controller;
 
-import com.github.pagehelper.Page;
-import com.github.pagehelper.PageHelper;
-import io.metersphere.api.dto.automation.ApiScenarioRequest;
-import io.metersphere.base.domain.IssueTemplate;
-import io.metersphere.commons.utils.PageUtils;
-import io.metersphere.commons.utils.Pager;
-import io.metersphere.controller.request.BaseQueryRequest;
-import io.metersphere.service.WorkspaceService;
 import io.metersphere.service.WorkstationService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 import java.util.Map;
 
 @RequestMapping("workstation")
