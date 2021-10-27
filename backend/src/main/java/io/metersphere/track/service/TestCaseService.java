359a06ed4036f7435ee6be4df50fdd0967627da3
wxg0103 <727495428@qq.com>
2021-10-27 14:27:40 +0800

    fix功能用例_保存关注人失效  --bug=1007413 --user=王孝刚 [功能用例]-编辑用例-增加关注人保存后修改未生效 https://www.tapd.cn/55049933/s/1059672

--- backend/src/main/java/io/metersphere/track/service/TestCaseService.java
+++ backend/src/main/java/io/metersphere/track/service/TestCaseService.java
@@ -1416,6 +1416,7 @@ public class TestCaseService {
         }
         this.setNode(request);
         editTestCase(request);
+        saveFollows(request.getId(), request.getFollows());
         return testCaseWithBLOBs;
     }
 
