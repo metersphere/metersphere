1db8bf7579e7b6dca3770aac20ad2c3a766f2e3f
CaptainB <bin@fit2cloud.com>
2021-10-27 11:01:16 +0800

    refactor: 性能测试页面缓存配置修改

--- frontend/src/business/components/performance/PerformanceTest.vue
+++ frontend/src/business/components/performance/PerformanceTest.vue
@@ -3,7 +3,7 @@
     <performance-header-menus/>
     <div>
       <transition>
-        <keep-alive>
+        <keep-alive :exclude="excludeCaches">
           <router-view :baseUrl="baseUrl"/>
         </keep-alive>
       </transition>
@@ -13,17 +13,19 @@
 
 <script>
 
-  import PerformanceHeaderMenus from "./head/PerformanceHeaderMenus";
+import PerformanceHeaderMenus from "./head/PerformanceHeaderMenus";
 
-  export default {
-    name: "PerformanceTest",
-    components: {PerformanceHeaderMenus},
-    data() {
-      return {
-        baseUrl: "performance"
-      }
-    }
-  }
+export default {
+  name: "PerformanceTest",
+  components: {PerformanceHeaderMenus},
+  data() {
+    return {
+      baseUrl: "performance",
+      // 不缓存的路由
+      excludeCaches: ['PerformanceReportView'],
+    };
+  },
+};
 
 </script>
 
