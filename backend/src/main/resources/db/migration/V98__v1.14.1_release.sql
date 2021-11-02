9aa6411bd39e7a769c12df8f2b5f37c222a3ff4e
shiziyuan9527 <yuhao.li@fit2cloud.com>
2021-11-02 14:08:59 +0800

    feat(系统设置): 项目管理没有上传jar包的权限，但是上传后不显示
    --bug=1007506 --user=lyh 【系统设置】项目管理没有上传jar包的权限，但是上传后不显示
    https://www.tapd.cn/55049933/s/1062972

--- backend/src/main/resources/db/migration/V98__v1.14.1_release.sql
+++ backend/src/main/resources/db/migration/V98__v1.14.1_release.sql
@@ -1,1 +1,6 @@
 ALTER TABLE mock_expect_config ADD COLUMN expect_num varchar(50);
+
+insert into user_group_permission (id, group_id, permission_id, module_id)
+values (UUID(), 'ws_admin', 'WORKSPACE_PROJECT_MANAGER:READ+UPLOAD_JAR', 'WORKSPACE_PROJECT_MANAGER');
+insert into user_group_permission (id, group_id, permission_id, module_id)
+values (UUID(), 'ws_member', 'WORKSPACE_PROJECT_MANAGER:READ+UPLOAD_JAR', 'WORKSPACE_PROJECT_MANAGER');
