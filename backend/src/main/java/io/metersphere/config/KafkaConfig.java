840452b4e560764a1b995d35eb2b797643946a5d
fit2-zhao <yong.zhao@fit2cloud.com>
2021-11-09 14:17:41 +0800

    fix(接口定义): csv优化

--- backend/src/main/java/io/metersphere/config/KafkaConfig.java
+++ backend/src/main/java/io/metersphere/config/KafkaConfig.java
@@ -7,9 +7,9 @@ import org.springframework.kafka.config.TopicBuilder;
 @Configuration
 public class KafkaConfig {
     // 执行内容监听
-    public final static String EXEC_TOPIC = "ms-automation-exec-topic";
+    public final static String EXEC_TOPIC = "ms-automation-exec-topic123";
     //执行结果回传
-    public static final String TOPICS = "ms-api-exec-topic";
+    public static final String TOPICS = "ms-api-exec-topic123";
 
     @Bean
     public NewTopic apiExecTopic() {
