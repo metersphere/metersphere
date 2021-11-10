bcfc14101d415f0b4e7a95a81250fe38784dde9a
fit2-zhao <yong.zhao@fit2cloud.com>
2021-11-10 10:16:09 +0800

    fix(接口定义): 恢复topic名称

--- backend/src/main/java/io/metersphere/config/KafkaConfig.java
+++ backend/src/main/java/io/metersphere/config/KafkaConfig.java
@@ -7,9 +7,9 @@ import org.springframework.kafka.config.TopicBuilder;
 @Configuration
 public class KafkaConfig {
     // 执行内容监听
-    public final static String EXEC_TOPIC = "ms-automation-exec-topic123";
+    public final static String EXEC_TOPIC = "ms-automation-exec-topic";
     //执行结果回传
-    public static final String TOPICS = "ms-api-exec-topic123";
+    public static final String TOPICS = "ms-api-exec-topic";
 
     @Bean
     public NewTopic apiExecTopic() {
