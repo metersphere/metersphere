f2164f8944646594a030d5b218a9b92ef81baad2
fit2-zhao <yong.zhao@fit2cloud.com>
2021-11-15 10:57:55 +0800

    fix(接口定义): 增加encode设置

--- frontend/src/business/components/api/definition/components/ApiVariableSetting.vue
+++ frontend/src/business/components/api/definition/components/ApiVariableSetting.vue
@@ -1,21 +1,33 @@
 <template>
   <ms-edit-dialog
     :visible.sync="visible"
-    width="30%"
+    width="700px"
     :title="$t('run_mode.other_config')"
     :with-footer="false"
     :append-to-body="appendToBody"
     :close-on-click-modal="true">
     <el-form>
       <el-row>
-        <el-col :span="12">
-            <el-form-item :label="$t('schema.minLength')">
+        <el-col :span="8">
+          <el-form-item :label="$t('schema.minLength')">
             <el-input-number :min="0" v-model="data.min" :placeholder="$t('schema.minLength')" size="small"/>
           </el-form-item>
         </el-col>
-        <el-col :span="12">
+        <el-col :span="8">
           <el-form-item :label="$t('schema.maxLength')">
             <el-input-number :min="0" v-model="data.max" :placeholder="$t('schema.maxLength')" size="small"/>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="编码">
+            <el-select v-model="data.urlEncode" size="small" clearable style="width: 50%;">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
           </el-form-item>
         </el-col>
       </el-row>
@@ -25,32 +37,40 @@
 
 <script>
 
-  import MsEditDialog from "@/business/components/common/components/MsEditDialog";
-  export default {
-    name: "ApiVariableSetting",
-    components: {MsEditDialog},
-    data() {
-      return {
-        visible: false,
-        data: {}
-      }
-    },
-    props:{
-      appendToBody: {
-        type: Boolean,
-        default() {
-          return false;
-        }
-      },
-    },
-    methods: {
-      open(item) {
-        this.visible = true;
-        this.data = item;
-      }
-    }
+import MsEditDialog from "@/business/components/common/components/MsEditDialog";
 
+export default {
+  name: "ApiVariableSetting",
+  components: {MsEditDialog},
+  data() {
+    return {
+      visible: false,
+      data: {},
+      options: [{
+        value: true,
+        label: '是'
+      }, {
+        value: false,
+        label: '否'
+      }],
+    }
+  },
+  props: {
+    appendToBody: {
+      type: Boolean,
+      default() {
+        return false;
+      }
+    },
+  },
+  methods: {
+    open(item) {
+      this.visible = true;
+      this.data = item;
+    }
   }
+
+}
 </script>
 
 <style scoped>
