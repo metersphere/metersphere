f9de1d0ed0190627eb343f22ace4423019cddf5f
CaptainB <bin@fit2cloud.com>
2021-11-15 15:30:10 +0800

    fix: 系统用户组和项目用户组页面权限控制

--- backend/src/main/resources/db/migration/V99__v1.15_release.sql
+++ backend/src/main/resources/db/migration/V99__v1.15_release.sql
@@ -1,19 +1,23 @@
-CREATE TABLE `issue_follow` (
-                                `issue_id` varchar(50) DEFAULT NULL,
-                                `follow_id` varchar(50) DEFAULT NULL,
-                                UNIQUE KEY `issue_follow_pk` (`issue_id`,`follow_id`),
-                                KEY `issue_follow_follow_id_index` (`follow_id`),
-                                KEY `issue_follow_issue_id_index` (`issue_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `issue_follow`
+(
+    `issue_id`  varchar(50) DEFAULT NULL,
+    `follow_id` varchar(50) DEFAULT NULL,
+    UNIQUE KEY `issue_follow_pk` (`issue_id`, `follow_id`),
+    KEY `issue_follow_follow_id_index` (`follow_id`),
+    KEY `issue_follow_issue_id_index` (`issue_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
 -- group
-insert into user_group_permission (id, group_id, permission_id, module_id)
-values (UUID(), 'project_group', 'PROJECT_GROUP:READ', 'PROJECT_GROUP');
-insert into user_group_permission (id, group_id, permission_id, module_id)
-values (UUID(), 'project_group', 'PROJECT_GROUP:READ+CREATE', 'PROJECT_GROUP');
-insert into user_group_permission (id, group_id, permission_id, module_id)
-values (UUID(), 'project_group', 'PROJECT_GROUP:READ+EDIT', 'PROJECT_GROUP');
-insert into user_group_permission (id, group_id, permission_id, module_id)
-values (UUID(), 'project_group', 'PROJECT_GROUP:READ+DELETE', 'PROJECT_GROUP');
+INSERT INTO user_group_permission (id, group_id, permission_id, module_id)
+VALUES (UUID(), 'project_group', 'PROJECT_GROUP:READ', 'PROJECT_GROUP');
+INSERT INTO user_group_permission (id, group_id, permission_id, module_id)
+VALUES (UUID(), 'project_group', 'PROJECT_GROUP:READ+CREATE', 'PROJECT_GROUP');
+INSERT INTO user_group_permission (id, group_id, permission_id, module_id)
+VALUES (UUID(), 'project_group', 'PROJECT_GROUP:READ+EDIT', 'PROJECT_GROUP');
+INSERT INTO user_group_permission (id, group_id, permission_id, module_id)
+VALUES (UUID(), 'project_group', 'PROJECT_GROUP:READ+DELETE', 'PROJECT_GROUP');
+INSERT INTO user_group_permission (id, group_id, permission_id, module_id)
+VALUES (UUID(), 'project_group', 'PROJECT_GROUP:READ+SETTING_PERMISSION', 'PROJECT_GROUP');
 
 
--- frontend/src/business/components/project/menu/UserGroup.vue
+++ frontend/src/business/components/project/menu/UserGroup.vue
@@ -45,13 +45,13 @@
             <el-table-column :label="$t('commons.operating')" min-width="120">
               <template v-slot:default="scope">
                 <div>
-                  <ms-table-operator :edit-permission="['SYSTEM_GROUP:READ+EDIT', 'ORGANIZATION_GROUP:READ+EDIT']"
-                                     :delete-permission="['SYSTEM_GROUP:READ+DELETE', 'ORGANIZATION_GROUP:READ+DELETE']"
+                  <ms-table-operator :edit-permission="['PROJECT_GROUP:READ+EDIT']"
+                                     :delete-permission="['PROJECT_GROUP:READ+DELETE']"
                                      @editClick="edit(scope.row)" @deleteClick="del(scope.row)">
                     <template v-slot:middle>
                       <!--                <ms-table-operator-button tip="复制" icon="el-icon-document-copy" @exec="copy(scope.row)"/>-->
                       <ms-table-operator-button
-                        v-permission="['SYSTEM_GROUP:READ+SETTING_PERMISSION', 'ORGANIZATION_GROUP:READ+SETTING_PERMISSION']"
+                        v-permission="['PROJECT_GROUP:READ+SETTING_PERMISSION']"
                         :tip="$t('group.set_permission')" icon="el-icon-s-tools" @exec="setPermission(scope.row)"/>
                     </template>
                   </ms-table-operator>
