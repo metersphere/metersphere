ed54c476ba4ee077f8051285affdc266ca21e3a7
song-tianyang <tianyang.song@fit2cloud.com>
2021-11-16 11:29:31 +0800

    fix(性能测试、测试计划): #1007796 #1007758 一键转性能测试会出现csv文件重复的问题、执行测试计划场景没有按照顺序执行的问题
    --bug=1007758 --user=宋天阳 【性能测试】性能测试引用带csv文件的场景，csv文件会显示好多次
    https://www.tapd.cn/55049933/s/1068040、--bug=1007796 --user=宋天阳
    【Github#7579】测试计划中拖拽编排场景用例，只能调整显示顺序，但无法作用于执行顺序
    https://www.tapd.cn/55049933/s/1068038

--- backend/src/main/java/io/metersphere/api/service/APITestService.java
+++ backend/src/main/java/io/metersphere/api/service/APITestService.java
@@ -521,6 +521,9 @@ public class APITestService {
                     attachmentFilePathList.addAll(this.parseAttachmentFileInfo(element));
                 }
 
+                for (String filePath :this.parseAttachmentFileInfo(innerHashTreeElement)) {
+
+                }
                 //如果存在证书文件，也要匹配出来
                 attachmentFilePathList.addAll(this.parseAttachmentFileInfo(innerHashTreeElement));
             }
@@ -535,7 +538,10 @@ public class APITestService {
 
         //处理附件
         Map<String, String> attachmentFiles = new HashMap<>();
-
+        //去重处理
+        if(!CollectionUtils.isEmpty(attachmentFilePathList)){
+            attachmentFilePathList = attachmentFilePathList.stream().distinct().collect(Collectors.toList());
+        }
         List<FileMetadata> fileMetadataList = new ArrayList<>();
         for (String filePath: attachmentFilePathList) {
             File file  = new File(filePath);
@@ -555,8 +561,8 @@ public class APITestService {
         return returnDTO;
     }
 
-    private List<String> parseAttachmentFileInfo(Element parentHashTreeElement) {
-        List<String> attachmentFilePathList = new ArrayList<>();
+private List<String> parseAttachmentFileInfo(Element parentHashTreeElement) {
+         List<String> attachmentFilePathList = new ArrayList<>();
         List<Element> parentElementList = parentHashTreeElement.elements();
         for (Element parentElement: parentElementList) {
             String qname = parentElement.getQName().getName();
