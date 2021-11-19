d14ce3b3c68d960c7a7b82c7fe583b32a8946b34
wxg0103 <727495428@qq.com>
2021-11-19 13:55:49 +0800

    fix(接口测试): 接口名称过长时长度小于100时执行接口test一直转圈
    --bug=1008069 --user=王孝刚 接口名称过长时（长度小于100），执行接口test一直转圈
    https://www.tapd.cn/55049933/s/1070409

--- backend/src/main/resources/db/migration/V99__v1.15_release.sql
+++ backend/src/main/resources/db/migration/V99__v1.15_release.sql
@@ -24,5 +24,10 @@ alter table test_plan_load_case
     add advanced_configuration TEXT null;
 
 
-INSERT INTO custom_field (id,name,scene,`type`,remark,`options`,`system`,`global`,workspace_id,create_time,update_time)
-VALUES ('e392af07-fdfe-4475-a459-87d59f0b1625','测试阶段','PLAN','select','','[{"text":"test_track.plan.smoke_test","value":"smoke","system": true},{"text":"test_track.plan.system_test","value":"system","system": true},{"text":"test_track.plan.regression_test","value":"regression","system": true}]',1,1,'global',unix_timestamp() * 1000,unix_timestamp() * 1000);
+INSERT INTO custom_field (id, name, scene, `type`, remark, `options`, `system`, `global`, workspace_id, create_time,
+                          update_time)
+VALUES ('e392af07-fdfe-4475-a459-87d59f0b1625', '测试阶段', 'PLAN', 'select', '',
+        '[{"text":"test_track.plan.smoke_test","value":"smoke","system": true},{"text":"test_track.plan.system_test","value":"system","system": true},{"text":"test_track.plan.regression_test","value":"regression","system": true}]',
+        1, 1, 'global', unix_timestamp() * 1000, unix_timestamp() * 1000);
+
+ALTER TABLE api_definition_exec_result MODIFY COLUMN name VARCHAR (100);
