b29a2fe69aa64d5f94d59d6ed2a02c5a8e2b018b
CaptainB <bin@fit2cloud.com>
2021-11-19 22:44:38 +0800

    fix(测试用例): 修复测试用例上传附件不能传txt的问题
    --bug=1008128 --user=刘瑞斌 【测试跟踪】测试用例附件不能添加txt文件 https://www.tapd.cn/55049933/s/1070860

--- frontend/src/business/components/track/case/components/TestCaseEditOtherInfo.vue
+++ frontend/src/business/components/track/case/components/TestCaseEditOtherInfo.vue
@@ -52,7 +52,7 @@
       <el-row>
         <el-col :span="20" :offset="1">
           <el-upload
-            accept=".jpg,.jpeg,.png,.xlsx,.doc,.pdf,.docx"
+            accept=".jpg,.jpeg,.png,.xlsx,.doc,.pdf,.docx,.txt"
             action=""
             :show-file-list="false"
             :before-upload="beforeUpload"
