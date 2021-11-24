8a0b3da88ae39cd3d21ad9cfe20f13dbbebc4643
CaptainB <bin@fit2cloud.com>
2021-11-24 09:57:12 +0800

    fix: 修复测试计划执行性能测试时 trigger_mode 对应不上的问题

--- frontend/src/business/components/common/tableItem/ReportTriggerModeItem.vue
+++ frontend/src/business/components/common/tableItem/ReportTriggerModeItem.vue
@@ -2,7 +2,8 @@
   <span>
     <span v-if="triggerMode === 'MANUAL'">{{$t('commons.trigger_mode.manual')}}</span>
     <span v-if="triggerMode === 'SCHEDULE'">{{$t('commons.trigger_mode.schedule')}}</span>
-    <span v-if="triggerMode === 'TEST_PLAN_SCHEDULE'">{{$t('commons.trigger_mode.schedule')}}</span>
+    <span v-if="triggerMode === 'TEST_PLAN_SCHEDULE'">{{$t('commons.trigger_mode.test_plan_schedule')}}</span>
+    <span v-if="triggerMode === 'TEST_PLAN_API'">{{$t('commons.trigger_mode.test_plan_api')}}</span>
     <span v-if="triggerMode === 'API'">{{$t('commons.trigger_mode.api')}}</span>
     <span v-if="triggerMode === 'BATCH'">{{$t('api_test.automation.batch_execute')}}</span>
   </span>
