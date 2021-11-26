6eca1344bd3c61496f5f6c08a44940300c705364
shiziyuan9527 <yuhao.li@fit2cloud.com>
2021-11-26 17:48:38 +0800

    fix: flyway问题

--- backend/src/main/resources/db/migration/V100__v1.15.2_release.sql
+++ backend/src/main/resources/db/migration/V100__v1.15.2_release.sql
@@ -1,21 +1,12 @@
--- project admin
+-- ws admin
 insert into user_group_permission (id, group_id, permission_id, module_id)
-values (UUID(), 'project_admin', 'WORKSPACE_PROJECT_ENVIRONMENT:READ+CREATE_GROUP', 'WORKSPACE_PROJECT_ENVIRONMENT');
+values (UUID(), 'ws_admin', 'WORKSPACE_PROJECT_ENVIRONMENT:READ+CREATE_GROUP', 'WORKSPACE_PROJECT_ENVIRONMENT');
 insert into user_group_permission (id, group_id, permission_id, module_id)
-values (UUID(), 'project_admin', 'WORKSPACE_PROJECT_ENVIRONMENT:READ+EDIT_GROUP', 'WORKSPACE_PROJECT_ENVIRONMENT');
+values (UUID(), 'ws_admin', 'WORKSPACE_PROJECT_ENVIRONMENT:READ+EDIT_GROUP', 'WORKSPACE_PROJECT_ENVIRONMENT');
 insert into user_group_permission (id, group_id, permission_id, module_id)
-values (UUID(), 'project_admin', 'WORKSPACE_PROJECT_ENVIRONMENT:READ+COPY_GROUP', 'WORKSPACE_PROJECT_ENVIRONMENT');
+values (UUID(), 'ws_admin', 'WORKSPACE_PROJECT_ENVIRONMENT:READ+COPY_GROUP', 'WORKSPACE_PROJECT_ENVIRONMENT');
 insert into user_group_permission (id, group_id, permission_id, module_id)
-values (UUID(), 'project_admin', 'WORKSPACE_PROJECT_ENVIRONMENT:READ+DELETE_GROUP', 'WORKSPACE_PROJECT_ENVIRONMENT');
--- project member
-insert into user_group_permission (id, group_id, permission_id, module_id)
-values (UUID(), 'project_member', 'WORKSPACE_PROJECT_ENVIRONMENT:READ+CREATE_GROUP', 'WORKSPACE_PROJECT_ENVIRONMENT');
-insert into user_group_permission (id, group_id, permission_id, module_id)
-values (UUID(), 'project_member', 'WORKSPACE_PROJECT_ENVIRONMENT:READ+EDIT_GROUP', 'WORKSPACE_PROJECT_ENVIRONMENT');
-insert into user_group_permission (id, group_id, permission_id, module_id)
-values (UUID(), 'project_member', 'WORKSPACE_PROJECT_ENVIRONMENT:READ+COPY_GROUP', 'WORKSPACE_PROJECT_ENVIRONMENT');
-insert into user_group_permission (id, group_id, permission_id, module_id)
-values (UUID(), 'project_member', 'WORKSPACE_PROJECT_ENVIRONMENT:READ+DELETE_GROUP', 'WORKSPACE_PROJECT_ENVIRONMENT');
+values (UUID(), 'ws_admin', 'WORKSPACE_PROJECT_ENVIRONMENT:READ+DELETE_GROUP', 'WORKSPACE_PROJECT_ENVIRONMENT');
 -- delete old permission
 delete
 from user_group_permission
