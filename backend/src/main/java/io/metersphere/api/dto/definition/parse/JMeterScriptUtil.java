454d84037c9c68fe559e8443f30e8e1f14e4fece
song-tianyang <tianyang.song@fit2cloud.com>
2021-11-26 16:44:35 +0800

    fix(SQL用例执行): 修复SQL用例执行时自动添加script空脚本的问题
    --bug=1008370 --user=宋天阳 【接口测试】创建sql用例，有一个步骤，但是步骤里啥都没有，执行的时候还会报错
    https://www.tapd.cn/55049933/s/1075620

---
+++ backend/src/main/java/io/metersphere/api/dto/definition/parse/JMeterScriptUtil.java
@@ -0,0 +1,92 @@
+package io.metersphere.api.dto.definition.parse;
+
+import io.metersphere.api.dto.definition.request.ParameterConfig;
+import io.metersphere.api.dto.definition.request.processors.post.MsJSR223PostProcessor;
+import io.metersphere.api.dto.definition.request.processors.pre.MsJSR223PreProcessor;
+import io.metersphere.api.dto.scenario.environment.EnvironmentConfig;
+import io.metersphere.api.dto.scenario.environment.GlobalScriptConfig;
+import io.metersphere.plugin.core.MsTestElement;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.jorphan.collections.HashTree;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+
+public class JMeterScriptUtil {
+
+    /**
+     * 判断脚本是否被过滤
+     *
+     * @param filterProtocals 要过滤掉的请求类型
+     * @param protocal        当前请求类型
+     * @return
+     */
+    public static boolean isScriptFilter(List<String> filterProtocals, String protocal) {
+        if (!CollectionUtils.isEmpty(filterProtocals)) {
+            return filterProtocals.contains(protocal);
+        } else {
+            return false;
+        }
+    }
+
+    public static MsJSR223PreProcessor getPreScript(EnvironmentConfig envConfig) {
+        if (envConfig != null && envConfig.getPreProcessor() != null && StringUtils.isNotEmpty(envConfig.getPreProcessor().getScript())) {
+            return envConfig.getPreProcessor();
+        } else {
+            return null;
+        }
+    }
+
+    public static MsJSR223PostProcessor getPostScript(EnvironmentConfig envConfig) {
+        if (envConfig != null && envConfig.getPostProcessor() != null && StringUtils.isNotEmpty(envConfig.getPostProcessor().getScript())) {
+            return envConfig.getPostProcessor();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Samper中放置脚本
+     *
+     * @param envConfig            环境配置信息
+     * @param samplerHashTree      sampler的hashtree
+     * @param isAfterPrivateScript 是否将脚本放置在sampler的私有脚本之后
+     * @param protocal             请求类型
+     * @param environmentId        环境ID
+     * @param config               参数配置
+     */
+    public static void setScript(EnvironmentConfig envConfig, HashTree samplerHashTree, String protocal, String environmentId, ParameterConfig config, boolean isAfterPrivateScript) {
+        GlobalScriptConfig globalScriptConfig = envConfig != null ? envConfig.getGlobalScriptConfig() : null;
+
+        boolean isPreScriptExecAfterPrivateScript = globalScriptConfig == null ? false : globalScriptConfig.isPreScriptExecAfterPrivateScript();
+        boolean isPostScriptExecAfterPrivateScript = globalScriptConfig == null ? false : globalScriptConfig.isPostScriptExecAfterPrivateScript();
+        List<String> preFilterProtocal = globalScriptConfig == null ? null : globalScriptConfig.getFilterRequestPreScript();
+        List<String> postFilterProtocal = globalScriptConfig == null ? null : globalScriptConfig.getFilterRequestPostScript();
+        MsJSR223PreProcessor preProcessor = JMeterScriptUtil.getPreScript(envConfig);
+        MsJSR223PostProcessor postProcessor = JMeterScriptUtil.getPostScript(envConfig);
+        boolean globalPreScriptIsFilter = JMeterScriptUtil.isScriptFilter(preFilterProtocal, protocal);
+        boolean globalPostScriptIsFilter = JMeterScriptUtil.isScriptFilter(postFilterProtocal, protocal);
+        if (isAfterPrivateScript) {
+            if (isPreScriptExecAfterPrivateScript && !globalPreScriptIsFilter) {
+                addItemHashTree(preProcessor, samplerHashTree, config, environmentId);
+            }
+            if (isPostScriptExecAfterPrivateScript && !globalPostScriptIsFilter) {
+                addItemHashTree(postProcessor, samplerHashTree, config, environmentId);
+            }
+        } else {
+            if (!isPreScriptExecAfterPrivateScript && !globalPreScriptIsFilter) {
+                addItemHashTree(preProcessor, samplerHashTree, config, environmentId);
+            }
+            if (!isPostScriptExecAfterPrivateScript && !globalPostScriptIsFilter) {
+                addItemHashTree(postProcessor, samplerHashTree, config, environmentId);
+            }
+        }
+    }
+
+    private static void addItemHashTree(MsTestElement element, HashTree samplerHashTree, ParameterConfig config, String enviromentId) {
+        if (element != null && element.getEnvironmentId() == null) {
+            element.setEnvironmentId(enviromentId);
+            element.toHashTree(samplerHashTree, element.getHashTree(), config);
+        }
+    }
+}
