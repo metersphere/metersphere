b4f2854b0e1aa198e0758586025f1b7f6a741f25
fit2-zhao <yong.zhao@fit2cloud.com>
2021-12-16 15:26:36 +0800

    refactor(接口测试): 定时检查测试计划报告状态

--- backend/src/main/java/io/metersphere/api/jmeter/MessageCache.java
+++ backend/src/main/java/io/metersphere/api/jmeter/MessageCache.java
@@ -4,16 +4,20 @@ import io.metersphere.base.domain.ApiDefinitionExecResult;
 
 import javax.websocket.Session;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class MessageCache {
-    public static Map<String, ReportCounter> concurrencyCounter = new HashMap<>();
+    public final static Map<String, ReportCounter> concurrencyCounter = new HashMap<>();
 
-    public static ConcurrentHashMap<String, Session> reportCache = new ConcurrentHashMap<>();
-
+    public final static ConcurrentHashMap<String, Session> reportCache = new ConcurrentHashMap<>();
     // 用例并发锁
-    public static ConcurrentHashMap<String, ApiDefinitionExecResult> caseExecResourceLock = new ConcurrentHashMap<>();
+    public final static ConcurrentHashMap<String, ApiDefinitionExecResult> caseExecResourceLock = new ConcurrentHashMap<>();
 
-    public static Map<String, Long> jmeterLogTask = new HashMap<>();
+    public final static Map<String, Long> jmeterLogTask = new HashMap<>();
+
+    // 定时任务报告
+    public final static List<String> jobReportCache = new LinkedList<>();
 }
