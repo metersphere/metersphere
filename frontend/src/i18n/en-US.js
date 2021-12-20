29b733ce7ba3785fb7a5b3a4cc95c7cc1a65cf29
CaptainB <bin@fit2cloud.com>
2021-12-20 15:45:01 +0800

    fix: 修复企业微信名称翻译的问题
    --bug=1008990 --user=刘瑞斌 【消息通知】变量显示问题汇总 https://www.tapd.cn/55049933/s/1083464

--- frontend/src/i18n/en-US.js
+++ frontend/src/i18n/en-US.js
@@ -574,7 +574,7 @@ export default {
       mail: 'Email',
       in_site: 'InSite',
       nail_robot: 'Nail robot',
-      enterprise_wechat_robot: 'Enterprise wechat robot',
+      enterprise_wechat_robot: 'WeCom robot',
       lark: "Lark Robot",
       notes: '1. Nail and create a custom robot in the enterprise group, and then copy the webhook address on our platform;<br/>' +
         '2. Robots are selected as swarm robots, and "custom keyword" is selected for security verification: "task notification";<br/>' +
