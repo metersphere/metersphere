a910dc2ced767dc5ace28280380de2b20b8e1c15
guoyuqi <xiaomeinvG@126.com>
2021-12-23 15:49:01 +0800

    fix(接口自动化): 断言将忽略移到最外层
    --bug=1004217 --user=郭雨琦 断言将忽略移到最外层

--- frontend/src/business/components/api/automation/scenario/common/ApiBaseComponent.vue
+++ frontend/src/business/components/api/automation/scenario/common/ApiBaseComponent.vue
@@ -33,6 +33,9 @@
       <div class="header-right" @click.stop>
         <slot name="message"></slot>
         <slot name="debugStepCode"></slot>
+        <el-checkbox v-model="data.assumeSuccess" class="assertion-checkbox" >
+          {{ $t('api_test.request.assertions.ignore_status') }}
+        </el-checkbox>
         <el-tooltip :content="$t('test_resource_pool.enable_disable')" placement="top" v-if="showBtn">
           <el-switch v-model="data.enable" class="enable-switch" size="mini" :disabled="data.disabled && !data.root" style="width: 30px"/>
         </el-tooltip>
@@ -259,5 +262,8 @@ fieldset {
   cursor: pointer;
   border-color: #783887;
 }
-
+.assertion-checkbox {
+  text-align: center;
+  width: 120px;
+}
 </style>
