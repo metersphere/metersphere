67a143588cfe03357b5823c01767de391d89c7e7
fit2-zhao <yong.zhao@fit2cloud.com>
2021-12-23 12:20:07 +0800

    fix(接口测试): 修复场景中调试状态未及时更新问题

--- backend/src/main/java/io/metersphere/api/service/ApiDefinitionExecResultService.java
+++ backend/src/main/java/io/metersphere/api/service/ApiDefinitionExecResultService.java
@@ -317,7 +317,10 @@ public class ApiDefinitionExecResultService {
 
     private ApiDefinitionExecResult save(RequestResult item, String reportId, String console, int expectProcessResultCount, String type, String testId, boolean isFirst) {
         if (!StringUtils.startsWithAny(item.getName(), "PRE_PROCESSOR_ENV_", "POST_PROCESSOR_ENV_")) {
-            ApiDefinitionExecResult saveResult = new ApiDefinitionExecResult();
+            ApiDefinitionExecResult saveResult = apiDefinitionExecResultMapper.selectByPrimaryKey(reportId);
+            if (saveResult == null) {
+                saveResult = new ApiDefinitionExecResult();
+            }
             item.getResponseResult().setConsole(console);
             saveResult.setId(reportId);
             saveResult.setActuator("LOCAL");
