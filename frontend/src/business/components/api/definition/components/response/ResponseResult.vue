196ff3c88e0a2d27ad30627cc5e2eb4c46698699
fit2-zhao <yong.zhao@fit2cloud.com>
2021-12-27 10:10:42 +0800

    fix(接口测试): 修复执行测试用例显示测试中，无法停止问题
    --bug=1009232 --user=赵勇 【接口测试】执行测试用例还是一直执行中【+github#8807】 https://www.tapd.cn/55049933/s/1087011

--- frontend/src/business/components/api/definition/components/response/ResponseResult.vue
+++ frontend/src/business/components/api/definition/components/response/ResponseResult.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="text-container">
-
+  <div class="text-container" v-if="responseResult">
     <el-tabs v-model="activeName" v-show="isActive">
       <el-tab-pane :label="$t('api_test.definition.request.response_body')" name="body" class="pane">
         <ms-sql-result-table v-if="isSqlType" :body="responseResult.body"/>
@@ -38,149 +37,147 @@
 </template>
 
 <script>
-  import MsAssertionResults from "./AssertionResults";
-  import MsCodeEdit from "../MsCodeEdit";
-  import MsDropdown from "../../../../common/components/MsDropdown";
-  import {BODY_FORMAT} from "../../model/ApiTestModel";
-  import MsSqlResultTable from "./SqlResultTable";
+import MsAssertionResults from "./AssertionResults";
+import MsCodeEdit from "../MsCodeEdit";
+import MsDropdown from "../../../../common/components/MsDropdown";
+import {BODY_FORMAT} from "../../model/ApiTestModel";
+import MsSqlResultTable from "./SqlResultTable";
 
-  export default {
-    name: "MsResponseResult",
+export default {
+  name: "MsResponseResult",
 
-    components: {
-      MsDropdown,
-      MsCodeEdit,
-      MsAssertionResults,
-      MsSqlResultTable
-    },
+  components: {
+    MsDropdown,
+    MsCodeEdit,
+    MsAssertionResults,
+    MsSqlResultTable
+  },
 
-    props: {
-      response: Object,
-      currentProtocol: String,
-    },
+  props: {
+    response: Object,
+    currentProtocol: String,
+  },
 
-    data() {
-      return {
-        isActive: true,
-        activeName: "body",
-        modes: ['text', 'json', 'xml', 'html'],
-        sqlModes: ['text', 'table'],
-        mode: BODY_FORMAT.TEXT,
-        isMsCodeEditShow: true,
-        reqMessages: "",
-      }
-    },
-    watch: {
-      response() {
-        this.setBodyType();
-        this.setReqMessage();
-      }
-    },
-    methods: {
-      modeChange(mode) {
-        this.mode = mode;
-      },
-      sqlModeChange(mode) {
-        this.mode = mode;
-      },
-      setBodyType() {
-        if (!this.response.responseResult || !this.response.responseResult.headers) {
-          return;
-        }
-        if (this.response.responseResult.headers.indexOf("Content-Type: application/json") > 0) {
-          this.mode = BODY_FORMAT.JSON;
-          this.$nextTick(() => {
-            if (this.$refs.modeDropdown) {
-              this.$refs.modeDropdown.handleCommand(BODY_FORMAT.JSON);
-              this.msCodeReload();
-            }
-          })
-        }
-      },
-      msCodeReload() {
-        this.isMsCodeEditShow = false;
-        this.$nextTick(() => {
-          this.isMsCodeEditShow = true;
-        });
-      },
-      setReqMessage() {
-        if (this.response) {
-          if (!this.response.url) {
-            this.response.url = "";
-          }
-          if (!this.response.headers) {
-            this.response.headers = "";
-          }
-          if (!this.response.cookies) {
-            this.response.cookies = "";
-          }
-          if (!this.response.body) {
-            this.response.body = "";
-          }
-          if (!this.response.responseResult) {
-            this.response.responseResult = {};
-          }
-          if (!this.response.responseResult.vars) {
-            this.response.responseResult.vars = "";
-          }
-          this.reqMessages = this.$t('api_test.request.address') + ":\n" + this.response.url + "\n" +
-            this.$t('api_test.scenario.headers') + ":\n" + this.response.headers + "\n" + "Cookies :\n" +
-            this.response.cookies + "\n" + "Body:" + "\n" + this.response.body;
-        }
-      },
-    },
-    mounted() {
+  data() {
+    return {
+      isActive: true,
+      activeName: "body",
+      modes: ['text', 'json', 'xml', 'html'],
+      sqlModes: ['text', 'table'],
+      mode: BODY_FORMAT.TEXT,
+      isMsCodeEditShow: true,
+      reqMessages: "",
+    }
+  },
+  watch: {
+    response() {
       this.setBodyType();
       this.setReqMessage();
+    }
+  },
+  methods: {
+    modeChange(mode) {
+      this.mode = mode;
     },
-    computed: {
-      isSqlType() {
-        return (this.currentProtocol === "SQL" && this.response.responseResult.responseCode === '200' && this.mode === 'table');
-      },
-      responseResult() {
-        return this.response && this.response.responseResult ? this.response.responseResult : {};
+    sqlModeChange(mode) {
+      this.mode = mode;
+    },
+    setBodyType() {
+      if (this.response && this.response.responseResult && this.response.responseResult.headers
+        && this.response.responseResult.headers.indexOf("Content-Type: application/json") > 0) {
+        this.mode = BODY_FORMAT.JSON;
+        this.$nextTick(() => {
+          if (this.$refs.modeDropdown) {
+            this.$refs.modeDropdown.handleCommand(BODY_FORMAT.JSON);
+            this.msCodeReload();
+          }
+        })
       }
+    },
+    msCodeReload() {
+      this.isMsCodeEditShow = false;
+      this.$nextTick(() => {
+        this.isMsCodeEditShow = true;
+      });
+    },
+    setReqMessage() {
+      if (this.response) {
+        if (!this.response.url) {
+          this.response.url = "";
+        }
+        if (!this.response.headers) {
+          this.response.headers = "";
+        }
+        if (!this.response.cookies) {
+          this.response.cookies = "";
+        }
+        if (!this.response.body) {
+          this.response.body = "";
+        }
+        if (!this.response.responseResult) {
+          this.response.responseResult = {};
+        }
+        if (!this.response.responseResult.vars) {
+          this.response.responseResult.vars = "";
+        }
+        this.reqMessages = this.$t('api_test.request.address') + ":\n" + this.response.url + "\n" +
+          this.$t('api_test.scenario.headers') + ":\n" + this.response.headers + "\n" + "Cookies :\n" +
+          this.response.cookies + "\n" + "Body:" + "\n" + this.response.body;
+      }
+    },
+  },
+  mounted() {
+    this.setBodyType();
+    this.setReqMessage();
+  },
+  computed: {
+    isSqlType() {
+      return (this.currentProtocol === "SQL" && this.response.responseResult.responseCode === '200' && this.mode === 'table');
+    },
+    responseResult() {
+      return this.response && this.response.responseResult ? this.response.responseResult : {};
     }
   }
+}
 </script>
 
 <style scoped>
-  .text-container .icon {
-    padding: 5px;
-  }
+.text-container .icon {
+  padding: 5px;
+}
 
-  .text-container .collapse {
-    cursor: pointer;
-  }
+.text-container .collapse {
+  cursor: pointer;
+}
 
-  .text-container .collapse:hover {
-    opacity: 0.8;
-  }
+.text-container .collapse:hover {
+  opacity: 0.8;
+}
 
-  .text-container .icon.is-active {
-    transform: rotate(90deg);
-  }
+.text-container .icon.is-active {
+  transform: rotate(90deg);
+}
 
-  .text-container .pane {
-    background-color: #F5F5F5;
+.text-container .pane {
+  background-color: #F5F5F5;
   padding: 1px 0;
-    height: 250px;
-    overflow-y: auto;
-  }
+  height: 250px;
+  overflow-y: auto;
+}
 
-  .text-container .pane.cookie {
-    padding: 0;
-  }
+.text-container .pane.cookie {
+  padding: 0;
+}
 
-  /deep/ .el-tabs__nav-wrap::after {
-    height: 0px;
-  }
+/deep/ .el-tabs__nav-wrap::after {
+  height: 0px;
+}
 
-  .ms-div {
-    margin-top: 20px;
-  }
+.ms-div {
+  margin-top: 20px;
+}
 
-  pre {
-    margin: 0;
-  }
+pre {
+  margin: 0;
+}
 </style>
