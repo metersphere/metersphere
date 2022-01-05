9927d2c587a2b388ee5d633f6cc31346df4415de
song-tianyang <tianyang.song@fit2cloud.com>
2022-01-05 22:32:54 +0800

    fix(测试计划): 修复自定义插件安全漏洞及用例模块匹配问题

--- backend/src/main/java/io/metersphere/commons/utils/ShiroUtils.java
+++ backend/src/main/java/io/metersphere/commons/utils/ShiroUtils.java
@@ -71,9 +71,6 @@ public class ShiroUtils {
         //mock接口
         filterChainDefinitionMap.put("/mock/**", "anon");
         filterChainDefinitionMap.put("/ws/**", "anon");
-
-        filterChainDefinitionMap.put("/plugin/**", "anon");
-
     }
 
     public static void ignoreCsrfFilter(Map<String, String> filterChainDefinitionMap) {
