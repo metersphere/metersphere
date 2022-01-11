17115054095ad48c30355623b1cf6e29ed08163f
fit2-zhao <yong.zhao@fit2cloud.com>
2022-01-11 16:38:52 +0800

    refactor(测试计划): 重构测试计划执行报告状态处理

--- backend/src/main/java/io/metersphere/base/mapper/ext/ExtApiExecutionQueueMapper.java
+++ backend/src/main/java/io/metersphere/base/mapper/ext/ExtApiExecutionQueueMapper.java
@@ -1,5 +1,11 @@
 package io.metersphere.base.mapper.ext;
 
+import io.metersphere.base.domain.ApiExecutionQueue;
+
+import java.util.List;
+
 public interface ExtApiExecutionQueueMapper {
     void delete();
+
+    List<ApiExecutionQueue> findTestPlanReportQueue();
 }
