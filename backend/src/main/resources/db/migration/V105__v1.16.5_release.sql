6c45720014757b7665ab682b970bd384eed650a9
song-tianyang <tianyang.song@fit2cloud.com>
2022-01-11 15:11:48 +0800

    refactor(数据库字段命名优化): 数据库字段命名优化
    数据库字段命名优化

--- backend/src/main/resources/db/migration/V105__v1.16.5_release.sql
+++ backend/src/main/resources/db/migration/V105__v1.16.5_release.sql
@@ -1,3 +1,3 @@
-ALTER TABLE test_plan_report_content ADD scenario_report_id longtext NULL;
-ALTER TABLE test_plan_report_content ADD api_case_report_id longtext NULL;
-ALTER TABLE test_plan_report_content ADD load_case_report_id longtext NULL;
+ALTER TABLE test_plan_report_content ADD plan_scenario_report_struct longtext NULL;
+ALTER TABLE test_plan_report_content ADD plan_api_case_report_struct longtext NULL;
+ALTER TABLE test_plan_report_content ADD plan_load_case_report_struct longtext NULL;
