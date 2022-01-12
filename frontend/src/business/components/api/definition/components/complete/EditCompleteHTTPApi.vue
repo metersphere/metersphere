2d9ac018dfcfabc98a4599317bdf62d52968b9f7
wxg0103 <727495428@qq.com>
2022-01-12 16:06:02 +0800

    fix(接口测试): 创建接口GET请求参数不能正确显示在QUERY区域
    --bug=1009554 --user=王孝刚 [接口定义]-github#9268-创建接口GET请求参数不能正确显示在QUERY区域
    https://www.tapd.cn/55049933/s/1091015

--- frontend/src/business/components/api/definition/components/complete/EditCompleteHTTPApi.vue
+++ frontend/src/business/components/api/definition/components/complete/EditCompleteHTTPApi.vue
@@ -375,7 +375,7 @@
         try {
           let url = new URL(urlStr);
           url.searchParams.forEach((value, key) => {
-            if (key && value) {
+            if (key) {
               this.request.arguments.splice(0, 0, new KeyValue({name: key, required: false, value: value}));
             }
           });
