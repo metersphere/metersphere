e143237ca0325b95fc87059b130e77730e609c2d
chenjianxing <jianxing.chen@fit2cloud.com>
2022-01-18 15:34:25 +0800

    refactor: 首页数量统计显示优化
    --bug=1009703 --user=陈建星 [测试跟踪] github#9466测试跟踪首页用例数量统计显示不全 https://www.tapd.cn/55049933/s/10932477

---
+++ frontend/src/business/components/common/chart/MsCountRingChart.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-tooltip :content="content + ''" placement="top-start">
+    <div class="main-number-show">
+      <span class="count-number">
+        {{content}}
+      </span>
+      <span style="color: #6C317C;">
+        {{$t('api_test.home_page.unit_of_measurement')}}
+      </span>
+    </div>
+  </el-tooltip>
+</template>
+
+<script>
+export default {
+  name: "MsCountRingChart",
+  props: {
+    content: {
+      type: [Number, String]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.main-number-show {
+  width: 100px;
+  height: 100px;
+  border-style: solid;
+  border-width: 7px;
+  border-color: var(--count_number_shallow);
+  border-radius:50%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap
+}
+
+.count-number {
+  font-family:'ArialMT', 'Arial', sans-serif;
+  font-size: 33px;
+  color: var(--count_number);
+  position: relative;
+}
+
+</style>
