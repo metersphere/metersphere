35c5b715f48a88716231b95650efc1e2c95fb723
chenjianxing <jianxing.chen@fit2cloud.com>
2022-01-20 14:53:35 +0800

    fix: 勾选第三方模板报告人选不上 --bug=1009269 --user=陈建星 [ github#8862]对接jira填完了但是报“报告人不能为空” https://www.tapd.cn/55049933/s/1095003

--- backend/src/main/java/io/metersphere/track/issue/domain/jira/JiraUser.java
+++ backend/src/main/java/io/metersphere/track/issue/domain/jira/JiraUser.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 @Getter
 public class JiraUser {
     private String accountId;
+    private String key;
     private String displayName;
     private String emailAddress;
     private Boolean active;
