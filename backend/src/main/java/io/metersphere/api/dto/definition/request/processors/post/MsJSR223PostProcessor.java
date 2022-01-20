bf9ab76740efd81eed51f098b804d7537224fcbd
song-tianyang <tianyang.song@fit2cloud.com>
2022-01-20 19:05:40 +0800

    fix(场景测试转性能测试): 优化场景测试转性能测试时的数据结构处理
    优化场景测试转性能测试时的数据结构处理

--- backend/src/main/java/io/metersphere/api/dto/definition/request/processors/post/MsJSR223PostProcessor.java
+++ backend/src/main/java/io/metersphere/api/dto/definition/request/processors/post/MsJSR223PostProcessor.java
@@ -57,6 +57,11 @@ public class MsJSR223PostProcessor extends MsTestElement {
         //替换Metersphere环境变量
         script = StringUtils.replace(script, RunningParamKeys.API_ENVIRONMENT_ID, "\"" + RunningParamKeys.RUNNING_PARAMS_PREFIX + this.getEnvironmentId() + ".\"");
 
+        if(config.isOperating()){
+            if (script.startsWith("io.metersphere.utils.JMeterVars.addVars")) {
+                return;
+            }
+        }
         // 非导出操作，且不是启用状态则跳过执行
         if (!config.isOperating() && !this.isEnable()) {
             return;
