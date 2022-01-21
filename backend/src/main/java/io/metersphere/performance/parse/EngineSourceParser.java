9813af4cc0f334fd03f446bfc52be1d328b6382e
CaptainB <bin@fit2cloud.com>
2022-01-21 13:48:09 +0800

    refactor(性能测试): 重构性能测试解析jmx的方式

--- backend/src/main/java/io/metersphere/performance/parse/EngineSourceParser.java
+++ backend/src/main/java/io/metersphere/performance/parse/EngineSourceParser.java
@@ -5,5 +5,5 @@ import io.metersphere.performance.engine.EngineContext;
 import java.io.InputStream;
 
 public interface EngineSourceParser {
-    String parse(EngineContext context, InputStream source) throws Exception;
+    byte[] parse(EngineContext context, InputStream source) throws Exception;
 }
--- backend/src/main/java/io/metersphere/performance/parse/xml/reader/DocumentParser.java
+++
@@ -1,8 +0,0 @@
-package io.metersphere.performance.parse.xml.reader;
-
-import io.metersphere.performance.engine.EngineContext;
-import org.w3c.dom.Document;
-
-public interface DocumentParser {
-    String parse(EngineContext context, Document document) throws Exception;
-}
