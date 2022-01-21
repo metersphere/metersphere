fa6117e27654fa6bc7d85b45acccbfe98039d227
chenjianxing <jianxing.chen@fit2cloud.com>
2022-01-21 11:19:10 +0800

    refactor: swagger导入支持多层级的tag

--- backend/src/main/java/io/metersphere/api/dto/definition/parse/SwaggerAbstractParser.java
+++ backend/src/main/java/io/metersphere/api/dto/definition/parse/SwaggerAbstractParser.java
@@ -19,15 +19,32 @@ public abstract class SwaggerAbstractParser extends ApiImportAbstractParser<ApiD
             }
         } else {
             tags.forEach(tag -> {
-                ApiModule module = ApiDefinitionImportUtil.buildModule(parentModule, tag, this.projectId);
-                apiDefinition.setModuleId(module.getId());
-                if (StringUtils.isNotBlank(selectModulePath)) {
-                    apiDefinition.setModulePath(selectModulePath + "/" + tag);
+                // 涉及到多级目录的结构，如： 一级目录/二级目录
+                if (tag.contains("/")) {
+                    String[] tagTree = tag.split("/");
+                    ApiModule pModule = parentModule;
+                    String prefix = selectModulePath;
+                    for (String item : tagTree) {
+                        pModule = buildModule(pModule, apiDefinition, item, prefix);
+                        prefix += "/" + item;
+                    }
                 } else {
-                    apiDefinition.setModulePath("/" + tag);
+                    buildModule(parentModule, apiDefinition, tag, selectModulePath);
                 }
             });
         }
+    }
+
+    private ApiModule buildModule(ApiModule parentModule, ApiDefinitionWithBLOBs apiDefinition,
+                               String tag, String selectModulePath) {
+        ApiModule module = ApiDefinitionImportUtil.buildModule(parentModule, tag, this.projectId);
+        apiDefinition.setModuleId(module.getId());
+        if (StringUtils.isNotBlank(selectModulePath)) {
+            apiDefinition.setModulePath(selectModulePath + "/" + tag);
+        } else {
+            apiDefinition.setModulePath("/" + tag);
+        }
+        return module;
     }
 
 }
