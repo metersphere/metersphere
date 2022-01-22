a490c219772a890c412fd9ba7cf2421b0acd054a
fit2-zhao <yong.zhao@fit2cloud.com>
2022-01-22 18:45:31 +0800

    fix(接口测试) 用例不选环境执行结果状态不更新问题
    --bug=1009883 --user=赵勇 【接口定义】-批量执行接口CASE后-CASE列表中执行结果不会同步更新 https://www.tapd.cn/55049933/s/1096574
    fix(误报库): 优化误报库前端页面展示
    
    优化误报库前端页面展示

--- backend/src/main/java/io/metersphere/api/exec/api/ApiCaseParallelExecuteService.java
+++ backend/src/main/java/io/metersphere/api/exec/api/ApiCaseParallelExecuteService.java
@@ -34,7 +34,7 @@ public class ApiCaseParallelExecuteService {
                         String reportId = result.getId();
                         HashTree hashTree = null;
                         if (!GenerateHashTreeUtil.isResourcePool(config.getResourcePoolId()).isPool()) {
-                            hashTree = apiScenarioSerialService.generateHashTree(testId, config.getEnvMap());
+                            hashTree = apiScenarioSerialService.generateHashTree(testId, result.getId(), runMode, config.getEnvMap());
                         }
                         JmeterRunRequestDTO runRequest = new JmeterRunRequestDTO(testId, reportId, runMode, hashTree);
                         runRequest.setPool(GenerateHashTreeUtil.isResourcePool(config.getResourcePoolId()));
