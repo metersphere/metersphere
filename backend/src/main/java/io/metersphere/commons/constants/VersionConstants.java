04c6e4a251dda653f65d5d66c9eb314fe9c9f13f
zhangdahai112 <dahai.zhang@fit2cloud.com>
2022-01-23 13:10:41 +0800

    refactor: 优化版本sql

--- backend/src/main/java/io/metersphere/commons/constants/VersionConstants.java
+++
@@ -1,8 +0,0 @@
-package io.metersphere.commons.constants;
-
-public class VersionConstants {
-    //最新版本
-    public static Byte LATEST = 1;
-    //非最新版本 可以不用设置数据库 default = 0
-    public static Byte NOT_LATEST = 0;
-}
