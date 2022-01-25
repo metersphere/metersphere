017882a7b162c8db712431be0bca3ae0227f078d
fit2-zhao <yong.zhao@fit2cloud.com>
2022-01-25 18:43:22 +0800

    refactor(操作日志): 操作日志和操作资源的关系解耦

--- backend/src/main/java/io/metersphere/log/service/OperatingLogService.java
+++ backend/src/main/java/io/metersphere/log/service/OperatingLogService.java
@@ -1,8 +1,10 @@
 package io.metersphere.log.service;
 
 import com.alibaba.fastjson.JSON;
+import io.metersphere.base.domain.OperatingLogResource;
 import io.metersphere.base.domain.OperatingLogWithBLOBs;
 import io.metersphere.base.mapper.OperatingLogMapper;
+import io.metersphere.base.mapper.OperatingLogResourceMapper;
 import io.metersphere.base.mapper.ext.ExtOperatingLogMapper;
 import io.metersphere.commons.utils.BeanUtils;
 import io.metersphere.log.vo.OperatingLogDTO;
@@ -10,12 +12,17 @@ import io.metersphere.log.vo.OperatingLogDetails;
 import io.metersphere.log.vo.OperatingLogRequest;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.ibatis.session.ExecutorType;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Transactional(rollbackFor = Exception.class)
@@ -24,9 +31,36 @@ public class OperatingLogService {
     private OperatingLogMapper operatingLogMapper;
     @Resource
     private ExtOperatingLogMapper extOperatingLogMapper;
+    @Resource
+    private SqlSessionFactory sqlSessionFactory;
 
-    public void create(OperatingLogWithBLOBs log) {
+    public void create(OperatingLogWithBLOBs log, String sourceIds) {
+        log.setSourceId("");
         operatingLogMapper.insert(log);
+        System.out.println(JSON.toJSONString(log));
+        if (StringUtils.isNotEmpty(sourceIds)) {
+            List<String> ids = new ArrayList<>();
+            if (sourceIds.startsWith("[")) {
+                ids = JSON.parseObject(sourceIds, List.class);
+            } else {
+                ids.add(sourceIds.replace("\"", ""));
+            }
+            SqlSession sqlSession = sqlSessionFactory.openSession(ExecutorType.BATCH);
+            OperatingLogResourceMapper batchMapper = sqlSession.getMapper(OperatingLogResourceMapper.class);
+            if (CollectionUtils.isNotEmpty(ids)) {
+                ids.forEach(item -> {
+                    OperatingLogResource resource = new OperatingLogResource();
+                    resource.setId(UUID.randomUUID().toString());
+                    resource.setOperatingLogId(log.getId());
+                    resource.setSourceId(item);
+                    batchMapper.insert(resource);
+                });
+                sqlSession.flushStatements();
+                if (sqlSession != null && sqlSessionFactory != null) {
+                    SqlSessionUtils.closeSqlSession(sqlSession, sqlSessionFactory);
+                }
+            }
+        }
     }
 
     public List<OperatingLogDTO> list(OperatingLogRequest request) {
@@ -48,7 +82,6 @@ public class OperatingLogService {
     }
 
     public List<OperatingLogDTO> findBySourceId(OperatingLogRequest request) {
-        request.setSourceId("%" + request.getSourceId() + "%");
         List<OperatingLogDTO> logWithBLOBs = extOperatingLogMapper.findBySourceId(request);
         List<OperatingLogDTO> dtos = new ArrayList<>();
         if (CollectionUtils.isNotEmpty(logWithBLOBs)) {
