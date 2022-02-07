688857e62b5e78b9e9c2b70a4a3174c26b9de4ca
CaptainB <bin@fit2cloud.com>
2022-02-07 18:33:29 +0800

    style: 规范 sql 建表语句

--- backend/src/main/resources/db/migration/V31__message_task.sql
+++ backend/src/main/resources/db/migration/V31__message_task.sql
@@ -1,4 +1,5 @@
-CREATE TABLE IF NOT EXISTS message_task (
+CREATE TABLE IF NOT EXISTS message_task
+(
     id        varchar(50)  NOT NULL,
     type      varchar(50)  NOT NULL COMMENT '消息类型',
     event     varchar(255) NOT NULL COMMENT '通知事件类型',
@@ -9,4 +10,5 @@ CREATE TABLE IF NOT EXISTS message_task (
         PRIMARY KEY (id)
 )
     ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4;
+    DEFAULT CHARSET = utf8mb4
+    COLLATE utf8mb4_general_ci;
