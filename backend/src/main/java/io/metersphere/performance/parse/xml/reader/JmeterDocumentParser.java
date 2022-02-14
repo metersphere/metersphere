0f0af3ba278712d86e2eaabd2196801f9d7635eb
CaptainB <bin@fit2cloud.com>
2022-02-14 10:57:18 +0800

    fix(性能测试): 修复上传jmx中已有 backend listener 的时候没有去重的问题
    --bug=1010227 --user=刘瑞斌 【性能测试】github#10429，性能测试报告日志详情中的summary的值比请求统计中的samples的total值小一半 https://www.tapd.cn/55049933/s/1103469
    
    Closes #10429

--- backend/src/main/java/io/metersphere/performance/parse/xml/reader/JmeterDocumentParser.java
+++ backend/src/main/java/io/metersphere/performance/parse/xml/reader/JmeterDocumentParser.java
@@ -73,8 +73,7 @@ public class JmeterDocumentParser implements EngineSourceParser {
 
         if (hashTree.elements().size() > 0) {
             final List<Element> childNodes = hashTree.elements();
-            for (int i = 0; i < childNodes.size(); i++) {
-                Element ele = childNodes.get(i);
+            for (Element ele : childNodes) {
                 if (nodeNameEquals(ele, HASH_TREE_ELEMENT)) {
                     parseHashTree(ele);
                 } else if (nodeNameEquals(ele, TEST_PLAN)) {
@@ -610,6 +609,15 @@ public class JmeterDocumentParser implements EngineSourceParser {
         }
 
         Element listenerParent = getNextSibling(element);
+
+        List<Element> childNodes = listenerParent.elements();
+        for (Element item : childNodes) {
+            if (nodeNameEquals(item, BACKEND_LISTENER)) {
+                // 如果已经存在，不再添加
+                removeChildren(item);
+                return;
+            }
+        }
 
         // add class name
         Element backendListener = listenerParent.addElement(BACKEND_LISTENER);
