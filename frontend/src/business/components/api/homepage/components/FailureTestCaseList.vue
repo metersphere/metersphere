28deabd68fb1ae1044a8e7e919c7f46f136908ba
shiziyuan9527 <yuhao.li@fit2cloud.com>
2022-02-17 17:25:51 +0800

    fix(测试跟踪): 测试计划失败用例TOP10无数据
    --bug=1010277 --user=lyh 【测试跟踪】+【接口测试】github#10501v1.17.2 测试跟踪 和 接口测试
    模块首页的 失败用例TOP10 无数据 https://www.tapd.cn/55049933/s/1105255
    
    Closes #10501

--- frontend/src/business/components/api/homepage/components/FailureTestCaseList.vue
+++ frontend/src/business/components/api/homepage/components/FailureTestCaseList.vue
@@ -21,15 +21,15 @@
         width="150"
         show-overflow-tooltip>
         <template v-slot:default="scope">
-          <ms-tag v-if="scope.row.caseType == 'apiCase'" type="success" effect="plain" :content="$t('api_test.home_page.failed_case_list.table_value.case_type.api')"/>
-          <ms-tag v-if="scope.row.caseType == 'scenario'" type="warning" effect="plain" :content="$t('api_test.home_page.failed_case_list.table_value.case_type.scene')"/>
-          <ms-tag v-if="scope.row.caseType == 'load'" type="danger" effect="plain" :content="$t('api_test.home_page.failed_case_list.table_value.case_type.load')"/>
+          <ms-tag v-if="scope.row.caseType === 'apiCase'" type="success" effect="plain" :content="$t('api_test.home_page.failed_case_list.table_value.case_type.api')"/>
+          <ms-tag v-if="scope.row.caseType === 'scenario'" type="warning" effect="plain" :content="$t('api_test.home_page.failed_case_list.table_value.case_type.scene')"/>
+          <ms-tag v-if="scope.row.caseType === 'load'" type="danger" effect="plain" :content="$t('api_test.home_page.failed_case_list.table_value.case_type.load')"/>
         </template>
       </el-table-column>
       <el-table-column prop="testPlan"  :label="$t('api_test.home_page.failed_case_list.table_coloum.test_plan')">
         <template v-slot:default="{row}">
           <div v-for="(testPlan, index) in row.testPlanDTOList" :key="index">
-            <el-link type="info" @click="redirect('testPlanEdit',testPlan.id)">
+            <el-link type="info" @click="redirect('testPlanEdit',testPlan.id)" v-if="testPlan.name === row.testPlan">
               {{ testPlan.name }};
             </el-link>
           </div>
