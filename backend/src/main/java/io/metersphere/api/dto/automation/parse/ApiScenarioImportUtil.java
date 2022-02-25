de26483f74a2d2b9ffa4d17b15980e8a8c860378
guoyuqi <xiaomeinvG@126.com>
2022-02-25 19:15:15 +0800

    feat(接口测试): 接口自动化场景导出再导入保留引用关系支持跨空间补充数据
    --user=郭雨琦
    接口自动化场景导出再导入保留引用关系 场景导入导出跨空间，增加数据
    https://www.tapd.cn/55049933/prong/stories/view/1155049933001003780

--- backend/src/main/java/io/metersphere/api/dto/automation/parse/ApiScenarioImportUtil.java
+++ backend/src/main/java/io/metersphere/api/dto/automation/parse/ApiScenarioImportUtil.java
@@ -1,16 +1,28 @@
 package io.metersphere.api.dto.automation.parse;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import io.metersphere.api.dto.automation.ApiScenarioModuleDTO;
+import io.metersphere.api.dto.definition.ApiDefinitionRequest;
+import io.metersphere.api.dto.definition.ApiDefinitionResult;
+import io.metersphere.api.dto.definition.SaveApiTestCaseRequest;
 import io.metersphere.api.dto.definition.parse.ms.NodeTree;
+import io.metersphere.api.service.ApiDefinitionService;
 import io.metersphere.api.service.ApiScenarioModuleService;
-import io.metersphere.base.domain.ApiScenarioModule;
+import io.metersphere.api.service.ApiTestCaseService;
+import io.metersphere.base.domain.*;
+import io.metersphere.base.mapper.ApiDefinitionMapper;
+import io.metersphere.base.mapper.ApiTestCaseMapper;
+import io.metersphere.commons.constants.APITestStatus;
 import io.metersphere.commons.utils.BeanUtils;
 import io.metersphere.commons.utils.CommonBeanFactory;
+import io.metersphere.commons.utils.SessionUtils;
+import io.metersphere.service.ProjectService;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class ApiScenarioImportUtil {
 
@@ -103,6 +115,191 @@ public class ApiScenarioImportUtil {
             NodeTree node = iterator.next();
             createNodeTree(node, hasModuleSelected ? selectModule.getId() : null,
                     projectId, apiModuleService, "/", hasModuleSelected ? selectModule.getLevel() : 0);
+        }
+    }
+
+    public static boolean checkWorkSpace(String projectId, String currentProjectId) {
+        if(!Objects.equals(projectId, currentProjectId)){
+            ProjectService projectService = CommonBeanFactory.getBean(ProjectService.class);
+            Project project = projectService.getProjectById(projectId);
+            return Objects.equals(project.getWorkspaceId(), SessionUtils.getCurrentWorkspaceId());
+        }
+        return true;
+    }
+
+    private static ApiDefinitionResult getApiDefinitionResult(JSONObject object, ApiDefinitionService apiDefinitionService) {
+        ApiDefinitionRequest apiDefinitionRequest = new ApiDefinitionRequest();
+        apiDefinitionRequest.setPath(object.getString("path"));
+        apiDefinitionRequest.setMethod(object.getString("method"));
+        apiDefinitionRequest.setPath(object.getString("protocol"));
+        return apiDefinitionService.getApiDefinitionResult(apiDefinitionRequest);
+    }
+
+    private static ApiTestCaseWithBLOBs getApiTestCase(JSONObject object, ApiTestCaseService testCaseService, ApiDefinitionResult apiDefinitionResult) {
+        SaveApiTestCaseRequest request = new SaveApiTestCaseRequest();
+        request.setName(object.getString("name"));
+        request.setApiDefinitionId(apiDefinitionResult.getId());
+        return testCaseService.getSameCaseWithBLOBs(request);
+    }
+
+    public static void checkCase(JSONObject object, String versionId, String projectId, ApiTestCaseMapper apiTestCaseMapper, ApiDefinitionMapper apiDefinitionMapper) {
+        ApiTestCaseService testCaseService = CommonBeanFactory.getBean(ApiTestCaseService.class);
+        ApiDefinitionService apiDefinitionService = CommonBeanFactory.getBean(ApiDefinitionService.class);
+        ApiTestCaseWithBLOBs bloBs = testCaseService.get(object.getString("id"));
+        if (bloBs != null) {
+            boolean isSameWorkSpace = checkWorkSpace(bloBs.getProjectId(),projectId);
+            if(!isSameWorkSpace){
+                ApiDefinitionResult apiDefinition = apiDefinitionService.getById(bloBs.getApiDefinitionId());
+                String apiDefinitionId = checkDefinition(apiDefinitionService,apiDefinition, versionId,projectId,apiDefinitionMapper);
+                structureCaseData(bloBs,apiDefinitionId,versionId,projectId,apiTestCaseMapper);
+                object.put("projectId", projectId);
+                object.put("id", bloBs.getId());
+            }
+        }else{
+            ApiDefinitionResult apiDefinition = getApiDefinitionResult(object,apiDefinitionService);
+            ApiTestCaseWithBLOBs testCase;
+            if(apiDefinition!=null){
+                testCase= getApiTestCase(object, testCaseService, apiDefinition);
+                if (testCase != null) {
+                    boolean isSameWorkSpace = checkWorkSpace(testCase.getProjectId(),projectId);
+                    if(!isSameWorkSpace){
+                        String apiDefinitionId = checkDefinition(apiDefinitionService, apiDefinition, versionId,projectId,apiDefinitionMapper);
+                        structureCaseData(testCase,apiDefinitionId,versionId,projectId,apiTestCaseMapper);
+                    }
+                }else{
+                    String apiDefinitionId = checkDefinition(apiDefinitionService, apiDefinition, versionId,projectId,apiDefinitionMapper);
+                    testCase= structureCaseByJson(object, versionId, projectId, apiDefinitionId,apiTestCaseMapper);
+                }
+            }else{
+                ApiDefinitionResult apiDefinitionResult = structureApiDefinitionByJson(apiDefinitionService, object, versionId, projectId, apiDefinitionMapper);
+                testCase = structureCaseByJson(object, versionId, projectId, apiDefinitionResult.getId(),apiTestCaseMapper);
+            }
+            object.put("projectId", projectId);
+            object.put("id", testCase.getId());
+        }
+    }
+
+    private static String checkDefinition(ApiDefinitionService apiDefinitionService, ApiDefinitionResult apiDefinition, String versionId, String projectId,ApiDefinitionMapper apiDefinitionMapper) {
+        boolean isSameWorkspace = checkWorkSpace(apiDefinition.getProjectId(),projectId);
+        if(!isSameWorkspace){
+            String requestStr = apiDefinition.getRequest();
+            JSONObject objectDefinition = JSONObject.parseObject(requestStr);
+            ApiDefinitionResult apiDefinitionResult1 = insertDefinitionByApiDefinition(apiDefinitionService,apiDefinition, objectDefinition, versionId, projectId,apiDefinitionMapper);
+            return apiDefinitionResult1.getId();
+        }else{
+            return apiDefinition.getId();
+        }
+    }
+
+    private static void structureCaseData(ApiTestCaseWithBLOBs testCase, String apiDefinitionId, String versionId, String projectId,ApiTestCaseMapper apiTestCaseMapper) {
+        String requestStr = testCase.getRequest();
+        JSONObject objectCase = JSONObject.parseObject(requestStr);
+        testCase.setApiDefinitionId(apiDefinitionId);
+        insertCaseByApiTestCase(testCase,objectCase,versionId,projectId,apiTestCaseMapper);
+    }
+
+    public static ApiDefinitionResult structureApiDefinitionByJson(ApiDefinitionService apiDefinitionService,JSONObject object, String versionId, String projectId,ApiDefinitionMapper apiDefinitionMapper) {
+        ApiDefinitionResult test = new ApiDefinitionResult();
+        apiDefinitionService.checkQuota();
+        String protocal = object.getString("protocal");
+        if (StringUtils.equals(protocal, "DUBBO")) {
+            test.setMethod("dubbo://");
+        }else{
+            test.setMethod(protocal);
+        }
+        apiDefinitionService.initModulePathAndId(projectId, test);
+        String id = UUID.randomUUID().toString();
+        test.setId(id);
+        test.setName(object.getString("name"));
+        test.setPath(object.getString("path"));
+        test.setCreateUser(SessionUtils.getUserId());
+        test.setProjectId(projectId);
+        test.setCreateTime(System.currentTimeMillis());
+        test.setUpdateTime(System.currentTimeMillis());
+        test.setStatus(APITestStatus.Underway.name());
+        test.setRefId(id);
+        test.setLatest(true);
+        test.setVersionId(versionId);
+        object.put("id", test.getId());
+        object.put("resourceId", test.getId());
+        object.put("projectId", projectId);
+        object.put("useEnvironment","");
+        test.setRequest(object.toJSONString());
+        test.setUserId(SessionUtils.getUserId());
+        test.setLatest(true);
+        test.setOrder(apiDefinitionService.getImportNextOrder(projectId));
+        apiDefinitionMapper.insert(test);
+        return test;
+    }
+
+    public static ApiDefinitionResult insertDefinitionByApiDefinition(ApiDefinitionService apiDefinitionService, ApiDefinitionResult apiDefinitionResult, JSONObject objectDefinition, String versionId, String projectId,ApiDefinitionMapper apiDefinitionMapper){
+        String id = UUID.randomUUID().toString();
+        apiDefinitionResult.setId(id);
+        apiDefinitionResult.setProjectId(projectId);
+        apiDefinitionResult.setVersionId(versionId);
+        apiDefinitionResult.setRefId(id);
+        apiDefinitionResult.setLatest(true);
+        objectDefinition.put("id", id);
+        objectDefinition.put("resourceId", id);
+        objectDefinition.put("projectId", projectId);
+        objectDefinition.put("useEnvironment","");
+        apiDefinitionService.initModulePathAndId(projectId, apiDefinitionResult);
+        apiDefinitionResult.setRequest(JSON.toJSONString(objectDefinition));
+        apiDefinitionResult.setOrder(apiDefinitionService.getImportNextOrder(projectId));
+        apiDefinitionMapper.insert(apiDefinitionResult);
+        return apiDefinitionResult;
+    }
+
+    public static ApiTestCaseWithBLOBs structureCaseByJson(JSONObject object, String versionId, String projectId, String apiDefinitionId,ApiTestCaseMapper apiTestCaseMapper) {
+        ApiDefinitionService apiDefinitionService = CommonBeanFactory.getBean(ApiDefinitionService.class);
+        ApiTestCaseWithBLOBs apiTestCase = new ApiTestCaseWithBLOBs();
+        String id = UUID.randomUUID().toString();
+        apiTestCase.setId(id);
+        apiTestCase.setName(object.getString("name"));
+        apiTestCase.setCaseStatus(APITestStatus.Underway.name());
+        apiTestCase.setApiDefinitionId(apiDefinitionId);
+        apiTestCase.setCreateUserId(Objects.requireNonNull(SessionUtils.getUser()).getId());
+        apiTestCase.setUpdateUserId(Objects.requireNonNull(SessionUtils.getUser()).getId());
+        apiTestCase.setProjectId(projectId);
+        apiTestCase.setCreateTime(System.currentTimeMillis());
+        apiTestCase.setUpdateTime(System.currentTimeMillis());
+        apiTestCase.setVersionId(versionId);
+        object.put("id", apiTestCase.getId());
+        object.put("resourceId", apiTestCase.getId());
+        object.put("projectId", projectId);
+        object.put("useEnvironment","");
+        apiTestCase.setRequest(object.toJSONString());
+        apiTestCase.setOrder(apiDefinitionService.getImportNextCaseOrder(projectId));
+        apiTestCaseMapper.insert(apiTestCase);
+        return  apiTestCase;
+    }
+
+    public static void insertCaseByApiTestCase(ApiTestCaseWithBLOBs bloBs, JSONObject objectCase, String versionId, String projectId,ApiTestCaseMapper apiTestCaseMapper){
+        ApiDefinitionService apiDefinitionService = CommonBeanFactory.getBean(ApiDefinitionService.class);
+        String id = UUID.randomUUID().toString();
+        bloBs.setId(id);
+        bloBs.setProjectId(projectId);
+        bloBs.setVersionId(versionId);
+        objectCase.put("id", id);
+        objectCase.put("resourceId", id);
+        objectCase.put("projectId", projectId);
+        objectCase.put("useEnvironment","");
+        bloBs.setRequest(JSON.toJSONString(objectCase));
+        bloBs.setOrder(apiDefinitionService.getImportNextCaseOrder(projectId));
+        apiTestCaseMapper.insert(bloBs);
+    }
+
+    public static void formatHashTree(JSONArray hashTree) {
+        if (CollectionUtils.isNotEmpty(hashTree)) {
+            for (int i = 0; i < hashTree.size(); i++) {
+                JSONObject object = (JSONObject) hashTree.get(i);
+                object.put("index", i + 1);
+                object.put("resourceId", UUID.randomUUID().toString());
+                hashTree.set(i, object);
+                if (CollectionUtils.isNotEmpty(object.getJSONArray("hashTree"))) {
+                    formatHashTree(object.getJSONArray("hashTree"));
+                }
+            }
         }
     }
 
