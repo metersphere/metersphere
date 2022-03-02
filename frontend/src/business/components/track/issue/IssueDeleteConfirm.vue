9ebde184e920783f260a019ad86c80b3594c12cc
shiziyuan9527 <yuhao.li@fit2cloud.com>
2022-03-02 15:06:40 +0800

    refactor(缺陷管理): 删除增加二次提醒
    --story=1005998 --user=李玉号 删除增加二次提醒功能
    https://www.tapd.cn/55049933/s/1112045

---
+++ frontend/src/business/components/track/issue/IssueDeleteConfirm.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-dialog
+    title=""
+    :visible.sync="dialogVisible"
+    width="25%">
+    <span>{{ $t('test_track.issue.delete_tip') }}{{issue.title}} ?</span>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="dialogVisible = false" size="medium">{{$t('commons.cancel')}}</el-button>
+      <el-button type="primary" @click="confirm" size="medium">{{$t('commons.confirm')}}</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  name: "IssueDeleteConfirm",
+  data() {
+    return {
+      dialogVisible: false,
+      issue: {
+        title: ""
+      }
+    }
+  },
+  methods: {
+    open(issue) {
+      this.issue = issue;
+      this.dialogVisible = true;
+    },
+    confirm() {
+      this.$emit('delete', this.issue);
+      this.dialogVisible = false;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
