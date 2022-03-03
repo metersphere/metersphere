67af24bc8443768a9f58452355a5ef8764f78f17
AnAngle <1323481023@qq.com>
2022-03-03 22:18:22 +0800

    feat: 添加remoteDriver依赖

--- backend/src/test/java/com/test/UITest.java
+++ backend/src/test/java/com/test/UITest.java
@@ -3,6 +3,7 @@ package com.test;
 import com.google.gson.Gson;
 import io.metersphere.Application;
 import io.metersphere.xpack.ui.domain.SideDTO;
+import io.metersphere.xpack.ui.service.UiAutomationService;
 import io.metersphere.xpack.ui.util.TemplateUtils;
 import io.metersphere.xpack.ui.util.WebDriverSamplerHelper;
 import org.junit.Test;
@@ -10,7 +11,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.io.IOException;
+import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -19,13 +20,26 @@ import java.io.IOException;
  */
 public class UITest {
 
+    @Resource
+    private UiAutomationService uiAutomationService;
+
     /**
      * 测试脚本转换是否正常
      */
     @Test
-    public void testWebdriverScript() throws IOException {
+    public void testWebdriverScript() {
         String sidePath = this.getClass().getClassLoader().getResource("selenium-example/baidu.side").getPath();
         String sideDefinition = TemplateUtils.readContent(sidePath);
         System.out.println(WebDriverSamplerHelper.getFullWebDriverScript(new Gson().fromJson(sideDefinition, SideDTO.class)));
+    }
+
+    @Test
+    public void testUIRun() {
+        uiAutomationService.run();
+        try {
+            Thread.sleep(1000 * 60 * 5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
