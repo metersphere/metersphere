b0d69a66ee9650b5d63b53151b47e8db8067c22a
shiziyuan9527 <yuhao.li@fit2cloud.com>
2022-03-07 11:16:11 +0800

    refactor(系统设置): 对话框按钮显示问题
    --bug=1010843 --user=李玉号 【系统设置】-环境管理-编辑/创建环境页面-取消和确定按钮离下拉框太近
    https://www.tapd.cn/55049933/s/1114357
    --bug=1010842 --user=李玉号
    【接口测试】-编辑case-环境配置-取消和确定按钮未置顶显示 https://www.tapd.cn/55049933/s/1114355

--- frontend/src/business/components/settings/workspace/environment/EnvironmentList.vue
+++ frontend/src/business/components/settings/workspace/environment/EnvironmentList.vue
@@ -62,7 +62,12 @@
     </el-card>
 
     <!-- 创建、编辑、复制环境时的对话框 -->
-    <el-dialog :visible.sync="dialogVisible" :close-on-click-modal="false" :title="dialogTitle" top="50px" width="66%">
+    <el-dialog :visible.sync="dialogVisible" :close-on-click-modal="false" top="50px" width="66%">
+      <template #title>
+        <ms-dialog-header :title="dialogTitle"
+                          @cancel="dialogVisible = false"
+                          @confirm="save"/>
+      </template>
       <el-form label-width="80px" :rules="rules" style="display: flow-root">
         <el-form-item class="project-item" prop="currentProjectId" :label="$t('project.select')">
           <el-select @change="handleProjectChange" v-model="currentProjectId" filterable clearable>
