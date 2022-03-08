a2387a85536bbb892ad5cf2ca4cad117f3dffd7c
shiziyuan9527 <yuhao.li@fit2cloud.com>
2022-03-08 11:43:50 +0800

    fix(系统设置): 数据库配置oracle驱动，默认生成的数据库连接URL不对
    --bug=1010933 --user=李玉号
    【系统设置】github#11245，环境配置-数据库配置-oracle驱动，默认生成的数据库连接URL不对，ip 前少了个 @
    https://www.tapd.cn/55049933/s/1114988
    
    Closes #11245

--- frontend/src/business/components/api/definition/components/request/database/DatabaseFrom.vue
+++ frontend/src/business/components/api/definition/components/request/database/DatabaseFrom.vue
@@ -141,7 +141,7 @@
           }else if(type === "org.postgresql.Driver"){
             this.currentConfig.dbUrl = "jdbc:postgresql://127.0.0.1:5432/database";
           }else if(type === "oracle.jdbc.OracleDriver"){
-            this.currentConfig.dbUrl = "jdbc:oracle:thin:192.168.2.1:1521:database";
+            this.currentConfig.dbUrl = "jdbc:oracle:thin:@192.168.2.1:1521:database";
           }
         },
       }
