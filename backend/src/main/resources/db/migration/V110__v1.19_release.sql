ef1c0acb833ea30d6618444887140cae1136c6ac
CaptainB <bin@fit2cloud.com>
2022-03-09 17:52:08 +0800

    build: 调整flyway

--- backend/src/main/resources/db/migration/V110__v1.19_release.sql
+++ backend/src/main/resources/db/migration/V110__v1.19_release.sql
@@ -1,6 +1,3 @@
--- module management
-INSERT INTO system_parameter (param_key, param_value, type, sort)
-VALUES ('metersphere.module.workstation', 'ENABLE', 'text', 1);
 
 DROP PROCEDURE IF EXISTS project_api_appl;
 DELIMITER //
