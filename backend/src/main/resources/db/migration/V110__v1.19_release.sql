af14559d2d61b104a1803a027d2f35cab21179f0
guoyuqi <xiaomeinvG@126.com>
2022-03-11 17:21:32 +0800

    perf(修改flyway): 修改flyway
    --user=郭雨琦 修改flyway

--- backend/src/main/resources/db/migration/V110__v1.19_release.sql
+++ backend/src/main/resources/db/migration/V110__v1.19_release.sql
@@ -654,3 +654,16 @@ delete
 from user_group_permission
 where group_id = 'admin'
   and module_id = 'PROJECT_ERROR_REPORT_LIBRARY';
+
+CREATE TABLE IF NOT EXISTS `api_definition_scenario_relevance`
+(
+    `report_id` varchar(50) NOT NULL COMMENT 'ID',
+    PRIMARY KEY (`report_id`),
+    KEY `relevance_report_id_index` (`report_id`)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_general_ci;
+
+ALTER TABLE `api_scenario_report` ADD INDEX update_time_index ( `update_time` );
+
+ALTER TABLE `api_definition_exec_result` ADD INDEX update_time_index ( `create_time` );
+
+ALTER TABLE `api_definition_exec_result` ADD INDEX integrated_report_id_index ( `integrated_report_id` );
