7de2e24254f58fa354b4070713a60dc503757d3d
fit2-zhao <yong.zhao@fit2cloud.com>
2022-03-14 19:10:26 +0800

    fix (接口测试): 修复场景报告路由失效问题

--- frontend/src/business/components/api/automation/report/ApiReportView.vue
+++ frontend/src/business/components/api/automation/report/ApiReportView.vue
@@ -1,37 +1,28 @@
 <template>
-  <ms-api-report :report-id="reportIdValue" :share-id="shareId" :is-share="isShare" :is-plan="isPlanReport" :is-template="false"></ms-api-report>
+  <ms-api-report :report-id="reportIdByPath" :share-id="shareId" :is-share="isShare" :is-plan="isPlanReport" :is-template="false"></ms-api-report>
 </template>
 
 <script>
 import MsApiReport from "@/business/components/api/automation/report/ApiReportDetail";
+
 export default {
   name: "ApiReportView",
   components: {MsApiReport},
   props: {
-    reportId:String,
+    reportId: String,
     isShare: Boolean,
     shareId: String,
     isPlanReport: Boolean,
   },
   computed: {
-    reportIdByPath:function (){
-      if(this.$route){
+    reportIdByPath: function () {
+      if (this.$route) {
         return this.$route.params.reportId
-      }else{
+      } else {
         return null;
       }
     }
   },
-  data(){
-    return {
-      reportIdValue:'',
-    }
-  },
-  created() {
-    if(!this.reportIdValue){
-      this.reportIdValue = this.reportIdByPath ? this.reportIdByPath:this.reportId;
-    }
-  }
 }
 
 </script>
