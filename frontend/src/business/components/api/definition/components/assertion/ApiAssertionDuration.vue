b1689a6bcef7d03f504fd1141711183f7412922c
fit2-zhao <yong.zhao@fit2cloud.com>
2022-03-21 11:24:56 +0800

    style(接口测试): 统一场景步骤二级显示样式
    --bug=1011446 --user=赵勇 【接口测试】场景步骤展开的时候，二级步骤会和一级步骤一样齐 https://www.tapd.cn/55049933/s/1121762

--- frontend/src/business/components/api/definition/components/assertion/ApiAssertionDuration.vue
+++ frontend/src/business/components/api/definition/components/assertion/ApiAssertionDuration.vue
@@ -5,9 +5,9 @@
         <el-input :disabled="isReadOnly" :value="value" v-bind="$attrs" step="100" size="small" type="number" @change="change" @input="input" :min="0"
                   :placeholder="$t('api_test.request.assertions.response_in_time')"/>
       </el-col>
-      <el-col class="assertion-btn">
+      <el-col :class="edit?'assertion-remove-btn': 'assertion-btn'">
         <el-button :disabled="isReadOnly" type="danger" size="mini" icon="el-icon-delete" circle @click="remove" v-if="edit"/>
-        <el-button :disabled="isReadOnly" type="primary" size="small" @click="add" v-else>
+        <el-button :disabled="isReadOnly" type="primary" size="mini" @click="add" v-else>
           {{ $t('api_test.request.assertions.add') }}
         </el-button>
       </el-col>
@@ -17,53 +17,58 @@
 
 <script>
 
-  export default {
-    name: "MsApiAssertionDuration",
+export default {
+  name: "MsApiAssertionDuration",
 
-    props: {
-      value: [Number, String],
-      duration: {},
-      edit: Boolean,
-      callback: Function,
-      isReadOnly: {
-        type: Boolean,
-        default: false
+  props: {
+    value: [Number, String],
+    duration: {},
+    edit: Boolean,
+    callback: Function,
+    isReadOnly: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  methods: {
+    add() {
+      if (this.validate()) {
+        this.duration.value = this.value;
+        this.callback();
       }
     },
+    remove() {
+      this.duration.value = undefined;
 
-    methods: {
-      add() {
-        if (this.validate()) {
-          this.duration.value = this.value;
-          this.callback();
-        }
-      },
-      remove() {
-        this.duration.value = undefined;
-
-      },
-      change(value) {
-        if (this.validate()) {
-          this.$emit('change', value);
-        }
-      },
-      input(value) {
-        this.$emit('input', value);
-      },
-      validate() {
-        if (this.value == '' || Number(this.value) <= 0) {
-          this.$error(this.$t('commons.formatErr'));
-          return false;
-        }
-        return true;
+    },
+    change(value) {
+      if (this.validate()) {
+        this.$emit('change', value);
       }
+    },
+    input(value) {
+      this.$emit('input', value);
+    },
+    validate() {
+      if (this.value == '' || Number(this.value) <= 0) {
+        this.$error(this.$t('commons.formatErr'));
+        return false;
+      }
+      return true;
     }
   }
+}
 </script>
 
 <style scoped>
-  .assertion-btn {
-    text-align: center;
-    width: 40px;
-  }
+.assertion-remove-btn {
+  text-align: center;
+  width: 40px;
+}
+
+.assertion-btn {
+  text-align: center;
+  width: 80px;
+}
 </style>
