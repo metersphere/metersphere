1796aed406d950d95ff376f03baec822fc71b726
chenjianxing <jianxing.chen@fit2cloud.com>
2022-03-25 23:00:59 +0800

    feat: UI前后置操作
    (cherry picked from commit a4f5eab58d3ed4e394f6b32b45554c66e3e0c2f8)

---
+++ frontend/src/business/components/api/definition/components/jmeter/components/ui/ui-command/index.js
@@ -0,0 +1,34 @@
+import HashTreeElement from "@/business/components/api/definition/components/jmeter/hashtree";
+
+export default class MsUiCommand extends HashTreeElement {
+  constructor(options = {}) {
+    super(options);
+    this.type = "MsUiCommand";
+    this.clazzName = "io.metersphere.xpack.ui.hashtree.MsUiCommand";
+
+    this.enable = true;
+    this.resourceId = null;
+    this.index = null;
+
+    this.value = null;
+    this.target = null;
+    this.targets = null;
+    this.valueVO = {};
+    this.targetVO = {};
+
+    this.command = null;
+    this.comment = null;
+    this.description = null;
+
+    this.commandConfig = null;
+    this.hashTree = [];
+    this.coreCommands = [];
+    this.preCommands = [];
+    this.postCommands = [];
+  }
+}
+
+export const schema = {
+  MsUiCommand: MsUiCommand
+}
+
