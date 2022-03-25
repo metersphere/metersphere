b9455c1507523351160bbbe2b322666648f9e875
CaptainB <bin@fit2cloud.com>
2022-03-25 14:06:07 +0800

    refactor(性能测试): 优化性能测试报告页面的批量操作

--- backend/src/main/java/io/metersphere/performance/controller/request/DeleteReportRequest.java
+++ backend/src/main/java/io/metersphere/performance/controller/request/DeleteReportRequest.java
@@ -1,13 +1,13 @@
 package io.metersphere.performance.controller.request;
 
-import lombok.Getter;
-import lombok.Setter;
+import io.metersphere.track.request.testplancase.TestPlanFuncCaseConditions;
+import lombok.Data;
 
 import java.util.List;
 
-@Getter
-@Setter
+@Data
 public class DeleteReportRequest {
-
     private List<String> ids;
+    private String projectId;
+    private TestPlanFuncCaseConditions condition;
 }
