a64a4319b1c273c9fc0f0b8c21ec57e1d0fb41da
zhaoqian <qian.zhao@fit2cloud.com>
2022-03-29 19:48:45 +0800

    fix:azure devops平台创建的bug未同步到metersphere(#ID1010762) fix:github#11072，Azure Devops 提交缺陷 ，不支持上传图片(#ID1010759) feat:tfs 缺陷和需求关联，缺陷能够挂在需求下(ID1004746)

--- backend/src/main/java/io/metersphere/track/issue/IssuesPlatform.java
+++ backend/src/main/java/io/metersphere/track/issue/IssuesPlatform.java
@@ -7,6 +7,7 @@ import io.metersphere.dto.IssueTemplateDao;
 import io.metersphere.dto.UserDTO;
 import io.metersphere.track.dto.DemandDTO;
 import io.metersphere.track.issue.domain.PlatformUser;
+import io.metersphere.track.request.testcase.EditTestCaseRequest;
 import io.metersphere.track.request.testcase.IssuesRequest;
 import io.metersphere.track.request.testcase.IssuesUpdateRequest;
 
@@ -85,4 +86,17 @@ public interface IssuesPlatform {
      * @return Boolean
      */
     Boolean checkProjectExist(String relateId);
+
+    /**
+     * 更新缺陷关联关系
+     * @param request
+     */
+    void removeIssueParentLink(IssuesUpdateRequest request);
+
+    /**
+     * 更新需求与缺陷关联关系
+     *
+     * @param testCase
+     */
+    void updateDemandIssueLink(EditTestCaseRequest testCase, Project project);
 }
