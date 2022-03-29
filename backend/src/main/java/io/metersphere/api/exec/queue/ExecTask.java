0c5dc6c5958919f0b12b2cf6b2c11527aaf7c715
fit2-zhao <yong.zhao@fit2cloud.com>
2022-03-29 14:21:45 +0800

    perf(接口测试): 及时释放部分全局变量
    (cherry picked from commit 24fab2517bdfd2bbeb324a4fb0491374ae3fde0f)

--- backend/src/main/java/io/metersphere/api/exec/queue/ExecTask.java
+++ backend/src/main/java/io/metersphere/api/exec/queue/ExecTask.java
@@ -2,6 +2,7 @@ package io.metersphere.api.exec.queue;
 
 import io.metersphere.api.jmeter.JMeterService;
 import io.metersphere.api.jmeter.JmeterThreadUtils;
+import io.metersphere.cache.JMeterEngineCache;
 import io.metersphere.commons.utils.CommonBeanFactory;
 import io.metersphere.dto.JmeterRunRequestDTO;
 import io.metersphere.utils.LoggerUtil;
@@ -20,11 +21,13 @@ public class ExecTask implements Runnable {
     @Override
     public void run() {
         LoggerUtil.info("开始执行报告ID：【 " + request.getReportId() + " 】,资源ID【 " + request.getTestId() + " 】");
-        JMeterService jMeterService = CommonBeanFactory.getBean(JMeterService.class);
-        jMeterService.addQueue(request);
+        CommonBeanFactory.getBean(JMeterService.class).addQueue(request);
         Object res = PoolExecBlockingQueueUtil.take(request.getReportId());
         if (res == null && !JmeterThreadUtils.isRunning(request.getReportId(), request.getTestId())) {
             LoggerUtil.info("执行报告：【 " + request.getReportId() + " 】,资源ID【 " + request.getTestId() + " 】执行超时");
+            if (JMeterEngineCache.runningEngine.containsKey(request.getReportId())) {
+                JMeterEngineCache.runningEngine.remove(request.getReportId());
+            }
         }
         LoggerUtil.info("任务：【 " + request.getReportId() + " 】执行完成");
     }
