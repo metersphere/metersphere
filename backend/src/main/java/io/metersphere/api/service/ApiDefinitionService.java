5caaf074a1986b69e553a43504965e279f1989a3
song-tianyang <tianyang.song@fit2cloud.com>
2022-03-31 12:32:54 +0800

    refactor(接口测试): 优化接口定义列表按照用例数排序时字段解析为字符串而非数字的问题
    优化接口定义列表按照用例数排序时字段解析为字符串而非数字的问题
    
    (cherry picked from commit 8e41fe86cf60c950b38672967a50e069ed311da5)

--- backend/src/main/java/io/metersphere/api/service/ApiDefinitionService.java
+++ backend/src/main/java/io/metersphere/api/service/ApiDefinitionService.java
@@ -1510,7 +1510,7 @@ public class ApiDefinitionService {
                     }
                 } else {
                     res.setCaseType("apiCase");
-                    res.setCaseTotal("-");
+                    res.setCaseTotal("0");
                     res.setCasePassingRate("-");
                     res.setCaseStatus("-");
                 }
