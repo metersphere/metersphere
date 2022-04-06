0cb01740c5015754f6c514e76b28c2c6b42a68ff
CaptainB <bin@fit2cloud.com>
2022-04-06 19:01:15 +0800

    build: 删除无用的类
    (cherry picked from commit 5e4da873598994c01e0d25d1192b1ac00ae0e5ab)

--- backend/src/main/java/io/metersphere/api/mock/utils/MockScriptEngineUtils.java
+++ backend/src/main/java/io/metersphere/api/mock/utils/MockScriptEngineUtils.java
@@ -13,7 +13,6 @@ import javax.script.ScriptEngineManager;
 import java.io.File;
 import java.lang.reflect.Method;
 import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
