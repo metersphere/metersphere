15035c46ee0c1bc5e7d25d4f25432133e24b6e89
guoyuqi <xiaomeinvG@126.com>
2022-04-06 19:37:38 +0800

    fix(接口测试): flyway
    --user=郭雨琦
    
    (cherry picked from commit 382aa6249d7b9819e8130615916af71c9a49995f)

--- backend/src/main/resources/db/migration/V112__v1.19.2_release.sql
+++ backend/src/main/resources/db/migration/V112__v1.19.2_release.sql
@@ -1,2 +1,16 @@
 ALTER TABLE test_plan
     MODIFY name VARCHAR(128) NOT NULL COMMENT 'Plan name';
+
+DROP PROCEDURE IF EXISTS schema_change_api;
+DELIMITER //
+CREATE PROCEDURE schema_change_api() BEGIN
+    DECLARE  CurrentDatabase VARCHAR(100);
+    SELECT DATABASE() INTO CurrentDatabase;
+    IF NOT EXISTS (SELECT * FROM information_schema.statistics WHERE table_schema=CurrentDatabase AND table_name = 'test_plan_test_case' AND index_name = 'plan_id_index') THEN
+        ALTER TABLE `test_plan_test_case` ADD INDEX plan_id_index ( `plan_id` );
+    END IF;
+END//
+DELIMITER ;
+CALL schema_change_api();
+
+
