5022c07117a148bbb38db3ea9a95238b46e43283
zhaoqian <qian.zhao@fit2cloud.com>
2022-04-07 18:22:34 +0800

    fix(接口测试): 第三方平台 禅道 & azure devops 缺陷同步问题
    --bug=1010929 --user=zhaoqian
    禅道：
    [缺陷管理]github#11147metersphere对接禅道企业版v9.0.3提交带有图片的缺陷同步至禅道后图片打不开
    AzureDevops:
    1、缺陷同步信息丢失
    2、在测试计划的功能用例下新建缺陷（用例已关联TFS需求），推回TFS后仍是游离缺陷，没有挂在用户故事下
    3、在测试计划的功能用例下变更需求，再次打开需求被置空且azure devops平台的需求与缺陷的关联关系没有变更
    4、删除用例时，删除用例与需求的关联关系
    5、功能用例关联TFS用户故事时仅第一次会同步，后续如果功能用例名称修改或者再次关联，就不会同步
    6、新增缺陷图片上传后，将图片信息添加到tfs描述信息中
    7、同步超链接图片未登陆第三方平台时，图片无法预览的问题
    
    Closes #11147

--- backend/src/main/java/io/metersphere/track/service/TestCaseService.java
+++ backend/src/main/java/io/metersphere/track/service/TestCaseService.java
@@ -215,20 +215,18 @@ public class TestCaseService {
         return request;
     }
 
-    private void addDemandHyperLink(EditTestCaseRequest request, String type) {
-        if (StringUtils.isNotEmpty(request.getDemandId())) {
-            IssuesRequest updateRequest = new IssuesRequest();
-            updateRequest.setId(request.getId());
-            updateRequest.setResourceId(request.getDemandId());
-            updateRequest.setProjectId(request.getProjectId());
-            updateRequest.setTestCaseId(request.getId());
-            Project project = projectService.getProjectById(request.getProjectId());
-            updateRequest.setWorkspaceId(project.getWorkspaceId());
-            List<AbstractIssuePlatform> platformList = getAddPlatforms(updateRequest);
-            platformList.forEach(platform -> {
-                platform.updateDemandHyperLink(request, project, type);
-            });
-        }
+    public void addDemandHyperLink(EditTestCaseRequest request, String type) {
+        IssuesRequest updateRequest = new IssuesRequest();
+        updateRequest.setId(request.getId());
+        updateRequest.setResourceId(request.getDemandId());
+        updateRequest.setProjectId(request.getProjectId());
+        updateRequest.setTestCaseId(request.getId());
+        Project project = projectService.getProjectById(request.getProjectId());
+        updateRequest.setWorkspaceId(project.getWorkspaceId());
+        List<AbstractIssuePlatform> platformList = getAddPlatforms(updateRequest);
+        platformList.forEach(platform -> {
+            platform.updateDemandHyperLink(request, project, type);
+        });
     }
 
     private List<AbstractIssuePlatform> getAddPlatforms(IssuesRequest request) {
@@ -352,7 +350,7 @@ public class TestCaseService {
      * 判断azure devops用例关联的需求是否发生变更，若发生变更，则重新建立需求与缺陷的关联关系
      * @param testCase
      */
-    private void updateThirdPartyIssuesLink(EditTestCaseRequest testCase) {
+    public void updateThirdPartyIssuesLink(EditTestCaseRequest testCase) {
         try {
             if (Class.forName("io.metersphere.xpack.issue.service.XpackIssueService") != null) {
                 Class clazz = Class.forName("io.metersphere.xpack.issue.service.XpackIssueService");
@@ -596,6 +594,15 @@ public class TestCaseService {
         testCase.setId(testCaseId);
         testCase.setDeleteUserId(SessionUtils.getUserId());
         testCase.setDeleteTime(System.currentTimeMillis());
+
+        // 同步删除用例与需求的关联关系
+        TestCaseWithBLOBs testCaseWithBLOBs = testCaseMapper.selectByPrimaryKey(testCaseId);
+        if (testCaseWithBLOBs != null) {
+            EditTestCaseRequest request = new EditTestCaseRequest();
+            BeanUtils.copyBean(request, testCaseWithBLOBs);
+            addDemandHyperLink(request, "delete");
+        }
+
         return extTestCaseMapper.deleteToGc(testCase);
     }
 
