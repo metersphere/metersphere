bfcc2662f791dc5043084fdd1679cbf69fb92737
guoyuqi <xiaomeinvG@126.com>
2022-04-07 16:32:43 +0800

    feat(测试跟踪): 新增批量执行测试计划功能
    --user=郭雨琦
    
    (cherry picked from commit 429ac0ff4173d2d936fedc1b7e644421572d544d)

--- backend/src/main/java/io/metersphere/base/domain/TestPlanWithBLOBs.java
+++ backend/src/main/java/io/metersphere/base/domain/TestPlanWithBLOBs.java
@@ -15,7 +15,7 @@ public class TestPlanWithBLOBs extends TestPlan implements Serializable {
 
     private String reportConfig;
 
-    private String request;
+    private String runModeConfig;
 
     private static final long serialVersionUID = 1L;
 }
