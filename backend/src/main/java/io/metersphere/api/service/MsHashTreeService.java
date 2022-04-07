d2ac8ae5c626a16c28d52066eebfca90e2146383
fit2-zhao <yong.zhao@fit2cloud.com>
2022-04-07 14:42:01 +0800

    perf(接口测试): 保存场景优化

--- backend/src/main/java/io/metersphere/api/service/MsHashTreeService.java
+++ backend/src/main/java/io/metersphere/api/service/MsHashTreeService.java
@@ -34,32 +34,33 @@ public class MsHashTreeService {
     @Resource
     private ExtApiScenarioMapper extApiScenarioMapper;
 
-    private static final String CASE = "CASE";
-    private static final String REFERENCED = "referenced";
-    private static final String REF = "REF";
-    private static final String REF_TYPE = "refType";
-    private static final String ID = "id";
-    private static final String NAME = "name";
-    private static final String SCENARIO = "scenario";
-    private static final String TYPE = "type";
-    private static final String HASH_TREE = "hashTree";
-    private static final String PATH = "path";
-    private static final String METHOD = "method";
-    private static final String ENABLE = "enable";
-    private static final String NUM = "num";
-    private static final String ENV_ENABLE = "environmentEnable";
-    private static final String VARIABLE_ENABLE = "variableEnable";
-    private static final String DISABLED = "disabled";
-    private static final String VERSION_NAME = "versionName";
-    private static final String VERSION_ENABLE = "versionEnable";
-    private static final String URL = "url";
-    private static final String HEADERS = "headers";
-    private static final String REST = "rest";
-    private static final String BODY = "body";
-    private static final String ARGUMENTS = "arguments";
-    private static final String AUTH_MANAGER = "authManager";
-    private static final String PROJECT_ID = "projectId";
-    private static final String ACTIVE = "active";
+    public static final String CASE = "CASE";
+    public static final String REFERENCED = "referenced";
+    public static final String REF = "REF";
+    public static final String CREATED = "Created";
+    public static final String REF_TYPE = "refType";
+    public static final String ID = "id";
+    public static final String NAME = "name";
+    public static final String SCENARIO = "scenario";
+    public static final String TYPE = "type";
+    public static final String HASH_TREE = "hashTree";
+    public static final String PATH = "path";
+    public static final String METHOD = "method";
+    public static final String ENABLE = "enable";
+    public static final String NUM = "num";
+    public static final String ENV_ENABLE = "environmentEnable";
+    public static final String VARIABLE_ENABLE = "variableEnable";
+    public static final String DISABLED = "disabled";
+    public static final String VERSION_NAME = "versionName";
+    public static final String VERSION_ENABLE = "versionEnable";
+    public static final String URL = "url";
+    public static final String HEADERS = "headers";
+    public static final String REST = "rest";
+    public static final String BODY = "body";
+    public static final String ARGUMENTS = "arguments";
+    public static final String AUTH_MANAGER = "authManager";
+    public static final String PROJECT_ID = "projectId";
+    public static final String ACTIVE = "active";
 
     public void setHashTree(JSONArray hashTree) {
         // 将引用转成复制
