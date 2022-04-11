78aea6b28bf82987e64c4023339fa0671f732e66
shiziyuan9527 <yuhao.li@fit2cloud.com>
2022-04-11 18:11:14 +0800

    fix(测试跟踪): 默认项目用例模版，新增自定义字段未显示
    --bug=1012082 --user=李玉号 【测试跟踪】-github#12441 默认项目用例模版/缺陷模版。新增自定义字段
    未显示
    https://www.tapd.cn/55049933/s/1132582
    
    Closes #12441
    
    (cherry picked from commit 46fda9c1a0d56b16ef1215fb0e14ad25581ae5b4)

--- backend/src/main/resources/db/migration/V113__v1.19.3_release.sql
+++ backend/src/main/resources/db/migration/V113__v1.19.3_release.sql
@@ -1,2 +1,18 @@
 ALTER TABLE test_plan_report
     MODIFY name varchar(128) NULL COMMENT 'name';
+
+update project set case_template_id = (
+    select id
+    from test_case_template
+    where `global` = 0
+      and `system` = 1
+      and project_id = project.id limit 1
+) where case_template_id is null or case_template_id = '';
+
+update project set case_template_id = (
+    select id
+    from test_case_template
+    where `global` = 1
+      and `system` = 1
+      and project_id = 'global' limit 1
+) where case_template_id is null or case_template_id = '';
