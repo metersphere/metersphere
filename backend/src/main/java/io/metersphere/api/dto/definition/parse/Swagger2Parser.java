199b3c3943384f5f1780c3eaae1acf4e090aa931
guoyuqi <xiaomeinvG@126.com>
2022-04-12 15:57:47 +0800

    fix(接口测试): swagger2.0导入错误
    --user=郭雨琦
    --bug=1012085
    【接口测试】swagger
    导入导出问题
    https://www.tapd.cn/55049933/bugtrace/bugs/view?bug_id=1155049933001012085

--- backend/src/main/java/io/metersphere/api/dto/definition/parse/Swagger2Parser.java
+++ backend/src/main/java/io/metersphere/api/dto/definition/parse/Swagger2Parser.java
@@ -483,9 +483,9 @@ public class Swagger2Parser extends SwaggerAbstractParser {
     private void handleBaseProperties(JsonSchemaItem item, Property value) {
         if (value instanceof StringProperty || value instanceof DateProperty || value instanceof DateTimeProperty ) {
             item.setType("string");
-        } else if (value instanceof IntegerProperty) {
+        } else if (value instanceof IntegerProperty || value instanceof BaseIntegerProperty) {
             item.setType("integer");
-        } else if (value instanceof BooleanProperty) {
+        }else if (value instanceof BooleanProperty) {
             item.setType("boolean");
         } else if (value instanceof LongProperty || value instanceof FloatProperty
                 || value instanceof DecimalProperty || value instanceof DoubleProperty) {
