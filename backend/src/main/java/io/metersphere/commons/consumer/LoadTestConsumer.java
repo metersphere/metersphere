05c3ab596901007b66cb195fb590ba29bb193300
CaptainB <bin@fit2cloud.com>
2022-04-21 18:19:08 +0800

    perf: 去掉反射，使用spring的获取子类的方式

--- backend/src/main/java/io/metersphere/commons/consumer/LoadTestConsumer.java
+++ backend/src/main/java/io/metersphere/commons/consumer/LoadTestConsumer.java
@@ -5,52 +5,29 @@ import io.metersphere.base.domain.LoadTestReport;
 import io.metersphere.commons.utils.CommonBeanFactory;
 import io.metersphere.commons.utils.LogUtil;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.reflections.Reflections;
-import org.reflections.scanners.Scanners;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Set;
-import java.util.concurrent.Executors;
+import java.util.Map;
 
 @Service
 @Transactional(rollbackFor = Exception.class)
-public class LoadTestConsumer implements ApplicationRunner {
+public class LoadTestConsumer {
     public static final String CONSUME_ID = "load-test-data";
 
-    private static Set<Class<? extends LoadTestFinishEvent>> subTypes;
 
     @KafkaListener(id = CONSUME_ID, topics = "${kafka.test.topic}", groupId = "${spring.kafka.consumer.group-id}")
     public void consume(ConsumerRecord<?, String> record) {
         LoadTestReport loadTestReport = JSON.parseObject(record.value(), LoadTestReport.class);
-        Set<Class<? extends LoadTestFinishEvent>> subTypes = getSubTypes();
-        subTypes.forEach(s -> {
+
+        Map<String, LoadTestFinishEvent> subTypes = CommonBeanFactory.getBeansOfType(LoadTestFinishEvent.class);
+        subTypes.forEach((k, t) -> {
             try {
-                CommonBeanFactory.getBean(s).execute(loadTestReport);
+                t.execute(loadTestReport);
             } catch (Exception e) {
                 LogUtil.error(e);
             }
-        });
-    }
-
-    private synchronized Set<Class<? extends LoadTestFinishEvent>> getSubTypes() {
-        if (subTypes != null) {
-            return subTypes;
-        }
-        Reflections reflections = new Reflections("io.metersphere", Scanners.SubTypes);
-        subTypes = reflections.getSubTypesOf(LoadTestFinishEvent.class);
-        return subTypes;
-    }
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        Executors.newSingleThreadExecutor().execute(() -> {
-            LogUtil.info("查询 LoadTestFinishEvent 实现类:");
-            getSubTypes();
-            LogUtil.info("查询 LoadTestFinishEvent 实现类: " + subTypes.size());
         });
     }
 }
