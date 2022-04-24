2a3578041057471ac1802c64946e20dfcd08431a
wxg0103 <727495428@qq.com>
2022-04-24 14:20:28 +0800

    fix(接口测试): 修复接口定义sql协议编辑保存后仍提示未保存的缺陷
    --bug=1012615 --user=王孝刚 【接口测试】接口定义-编辑接口定义后保存成功了，关闭时仍提示未保存。
    https://www.tapd.cn/55049933/s/1145356

--- frontend/src/business/components/api/definition/components/complete/EditCompleteDubboApi.vue
+++ frontend/src/business/components/api/definition/components/complete/EditCompleteDubboApi.vue
@@ -188,6 +188,8 @@ export default {
           this.basisData.tags = JSON.stringify(this.basisData.tags);
         }
         this.$emit('saveApi', this.basisData);
+        this.$store.state.apiStatus.set("fromChange", false);
+        this.$store.state.apiMap.set(this.basisData.id, this.$store.state.apiStatus);
       }
     },
     runTest() {
