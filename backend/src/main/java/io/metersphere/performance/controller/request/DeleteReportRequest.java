3fd4be6de49bd289183b6686fbb0c3b9ea610785
CaptainB <bin@fit2cloud.com>
2022-05-06 14:24:26 +0800

    fix(性能测试): 修复性能测试批量删除时没有把查询条件带上的问题
    --bug=1012991 --user=刘瑞斌 性能测试报告高级搜索后，批量删除全部，会将所的报告全部删除 https://www.tapd.cn/55049933/s/1153361

--- backend/src/main/java/io/metersphere/performance/controller/request/DeleteReportRequest.java
+++ backend/src/main/java/io/metersphere/performance/controller/request/DeleteReportRequest.java
@@ -1,6 +1,5 @@
 package io.metersphere.performance.controller.request;
 
-import io.metersphere.track.request.testplancase.TestPlanFuncCaseConditions;
 import lombok.Data;
 
 import java.util.List;
@@ -9,5 +8,5 @@ import java.util.List;
 public class DeleteReportRequest {
     private List<String> ids;
     private String projectId;
-    private TestPlanFuncCaseConditions condition;
+    private ReportRequest condition;
 }
