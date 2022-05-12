028a3fddb9ea26f8a3e75fdd66c98b34143f1d1a
nathan.liu <yao.liu@fit2cloud.com>
2022-05-12 14:26:53 +0800

    fix(UI自动化): 修改UI设置默认提示地址、调整UI测试tab的顺序

--- frontend/src/business/components/project/menu/appmanage/AppManage.vue
+++ frontend/src/business/components/project/menu/appmanage/AppManage.vue
@@ -39,28 +39,6 @@
               </el-row>
             </el-tab-pane>
 
-            <!-- UI 测试 -->
-            <el-tab-pane v-if="isXpack" :label="$t('commons.ui_test')" name="ui_test" >
-              <el-row style="margin-top: 10px">
-                <span style="font-weight:bold">{{ $t('commons.view_settings') }}</span>
-              </el-row>
-              <el-row style="margin-top: 15px">
-                <app-manage-item :title="$t('ui.ui_debug_mode')"
-                                 :append-span="12" :prepend-span="12" :middle-span="0">
-                  <template #append>
-                    <el-radio-group v-model="config.uiQuickMenu" @change="switchChange('UI_QUICK_MENU', $event)">
-                      <el-radio label="local" value="local">
-                        {{ $t('ui.ui_local_debug') }}
-                      </el-radio>
-                      <el-radio label="server" value="server">
-                        {{ $t('ui.ui_server_debug') }}
-                      </el-radio>
-                    </el-radio-group>
-                  </template>
-                </app-manage-item>
-              </el-row>
-            </el-tab-pane>
-
             <el-tab-pane :label="$t('commons.api')" name="api">
               <el-row :gutter="20">
                 <el-col :span="8">
@@ -120,6 +98,28 @@
                     </app-manage-item>
                   </el-row>
                 </el-col>
+              </el-row>
+            </el-tab-pane>
+            
+            <!-- UI 测试 -->
+            <el-tab-pane v-if="isXpack" :label="$t('commons.ui_test')" name="ui_test" >
+              <el-row style="margin-top: 10px">
+                <span style="font-weight:bold">{{ $t('commons.view_settings') }}</span>
+              </el-row>
+              <el-row style="margin-top: 15px">
+                <app-manage-item :title="$t('ui.ui_debug_mode')"
+                                 :append-span="12" :prepend-span="12" :middle-span="0">
+                  <template #append>
+                    <el-radio-group v-model="config.uiQuickMenu" @change="switchChange('UI_QUICK_MENU', $event)">
+                      <el-radio label="local" value="local">
+                        {{ $t('ui.ui_local_debug') }}
+                      </el-radio>
+                      <el-radio label="server" value="server">
+                        {{ $t('ui.ui_server_debug') }}
+                      </el-radio>
+                    </el-radio-group>
+                  </template>
+                </app-manage-item>
               </el-row>
             </el-tab-pane>
 
