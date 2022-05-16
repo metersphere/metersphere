1782b2d5b21596191859ae6afdb1e917c98fc353
CaptainB <bin@fit2cloud.com>
2022-05-16 14:28:09 +0800

    refactor(消息通知): 优化消息通知配置

--- frontend/src/business/components/api/automation/schedule/ScheduleNotification.vue
+++ frontend/src/business/components/api/automation/schedule/ScheduleNotification.vue
@@ -10,108 +10,12 @@
     </el-row>
     <el-row>
       <el-col :span="24">
-        <el-table
-          :data="scheduleTask"
-          class="tb-edit"
-          border
-          :cell-style="rowClass"
-          :header-cell-style="headClass">
-          <el-table-column :label="$t('schedule.event')" prop="events" min-width="13%">
-            <template slot-scope="scope">
-              <el-select v-model="scope.row.event" size="mini"
-                         :placeholder="$t('organization.message.select_events')"
-                         prop="events" :disabled="!scope.row.isSet">
-                <el-option
-                  v-for="item in scheduleEventOptions"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value">
-                </el-option>
-              </el-select>
-            </template>
-          </el-table-column>
-          <el-table-column :label="$t('schedule.receiver')" prop="userIds" min-width="18%">
-            <template v-slot:default="{row}">
-              <el-select v-model="row.userIds" filterable multiple size="mini"
-                         :placeholder="$t('commons.please_select')" style="width: 100%;" :disabled="!row.isSet">
-                <el-option
-                  v-for="item in scheduleReceiverOptions"
-                  :key="item.id"
-                  :label="item.name"
-                  :value="item.id">
-                </el-option>
-              </el-select>
-            </template>
-          </el-table-column>
-          <el-table-column :label="$t('schedule.receiving_mode')" prop="type" min-width="15%">
-            <template slot-scope="scope">
-              <el-select v-model="scope.row.type" :placeholder="$t('organization.message.select_receiving_method')"
-                         size="mini"
-                         :disabled="!scope.row.isSet" @change="handleEdit(scope.$index, scope.row)"
-              >
-                <el-option
-                  v-for="item in receiveTypeOptions"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value">
-                </el-option>
-              </el-select>
-            </template>
-          </el-table-column>
-          <el-table-column label="webhook" min-width="20%" prop="webhook">
-            <template v-slot:default="scope">
-              <el-input v-model="scope.row.webhook" size="mini"
-                        :disabled="!scope.row.isSet||!scope.row.isReadOnly"></el-input>
-            </template>
-          </el-table-column>
-          <el-table-column :label="$t('commons.operating')" prop="result" min-width="25%">
-            <template v-slot:default="scope">
-              <ms-tip-button
-                circle
-                type="success"
-                size="mini"
-                v-if="scope.row.isSet"
-                v-xpack
-                @click="handleTemplate(scope.$index,scope.row)"
-                :tip="$t('organization.message.template')"
-                icon="el-icon-tickets"/>
-              <ms-tip-button
-                circle
-                type="primary"
-                size="mini"
-                v-show="scope.row.isSet"
-                @click="handleAddTask(scope.$index,scope.row)"
-                :tip="$t('commons.add')"
-                icon="el-icon-check"/>
-              <ms-tip-button
-                circle
-                size="mini"
-                v-show="scope.row.isSet"
-                @click="removeRowTask(scope.$index,scheduleTask)"
-                :tip="$t('commons.cancel')"
-                icon="el-icon-refresh-left"/>
-              <ms-tip-button
-                el-button
-                circle
-                type="primary"
-                size="mini"
-                icon="el-icon-edit"
-                v-show="!scope.row.isSet"
-                :tip="$t('commons.edit')"
-                @click="handleEditTask(scope.$index,scope.row)"
-                v-permission="['PROJECT_MESSAGE:READ+EDIT']"/>
-              <ms-tip-button
-                circle
-                type="danger"
-                icon="el-icon-delete"
-                size="mini"
-                v-show="!scope.row.isSet"
-                @click="deleteRowTask(scope.$index,scope.row)"
-                :tip="$t('commons.delete')"
-                v-permission="['PROJECT_MESSAGE:READ+EDIT']"/>
-            </template>
-          </el-table-column>
-        </el-table>
+        <notification-table :table-data="scheduleTask"
+                            :event-options="scheduleEventOptions"
+                            :receive-type-options="receiveTypeOptions"
+                            @handleReceivers="handleReceivers"
+                            @handleTemplate="handleTemplate"
+                            @refresh="initForm"/>
       </el-col>
     </el-row>
     <notice-template v-xpack ref="noticeTemplate" :variables="variables"/>
@@ -122,6 +26,7 @@
 import {hasLicense} from "@/common/js/utils";
 import MsCodeEdit from "@/business/components/api/definition/components/MsCodeEdit";
 import MsTipButton from "@/business/components/common/components/MsTipButton";
+import NotificationTable from "@/business/components/project/notification/NotificationTable";
 
 const requireComponent = require.context('@/business/components/xpack/', true, /\.vue$/);
 const noticeTemplate = requireComponent.keys().length > 0 ? requireComponent("./notice/NoticeTemplate.vue") : {};
@@ -129,6 +34,7 @@ const noticeTemplate = requireComponent.keys().length > 0 ? requireComponent("./
 export default {
   name: "ScheduleNotification",
   components: {
+    NotificationTable,
     MsTipButton,
     MsCodeEdit,
     "NoticeTemplate": noticeTemplate.default
@@ -162,9 +68,7 @@ export default {
       ],
       receiveTypeOptions: [
         {value: 'EMAIL', label: this.$t('organization.message.mail')},
-        {value: 'NAIL_ROBOT', label: this.$t('organization.message.nail_robot')},
-        {value: 'WECHAT_ROBOT', label: this.$t('organization.message.enterprise_wechat_robot')},
-        {value: 'LARK', label: this.$t('organization.message.lark')}
+        {value: 'WEBHOOK', label: 'Webhook'},
       ],
       variables: [
         {
@@ -310,13 +214,6 @@ export default {
         this.scheduleTask = response.data;
       });
     },
-    handleEdit(index, data) {
-      data.isReadOnly = true;
-      if (data.type === 'EMAIL') {
-        data.isReadOnly = !data.isReadOnly;
-        data.webhook = '';
-      }
-    },
     handleAddTaskModel() {
       let Task = {};
       Task.event = '';
@@ -328,57 +225,6 @@ export default {
       Task.taskType = 'SCHEDULE_TASK';
       Task.testId = this.testId;
       this.scheduleTask.unshift(Task);
-    },
-    handleEditTask(index, data) {
-      data.isSet = true;
-      data.testId = this.testId;
-      if (data.type === 'EMAIL') {
-        data.isReadOnly = false;
-        data.webhook = '';
-      } else {
-        data.isReadOnly = true;
-      }
-    },
-    handleAddTask(index, data) {
-      if (data.event && data.userIds.length > 0 && data.type) {
-        // console.log(data.type)
-        if (data.type === 'NAIL_ROBOT' || data.type === 'WECHAT_ROBOT' || data.type === 'LARK') {
-          if (!data.webhook) {
-            this.$warning(this.$t('organization.message.message_webhook'));
-          } else {
-            this.addTask(data);
-          }
-        } else {
-          this.addTask(data);
-        }
-      } else {
-        this.$warning(this.$t('organization.message.message'));
-      }
-    },
-    addTask(data) {
-      this.result = this.$post("/notice/save/message/task", data, () => {
-        this.initForm();
-        this.$success(this.$t('commons.save_success'));
-      });
-    },
-    removeRowTask(index, data) { //移除
-      if (!data[index].identification) {
-        data.splice(index, 1);
-      } else {
-        data[index].isSet = false;
-      }
-    },
-    deleteRowTask(index, data) { //删除
-      this.result = this.$get("/notice/delete/message/" + data.identification, response => {
-        this.$success(this.$t('commons.delete_success'));
-        this.initForm();
-      });
-    },
-    rowClass() {
-      return "text-align:center";
-    },
-    headClass() {
-      return "text-align:center;background:'#ededed'";
     },
     handleTemplate(index, row) {
       if (hasLicense()) {
@@ -395,6 +241,9 @@ export default {
         }
         this.$refs.noticeTemplate.open(row, robotTemplate);
       }
+    },
+    handleReceivers(row) {
+      row.receiverOptions = JSON.parse(JSON.stringify(this.scheduleReceiverOptions));
     }
   }
 };
