7ce6aa1bdcfedd62bb17a673cca24465f057e585
CaptainB <bin@fit2cloud.com>
2022-05-18 15:55:41 +0800

    refactor: 为序列化对象增加 serialVersionUID

--- backend/src/main/java/io/metersphere/dto/GroupResourceDTO.java
+++ backend/src/main/java/io/metersphere/dto/GroupResourceDTO.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 @Data
 public class GroupResourceDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private GroupResource resource;
     private List<GroupPermission> permissions;
     private String type;
