521b7f8a691859ff764db91770e739dcae3442e2
song-tianyang <tianyang.song@fit2cloud.com>
2022-05-27 17:20:16 +0800

    refactor(接口测试): 修复场景步骤中含有302内容数据时无法远程异步加载步骤内容的问题
    修复场景步骤中含有302内容数据时无法远程异步加载步骤内容的问题

--- frontend/src/business/components/api/automation/report/components/RequestResult.vue
+++ frontend/src/business/components/api/automation/report/components/RequestResult.vue
@@ -234,7 +234,7 @@ export default {
   },
   methods: {
     loadRequestInfoExpand() {
-      if( !this.request.responseResult || (!this.request.responseResult.body && this.request.responseResult.body === null)){
+      if( !this.request.responseResult || (!this.request.responseResult.body && (this.request.responseResult.body === null || this.request.responseResult.body === undefined))){
         this.$get("/api/scenario/report/selectReportContent/" + this.stepId, response => {
           let requestResult = response.data;
           if (requestResult) {
