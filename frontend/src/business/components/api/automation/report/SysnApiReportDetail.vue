c0960b5ab6a21bd3ae044778a3b6304f3d22eb5d
song-tianyang <tianyang.song@fit2cloud.com>
2022-06-01 15:07:56 +0800

    fix(接口测试): 【接口测试】同一个场景，调试的报告和测试报告详情，两边结果不一致
    --bug=1013740 --user=宋天阳 【接口测试】同一个场景，调试的报告和测试报告详情，两边结果不一致
    https://www.tapd.cn/55049933/s/1171409

--- frontend/src/business/components/api/automation/report/SysnApiReportDetail.vue
+++ frontend/src/business/components/api/automation/report/SysnApiReportDetail.vue
@@ -308,7 +308,8 @@ export default {
             this.content = {scenarios: []};
           }
           this.content.error = this.content.error;
-          this.content.success = (this.content.total - this.content.error - this.content.errorCode);
+
+          this.content.success = (this.content.total - this.content.error - this.content.errorCode - this.content.unExecute);
           this.totalTime = this.content.totalTime;
           this.fullTreeNodes = this.content.steps;
           this.recursiveSorting(this.fullTreeNodes);
