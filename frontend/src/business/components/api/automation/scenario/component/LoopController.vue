6660bb98d8c47e4a27aced22ed88d1370b9d089d
fit2-zhao <yong.zhao@fit2cloud.com>
2022-06-01 17:22:19 +0800

    refactor(接口测试): 次数循环控制器支持变量方式
    --bug=1013431 --user=赵勇 【接口测试】github#5872循环控制器的次数不支持变量 https://www.tapd.cn/55049933/s/1171792

--- frontend/src/business/components/api/automation/scenario/component/LoopController.vue
+++ frontend/src/business/components/api/automation/scenario/component/LoopController.vue
@@ -23,7 +23,7 @@
         <el-row>
           <el-col :span="8">
             <span class="ms-span ms-radio">{{ $t('loop.loops') }}</span>
-            <el-input-number size="small" v-model="controller.countController.loops" :placeholder="$t('commons.millisecond')" :max="100000000" :min="0"/>
+            <el-input size="small" v-model="controller.countController.loops" :placeholder="$t('commons.millisecond')" style="width: 200px"/>
             <span class="ms-span ms-radio">次</span>
           </el-col>
           <el-col :span="8">
