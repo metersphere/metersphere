461fa680a1309e33da41a999e8504621e861e536
song-tianyang <tianyang.song@fit2cloud.com>
2022-06-02 17:41:28 +0800

    fix(接口测试): 接口文档增加接口备注的字段展示
    --bug=1012730 --user=宋天阳
    [接口定义]github#13003接口字段备注只有打开编辑状态可以看到备注，文档中或者分享页面都看不到接口的字段备注
    https://www.tapd.cn/55049933/s/1172859

---
+++ frontend/src/business/components/api/definition/components/document/components/ApiRemarkShow.vue
@@ -0,0 +1,73 @@
+<template>
+  <div v-if="remarkObj">
+    <el-row class="apiInfoRow">
+      <div class="blackFontClass">
+        <el-row>
+          <div class="tip" style="float: left">
+            <span>{{$t("commons.remark")}}</span>
+          </div>
+        </el-row>
+      </div>
+    </el-row>
+    <el-row>
+      <el-form>
+        <form-rich-text-item class="remark-item" :disabled="true" :data="remarkObj" prop="remark" label-width="0"/>
+      </el-form>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import formRichTextItem from "@/business/components/track/case/components/FormRichTextItem";
+export default {
+  name: "ApiRemarkShow",
+  components: {formRichTextItem},
+  data() {
+    return {
+      active: true,
+      remarkObj:null
+    };
+  },
+  props: {
+    data: String,
+  },
+  created() {
+  },
+  activated() {
+  },
+  mounted() {
+  },
+  computed: {},
+  watch: {
+    data(){
+      if(this.data){
+        this.remarkObj = {
+          "remark":this.data,
+        };
+      }
+    }
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+.apiInfoRow {
+  margin: 10px 10px;
+}
+
+.apiInfoRow.el-row {
+  margin: 10px 10px;
+}
+
+.simpleFontClass {
+  font-weight: normal;
+  font-size: 14px;
+  margin-left: 10px;
+}
+
+.blackFontClass {
+  font-weight: bold;
+  font-size: 14px;
+}
+</style>
