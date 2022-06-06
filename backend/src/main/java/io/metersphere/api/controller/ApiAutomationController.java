42913533d29fdb53c471c0b263e12997b2717405
fit2-zhao <yong.zhao@fit2cloud.com>
2022-06-06 15:29:13 +0800

    refactor(接口测试): 场景调试失败后及时恢复调试状态和预加载调试数据

--- backend/src/main/java/io/metersphere/api/controller/ApiAutomationController.java
+++ backend/src/main/java/io/metersphere/api/controller/ApiAutomationController.java
@@ -198,12 +198,17 @@ public class ApiAutomationController {
 
     @PostMapping(value = "/run/debug")
     @MsAuditLog(module = OperLogModule.API_AUTOMATION, type = OperLogConstants.DEBUG, title = "#request.scenarioName", sourceId = "#request.scenarioId", project = "#request.projectId")
-    public void runDebug(@RequestPart("request") RunDefinitionRequest request,
-                         @RequestPart(value = "bodyFiles", required = false) List<MultipartFile> bodyFiles, @RequestPart(value = "scenarioFiles", required = false) List<MultipartFile> scenarioFiles) {
-        if (StringUtils.isEmpty(request.getExecuteType())) {
-            request.setExecuteType(ExecuteType.Debug.name());
+    public String runDebug(@RequestPart("request") RunDefinitionRequest request,
+                           @RequestPart(value = "bodyFiles", required = false) List<MultipartFile> bodyFiles, @RequestPart(value = "scenarioFiles", required = false) List<MultipartFile> scenarioFiles) {
+        try {
+            if (StringUtils.isEmpty(request.getExecuteType())) {
+                request.setExecuteType(ExecuteType.Debug.name());
+            }
+            apiAutomationService.debugRun(request, bodyFiles, scenarioFiles);
+        } catch (Exception e) {
+            return e.getMessage();
         }
-        apiAutomationService.debugRun(request, bodyFiles, scenarioFiles);
+        return "SUCCESS";
     }
 
     @PostMapping(value = "/run")
