0efbe57968cea94aa3fea42a13fdd03c74cd3e51
zhaoqian <qian.zhao@fit2cloud.com>
2022-06-08 15:51:42 +0800

    fix(缺陷管理): 缺陷管理列表平台状态字段无法编辑问题处理
    --bug=1012113
    --user=zhaoqian
    【缺陷列表】【平台状态】字段无法编辑，且ms字段与第三方平台字段重复
    https://www.tapd.cn/55049933/bugtrace/bugs/view/1155049933001012113

---
+++ backend/src/main/java/io/metersphere/track/dto/PlatformStatusDTO.java
@@ -0,0 +1,13 @@
+package io.metersphere.track.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+public class PlatformStatusDTO {
+    protected String value;
+    protected String lable;
+}
