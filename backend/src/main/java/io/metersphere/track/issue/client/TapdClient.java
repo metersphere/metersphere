71b44661ffb2f886a2012d8c15e96f3001cd5d3a
shiziyuan9527 <yuhao.li@fit2cloud.com>
2022-06-09 15:35:04 +0800

    fix(测试跟踪): 需求查询时间过长
    --bug=1010280 --user=李玉号 点击关联需求响应时间过长
    https://www.tapd.cn/55049933/s/1177728

--- backend/src/main/java/io/metersphere/track/issue/client/TapdClient.java
+++ backend/src/main/java/io/metersphere/track/issue/client/TapdClient.java
@@ -81,7 +81,7 @@ public class TapdClient extends BaseClient {
     }
 
     public JSONArray getDemands(String projectId) {
-        String url = getBaseUrl() + "/stories?workspace_id={1}&limit=200";
+        String url = getBaseUrl() + "/stories?workspace_id={1}&limit=200&fields=id,name";
         ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, getAuthHttpEntity(), String.class, projectId);
         return JSONArray.parseObject(response.getBody()).getJSONArray("data");
     }
