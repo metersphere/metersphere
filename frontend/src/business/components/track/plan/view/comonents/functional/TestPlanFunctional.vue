8416418725ec4709fd7b25d2e7da56b2359dbfaa
chenjianxing <jianxing.chen@fit2cloud.com>
2022-06-10 11:04:05 +0800

    refactor(测试跟踪): 导入用例前提示保存脑图
    --bug=1013915 --user=陈建星 【测试跟踪】脑图模式下，导入xmind用例页面没有刷新 https://www.tapd.cn/55049933/s/1178369

--- frontend/src/business/components/track/plan/view/comonents/functional/TestPlanFunctional.vue
+++ frontend/src/business/components/track/plan/view/comonents/functional/TestPlanFunctional.vue
@@ -49,8 +49,6 @@
       ref="testCaseRelevance"/>
 
     <is-change-confirm
-      :title="'请保存脑图'"
-      :tip="'脑图未保存，确认保存脑图吗？'"
       @confirm="changeConfirm"
       ref="isChangeConfirm"/>
   </ms-test-plan-common-component>
