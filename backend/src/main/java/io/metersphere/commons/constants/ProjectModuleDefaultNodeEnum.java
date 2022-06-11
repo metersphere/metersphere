ade9f5f3f6314a20988b06c81f20842a190741bb
songcc <changchang.song@fit2cloud.com>
2022-06-11 14:48:56 +0800

    fix(测试跟踪): 项目下模块默认节点创建重复的问题
    --bug=1013696 --user=宋昌昌 【测试跟踪】github#14110  升级导致模块未规划用例重复问题 https://www.tapd.cn/55049933/s/1179384

---
+++ backend/src/main/java/io/metersphere/commons/constants/ProjectModuleDefaultNodeEnum.java
@@ -0,0 +1,41 @@
+package io.metersphere.commons.constants;
+
+/**
+ * 项目模块默认节点
+ * @author song-cc
+ */
+public enum ProjectModuleDefaultNodeEnum {
+
+    /**
+     * 测试用例默认节点
+     */
+    TEST_CASE_DEFAULT_NODE("未规划用例", "test_case_node"),
+    /**
+     * 接口默认节点
+     */
+    API_MODULE_DEFAULT_NODE("未规划接口", "api_module"),
+    /**
+     * 接口场景默认节点
+     */
+    API_SCENARIO_DEFAULT_NODE("未规划场景", "api_scenario_module"),
+    /**
+     * 接口场景默认节点
+     */
+    UI_SCENARIO_DEFAULT_NODE("未规划场景", "ui_scenario_module");
+
+    private String nodeName;
+    private String tableName;
+
+    ProjectModuleDefaultNodeEnum(String nodeName, String tableName) {
+        this.nodeName = nodeName;
+        this.tableName = tableName;
+    }
+
+    public String getNodeName() {
+        return nodeName;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+}
