34f01f25c3768e5858488d2c761444848a23f3ce
fit2-zhao <yong.zhao@fit2cloud.com>
2022-06-13 18:20:33 +0800

    fix(接口测试): 修复首页Swagger定时任务跳转问题
    --bug=1014036 --user=赵勇 【接口定义】首页跳转swagger定时同步后，关闭swagger信息，接口定义列表不显示数据 https://www.tapd.cn/55049933/s/1180252

--- frontend/src/business/components/api/definition/components/list/ApiList.vue
+++ frontend/src/business/components/api/definition/components/list/ApiList.vue
@@ -119,7 +119,7 @@
           :field="item"
           :fields-width="fieldsWidth"
           min-width="80px"
-          :show-overflow-tooltip = false
+          :show-overflow-tooltip=false
           :label="$t('commons.tag')">
           <template v-slot:default="scope">
             <el-tooltip class="item" effect="dark" placement="top">
@@ -440,7 +440,7 @@ export default {
       selectDataCounts: 0,
       projectName: "",
       versionEnable: false,
-      isFirstInitTable:true,
+      isFirstInitTable: true,
     };
   },
   props: {
@@ -647,9 +647,9 @@ export default {
         });
       }
       if (this.needRefreshModule()) {
-        if(this.isFirstInitTable){
+        if (this.isFirstInitTable) {
           this.isFirstInitTable = false;
-        }else {
+        } else {
           this.$emit("refreshTree");
         }
       }
@@ -887,12 +887,10 @@ export default {
     getSelectDataRange() {
       let dataRange = this.$route.params.dataSelectRange;
       let dataType = this.$route.params.dataType;
-      if (dataType === 'api') {
-        this.selectDataRange = dataRange;
-      } else {
-        this.selectDataRange = 'all';
-      }
-      if (this.selectDataRange != null) {
+      this.selectDataRange = dataType === 'api' ? dataRange : "all";
+      if (this.selectDataRange &&
+        Object.prototype.toString.call(this.selectDataRange).match(/\[object (\w+)\]/)[1].toLowerCase() !== 'object'
+        && this.selectDataRange.indexOf(":") !== -1) {
         let selectParamArr = this.selectDataRange.split(":");
         if (selectParamArr.length === 2) {
           if (selectParamArr[0] === "apiList") {
@@ -959,14 +957,14 @@ export default {
         });
       }
     },
-    getTagToolTips(tags){
-      try{
+    getTagToolTips(tags) {
+      try {
         let showTips = "";
-        tags.forEach( item => {
+        tags.forEach(item => {
           showTips += item + ",";
         })
         return showTips.substr(0, showTips.length - 1);
-      }catch (e){
+      } catch (e) {
         return "";
       }
     }
