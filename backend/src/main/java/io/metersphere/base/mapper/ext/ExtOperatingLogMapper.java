7a6e409fb5e12ef42613bc5d1fe5667ac90953d6
shiziyuan9527 <yuhao.li@fit2cloud.com>
2022-06-14 18:13:45 +0800

    fix(系统设置): 操作日志点击查询数据加载时间过长
    --bug=1013896 --user=李玉号 【系统设置】66环境系统-操作日志点击查询数据加载时间过长
    https://www.tapd.cn/55049933/s/1181599

--- backend/src/main/java/io/metersphere/base/mapper/ext/ExtOperatingLogMapper.java
+++ backend/src/main/java/io/metersphere/base/mapper/ext/ExtOperatingLogMapper.java
@@ -14,5 +14,7 @@ public interface ExtOperatingLogMapper {
 
     List<OperatingLogDTO> findBySourceIdEnv(@Param("request") OperatingLogRequest request);
 
+    List<OperatingLogDTO> findSourceIdByLogIds(@Param("ids") List<String> ids);
+
     boolean isNewInstallation();
 }
