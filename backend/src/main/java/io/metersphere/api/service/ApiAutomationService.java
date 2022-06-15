22d7d9da9a3ebe1a1f722f8ab6261f4e2b08273c
song-cc-rock <changchang.song@fit2cloud.com>
2022-06-15 16:18:00 +0800

    fix(测试跟踪): 测试计划-接口测试用例-关联接口用例,场景用例后显示乱序
    --bug=1013849 --user=宋昌昌 【测试跟踪】测试计划-关联接口用例和场景用例后的显示顺序是乱序，不是按照选中的id顺序关联的 https://www.tapd.cn/55049933/s/1181619

--- backend/src/main/java/io/metersphere/api/service/ApiAutomationService.java
+++ backend/src/main/java/io/metersphere/api/service/ApiAutomationService.java
@@ -977,15 +977,15 @@ public class ApiAutomationService {
         Map<String, List<String>> mapping = request.getMapping();
         Map<String, String> envMap = request.getEnvMap();
         Set<String> set = mapping.keySet();
-        List<String> ids = new ArrayList<>(set);
-        Collections.reverse(ids);
+        List<String> relevanceIds = request.getSelectIds();
+        Collections.reverse(relevanceIds);
         String envType = request.getEnvironmentType();
         String envGroupId = request.getEnvGroupId();
         if (set.isEmpty()) {
             return;
         }
         Long nextOrder = ServiceUtils.getNextOrder(request.getPlanId(), extTestPlanScenarioCaseMapper::getLastOrder);
-        for (String id : ids) {
+        for (String id : relevanceIds) {
             Map<String, String> newEnvMap = new HashMap<>(16);
             List<String> list = mapping.get(id);
             list.forEach(l -> newEnvMap.put(l, envMap == null ? "" : envMap.getOrDefault(l, "")));
