d990a2550289856f420ac4a96686617dfee06372
song-cc-rock <changchang.song@fit2cloud.com>
2022-06-15 16:18:00 +0800

    fix(测试跟踪): 测试计划-接口测试用例-关联接口用例,场景用例后显示乱序
    --bug=1013849 --user=宋昌昌 【测试跟踪】测试计划-关联接口用例和场景用例后的显示顺序是乱序，不是按照选中的id顺序关联的 https://www.tapd.cn/55049933/s/1181619

--- frontend/src/business/components/track/plan/view/comonents/api/TestCaseScenarioRelevance.vue
+++ frontend/src/business/components/track/plan/view/comonents/api/TestCaseScenarioRelevance.vue
@@ -137,12 +137,17 @@ export default {
       if (!sign) {
         return false;
       }
+      let selectIds = [];
       let url = '/api/automation/relevance';
+      let selectRows = this.$refs.apiScenarioList.selectRows;
       const envMap = this.$refs.apiScenarioList.projectEnvMap;
       let envType = this.$refs.apiScenarioList.environmentType;
       let map = this.$refs.apiScenarioList.map;
       let envGroupId = this.$refs.apiScenarioList.envGroupId;
 
+      selectRows.forEach(row => {
+        selectIds.push(row.id);
+      })
       if (envType === ENV_TYPE.JSON && (!envMap || envMap.size < 1)) {
         this.$warning(this.$t("api_test.environment.select_environment"));
         return false;
@@ -156,6 +161,7 @@ export default {
       param.envMap = strMapToObj(envMap);
       param.environmentType = envType;
       param.envGroupId = envGroupId;
+      param.selectIds = selectIds;
 
       this.result = this.$post(url, param, () => {
         this.$success(this.$t('commons.save_success'));
