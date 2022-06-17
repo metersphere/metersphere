15ecae44355472d8aa542ad24eac9970c88786b8
chenjianxing <jianxing.chen@fit2cloud.com>
2022-06-17 11:08:12 +0800

    fix(测试跟踪): 模块增删改，脑图没有提示刷新
    --bug=1014167 --user=陈建星 【测试跟踪】脑图模式下，左侧模块增删改，脑图没有及时更新 https://www.tapd.cn/55049933/s/1184890

--- frontend/src/business/components/track/case/TestCase.vue
+++ frontend/src/business/components/track/case/TestCase.vue
@@ -119,6 +119,7 @@
               :tree-nodes="treeNodes"
               :project-id="projectId"
               :condition="condition"
+              :active-name="activeName"
               v-if="activeDom === 'right'"
               @refresh="minderSaveRefresh"
               ref="minder"/>
@@ -688,9 +689,6 @@ export default {
     refreshAll(data) {
       if (this.$refs.testCaseList) {
         this.$refs.testCaseList.initTableData();
-      }
-      if (this.$refs.minder) {
-        this.$refs.minder.initData();
       }
       this.$refs.nodeTree.list();
       this.setTable(data);
