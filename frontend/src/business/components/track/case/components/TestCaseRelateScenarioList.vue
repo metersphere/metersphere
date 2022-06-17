1b5d8dffa00b093c62ccf6c2d89b21411a5d2e48
song-cc-rock <changchang.song@fit2cloud.com>
2022-06-17 18:21:21 +0800

    fix(测试跟踪): 功能用例关联不同项目用例数据显示不正确
    --bug=1014217 --user=宋昌昌 【测试跟踪】功能用例关联不同项目用例数据显示不正确 https://www.tapd.cn/55049933/s/1185742

--- frontend/src/business/components/track/case/components/TestCaseRelateScenarioList.vue
+++ frontend/src/business/components/track/case/components/TestCaseRelateScenarioList.vue
@@ -113,7 +113,7 @@ export default {
         {text: 'P2', value: 'P2'},
         {text: 'P3', value: 'P3'}
       ],
-      screenHeight: '100vh - 400px',//屏幕高度
+      screenHeight: 'calc(100vh - 400px)',//屏幕高度
       tableData: [],
       currentPage: 1,
       pageSize: 10,
@@ -131,7 +131,6 @@ export default {
     }
   },
   created: function () {
-    this.initTable();
     this.getVersionOptions();
   },
   watch: {
