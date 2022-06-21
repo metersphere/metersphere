5561938921e947d56172b98ca0fc72655b104708
song-tianyang <tianyang.song@fit2cloud.com>
2022-06-21 15:59:23 +0800

    feat(接口测试): 快速创建场景和接口的弹窗中支持对所属模块的选择
    --story=1008293 --user=宋天阳 【Bug转需求】【接口测试】接口定义、场景，左上角更多操作-创建接口，没有模块信息
    https://www.tapd.cn/55049933/s/1187420

--- frontend/src/business/components/api/automation/scenario/AddBasisScenario.vue
+++ frontend/src/business/components/api/automation/scenario/AddBasisScenario.vue
@@ -8,6 +8,11 @@
         <el-input v-model="scenarioForm.name" autocomplete="off" :placeholder="$t('commons.name')"/>
       </el-form-item>
 
+      <el-form-item :label="$t('test_track.module.module')" prop="apiScenarioModuleId">
+        <ms-select-tree size="small" :data="moduleOptions" :defaultKey="scenarioForm.apiScenarioModuleId"
+                        @getValue="setModule" :obj="moduleObj" clearable checkStrictly/>
+      </el-form-item>
+
       <el-form-item :label="$t('api_test.automation.scenario.principal')" prop="principal">
         <el-select v-model="scenarioForm.principal"
                    :placeholder="$t('api_test.automation.scenario.principal')" filterable size="small"
@@ -58,85 +63,98 @@
 </template>
 
 <script>
-  import {WORKSPACE_ID} from '@/common/js/constants';
-  import {getCurrentProjectID, getCurrentUser, getUUID} from "@/common/js/utils";
-  import MsDialogFooter from "@/business/components/common/components/MsDialogFooter";
-  import {saveScenario} from "@/business/components/api/automation/api-automation";
+import {getCurrentProjectID, getCurrentUser, getUUID} from "@/common/js/utils";
+import MsDialogFooter from "@/business/components/common/components/MsDialogFooter";
+import {saveScenario} from "@/business/components/api/automation/api-automation";
+import MsSelectTree from "@/business/components/common/select-tree/SelectTree";
 
-  export default {
-    name: "MsAddBasisScenario",
-    components: {MsDialogFooter},
-    props: {},
-    data() {
-      return {
-        scenarioForm: {follows:[]},
-        visible: false,
-        currentModule: {},
-        userOptions: [],
-        rule: {
-          name: [
-            {required: true, message: this.$t('test_track.case.input_name'), trigger: 'blur'},
-            {max: 100, message: this.$t('test_track.length_less_than') + '100', trigger: 'blur'}
-          ],
-          principal: [{
-            required: true,
-            message: this.$t('api_test.automation.scenario.select_principal'),
-            trigger: 'change'
-          }],
-        },
-      }
-    },
-    computed: {
-      projectId() {
-        return getCurrentProjectID();
+export default {
+  name: "MsAddBasisScenario",
+  components: {MsDialogFooter, MsSelectTree},
+  props: {
+    moduleOptions: Array,
+  },
+  data() {
+    return {
+      scenarioForm: {follows: []},
+      visible: false,
+      currentModule: {},
+      userOptions: [],
+      moduleObj: {
+        id: 'id',
+        label: 'name',
       },
+      rule: {
+        name: [
+          {required: true, message: this.$t('test_track.case.input_name'), trigger: 'blur'},
+          {max: 100, message: this.$t('test_track.length_less_than') + '100', trigger: 'blur'}
+        ],
+        apiScenarioModuleId: [{required: true, message: this.$t('test_track.case.input_module'), trigger: 'change'}],
+        principal: [{
+          required: true,
+          message: this.$t('api_test.automation.scenario.select_principal'),
+          trigger: 'change'
+        }],
+      },
+    }
+  },
+  computed: {
+    projectId() {
+      return getCurrentProjectID();
     },
-    methods: {
-      saveScenario(saveAs) {
-        this.$refs['scenarioForm'].validate((valid) => {
-          if (valid) {
-            let path = "/api/automation/create";
-            this.setParameter();
-            if (saveAs) {
-              this.scenarioForm.request = JSON.stringify(this.scenarioForm.request);
-              this.$emit('saveAsEdit', this.scenarioForm);
-              this.visible = false;
-            } else {
-              saveScenario(path, this.scenarioForm, [], this,(response) => {
-                this.$success(this.$t('commons.save_success'));
-                this.visible = false;
-                this.$emit('refresh');
-              });
-            }
+  },
+  methods: {
+    setModule(id, data) {
+      this.scenarioForm.apiScenarioModuleId = id;
+      this.scenarioForm.modulePath = data.path;
+    },
+    saveScenario(saveAs) {
+      this.$refs['scenarioForm'].validate((valid) => {
+        if (valid) {
+          let path = "/api/automation/create";
+          this.setParameter();
+          if (saveAs) {
+            this.scenarioForm.request = JSON.stringify(this.scenarioForm.request);
+            this.$emit('saveAsEdit', this.scenarioForm);
+            this.visible = false;
           } else {
-            return false;
+            saveScenario(path, this.scenarioForm, [], this, (response) => {
+              this.$success(this.$t('commons.save_success'));
+              this.visible = false;
+              this.$emit('refresh');
+            });
           }
-        })
-      },
-      setParameter() {
-        this.scenarioForm.projectId = this.projectId;
-        this.scenarioForm.id = getUUID().substring(0, 8);
-        this.scenarioForm.protocol = this.currentProtocol;
-
-        if (this.currentModule && this.currentModule.id != "root") {
-          this.scenarioForm.modulePath = this.currentModule.method !== undefined ? this.currentModule.method : null;
-          this.scenarioForm.apiScenarioModuleId = this.currentModule.id;
         } else {
-          this.scenarioForm.modulePath = this.$t("commons.module_title");
-          this.scenarioForm.apiScenarioModuleId = "default-module";
+          return false;
         }
-      },
-      getMaintainerOptions() {
-        this.$get('/user/project/member/list', response => {
-          this.userOptions = response.data;
-        });
-      },
-      open(currentModule) {
-        this.scenarioForm = {principal: getCurrentUser().id};
-        this.currentModule = currentModule;
-        this.getMaintainerOptions();
-        this.visible = true;
+      })
+    },
+    setParameter() {
+      this.scenarioForm.projectId = this.projectId;
+      this.scenarioForm.id = getUUID().substring(0, 8);
+      this.scenarioForm.protocol = this.currentProtocol;
+
+      if (!this.scenarioForm.modulePath) {
+        this.scenarioForm.modulePath = this.$t("commons.module_title");
       }
+      if (!this.scenarioForm.apiScenarioModuleId) {
+        this.scenarioForm.apiScenarioModuleId = "default-module";
+      }
+    },
+    getMaintainerOptions() {
+      this.$get('/user/project/member/list', response => {
+        this.userOptions = response.data;
+      });
+    },
+    open(currentModule) {
+      this.scenarioForm = {principal: getCurrentUser().id};
+      this.currentModule = currentModule;
+      if (this.scenarioForm.apiScenarioModuleId === undefined) {
+        this.scenarioForm.apiScenarioModuleId = this.moduleOptions[0].id;
+      }
+      this.getMaintainerOptions();
+      this.visible = true;
     }
   }
+}
 </script>
