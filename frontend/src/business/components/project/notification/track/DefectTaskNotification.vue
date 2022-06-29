1d57d1b8ed690a8598b7102d0bb6239fe268487b
CaptainB <bin@fit2cloud.com>
2022-06-29 16:22:33 +0800

    fix(消息设置): 修复配置通知时无法展示候选人的问题
    --bug=1014373 --user=刘瑞斌 【项目设置】消息设置-用例评审任务通知-新建/评论，接收人显示无数据 https://www.tapd.cn/55049933/s/1191965
    --bug=1014374 --user=刘瑞斌 【项目设置】消息设置-用例评审任务通知-评审完成/删除/更新，接收人显示英文 https://www.tapd.cn/55049933/s/1191967

--- frontend/src/business/components/project/notification/track/DefectTaskNotification.vue
+++ frontend/src/business/components/project/notification/track/DefectTaskNotification.vue
@@ -224,7 +224,7 @@ export default {
         default:
           break;
       }
-      row.defectReceiverOptions = testPlanReceivers;
+      row.receiverOptions = testPlanReceivers;
     },
   },
   watch: {
