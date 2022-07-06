6f431de67b996c9b25171e31b50be7694692d59d
CaptainB <bin@fit2cloud.com>
2022-07-06 11:23:39 +0800

    fix: 修复用户登录后提示修改密码导致登录时间很长
    Closes #15509

--- backend/src/main/java/io/metersphere/base/mapper/ext/ExtOperatingLogMapper.java
+++ backend/src/main/java/io/metersphere/base/mapper/ext/ExtOperatingLogMapper.java
@@ -15,6 +15,4 @@ public interface ExtOperatingLogMapper {
     List<OperatingLogDTO> findBySourceIdEnv(@Param("request") OperatingLogRequest request);
 
     List<OperatingLogDTO> findSourceIdByLogIds(@Param("ids") List<String> ids);
-
-    boolean isNewInstallation();
 }
