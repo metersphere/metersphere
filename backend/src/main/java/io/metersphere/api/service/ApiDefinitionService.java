37136d280fe8943e5550a1a8d5505a6883c004db
wxg0103 <727495428@qq.com>
2022-07-06 14:15:57 +0800

    fix(接口测试): 修复接口定义回收站恢复数据报错的缺陷
    --bug=1014656 --user=王孝刚 【接口测试】接口定义，回收站恢复接口，报错了
    https://www.tapd.cn/55049933/s/1195815

--- backend/src/main/java/io/metersphere/api/service/ApiDefinitionService.java
+++ backend/src/main/java/io/metersphere/api/service/ApiDefinitionService.java
@@ -465,6 +465,7 @@ public class ApiDefinitionService {
                     apiDefinitionRequest.setPath(apiDefinition.getPath());
                     apiDefinitionRequest.setName(apiDefinition.getName());
                     apiDefinitionRequest.setId(apiDefinition.getId());
+                    apiDefinitionRequest.setModuleId(apiDefinition.getModuleId());
                     apiDefinitionRequest.setVersionId(extProjectVersionMapper.getDefaultVersion(request.getProjectId()));
                     checkNameExist(apiDefinitionRequest);
 
