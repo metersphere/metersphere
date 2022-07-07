81df1f9328a7cab8235b44d62525812f4f4e8583
guoyuqi <xiaomeinvG@126.com>
2022-07-07 20:12:10 +0800

    fix(接口测试): 接口场景导入版本修改
    --user=郭雨琦
    --bug=1014708
    --bug=1014732
    --bug=1014731
    --bug=1014727
    https://www.tapd.cn/55049933/bugtrace/bugs/view/1155049933001014708
    https://www.tapd.cn/55049933/bugtrace/bugs/view/1155049933001014732
    https://www.tapd.cn/55049933/bugtrace/bugs/view/1155049933001014731
    https://www.tapd.cn/55049933/bugtrace/bugs/view/1155049933001014727

--- backend/src/main/java/io/metersphere/base/mapper/ext/ExtApiScenarioMapper.java
+++ backend/src/main/java/io/metersphere/base/mapper/ext/ExtApiScenarioMapper.java
@@ -91,7 +91,7 @@ public interface ExtApiScenarioMapper {
 
     List<ApiScenarioWithBLOBs> selectByStatusIsNotTrash();
 
-    List<ApiScenarioWithBLOBs> selectRepeatByBLOBs(@Param("names") List<String> names, @Param("projectId") String projectId, @Param("versionIds") Set<String> versionIds);
+    List<ApiScenarioWithBLOBs> selectRepeatByBLOBs(@Param("names") List<String> names, @Param("projectId") String projectId);
 
 
 }
