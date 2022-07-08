ed7e4c3b13665442e79c08f4bd408ec4fa0979c6
chenjianxing <jianxing.chen@fit2cloud.com>
2022-07-08 18:35:14 +0800

    fix(测试跟踪): 项目数量较大时定时同步缺陷时数据库压力异常
    --bug=1014751 --user=陈建星 【测试跟踪】定时同步缺陷时数据库压力异常 https://www.tapd.cn/55049933/s/1198207

--- backend/src/main/java/io/metersphere/job/sechedule/IssueSyncJob.java
+++ backend/src/main/java/io/metersphere/job/sechedule/IssueSyncJob.java
@@ -2,22 +2,36 @@ package io.metersphere.job.sechedule;
 
 import io.metersphere.commons.constants.ScheduleGroup;
 import io.metersphere.commons.utils.CommonBeanFactory;
+import io.metersphere.commons.utils.LogUtil;
+import io.metersphere.service.ProjectService;
 import io.metersphere.track.service.IssuesService;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobKey;
 import org.quartz.TriggerKey;
 
+import java.util.List;
+
 public class IssueSyncJob extends MsScheduleJob {
 
     private IssuesService issuesService;
+    private ProjectService projectService;
 
     public IssueSyncJob() {
         issuesService = CommonBeanFactory.getBean(IssuesService.class);
+        projectService = CommonBeanFactory.getBean(ProjectService.class);
     }
 
     @Override
     void businessExecute(JobExecutionContext context) {
-        issuesService.syncThirdPartyIssues();
+        List<String> projectIds = projectService.getThirdPartProjectIds();
+        projectIds.forEach(id -> {
+            try {
+                // 一个项目开启一个事务，
+                issuesService.syncThirdPartyIssues(id);
+            } catch (Exception e) {
+                LogUtil.error(e.getMessage(), e);
+            }
+        });
     }
 
     public static JobKey getJobKey(String projectId) {
