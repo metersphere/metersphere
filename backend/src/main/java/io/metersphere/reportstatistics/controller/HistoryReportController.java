3f58644667162d2386eeb3a14f532953612c3d06
song-tianyang <tianyang.song@fit2cloud.com>
2022-07-11 15:13:12 +0800

    feat(报表统计): 修复报表统计中已创建的图标类型点击保存却另存为的缺陷
    --bug=1014761 --user=宋天阳 [ 报表统计]github
    #15634报表统计-项目报表，已创建的报表更改图表类型后，点击保存失败，而是变成了另存为
    https://www.tapd.cn/55049933/s/1198644

--- backend/src/main/java/io/metersphere/reportstatistics/controller/HistoryReportController.java
+++ backend/src/main/java/io/metersphere/reportstatistics/controller/HistoryReportController.java
@@ -1,9 +1,7 @@
 package io.metersphere.reportstatistics.controller;
 
-import com.alibaba.fastjson.JSONArray;
 import io.metersphere.base.domain.ReportStatistics;
 import io.metersphere.base.domain.ReportStatisticsWithBLOBs;
-import io.metersphere.commons.utils.LogUtil;
 import io.metersphere.reportstatistics.dto.ReportStatisticsSaveRequest;
 import io.metersphere.reportstatistics.service.ReportStatisticsService;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,13 +30,19 @@ public class HistoryReportController {
     }
 
     @PostMapping("/saveReport")
-    public ReportStatisticsWithBLOBs saveReport(@RequestBody ReportStatisticsSaveRequest request){
+    public ReportStatisticsWithBLOBs saveReport(@RequestBody ReportStatisticsSaveRequest request) {
         ReportStatisticsWithBLOBs returnData = reportStatisticsService.saveByRequest(request);
         return returnData;
     }
 
+    @PostMapping("/update")
+    public ReportStatisticsWithBLOBs update(@RequestBody ReportStatisticsSaveRequest request) {
+        ReportStatisticsWithBLOBs returnData = reportStatisticsService.update(request);
+        return returnData;
+    }
+
     @PostMapping("/updateReport")
-    public ReportStatisticsWithBLOBs updateReport(@RequestBody ReportStatisticsSaveRequest request){
+    public ReportStatisticsWithBLOBs updateReport(@RequestBody ReportStatisticsSaveRequest request) {
         ReportStatisticsWithBLOBs returnData = reportStatisticsService.updateByRequest(request);
         return returnData;
     }
--- frontend/src/business/components/reportstatistics/projectstatistics/ProjectStatistics.vue
+++ frontend/src/business/components/reportstatistics/projectstatistics/ProjectStatistics.vue
@@ -2,7 +2,7 @@
   <div>
     <el-row type="flex">
       <p class="tip">
-        <span class="ms-span">{{$t('commons.report_statistics.name')}}</span>
+        <span class="ms-span">{{ $t('commons.report_statistics.name') }}</span>
         <el-select v-model="reportType" class="ms-col-type" size="mini" style="width: 120px">
           <el-option :key="t.id" :value="t.id" :label="t.name" v-for="t in reportTypes"/>
         </el-select>
@@ -15,19 +15,24 @@
     </transition>
 
     <!-- 测试用例趋势页面 -->
-    <ms-drawer :visible="testCaseTrendDrawer" :size="100" @close="close" direction="right" :show-full-screen="false" :is-show-close="false" style="overflow: hidden">
+    <ms-drawer :visible="testCaseTrendDrawer" :size="100" @close="close" direction="right" :show-full-screen="false"
+               :is-show-close="false" style="overflow: hidden">
       <template v-slot:header>
         <report-header :title="$t('commons.report_statistics.test_case_analysis')" :history-report-id="historyReportId"
-                       @closePage="close" @saveReport="openSaveReportDialog('save')" @selectAndSaveReport="openSaveReportDialog('saveAs')"/>
+                       @closePage="close" @updateReport="updateReport"
+                       @selectAndSaveReport="openSaveReportDialog('saveAs')"/>
       </template>
       <test-analysis-container @initHistoryReportId="initHistoryReportId" ref="testAnalysisContainer"/>
     </ms-drawer>
 
     <!-- 测试用例分析页面 -->
-    <ms-drawer :visible="testCaseCountDrawer" :size="100" @close="close" direction="right" :show-full-screen="false" :is-show-close="false" style="overflow: hidden">
+    <ms-drawer :visible="testCaseCountDrawer" :size="100" @close="close" direction="right" :show-full-screen="false"
+               :is-show-close="false" style="overflow: hidden">
       <template v-slot:header>
         <report-header :title="$t('commons.report_statistics.test_case_count')" :history-report-id="historyReportId"
-                       @closePage="close" @saveReport="openSaveReportDialog('save')" @selectAndSaveReport="openSaveReportDialog('saveAs')"/>
+                       @closePage="close"
+                       @updateReport="updateReport"
+                       @selectAndSaveReport="openSaveReportDialog('saveAs')"/>
       </template>
       <test-case-count-container @initHistoryReportId="initHistoryReportId" ref="testCaseCountContainer"/>
     </ms-drawer>
@@ -44,97 +49,108 @@
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="saveReport">{{$t('commons.confirm')}}</el-button>
+        <el-button type="primary" @click="saveReport">{{ $t('commons.confirm') }}</el-button>
       </div>
     </el-dialog>
   </div>
 </template>
 
 <script>
-  import ReportCard from "@/business/components/reportstatistics/projectstatistics/ReportCard";
-  import TestAnalysisContainer from "@/business/components/reportstatistics/projectstatistics/track/TestAnalysisContainer";
-  import MsDrawer from "@/business/components/common/components/MsDrawer";
-  import ReportHeader from "@/business/components/reportstatistics/base/ReportHeader";
-  import TestCaseCountContainer from "@/business/components/reportstatistics/projectstatistics/casecount/TestCaseCountContainer";
+import ReportCard from "@/business/components/reportstatistics/projectstatistics/ReportCard";
+import TestAnalysisContainer
+  from "@/business/components/reportstatistics/projectstatistics/track/TestAnalysisContainer";
+import MsDrawer from "@/business/components/common/components/MsDrawer";
+import ReportHeader from "@/business/components/reportstatistics/base/ReportHeader";
+import TestCaseCountContainer
+  from "@/business/components/reportstatistics/projectstatistics/casecount/TestCaseCountContainer";
 
 
-  export default {
-    name: "ReportAnalysis",
-    components: {ReportCard, TestAnalysisContainer, MsDrawer, ReportHeader, TestCaseCountContainer},
-    data() {
-      return {
-        reportType: "track",
-        testCaseTrendDrawer: false,
-        testCaseCountDrawer: false,
-        historyReportId:"",
-        reportTypes: [{id: 'track', name: this.$t('test_track.test_track')}],
-        dialogFormVisible: false,
-        form: {
-          reportName: "",
-          saveType: "",
-        },
-        saveReportRules: {
-          reportName: [
-            { required: true, message: this.$t('commons.input_name'), trigger: 'blur' },
-            { min: 1, max: 20, message: '长度不大于20个字符', trigger: 'blur' }
-          ],
+export default {
+  name: "ReportAnalysis",
+  components: {ReportCard, TestAnalysisContainer, MsDrawer, ReportHeader, TestCaseCountContainer},
+  data() {
+    return {
+      reportType: "track",
+      testCaseTrendDrawer: false,
+      testCaseCountDrawer: false,
+      historyReportId: "",
+      reportTypes: [{id: 'track', name: this.$t('test_track.test_track')}],
+      dialogFormVisible: false,
+      form: {
+        reportName: "",
+        saveType: "",
+      },
+      saveReportRules: {
+        reportName: [
+          {required: true, message: this.$t('commons.input_name'), trigger: 'blur'},
+          {min: 1, max: 20, message: '长度不大于20个字符', trigger: 'blur'}
+        ],
+      }
+    }
+  },
+  methods: {
+    openCard(type) {
+      if (type === 'trackTestCase') {
+        this.testCaseTrendDrawer = true;
+      } else if (type === 'countTestCase') {
+        this.testCaseCountDrawer = true;
+      }
+    },
+    close() {
+      this.testCaseTrendDrawer = false;
+      this.testCaseCountDrawer = false;
+    },
+    openSaveReportDialog(saveType) {
+      this.form.saveType = saveType;
+      this.dialogFormVisible = true;
+    },
+    updateReport() {
+      if (this.historyReportId) {
+        if (this.testCaseTrendDrawer) {
+          this.$refs.testAnalysisContainer.updateReport(this.historyReportId);
+        } else if (this.testCaseCountDrawer) {
+          this.$refs.testCaseCountContainer.updateReport(this.historyReportId);
         }
       }
     },
-    methods: {
-      openCard(type) {
-        if(type === 'trackTestCase'){
-          this.testCaseTrendDrawer = true;
-        }else if(type === 'countTestCase'){
-          this.testCaseCountDrawer = true;
-        }
-      },
-      close() {
-        this.testCaseTrendDrawer = false;
-        this.testCaseCountDrawer = false;
-      },
-      openSaveReportDialog(saveType){
-        this.form.saveType = saveType;
-        this.dialogFormVisible = true;
-      },
-      saveReport(){
-        this.$refs['saveReportRuleForm'].validate((valid) => {
-          if (valid) {
-            if(this.testCaseTrendDrawer){
-              this.$refs.testAnalysisContainer.saveAndSaveAsReport(this.form.reportName,this.form.saveType);
-            }else if(this.testCaseCountDrawer){
-              this.$refs.testCaseCountContainer.saveAndSaveAsReport(this.form.reportName,this.form.saveType);
-            }
-            this.form.reportName = "";
-            this.form.saveType = "";
-            this.dialogFormVisible = false;
-          } else {
-            return false;
+    saveReport() {
+      this.$refs['saveReportRuleForm'].validate((valid) => {
+        if (valid) {
+          if (this.testCaseTrendDrawer) {
+            this.$refs.testAnalysisContainer.saveAndSaveAsReport(this.form.reportName, this.form.saveType);
+          } else if (this.testCaseCountDrawer) {
+            this.$refs.testCaseCountContainer.saveAndSaveAsReport(this.form.reportName, this.form.saveType);
           }
-        });
-      },
-      initHistoryReportId(reportId){
-        this.historyReportId = reportId;
-      },
-      handleCloseSaveReportDialog(){
-        this.form.reportName = "";
-        this.form.saveType = "";
-        this.dialogFormVisible = false;
-      }
+          this.form.reportName = "";
+          this.form.saveType = "";
+          this.dialogFormVisible = false;
+        } else {
+          return false;
+        }
+      });
     },
-  }
+    initHistoryReportId(reportId) {
+      this.historyReportId = reportId;
+    },
+    handleCloseSaveReportDialog() {
+      this.form.reportName = "";
+      this.form.saveType = "";
+      this.dialogFormVisible = false;
+    }
+  },
+}
 </script>
 
 <style scoped>
-  .ms-span {
-    margin: 10px 10px 0px
-  }
+.ms-span {
+  margin: 10px 10px 0px
+}
 
-  .tip {
-    float: left;
-    font-size: 14px;
-    border-radius: 2px;
-    border-left: 2px solid #783887;
-    margin: 10px 20px 0px;
-  }
+.tip {
+  float: left;
+  font-size: 14px;
+  border-radius: 2px;
+  border-left: 2px solid #783887;
+  margin: 10px 20px 0px;
+}
 </style>
