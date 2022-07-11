d5a57e272307cf4dbbcb1792b60ac53394710956
wxg0103 <727495428@qq.com>
2022-07-11 17:59:04 +0800

    fix(测试跟踪): 去掉测试计划中性能测试列表，批量执行的失败停止选项
    --bug=1014766 --user=王孝刚 【测试跟踪】测试计划-性能测试列表，批量执行不应该有失败停止选项
    https://www.tapd.cn/55049933/s/1198935

--- frontend/src/business/components/track/plan/common/PlanRunMode.vue
+++ frontend/src/business/components/track/plan/common/PlanRunMode.vue
@@ -12,54 +12,6 @@
         <el-radio label="parallel">{{ $t("run_mode.parallel") }}</el-radio>
       </el-radio-group>
     </div>
-    <div class="ms-mode-div" v-if="runConfig.mode === 'serial'">
-      <el-row>
-        <el-col :span="6">
-          <span class="ms-mode-span">{{ $t("run_mode.other_config") }}：</span>
-        </el-col>
-        <el-col :span="18">
-          <div>
-            <el-checkbox v-model="runConfig.onSampleError">失败停止</el-checkbox>
-          </div>
-          <div v-if="testType === 'API'" style="padding-top: 10px">
-            <el-checkbox v-model="runConfig.runWithinResourcePool" style="padding-right: 10px;">
-              {{ $t('run_mode.run_with_resource_pool') }}
-            </el-checkbox>
-            <el-select :disabled="!runConfig.runWithinResourcePool" v-model="runConfig.resourcePoolId" size="mini">
-              <el-option
-                v-for="item in resourcePools"
-                :key="item.id"
-                :label="item.name"
-                :value="item.id">
-              </el-option>
-            </el-select>
-          </div>
-        </el-col>
-      </el-row>
-    </div>
-    <div class="ms-mode-div" v-if="runConfig.mode === 'parallel'">
-      <el-row>
-        <el-col :span="6">
-          <span class="ms-mode-span">{{ $t("run_mode.other_config") }}：</span>
-        </el-col>
-        <el-col :span="18">
-          <div v-if="testType === 'API'">
-            <el-checkbox v-model="runConfig.runWithinResourcePool" style="padding-right: 10px;">
-              {{ $t('run_mode.run_with_resource_pool') }}
-            </el-checkbox>
-            <el-select :disabled="!runConfig.runWithinResourcePool" v-model="runConfig.resourcePoolId" size="mini">
-              <el-option
-                v-for="item in resourcePools"
-                :key="item.id"
-                :label="item.name"
-                :disabled="!item.api"
-                :value="item.id">
-              </el-option>
-            </el-select>
-          </div>
-        </el-col>
-      </el-row>
-    </div>
     <template v-slot:footer>
       <ms-dialog-footer @cancel="close" @confirm="handleRunBatch"/>
     </template>
@@ -86,9 +38,9 @@ export default {
       },
     };
   },
-  watch:{
-    'runConfig.runWithinResourcePool'(){
-      if(!this.runConfig.runWithinResourcePool){
+  watch: {
+    'runConfig.runWithinResourcePool'() {
+      if (!this.runConfig.runWithinResourcePool) {
         this.runConfig = {
           mode: this.runConfig.mode,
           reportType: "iddReport",
