2817c1d7cafc95d9a0ff0438f4305ffb825f1656
fit2-zhao <yong.zhao@fit2cloud.com>
2022-07-12 11:41:08 +0800

    feat(接口测试): 认证支持basic和digest两种方式
    --story=1008766 --user=赵勇 [接口测试]github#15669接口测试，Basic Auth 认证没生效 https://www.tapd.cn/55049933/s/1199311

--- backend/src/main/java/io/metersphere/api/dto/definition/request/auth/MsAuthManager.java
+++ backend/src/main/java/io/metersphere/api/dto/definition/request/auth/MsAuthManager.java
@@ -16,7 +16,9 @@ import org.apache.jmeter.save.SaveService;
 import org.apache.jmeter.testelement.TestElement;
 import org.apache.jorphan.collections.HashTree;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -52,18 +54,19 @@ public class MsAuthManager extends MsTestElement {
     @JSONField(ordinal = 28)
     private String environment;
 
+    public static final Map<String, AuthManager.Mechanism> mechanismMap = new HashMap<>() {{
+        this.put("Basic Auth", AuthManager.Mechanism.BASIC);
+        this.put("Digest Auth", AuthManager.Mechanism.DIGEST);
+    }};
+
     @Override
     public void toHashTree(HashTree tree, List<MsTestElement> hashTree, MsParameter msParameter) {
         if (!this.isEnable()) {
             return;
         }
-        if (StringUtils.equals(this.getVerification(), "Basic Auth")) {
+        if (mechanismMap.containsKey(this.getVerification())) {
             ParameterConfig config = (ParameterConfig) msParameter;
-            AuthManager authManager = new AuthManager();
-            authManager.setEnabled(true);
-            authManager.setName(StringUtils.isNotEmpty(this.getName()) ? this.getName() : "AuthManager");
-            authManager.setProperty(TestElement.TEST_CLASS, AuthManager.class.getName());
-            authManager.setProperty(TestElement.GUI_CLASS, SaveService.aliasToClass("AuthPanel"));
+            AuthManager authManager = initBase();
             Authorization auth = new Authorization();
             if (this.url != null) {
                 auth.setURL(this.url);
@@ -77,7 +80,7 @@ public class MsAuthManager extends MsTestElement {
             }
             auth.setUser(this.username);
             auth.setPass(this.password);
-            auth.setMechanism(AuthManager.Mechanism.DIGEST);
+            auth.setMechanism(mechanismMap.get(this.getVerification()));
             authManager.addAuth(auth);
             tree.add(authManager);
         }
@@ -85,20 +88,25 @@ public class MsAuthManager extends MsTestElement {
 
     public void setAuth(HashTree tree, MsAuthManager msAuthManager, HTTPSamplerProxy samplerProxy) {
         try {
-            AuthManager authManager = new AuthManager();
-            authManager.setEnabled(true);
-            authManager.setName(StringUtils.isNotEmpty(this.getName()) ? this.getName() : "AuthManager");
-            authManager.setProperty(TestElement.TEST_CLASS, AuthManager.class.getName());
-            authManager.setProperty(TestElement.GUI_CLASS, SaveService.aliasToClass("AuthPanel"));
+            AuthManager authManager = initBase();
             Authorization auth = new Authorization();
             auth.setURL(samplerProxy.getProtocol() + "://" + samplerProxy.getDomain());
             auth.setUser(msAuthManager.getUsername());
             auth.setPass(msAuthManager.getPassword());
-            auth.setMechanism(AuthManager.Mechanism.DIGEST);
+            auth.setMechanism(mechanismMap.get(msAuthManager.getVerification()));
             authManager.addAuth(auth);
             tree.add(authManager);
         } catch (Exception e) {
             LogUtil.error(e);
         }
+    }
+
+    private AuthManager initBase() {
+        AuthManager authManager = new AuthManager();
+        authManager.setEnabled(true);
+        authManager.setName(StringUtils.isNotEmpty(this.getName()) ? this.getName() : "AuthManager");
+        authManager.setProperty(TestElement.TEST_CLASS, AuthManager.class.getName());
+        authManager.setProperty(TestElement.GUI_CLASS, SaveService.aliasToClass("AuthPanel"));
+        return authManager;
     }
 }
