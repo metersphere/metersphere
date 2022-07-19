6136b6e42b40a8671aa4342e50ec73e4baa00dee
chenjianxing <jianxing.chen@fit2cloud.com>
2022-07-19 14:34:17 +0800

    fix(测试跟踪): 功能用例列表创建人排序报错
    --bug=1014994 --user=陈建星 【测试跟踪】功能用例列表-筛选的用例评审结果+同时筛选创建人-筛选失败-然后功能用例页面无法使用报sql错误 https://www.tapd.cn/55049933/s/1204118

--- frontend/src/common/js/default-table-header.js
+++ frontend/src/common/js/default-table-header.js
@@ -209,7 +209,7 @@ export let CUSTOM_TABLE_HEADER = {
     {id: 'versionId', key: 'b', label: 'project.version.name', xpack: true},
     {id: 'nodePath', key: '5', label: 'test_track.case.module'},
     {id: 'updateTime', key: '6', label: 'commons.update_time'},
-    {id: 'createName', key: '7', label: 'commons.create_user'},
+    {id: 'createUser', key: '7', label: 'commons.create_user'},
     {id: 'createTime', key: '8', label: 'commons.create_time'},
     {id: 'desc', key: '9', label: 'test_track.case.case_desc'},
     {id: 'lastExecuteResult', key: '0', label: 'test_track.plan_view.execute_result'},
