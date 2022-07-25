62d83a6cd0c092c32c4749a923c0c7a787c5a05a
chenjianxing <jianxing.chen@fit2cloud.com>
2022-07-25 16:14:41 +0800

    refactor(测试跟踪): 创建功能用例选择版本创建报错
    --bug=1015334 --user=陈建星 【测试跟踪】创建功能用例，选择版本-操作-新建，报500 https://www.tapd.cn/55049933/s/1210250

--- frontend/src/business/components/track/case/components/TestCaseEdit.vue
+++ frontend/src/business/components/track/case/components/TestCaseEdit.vue
@@ -1074,9 +1074,13 @@ export default {
     },
     hasOtherInfo() {
       return new Promise((resolve) => {
-          this.$get("test/case/hasOtherInfo/" + this.form.id, (res) => {
-            resolve(res.data);
-          })
+          if (this.form.id) {
+            this.$get("test/case/hasOtherInfo/" + this.form.id, (res) => {
+              resolve(res.data);
+            });
+          } else {
+            resolve();
+          }
         }
       );
     },
