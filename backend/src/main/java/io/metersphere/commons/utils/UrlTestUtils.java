6a701087d1b7082d5e29721ecc3b9854bd2d7a63
chenjianxing <jianxing.chen@fit2cloud.com>
2022-07-25 11:40:24 +0800

    fix(接口测试): swagger添加认证信息导入报错连接超时
    --bug=1014848 --user=陈建星 【接口测试】接口定义-使用 swagger url 导入 - 添加请求参数保存后，连接超时 https://www.tapd.cn/55049933/s/1209161

--- backend/src/main/java/io/metersphere/commons/utils/UrlTestUtils.java
+++ backend/src/main/java/io/metersphere/commons/utils/UrlTestUtils.java
@@ -3,6 +3,7 @@ package io.metersphere.commons.utils;
 import io.metersphere.ldap.service.CustomSSLSocketFactory;
 
 import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -10,16 +11,7 @@ public class UrlTestUtils {
 
     public static boolean testUrlWithTimeOut(String address, int timeOutMillSeconds) {
         try {
-            URL urlObj = new URL(address);
-            HttpURLConnection oc = (HttpURLConnection) urlObj.openConnection();
-            if (oc instanceof HttpsURLConnection) {
-                ((HttpsURLConnection) oc).setSSLSocketFactory(new CustomSSLSocketFactory());
-                ((HttpsURLConnection) oc).setHostnameVerifier((hostname, session) -> true);
-            }
-            oc.setUseCaches(false);
-            oc.setConnectTimeout(timeOutMillSeconds); // 设置超时时间
-            int status = oc.getResponseCode();// 请求状态
-            if (200 == status) {
+            if (200 == testUrl(address, timeOutMillSeconds)) {
                 return true;
             }
         } catch (Exception e) {
@@ -27,5 +19,17 @@ public class UrlTestUtils {
             return false;
         }
         return false;
+    }
+
+    public static int testUrl(String address, int timeOutMillSeconds) throws IOException {
+        URL urlObj = new URL(address);
+        HttpURLConnection oc = (HttpURLConnection) urlObj.openConnection();
+        if (oc instanceof HttpsURLConnection) {
+            ((HttpsURLConnection) oc).setSSLSocketFactory(new CustomSSLSocketFactory());
+            ((HttpsURLConnection) oc).setHostnameVerifier((hostname, session) -> true);
+        }
+        oc.setUseCaches(false);
+        oc.setConnectTimeout(timeOutMillSeconds); // 设置超时时间
+        return oc.getResponseCode();// 请求状态
     }
 }
