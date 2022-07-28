4f191b3744355575d0d848e42afcb1ae5b69800d
wxg0103 <727495428@qq.com>
2022-07-28 14:26:57 +0800

    fix(测试跟踪): 修复接口调用没有关联可执行用例测试计划执行时没有提示的缺陷
    --bug=1015449 --user=王孝刚
    [测试计划]github#16371测试计划里只包含功能用例，在页面无法执行，但是调用API可以执行，且可正常生成报告,报告里无任何信息
    https://www.tapd.cn/55049933/s/1214486

--- frontend/src/i18n/zh-CN.js
+++ frontend/src/i18n/zh-CN.js
@@ -2154,7 +2154,7 @@ export default {
       batch_copy: '批量复制',
       batch_add_public: '批量添加到公共用例库',
       batch_link_demand: '批量关联需求',
-      public_warning: '未开启公共库用例配置',
+      public_warning: '未开启公共用例库配置',
       generate_dependencies: '生成依赖关系',
       generate_dependencies_warning: '为了更好的体验，建议勾选一百条以下的数据',
       batch_unlink: '批量取消关联',
