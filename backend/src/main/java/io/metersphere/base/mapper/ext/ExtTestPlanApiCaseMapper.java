9d695af93a218e890a8ecfe17c6236ec971554f9
wxg0103 <727495428@qq.com>
2022-07-29 17:41:25 +0800

    fix(测试跟踪): 修复测试计划运行检查是否有可用行用例的缺陷

--- backend/src/main/java/io/metersphere/base/mapper/ext/ExtTestPlanApiCaseMapper.java
+++ backend/src/main/java/io/metersphere/base/mapper/ext/ExtTestPlanApiCaseMapper.java
@@ -21,7 +21,7 @@ public interface ExtTestPlanApiCaseMapper {
 
     List<String> getIdsByPlanId(String planId);
 
-    List<String> getNotRelevanceCaseIds(@Param("planId")String planId, @Param("relevanceProjectIds")List<String> relevanceProjectIds);
+    List<String> getNotRelevanceCaseIds(@Param("planId") String planId, @Param("relevanceProjectIds") List<String> relevanceProjectIds);
 
     List<String> getStatusByTestPlanId(String id);
 
@@ -37,17 +37,18 @@ public interface ExtTestPlanApiCaseMapper {
 
     List<TestPlanFailureApiDTO> getFailureList(@Param("planId") String planId, @Param("status") String status);
 
-    List<TestPlanFailureApiDTO> getFailureListByIds(@Param("ids") Collection<String> caseIdList,@Param("status") String status);
+    List<TestPlanFailureApiDTO> getFailureListByIds(@Param("ids") Collection<String> caseIdList, @Param("status") String status);
 
     List<String> selectPlanIds();
 
     List<String> getIdsOrderByUpdateTime(@Param("planId") String planId);
 
-    Long getPreOrder(@Param("planId")String planId, @Param("baseOrder") Long baseOrder);
+    Long getPreOrder(@Param("planId") String planId, @Param("baseOrder") Long baseOrder);
 
-    Long getLastOrder(@Param("planId")String planId, @Param("baseOrder") Long baseOrder);
+    Long getLastOrder(@Param("planId") String planId, @Param("baseOrder") Long baseOrder);
 
     List<TestPlanApiCase> selectByIdsAndStatusIsNotTrash(@Param("ids") List<String> ids);
 
+    List<TestPlanApiCase> selectPlanByIdsAndStatusIsNotTrash(@Param("ids") List<String> ids);
 }
 
