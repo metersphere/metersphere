f822c48890ba1fb6798ad70085a2f4c6870b62b2
guoyuqi <xiaomeinvG@126.com>
2022-07-29 19:10:40 +0800

    feat(测试跟踪): 测试计划保存的运行环境回显，并且作为当前计划下所有用例的环境的默认值
    --user=郭雨琦
    --story=1008894
    https://www.tapd.cn/55049933/prong/stories/view/1155049933001008894
    --story=1008895
    https://www.tapd.cn/55049933/prong/stories/view/1155049933001008895

--- backend/src/main/java/io/metersphere/track/request/testplan/TestPlanRunRequest.java
+++ backend/src/main/java/io/metersphere/track/request/testplan/TestPlanRunRequest.java
@@ -9,22 +9,22 @@ import java.util.Map;
 
 @Getter
 @Setter
-public class TestplanRunRequest {
+public class TestPlanRunRequest {
     private String testPlanId;
     private String projectId;
     private String userId;
     private String triggerMode;//触发方式
     private String mode;//运行模式
     private String reportType;//报告展示方式
-    private String onSampleError;//是否失败停止
-    private String runWithinResourcePool;//是否选择资源池
+    private boolean onSampleError;//是否失败停止
+    private boolean runWithinResourcePool;//是否选择资源池
     private String resourcePoolId;//资源池Id
     private Map<String, String> envMap;
     private String environmentType;
     private String environmentGroupId;
     private List<String> testPlanIds;
     private Boolean isAll;
-    private String  reportId;
+    private String reportId;
     private QueryTestPlanRequest queryTestPlanRequest;
 }
 
---
+++ backend/src/main/java/io/metersphere/track/service/utils/TestPlanRequestUtil.java
@@ -0,0 +1,24 @@
+package io.metersphere.track.service.utils;
+
+import com.alibaba.fastjson.JSONObject;
+
+
+public class TestPlanRequestUtil {
+    private static final String onSampleError = "onSampleError";
+    private static final String runWithinResourcePool = "runWithinResourcePool";
+
+    public static void changeStringToBoolean(JSONObject runModeConfig) {
+        if (runModeConfig != null) {
+            if (runModeConfig.get(onSampleError).equals("true") || runModeConfig.get(onSampleError).equals(true)) {
+                runModeConfig.put(onSampleError, true);
+            } else {
+                runModeConfig.put(onSampleError, false);
+            }
+            if (runModeConfig.get(runWithinResourcePool).equals("true") || runModeConfig.get(runWithinResourcePool).equals(true)) {
+                runModeConfig.put(runWithinResourcePool, true);
+            } else {
+                runModeConfig.put(runWithinResourcePool, false);
+            }
+        }
+    }
+}
