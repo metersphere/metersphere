b353988554d0737d63b9ddb7ea53bae0f76f1e0d
wxg0103 <727495428@qq.com>
2022-08-01 10:29:12 +0800

    refactor(测试跟踪): 优化测试计划运行检查可执行用例的方法

--- backend/src/main/java/io/metersphere/track/service/TestPlanService.java
+++ backend/src/main/java/io/metersphere/track/service/TestPlanService.java
@@ -2064,7 +2064,7 @@ public class TestPlanService {
 
     public String runPlan(TestplanRunRequest testplanRunRequest) {
         //检查测试计划下有没有可以执行的用例；
-        if (!haveExecCase(testplanRunRequest.getTestPlanId()) || !haveUiCase(testplanRunRequest.getTestPlanId())) {
+        if (!haveExecCase(testplanRunRequest.getTestPlanId()) && !haveUiCase(testplanRunRequest.getTestPlanId())) {
             MSException.throwException(Translator.get("plan_warning"));
         }
         String envType = testplanRunRequest.getEnvironmentType();
@@ -2311,7 +2311,7 @@ public class TestPlanService {
                     append.append("/");
                 }
             }
-            if (!haveExecCase(planList.get(i).getId()) || !haveUiCase(planList.get(i).getId())) {
+            if (!haveExecCase(planList.get(i).getId()) && !haveUiCase(planList.get(i).getId())) {
                 haveExecCaseBuilder.append(planList.get(i).getName()).append("; ");
             }
         }
