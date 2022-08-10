15b9d260b5658a5e08c728b71327551a2e37b635
shiziyuan9527 <yuhao.li@fit2cloud.com>
2022-08-10 13:25:05 +0800

    fix(系统设置): 权限页面的数据回显问题

--- backend/src/main/java/io/metersphere/dto/GroupJson.java
+++ backend/src/main/java/io/metersphere/dto/GroupJson.java
@@ -3,10 +3,13 @@ package io.metersphere.dto;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
-public class GroupJson {
+public class GroupJson implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private List<GroupResource> resource;
     private List<GroupPermission> permissions;
 }
--- backend/src/main/java/io/metersphere/dto/GroupResource.java
+++ backend/src/main/java/io/metersphere/dto/GroupResource.java
@@ -2,8 +2,12 @@ package io.metersphere.dto;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class GroupResource {
+public class GroupResource implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String id;
     private String name;
     private Boolean license = false;
