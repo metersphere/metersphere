acf13a286eb0b57dab8470895c4e96e7cbd75e59
song-tianyang <tianyang.song@fit2cloud.com>
2022-08-10 14:27:28 +0800

    fix(接口测试): 修复接口测试报告全选删除时删除所有的场景和接口报告的问题
    --bug=1015725 --user=宋天阳
    【接口测试】接口测试报告-全选接口报告-批量删除，会把场景报告也都删掉，全选场景报告也是
    https://www.tapd.cn/55049933/s/1221535

--- backend/src/main/java/io/metersphere/commons/constants/ReportTypeConstants.java
+++ backend/src/main/java/io/metersphere/commons/constants/ReportTypeConstants.java
@@ -1,6 +1,14 @@
 package io.metersphere.commons.constants;
 
 public enum ReportTypeConstants {
+    /**
+     * 所有场景类型，包括接口场景、UI场景
+     */
+    SCENARIO,
+    /**
+     * 所有接口类型，包括接口、接口用例
+     */
+    API,
     SCENARIO_INTEGRATED,
     SCENARIO_INDEPENDENT,
     API_INTEGRATED,
