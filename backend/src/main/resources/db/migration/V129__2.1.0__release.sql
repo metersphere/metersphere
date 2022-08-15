beb5c76c5be71337ae7dd50d4c938f8af0a543cb
CaptainB <bin@fit2cloud.com>
2022-08-15 10:53:36 +0800

    build: prepare 2.1
    (cherry picked from commit 230d5d8f2cbc8b9023b38f60367d697cb6e298d6)

---
+++ backend/src/main/resources/db/migration/V129__2.1.0__release.sql
@@ -0,0 +1,49 @@
+-- V128__2-1-0_update_api_scenario_load_report
+--
+ALTER TABLE `api_definition_exec_result`
+    ADD `relevance_test_plan_report_id` varchar(50) COMMENT '关联的测试计划报告ID（可以为空)';
+
+ALTER TABLE `api_scenario_report`
+    ADD `relevance_test_plan_report_id` varchar(50) COMMENT '关联的测试计划报告ID（可以为空)';
+
+ALTER TABLE `load_test_report`
+    ADD `relevance_test_plan_report_id` varchar(50) COMMENT '关联的测试计划报告ID（可以为空)';
+
+
+-- V128__2-1-0_init_permission
+--
+INSERT INTO user_group_permission(id, group_id, permission_id, module_id)
+SELECT UUID(), id, 'PROJECT_TRACK_HOME:READ', 'PROJECT_TRACK_HOME'
+FROM `group`
+WHERE type = 'PROJECT';
+
+
+INSERT INTO user_group_permission(id, group_id, permission_id, module_id)
+SELECT UUID(), id, 'PROJECT_API_HOME:READ', 'PROJECT_API_HOME'
+FROM `group`
+WHERE type = 'PROJECT';
+
+
+INSERT INTO user_group_permission(id, group_id, permission_id, module_id)
+SELECT UUID(), id, 'PROJECT_PERFORMANCE_HOME:READ', 'PROJECT_PERFORMANCE_HOME'
+FROM `group`
+WHERE type = 'PROJECT';
+
+-- V2_1_create_API_sync_rule
+--
+CREATE TABLE api_sync_rule_relation
+(
+    id                    varchar(50)          NOT NULL,
+    resource_id           varchar(50)          NOT NULL COMMENT '来源id',
+    resource_type         varchar(50)          NOT NULL COMMENT '来源类型',
+    show_update_rule      tinyint(1) DEFAULT 0 NULL COMMENT '是否显示',
+    api_sync_case_request longtext             NULL COMMENT '同步规则',
+    case_creator          tinyint(1) DEFAULT 1 NULL,
+    scenario_creator      tinyint(1) DEFAULT 1 NULL,
+    sync_case             tinyint(1) DEFAULT 0 NULL COMMENT '是否同步用例',
+    send_notice           tinyint(1) DEFAULT 0 NULL COMMENT '是否发送通知',
+    PRIMARY KEY (`id`),
+    INDEX `resource_id_index` (`resource_id`)
+) ENGINE = INNODB
+  CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
