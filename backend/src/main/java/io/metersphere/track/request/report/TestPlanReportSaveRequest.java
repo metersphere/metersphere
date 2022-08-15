d10a5c25c2f9a2fd3653959323321ab150e61cdf
zhangdahai112 <dahai.zhang@fit2cloud.com>
2022-08-15 18:27:10 +0800

    fix(UI 自动化) UI测试报告不同步测试计划，无消息发送
    --bug=1015636 --user=张大海 【UI 测试】测试计划 UI 测试结果与总报告结果不同步 https://www.tapd.cn/55049933/s/1223786
    --bug=1015719 --user=张大海 【测试跟踪】测试计划 - 只关联UI自动化场景 - 不发送消息通知 https://www.tapd.cn/55049933/s/1223784

--- backend/src/main/java/io/metersphere/track/request/report/TestPlanReportSaveRequest.java
+++ backend/src/main/java/io/metersphere/track/request/report/TestPlanReportSaveRequest.java
@@ -21,14 +21,13 @@ public class TestPlanReportSaveRequest {
     private boolean countResources;
     private boolean apiCaseIsExecuting;
     private boolean scenarioIsExecuting;
+    private boolean uiScenarioIsExecuting;
     private boolean performanceIsExecuting;
 
-//    private String apiCaseIdListJSON;
-//    private String scenarioIdListJSON;
-//    private String performanceIdListJSON;
-    Map<String,String> apiCaseIdMap;
-    Map<String,String> scenarioIdMap;
-    Map<String,String> performanceIdMap;
+    Map<String, String> apiCaseIdMap;
+    Map<String, String> scenarioIdMap;
+    Map<String, String> uiScenarioIdMap;
+    Map<String, String> performanceIdMap;
 
     public TestPlanReportSaveRequest(String reportID, String planId, String userId, String triggerMode) {
         this.reportID = reportID;
@@ -39,21 +38,106 @@ public class TestPlanReportSaveRequest {
         this.countResources = true;
     }
 
-    public TestPlanReportSaveRequest(String reportID, String planId, String userId, String triggerMode, boolean apiCaseIsExecuting, boolean scenarioIsExecuting, boolean performanceIsExecuting,
-                                     Map<String,String> apiCaseIdMap, Map<String,String> scenarioIdMap, Map<String,String> performanceIdMap) {
-        this.reportID = reportID;
-        this.planId = planId;
-        this.userId = userId;
-        this.triggerMode = triggerMode;
+    public TestPlanReportSaveRequest(Builder builder) {
+        this.reportID = builder.reportID;
+        this.planId = builder.planId;
+        this.userId = builder.userId;
+        this.triggerMode = builder.triggerMode;
+        this.countResources = builder.countResources;
+        this.apiCaseIsExecuting = builder.apiCaseIsExecuting;
+        this.scenarioIsExecuting = builder.scenarioIsExecuting;
+        this.uiScenarioIsExecuting = builder.uiScenarioIsExecuting;
+        this.performanceIsExecuting = builder.performanceIsExecuting;
+        this.apiCaseIdMap = builder.apiCaseIdMap;
+        this.scenarioIdMap = builder.scenarioIdMap;
+        this.uiScenarioIdMap = builder.uiScenarioIdMap;
+        this.performanceIdMap = builder.performanceIdMap;
+    }
 
-        this.countResources = false;
+    public static class Builder {
+        private String reportID;
+        private String planId;
+        private String userId;
+        private String triggerMode;
 
-        this.apiCaseIsExecuting = apiCaseIsExecuting;
-        this.scenarioIsExecuting = scenarioIsExecuting;
-        this.performanceIsExecuting = performanceIsExecuting;
+        private boolean countResources;
+        private boolean apiCaseIsExecuting;
+        private boolean scenarioIsExecuting;
+        private boolean uiScenarioIsExecuting;
+        private boolean performanceIsExecuting;
+        Map<String, String> apiCaseIdMap;
+        Map<String, String> scenarioIdMap;
+        Map<String, String> uiScenarioIdMap;
+        Map<String, String> performanceIdMap;
 
-        this.apiCaseIdMap = apiCaseIdMap;
-        this.scenarioIdMap = scenarioIdMap;
-        this.performanceIdMap = performanceIdMap;
+        public Builder setReportID(String reportID) {
+            this.reportID = reportID;
+            return this;
+        }
+
+        public Builder setPlanId(String planId) {
+            this.planId = planId;
+            return this;
+        }
+
+        public Builder setUserId(String userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public Builder setTriggerMode(String triggerMode) {
+            this.triggerMode = triggerMode;
+            return this;
+        }
+
+        public Builder setCountResources(boolean countResources) {
+            this.countResources = countResources;
+            return this;
+        }
+
+        public Builder setApiCaseIsExecuting(boolean apiCaseIsExecuting) {
+            this.apiCaseIsExecuting = apiCaseIsExecuting;
+            return this;
+        }
+
+        public Builder setScenarioIsExecuting(boolean scenarioIsExecuting) {
+            this.scenarioIsExecuting = scenarioIsExecuting;
+            return this;
+        }
+
+        public Builder setUiScenarioIsExecuting(boolean uiScenarioIsExecuting) {
+            this.uiScenarioIsExecuting = uiScenarioIsExecuting;
+            return this;
+        }
+
+        public Builder setPerformanceIsExecuting(boolean performanceIsExecuting) {
+            this.performanceIsExecuting = performanceIsExecuting;
+            return this;
+        }
+
+        public Builder setApiCaseIdMap(Map<String, String> apiCaseIdMap) {
+            this.apiCaseIdMap = apiCaseIdMap;
+            return this;
+        }
+
+        public Builder setScenarioIdMap(Map<String, String> scenarioIdMap) {
+            this.scenarioIdMap = scenarioIdMap;
+            return this;
+        }
+
+
+        public Builder setPerformanceIdMap(Map<String, String> performanceIdMap) {
+            this.performanceIdMap = performanceIdMap;
+            return this;
+        }
+
+        public Builder setUiScenarioIdMap(Map<String, String> uiScenarioIdMap) {
+            this.uiScenarioIdMap = uiScenarioIdMap;
+            return this;
+        }
+
+        public TestPlanReportSaveRequest build() {
+            return new TestPlanReportSaveRequest(this);
+        }
     }
 }
