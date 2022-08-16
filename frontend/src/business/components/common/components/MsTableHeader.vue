284a8a61baa23359a2a93def6617a1cd7a826855
wxg0103 <727495428@qq.com>
2022-08-16 17:42:41 +0800

    fix(系统设置): 修改插件管理上传图标
    --bug=1015858 --user=王孝刚 【系统设置】github#16910，上传按钮前的图标错误，如图
    https://www.tapd.cn/55049933/s/1224499

--- frontend/src/business/components/common/components/MsTableHeader.vue
+++ frontend/src/business/components/common/components/MsTableHeader.vue
@@ -3,14 +3,14 @@
   <div class="ms-table-header">
     <el-row v-if="title" class="table-title" type="flex" justify="space-between" align="middle">
       <slot name="title">
-        {{title}}
+        {{ title }}
       </slot>
     </el-row>
     <el-row type="flex" justify="space-between" align="middle">
       <span class="operate-button">
         <ms-table-button v-permission="createPermission" v-if="showCreate" icon="el-icon-circle-plus-outline"
                          :content="createTip" @click="create"/>
-        <ms-table-button icon="el-icon-download" v-if="showImport" v-permission="uploadPermission"
+        <ms-table-button icon="el-icon-upload2" v-if="showImport" v-permission="uploadPermission"
                          :content="importTip" @click="importData"/>
         <ms-table-button v-if="showRun" icon="el-icon-video-play"
                          type="primary"
@@ -36,161 +36,162 @@
 </template>
 
 <script>
-  import MsTableSearchBar from './MsTableSearchBar';
-  import MsTableButton from './MsTableButton';
-  import MsTableAdvSearchBar from "./search/MsTableAdvSearchBar";
-  import {getCurrentProjectID} from "@/common/js/utils";
-  import MsSearch from "@/business/components/common/components/search/MsSearch";
+import MsTableSearchBar from './MsTableSearchBar';
+import MsTableButton from './MsTableButton';
+import MsTableAdvSearchBar from "./search/MsTableAdvSearchBar";
+import {getCurrentProjectID} from "@/common/js/utils";
+import MsSearch from "@/business/components/common/components/search/MsSearch";
 
-  const requireComponent = require.context('@/business/components/xpack/', true, /\.vue$/);
-  const VersionSelect = requireComponent.keys().length > 0 ? requireComponent("./version/VersionSelect.vue") : {};
+const requireComponent = require.context('@/business/components/xpack/', true, /\.vue$/);
+const VersionSelect = requireComponent.keys().length > 0 ? requireComponent("./version/VersionSelect.vue") : {};
 
-  export default {
-    name: "MsTableHeader",
-    components: {MsTableAdvSearchBar, MsTableSearchBar, MsTableButton,'VersionSelect': VersionSelect.default, MsSearch},
-    data() {
-      return {
-        version:this.currentVersion
-      };
-    },
-    props: {
-      title: {
-        type: String,
-        default() {
-          return null;
-        }
-      },
-      showCreate: {
-        type: Boolean,
-        default: true
-      },
-      showImport: {
-        type: Boolean,
-        default: false
-      },
-      showRun: {
-        type: Boolean,
-        default: false
-      },
-      condition: {
-        type: Object
-      },
-      createTip: {
-        type: String,
-        default() {
-          return this.$t('commons.create');
-        }
-      },
-      importTip: {
-        type: String,
-        default() {
-          return this.$t('commons.import');
-        }
-      },
-      createPermission: {
-        type: Array,
-        default() {
-          return []
-        }
-      },
-      uploadPermission: {
-        type: Array,
-        default() {
-          return []
-        }
-      },
-      runTip: {
-        type: String,
-
-      },
-      currentVersion:{
-        type: String,
-      },
-      isShowVersion:{
-        type: Boolean,
-        default: false
-      },
-      isTesterPermission: {
-        type: Boolean,
-        default: false
-      },
-      tip: {
-        String,
-        default() {
-          return this.$t('commons.search_by_name');
-        }
-      },
-      haveSearch: {
-        Boolean,
-        default() {
-          return true;
-        }
-      },
-      versionOptions:{
-        type: Array,
-        default() {
-          return []
-        }
+export default {
+  name: "MsTableHeader",
+  components: {MsTableAdvSearchBar, MsTableSearchBar, MsTableButton, 'VersionSelect': VersionSelect.default, MsSearch},
+  data() {
+    return {
+      version: this.currentVersion
+    };
+  },
+  props: {
+    title: {
+      type: String,
+      default() {
+        return null;
       }
     },
-    methods: {
-      search(value) {
-        this.$emit('update:condition', this.condition);
-        this.$emit('search', value);
-      },
-      create() {
-        this.$emit('create');
-      },
-      importData() {
-        this.$emit('import');
-      },
-      runTest() {
-        this.$emit('runTest')
-      },
-      historicalDataUpgrade() {
-        this.$emit('historicalDataUpgrade');
-      },
-      changeVersion(type){
-        this.$emit('changeVersion',type);
-      },
-      resetSearchData() {
-        if (this.$refs.searchBar) {
-          this.$refs.searchBar.reset();
-        }
+    showCreate: {
+      type: Boolean,
+      default: true
+    },
+    showImport: {
+      type: Boolean,
+      default: false
+    },
+    showRun: {
+      type: Boolean,
+      default: false
+    },
+    condition: {
+      type: Object
+    },
+    createTip: {
+      type: String,
+      default() {
+        return this.$t('commons.create');
       }
     },
-    computed: {
-      isCombine() {
-        return this.condition.components !== undefined && this.condition.components.length > 0;
-      },
-      projectId() {
-        return getCurrentProjectID();
-      },
+    importTip: {
+      type: String,
+      default() {
+        return this.$t('commons.import');
+      }
+    },
+    createPermission: {
+      type: Array,
+      default() {
+        return []
+      }
+    },
+    uploadPermission: {
+      type: Array,
+      default() {
+        return []
+      }
+    },
+    runTip: {
+      type: String,
 
+    },
+    currentVersion: {
+      type: String,
+    },
+    isShowVersion: {
+      type: Boolean,
+      default: false
+    },
+    isTesterPermission: {
+      type: Boolean,
+      default: false
+    },
+    tip: {
+      String,
+      default() {
+        return this.$t('commons.search_by_name');
+      }
+    },
+    haveSearch: {
+      Boolean,
+      default() {
+        return true;
+      }
+    },
+    versionOptions: {
+      type: Array,
+      default() {
+        return []
+      }
     }
+  },
+  methods: {
+    search(value) {
+      this.$emit('update:condition', this.condition);
+      this.$emit('search', value);
+    },
+    create() {
+      this.$emit('create');
+    },
+    importData() {
+      this.$emit('import');
+    },
+    runTest() {
+      this.$emit('runTest')
+    },
+    historicalDataUpgrade() {
+      this.$emit('historicalDataUpgrade');
+    },
+    changeVersion(type) {
+      this.$emit('changeVersion', type);
+    },
+    resetSearchData() {
+      if (this.$refs.searchBar) {
+        this.$refs.searchBar.reset();
+      }
+    }
+  },
+  computed: {
+    isCombine() {
+      return this.condition.components !== undefined && this.condition.components.length > 0;
+    },
+    projectId() {
+      return getCurrentProjectID();
+    },
+
   }
+}
 </script>
 
 <style>
 
-  .table-title {
-    height: 40px;
-    font-weight: bold;
-    font-size: 18px;
-  }
+.table-title {
+  height: 40px;
+  font-weight: bold;
+  font-size: 18px;
+}
 
 </style>
 
 <style scoped>
 
-  .operate-button {
-    margin-bottom: -5px;
-  }
+.operate-button {
+  margin-bottom: -5px;
+}
 
-  .search-bar {
-    width: 240px
-  }
-  .version-select {
-    padding-left: 10px;
-  }
+.search-bar {
+  width: 240px
+}
+
+.version-select {
+  padding-left: 10px;
+}
 </style>
