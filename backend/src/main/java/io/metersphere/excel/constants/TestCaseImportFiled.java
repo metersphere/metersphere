c620f5e2ddba83c9b981937b292dda92acae36d5
chenjianxing <jianxing.chen@fit2cloud.com>
2022-08-17 11:27:30 +0800

    fix(测试跟踪): 用例导出缺少责任人字段

--- backend/src/main/java/io/metersphere/excel/constants/TestCaseImportFiled.java
+++ backend/src/main/java/io/metersphere/excel/constants/TestCaseImportFiled.java
@@ -1,28 +1,64 @@
 package io.metersphere.excel.constants;
 
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.nacos.common.utils.CollectionUtils;
+import io.metersphere.commons.utils.LogUtil;
+import io.metersphere.excel.domain.TestCaseExcelData;
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.function.Function;
 
 public enum TestCaseImportFiled {
 
-    ID("ID", "ID", "ID"), NAME("用例名称", "用例名稱", "Name"), MODULE("所属模块", "所屬模塊", "Module"),
-    TAG("标签", "標簽", "Tag"), PREREQUISITE("前置条件", "前置條件", "Prerequisite"), REMARK("备注", "備註", "Remark"),
-    STEP_DESC("步骤描述", "步驟描述", "Step description"), STEP_RESULT("预期结果", "預期結果", "Step result"), STEP_MODEL("编辑模式", "編輯模式", "Edit Model"),
-    STATUS("用例状态", "用例狀態", "Case status"), MAINTAINER("责任人", "責任人", "Maintainer"), PRIORITY("用例等级", "用例等級", "Priority");
+    ID("ID", "ID", "ID", TestCaseExcelData::getCustomNum),
+    NAME("用例名称", "用例名稱", "Name", TestCaseExcelData::getName),
+    MODULE("所属模块", "所屬模塊", "Module", TestCaseExcelData::getNodePath),
+    TAG("标签", "標簽", "Tag", TestCaseImportFiled::parseTags),
+    PREREQUISITE("前置条件", "前置條件", "Prerequisite", TestCaseExcelData::getPrerequisite),
+    REMARK("备注", "備註", "Remark", TestCaseExcelData::getRemark),
+    STEP_DESC("步骤描述", "步驟描述", "Step description", TestCaseExcelData::getStepDesc),
+    STEP_RESULT("预期结果", "預期結果", "Step result", TestCaseExcelData::getStepResult),
+    STEP_MODEL("编辑模式", "編輯模式", "Edit Model", TestCaseExcelData::getStepModel),
+    STATUS("用例状态", "用例狀態", "Case status", TestCaseExcelData::getStatus),
+    MAINTAINER("责任人", "責任人", "Maintainer", TestCaseExcelData::getMaintainer),
+    PRIORITY("用例等级", "用例等級", "Priority", TestCaseExcelData::getPriority);
 
     private Map<Locale, String> filedLangMap;
+    private Function<TestCaseExcelData, String> parseFunc;
 
     public Map<Locale, String> getFiledLangMap() {
         return this.filedLangMap;
     }
 
-    TestCaseImportFiled(String zn, String chineseTw, String us) {
+    TestCaseImportFiled(String zn, String chineseTw, String us, Function<TestCaseExcelData, String> parseFunc) {
         this.filedLangMap = new HashMap<>() {{
-           put(Locale.SIMPLIFIED_CHINESE, zn);
-           put(Locale.TRADITIONAL_CHINESE, chineseTw);
-           put(Locale.US, us);
+            put(Locale.SIMPLIFIED_CHINESE, zn);
+            put(Locale.TRADITIONAL_CHINESE, chineseTw);
+            put(Locale.US, us);
         }};
+        this.parseFunc = parseFunc;
+    }
+
+    public String parseExcelDataValue(TestCaseExcelData excelData) {
+        return parseFunc.apply(excelData);
+    }
+
+    private static String parseTags(TestCaseExcelData excelData) {
+        String tags = "";
+        try {
+            if (excelData.getTags() != null) {
+                JSONArray arr = JSONArray.parseArray(excelData.getTags());
+                if (CollectionUtils.isNotEmpty(arr)) {
+                    tags = StringUtils.joinWith(",", arr.toArray());
+                }
+            }
+        } catch (Exception e) {
+            LogUtil.error(e);
+        }
+        return tags;
     }
 }
