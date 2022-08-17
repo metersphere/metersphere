ededcb10420eb3096f7784b8cf0a4310d868ed32
song-tianyang <tianyang.song@fit2cloud.com>
2022-08-17 16:32:48 +0800

    fix(接口测试): 修复批量执行用例时未生成测试报告的问题
    修复批量执行用例时未生成测试报告的问题

--- backend/src/main/java/io/metersphere/base/mapper/ext/ExtApiScenarioReportProvider.java
+++ backend/src/main/java/io/metersphere/base/mapper/ext/ExtApiScenarioReportProvider.java
@@ -1,6 +1,7 @@
 package io.metersphere.base.mapper.ext;
 
 import io.metersphere.api.dto.automation.APIScenarioReportResult;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 
@@ -49,11 +50,16 @@ public class ExtApiScenarioReportProvider {
                     .append("','")
                     .append(result.getReportType())
                     .append("','")
-                    .append(result.getEnvConfig())
-                    .append("','")
-                    .append(result.getRelevanceTestPlanReportId())
-                    .append("'")
-                    .append(")");
+                    .append(result.getEnvConfig());
+            //判断有没有关联的测试报告ID
+            if (StringUtils.isBlank(result.getRelevanceTestPlanReportId())) {
+                sqlList.append("', null");
+            } else {
+                sqlList.append("','")
+                        .append(result.getRelevanceTestPlanReportId())
+                        .append("'");
+            }
+            sqlList.append(")");
             if (i < list.size() - 1) {
                 sqlList.append(",");
             }
