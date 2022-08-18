06aa62fd840953b4fc498e3d72acb59d29761f1c
fit2-zhao <yong.zhao@fit2cloud.com>
2022-08-18 15:58:36 +0800

    fix(文件管理): 修复默认多出一个默认模块问题
    --bug=1015975 --user=赵勇 【项目设置】文件管理，历史项目有两个默认模块 https://www.tapd.cn/55049933/s/1226824

--- backend/src/main/java/io/metersphere/commons/constants/ApiTestConstants.java
+++ backend/src/main/java/io/metersphere/commons/constants/ApiTestConstants.java
@@ -3,5 +3,4 @@ package io.metersphere.commons.constants;
 public class ApiTestConstants {
     public static final String JAR_PATH = "JAR_PATH";
     public static final String DEF_MODULE = "DEF_MODULE";
-    public static final String DEF_MODULE_ID = "moduleIds";
 }
