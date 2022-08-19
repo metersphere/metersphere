9084017d77581b9f39fc4400cf89d728b7de6407
zhangdahai112 <dahai.zhang@fit2cloud.com>
2022-08-19 17:07:06 +0800

    fix(UI 自动化): 选择所有ui场景执行报错修复
    --bug=1015973 --user=张大海 【UI测试】场景列表-选择所有数据-批量执行报错 https://www.tapd.cn/55049933/s/1228148

--- backend/src/main/java/io/metersphere/dto/RunUiScenarioRequest.java
+++ backend/src/main/java/io/metersphere/dto/RunUiScenarioRequest.java
@@ -4,7 +4,14 @@ import io.metersphere.api.dto.automation.RunScenarioRequest;
 import io.metersphere.track.dto.UiRunModeConfigDTO;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class RunUiScenarioRequest extends RunScenarioRequest {
     private UiRunModeConfigDTO uiConfig;
+
+    @Override
+    public void setIds(List<String> ids) {
+        super.setIds(ids);
+    }
 }
