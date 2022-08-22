49a6de5531d60470e02bb4bb4da811ddbc1714ae
zhangyong <yong.zhang@fit2cloud.com>
2022-08-22 18:35:41 +0800

    fix(测试跟踪): 多选导入用例时责任人id忽略大小写.
    --bug=1015896 --user=张勇 【测试跟踪】用户id是小写，导入用例时责任人id填写大写，导入失败 https://www.tapd.cn/55049933/s/1229922

--- backend/src/main/java/io/metersphere/track/validate/CustomFieldMultipleMemberValidator.java
+++ backend/src/main/java/io/metersphere/track/validate/CustomFieldMultipleMemberValidator.java
@@ -18,10 +18,13 @@ public class CustomFieldMultipleMemberValidator extends CustomFieldMemberValidat
         }
 
         for (String item : parse2Array(customField.getName(), value)) {
-            if (!userIdMap.containsKey(item) && !userNameMap.containsKey(item)) {
-                CustomFieldValidateException.throwException(String.format(Translator.get("custom_field_member_tip"), customField.getName()));
+            long count = userIdMap.entrySet().stream().filter(e -> e.getKey().toLowerCase().equals(item.toLowerCase()) ||
+                    e.getValue().toLowerCase().equals(item.toLowerCase())).count();
+            if(count > 0){
+                return;
             }
         }
+        CustomFieldValidateException.throwException(String.format(Translator.get("custom_field_member_tip"), customField.getName()));
     }
 
     @Override
