22acc0a44be2086cf89af7b96b71c7ddb102375d
guoyuqi <xiaomeinvG@126.com>
2022-08-22 16:40:37 +0800

    refactor(接口测试): 接口同步功能优化
    --user=郭雨琦
    https://www.tapd.cn/55049933/bugtrace/bugs/view/1155049933001016065
    https://www.tapd.cn/55049933/bugtrace/bugs/view/1155049933001015922
    refactor(接口测试): 接口同步发消息功能优化
    
    --user=郭雨琦
    https://www.tapd.cn/55049933/bugtrace/bugs/view/1155049933001015943
    refactor(接口测试): 接口同步功能优化
    
    --user=郭雨琦
    https://www.tapd.cn/55049933/bugtrace/bugs/view/1155049933001016065
    https://www.tapd.cn/55049933/bugtrace/bugs/view/1155049933001015922

--- backend/src/main/java/io/metersphere/api/service/ApiCaseBatchSyncService.java
+++ backend/src/main/java/io/metersphere/api/service/ApiCaseBatchSyncService.java
@@ -5,10 +5,12 @@ import io.metersphere.base.domain.ApiTestCase;
 import io.metersphere.base.domain.ApiTestCaseWithBLOBs;
 import io.metersphere.base.mapper.ApiTestCaseMapper;
 
+import java.util.Map;
+
 public interface ApiCaseBatchSyncService {
     void oneClickSyncCase(String apiUpdateRule, ApiDefinitionWithBLOBs apiDefinitionWithBLOBs, ApiTestCaseMapper apiTestCaseMapper, ApiTestCaseWithBLOBs testCases);
 
-    void sendApiNotice(ApiDefinitionWithBLOBs apiDefinitionWithBLOBs);
+    void sendApiNotice(ApiDefinitionWithBLOBs apiDefinitionWithBLOBs, Map<String, Object> paramMap);
 
     void sendCaseNotice(ApiTestCase apiTestCase);
 }
