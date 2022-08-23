4991da7c1e198cc26c2898fbacf359baeb9794f8
song-tianyang <tianyang.song@fit2cloud.com>
2022-08-23 17:04:07 +0800

    feat(测试跟踪、接口测试): 场景用例、接口用例、接口场景在点击模块节点时实时更新节点的数据统计数量
    --bug=1016043 --user=宋天阳 【UI测试】场景模块总数和场景列表数量不一致
    https://www.tapd.cn/55049933/s/1229521

--- frontend/src/business/components/track/module/TestCaseTrashNodeTree.vue
+++ frontend/src/business/components/track/module/TestCaseTrashNodeTree.vue
@@ -1,16 +1,17 @@
 <template>
   <ms-node-tree class="node-tree"
-           v-loading="result.loading"
-           local-suffix="test_case"
-           default-label="未规划用例"
-           @nodeSelectEvent="publicNodeChange"
-           :tree-nodes="trashTreeNodes"
-           ref="trashNodeTree"/>
+                v-loading="result.loading"
+                local-suffix="test_case"
+                default-label="未规划用例"
+                @nodeSelectEvent="publicNodeChange"
+                :tree-nodes="trashTreeNodes"
+                ref="trashNodeTree"/>
 </template>
 
 <script>
 import MsNodeTree from "@/business/components/track/common/NodeTree";
 import {getTestCaseTrashNodes} from "@/network/testCase";
+
 export default {
   name: "TestCaseTrashNodeTree",
   components: {MsNodeTree},
@@ -26,6 +27,26 @@ export default {
   methods: {
     publicNodeChange(node, nodeIds, pNodes) {
       this.$emit("nodeSelectEvent", node, node.data.id === 'root' ? [] : nodeIds, pNodes);
+      this.nohupReloadTree(node.data.id);
+    },
+    nohupReloadTree(selectNodeId) {
+      getTestCaseTrashNodes(this.caseCondition, data => {
+        if (data && data.length > 0) {
+          this.trashTreeNodes = data[0].children;
+        } else {
+          this.trashTreeNodes = [];
+        }
+        this.$nextTick(() => {
+          if (this.$refs.trashNodeTree) {
+            this.trashTreeNodes.forEach(firstLevel => {
+              this.$refs.trashNodeTree.nodeExpand(firstLevel);
+            })
+          }
+          if (selectNodeId) {
+            this.$refs.trashNodeTree.justSetCurrentKey(selectNodeId);
+          }
+        })
+      });
     },
     list() {
       this.result = getTestCaseTrashNodes(this.caseCondition, data => {
@@ -36,7 +57,7 @@ export default {
         }
         if (this.$refs.trashNodeTree) {
           this.trashTreeNodes.forEach(firstLevel => {
-              this.$refs.trashNodeTree.nodeExpand(firstLevel);
+            this.$refs.trashNodeTree.nodeExpand(firstLevel);
           })
         }
       });
