80c117dd26676ae18fac8d356dd05f8ffd82396d
zhangdahai112 <dahai.zhang@fit2cloud.com>
2022-08-31 01:00:06 +0800

    fix(UI 自动化): 导出测试报告，UI用例测试结果看不了
    --bug=1016327 --user=张大海 【测试跟踪】导出测试报告，UI用例测试结果看不了 https://www.tapd.cn/55049933/s/1236268
    --bug=1016143 --user=张大海 【测试跟踪】分享测试计划报告链接 / 导出测试计划报告，查看ui步骤结果，一直在转圈 https://www.tapd.cn/55049933/s/1236266

---
+++ backend/src/main/java/io/metersphere/constants/SystemConstants.java
@@ -0,0 +1,13 @@
+package io.metersphere.constants;
+
+/**
+ * 系统级别通用的常量和枚举
+ */
+public final class SystemConstants {
+
+    public enum TestTypeEnum {
+        API,
+        UI,
+        PERFORMANCE;
+    }
+}
