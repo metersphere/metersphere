ca9b65331a4ec4eb978bb230b2fbaae5f54831fb
song-cc-rock <changchang.song@fit2cloud.com>
2022-08-31 13:37:40 +0800

    fix(测试跟踪): 功能用例导入状态字段有误

---
+++ backend/src/main/java/io/metersphere/commons/constants/TestCaseStatusConstants.java
@@ -0,0 +1,14 @@
+package io.metersphere.commons.constants;
+
+public class TestCaseStatusConstants {
+
+    public static final String UNDERWAY_US = "Underway";
+    public static final String UNDERWAY_CN = "进行中";
+    public static final String UNDERWAY_TW = "進行中";
+    public static final String PREPARE_US = "Prepare";
+    public static final String PREPARE_CN = "未开始";
+    public static final String PREPARE_TW = "未開始";
+    public static final String COMPLETED_US = "Completed";
+    public static final String COMPLETED_CN = "已完成";
+    public static final String COMPLETED_TW = "已完成";
+}
