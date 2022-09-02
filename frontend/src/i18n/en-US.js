ed48629a01a66c27a1ee75f71e7d66a238656bad
zhangdahai112 <dahai.zhang@fit2cloud.com>
2022-09-02 13:33:40 +0800

    feat(UI 自动化): 元素库操作列增加描述列
    --story=1009477 --user=张大海 【UI测试】UI优化 https://www.tapd.cn/55049933/s/1238256

--- frontend/src/i18n/en-US.js
+++ frontend/src/i18n/en-US.js
@@ -2915,6 +2915,7 @@ export default {
     not_screentshot: "No screenshot",
     error_step_screenshot: "Screenshot of exception",
     downloadScreenshot: "Download screenshot file",
+    description: "remark",
   },
   project_application: {
     workstation: {
