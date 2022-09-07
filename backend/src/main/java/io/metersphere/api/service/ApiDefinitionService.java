a17566fc397a8f9782be84f31bd2a75ee802c807
wxg0103 <727495428@qq.com>
2022-09-07 11:51:25 +0800

    fix(接口定义): 无法使用jsonpath推荐断言
    --bug=1016162 --user=王孝刚
    【接口测试】接口提取参数，执行ms登录接口，使用推荐jsonpath提取，提示响应结果不是json格式【+github#17747】
    https://www.tapd.cn/55049933/s/1239847

--- backend/src/main/java/io/metersphere/api/service/ApiDefinitionService.java
+++ backend/src/main/java/io/metersphere/api/service/ApiDefinitionService.java
@@ -357,8 +357,7 @@ public class ApiDefinitionService {
         MockConfigService mockConfigService = CommonBeanFactory.getBean(MockConfigService.class);
         mockConfigService.updateMockReturnMsgByApi(returnModel);
         FileUtils.createBodyFiles(request.getRequest().getId(), bodyFiles);
-        //删除不需要的文件
-        FileUtils.deleteBodyFiles(request.getRequest());
+        
         return getById(returnModel.getId());
     }
 
