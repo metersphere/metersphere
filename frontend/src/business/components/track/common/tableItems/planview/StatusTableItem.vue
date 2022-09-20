f33df832cd7e20e8d041326aafa0de8fff88cb76
wxg0103 <727495428@qq.com>
2022-09-20 11:36:20 +0800

    fix(测试跟踪): 修复关联场景执行状态不一致的缺陷
    --bug=1016868 --user=王孝刚 【接口测试】测试计划中的场景用例列表中和报告统计中的“执行结果”状态不一致
    https://www.tapd.cn/55049933/s/1246436

--- frontend/src/business/components/track/common/tableItems/planview/StatusTableItem.vue
+++ frontend/src/business/components/track/common/tableItems/planview/StatusTableItem.vue
@@ -1,6 +1,6 @@
 <template>
     <span>
-      <ms-tag v-if="value == 'Prepare'" type="info" :content="$t('test_track.plan.plan_status_prepare')"/>
+      <ms-tag v-if="value == 'Prepare'" :content="$t('api_test.home_page.detail_card.unexecute')" type="info"/>
       <ms-tag v-else-if="value == 'Underway'" type="primary" :content="$t('test_track.plan.plan_status_running')"/>
       <ms-tag v-else-if="value == 'Pass'" type="success" :content="$t('test_track.plan_view.pass')"/>
       <ms-tag v-else-if="value == 'UnPass'" type="danger" :content="$t('test_track.plan_view.not_pass')"/>
@@ -9,24 +9,28 @@
       <ms-tag v-else-if="value == 'Failure'" type="danger" :content="$t('test_track.plan_view.failure')"/>
       <ms-tag v-else-if="value == 'Blocking'" type="warning" :content="$t('test_track.plan_view.blocking')"/>
       <ms-tag v-else-if="value == 'Skip'" type="info" :content="$t('test_track.plan_view.skip')"/>
-      <ms-tag v-else-if="value == 'ErrorReportResult'" type="warning" style="background-color: #F6972A; border-color: #F6972A" :content="$t('error_report_library.option.name')"/>
+      <ms-tag v-else-if="value == 'ErrorReportResult'" :content="$t('error_report_library.option.name')"
+              style="background-color: #F6972A; border-color: #F6972A"
+              type="warning"/>
       <ms-tag v-else-if="value == 'ApiCaseStop'" type="info" :content="$t('api_test.home_page.detail_card.unexecute')"/>
-      <ms-tag v-else-if="value == 'ApiCasePrepare'" type="info" :content="$t('api_test.home_page.detail_card.unexecute')"/>
+      <ms-tag v-else-if="value == 'ApiCasePrepare'" :content="$t('api_test.home_page.detail_card.unexecute')"
+              type="info"/>
       <ms-tag v-else type="info" :content="value"/>
     </span>
 </template>
 
 <script>
-    import MsTag from "../../../../common/components/MsTag";
-    export default {
-      name: "StatusTableItem",
-      components: {MsTag},
-      props: {
-        value: {
-          type: String
-        }
-      }
+import MsTag from "../../../../common/components/MsTag";
+
+export default {
+  name: "StatusTableItem",
+  components: {MsTag},
+  props: {
+    value: {
+      type: String
     }
+  }
+}
 </script>
 
 <style scoped>
