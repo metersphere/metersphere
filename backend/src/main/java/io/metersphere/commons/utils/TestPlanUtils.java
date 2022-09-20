4710e9e25bc3113932fa7ad476c18614d2a0ef71
chenjianxing <jianxing.chen@fit2cloud.com>
2022-09-20 13:55:07 +0800

    fix(测试跟踪): 测试计划报告执行率计算有误
    --bug=1016931 --user=陈建星 【测试跟踪】报告列表查看测试报告，执行率计算错误 https://www.tapd.cn/55049933/s/1246742

--- backend/src/main/java/io/metersphere/commons/utils/TestPlanUtils.java
+++ backend/src/main/java/io/metersphere/commons/utils/TestPlanUtils.java
@@ -67,7 +67,8 @@ public class TestPlanUtils {
             report.setCaseCount((report.getCaseCount() == null ? 0 : report.getCaseCount()) + 1);
             String status = item.getStatus();
             if (StringUtils.isNotBlank(status)
-                    && !StringUtils.equalsAny(status, TestPlanTestCaseStatus.Underway.name(), TestPlanTestCaseStatus.Prepare.name())) {
+                    && !StringUtils.equalsAnyIgnoreCase(status, TestPlanTestCaseStatus.Underway.name(), TestPlanTestCaseStatus.Prepare.name(),
+                    ExecuteResult.UN_EXECUTE.getValue(), ExecuteResult.STOP.getValue())) {
                 // 计算执行过的数量
                 report.setExecuteCount(report.getExecuteCount() + 1);
                 if (StringUtils.equals(successStatus, status)) {
