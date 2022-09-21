d64251d44d7be6865984cb65961a59d1e292c3d9
chenjianxing <jianxing.chen@fit2cloud.com>
2022-09-21 10:59:32 +0800

    fix(测试跟踪): 富文本框自定义字段非空校验不准确
    --bug=1016851 --user=陈建星 【项目设置】接口模版，富文本框设置必填后，填写内容再删除，保存没提示必填 https://www.tapd.cn/55049933/s/1248374

--- frontend/src/business/components/track/case/components/MsMarkDownText.vue
+++ frontend/src/business/components/track/case/components/MsMarkDownText.vue
@@ -1,7 +1,7 @@
 <template>
   <mavon-editor :id="id" :editable="!disabled" @imgAdd="imgAdd" :default-open="defaultOpenValue"
                 :xss-options="xssOptions" :style="{'min-height': customMinHeight + 'px', 'min-width': '100px'}"
-                @change="$emit('change')"
+                @change="change"
                 :subfield="false" :toolbars="toolbars" :language="language" :toolbarsFlag="!disabled"
                 @imgDel="imgDel" v-model="data[prop]" ref="md"/>
 </template>
@@ -202,6 +202,11 @@ export default {
     },
     toolbar_left_click(param) {
       this.$refs.md.toolbar_left_click(param);
+    },
+    change() {
+      this.$nextTick(() => {
+        this.$emit('change');
+      });
     }
   }
 }
