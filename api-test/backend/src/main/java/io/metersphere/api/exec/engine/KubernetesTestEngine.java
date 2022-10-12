2eca87f9631e00fc8ae176bcdc2d74e7fe47d0e5
fit2-zhao <yong.zhao@fit2cloud.com>
2022-10-12 17:22:04 +0800

    fix(接口测试): 修复k8s无法执行问题
    --bug=1017872 --user=赵勇 【接口测试】接口CASE列表中批量执行case-选择K8s资源池-集合报告执行被停止 https://www.tapd.cn/55049933/s/1259827

--- api-test/backend/src/main/java/io/metersphere/api/exec/engine/KubernetesTestEngine.java
+++ api-test/backend/src/main/java/io/metersphere/api/exec/engine/KubernetesTestEngine.java
@@ -1,6 +1,73 @@
 package io.metersphere.api.exec.engine;
 
-import io.metersphere.engine.Engine;
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.metersphere.base.domain.TestResource;
+import io.metersphere.commons.exception.MSException;
+import io.metersphere.commons.utils.JSON;
+import io.metersphere.dto.JmeterRunRequestDTO;
+import io.metersphere.utils.LoggerUtil;
+import io.metersphere.xpack.resourcepool.engine.provider.ClientCredential;
+import io.metersphere.xpack.resourcepool.engine.provider.KubernetesProvider;
+import org.apache.commons.beanutils.ConstructorUtils;
+import org.apache.commons.collections4.CollectionUtils;
+import org.reflections.Reflections;
 
-public interface KubernetesTestEngine extends Engine {
+import java.util.Set;
+
+public class KubernetesTestEngine extends AbstractEngine {
+    private JmeterRunRequestDTO runRequest;
+
+    // 初始化API调用
+    public KubernetesTestEngine(JmeterRunRequestDTO runRequest) {
+        super.initApiConfig(runRequest);
+        this.runRequest = runRequest;
+    }
+
+    private static Class<? extends KubernetesProvider> providerClass = null;
+
+    static {
+        Set<Class<? extends KubernetesProvider>> subTypes = new Reflections("io.metersphere.xpack").getSubTypesOf(KubernetesProvider.class);
+        if (CollectionUtils.isNotEmpty(subTypes)) {
+            providerClass = subTypes.stream().findFirst().get();
+        }
+    }
+
+    @Override
+    public void start() {
+        resourceList.forEach(r -> {
+            runApi(r);
+        });
+    }
+
+    private void runApi(TestResource resource) {
+        try {
+            ClientCredential clientCredential = JSON.parseObject(resource.getConfiguration(), ClientCredential.class);
+            KubernetesProvider kubernetesProvider = ConstructorUtils.invokeConstructor(providerClass, clientCredential);
+            KubernetesClient client = kubernetesProvider.getClient();
+            Pod pod = KubernetesApiExec.getExecPod(clientCredential, client);
+
+            StringBuffer logMsg = new StringBuffer("当前报告：【" + runRequest.getReportId() + "】资源：【" + runRequest.getTestId() + "】")
+                    .append("\n").append("namespace：").append(clientCredential.getNamespace())
+                    .append("\n").append("Pod信息：【 ")
+                    .append(JSON.toJSONString(pod.getMetadata())).append(" 】");
+            LoggerUtil.info(logMsg);
+            // 拼接CURL执行命令
+            StringBuffer command = new StringBuffer("curl -H \"Accept: application/json\" -H \"Content-type: application/json\" -X POST -d").append(" ");
+            command.append("'").append(JSON.toJSONString(runRequest)).append("'"); // 请求参数
+            command.append(" ").append("--connect-timeout 30");  // 设置连接超时时间为30S
+            command.append(" ").append("--max-time 120");  // 设置请求超时时间为120S
+            command.append(" ").append("--retry 3");  // 设置重试次数3次
+            command.append(" ").append("http://127.0.0.1:8082/jmeter/api/start");
+            KubernetesApiExec.newExecWatch(client, clientCredential.getNamespace(), pod.getMetadata().getName(), command.toString());
+        } catch (Exception e) {
+            LoggerUtil.error("当前报告：【" + runRequest.getReportId() + "】资源：【" + runRequest.getTestId() + "】CURL失败：", e);
+            MSException.throwException(e);
+        }
+    }
+
+    @Override
+    public void stop() {
+
+    }
 }
