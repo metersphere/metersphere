cdf302e2fb44061fcb61d009f0486e80f4d85d3f
nathan.liu <yao.liu@fit2cloud.com>
2022-10-16 13:40:33 +0800

    fix(UI自动化): 修复UI grid配置兼容localhost

--- framework/sdk-parent/frontend/src/components/personal/UiSetting.vue
+++ framework/sdk-parent/frontend/src/components/personal/UiSetting.vue
@@ -84,7 +84,8 @@ export default {
     isURL(url) {
       let regEx =
         /^(http|https):\/\/(([0-9]{1,3}\.){3}[0-9]{1,3}|([0-9a-z_!~*'()-]+\.)*([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\.[a-z]{2,6})(:[0-9]{1,5})?$/;
-      if (regEx.test(url)) {
+      let localhostRegEx = /^http:\/\/localhost:[0-9]{1,5}$/;
+      if (regEx.test(url) || localhostRegEx.test(url)) {
         return true;
       } else {
         return false;
