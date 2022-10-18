778a3283075e0c9068b2de0b314adf192b52e3db
fit2-zhao <yong.zhao@fit2cloud.com>
2022-10-18 19:08:04 +0800

    fix(接口测试): 用例消息通知跳转路由错误修改
    --bug=1018369 --user=赵勇 【接口测试】接口用例的保存&同步设置窗口中，case消息通知链接不对 https://www.tapd.cn/55049933/s/1266724

--- api-test/frontend/src/business/definition/components/case/ApiCaseItem.vue
+++ api-test/frontend/src/business/definition/components/case/ApiCaseItem.vue
@@ -804,7 +804,7 @@ export default {
     },
     gotoApiMessage() {
       let apiResolve = this.$router.resolve({
-        name: 'MessageSettings'
+        name: '/project/messagesettings'
       });
       window.open(apiResolve.href, '_blank');
     },
