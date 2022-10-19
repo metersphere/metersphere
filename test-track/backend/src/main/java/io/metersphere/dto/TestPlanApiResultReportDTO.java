aad2f4f52e54540d5101d5a373e298de4d634e6f
CaptainB <bin@fit2cloud.com>
2022-10-19 12:55:24 +0800

    feat(消息通知): 测试计划增加执行结果变量

--- test-track/backend/src/main/java/io/metersphere/dto/TestPlanApiResultReportDTO.java
+++ test-track/backend/src/main/java/io/metersphere/dto/TestPlanApiResultReportDTO.java
@@ -4,11 +4,12 @@ import io.metersphere.plan.dto.TestCaseReportStatusResultDTO;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Getter
 @Setter
-public class TestPlanApiResultReportDTO {
+public class TestPlanApiResultReportDTO implements Serializable {
     private List<TestCaseReportStatusResultDTO> apiCaseData;
     private List<TestCaseReportStatusResultDTO> apiScenarioData;
     private List<TestCaseReportStatusResultDTO> apiScenarioStepData;
--- test-track/backend/src/main/java/io/metersphere/dto/TestPlanLoadResultReportDTO.java
+++ test-track/backend/src/main/java/io/metersphere/dto/TestPlanLoadResultReportDTO.java
@@ -4,11 +4,12 @@ import io.metersphere.plan.dto.TestCaseReportStatusResultDTO;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Getter
 @Setter
-public class TestPlanLoadResultReportDTO {
+public class TestPlanLoadResultReportDTO implements Serializable {
     private List<TestCaseReportStatusResultDTO> caseData;
 }
 
