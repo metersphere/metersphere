419003fd1626c86a84573145529711672c86c725
song-tianyang <tianyang.song@fit2cloud.com>
2022-10-20 18:12:42 +0800

    fix(测试跟踪): 修复测试计划获取不到失败通知的问题
    --bug=1018567 --user=宋天阳 【测试跟踪】定时任务执行测试计划，执行失败收不到通知
    https://www.tapd.cn/55049933/s/1270053

--- test-track/backend/src/main/java/io/metersphere/plan/service/remote/api/PlanApiDefinitionExecResultService.java
+++ test-track/backend/src/main/java/io/metersphere/plan/service/remote/api/PlanApiDefinitionExecResultService.java
@@ -2,8 +2,6 @@ package io.metersphere.plan.service.remote.api;
 
 import io.metersphere.dto.PlanReportCaseDTO;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Map;
@@ -14,10 +12,10 @@ public class PlanApiDefinitionExecResultService extends ApiTestService {
     private static final String BASE_UEL = "/api/definition/exec/result";
 
     public List<PlanReportCaseDTO> selectForPlanReport(List<String> apiReportIds) {
-       return microService.postForDataArray(serviceName, BASE_UEL + "/plan/report", apiReportIds, PlanReportCaseDTO.class);
+        return microService.postForDataArray(serviceName, BASE_UEL + "/plan/report", apiReportIds, PlanReportCaseDTO.class);
     }
 
     public Map<String, String> selectReportResultByReportIds(List<String> reportIds) {
-        return (Map<String, String>) microService.postForData(serviceName, BASE_UEL + "/plan/status/map", reportIds);
+        return microService.postForData(serviceName, BASE_UEL + "/plan/status/map", reportIds, Map.class);
     }
 }
