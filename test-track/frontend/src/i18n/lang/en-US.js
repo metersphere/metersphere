ce4d133fc394263c9dcd27b1f08770aa7c7a8df3
chenjianxing <jianxing.chen@fit2cloud.com>
2022-10-21 15:15:51 +0800

    fix(测试跟踪): 测试跟踪首页国际化问题

--- test-track/frontend/src/i18n/lang/en-US.js
+++ test-track/frontend/src/i18n/lang/en-US.js
@@ -2,7 +2,26 @@ import el from "element-ui/lib/locale/lang/en";
 import fu from "fit2cloud-ui/src/locale/lang/en_US"; // 加载fit2cloud的内容
 import mf from "metersphere-frontend/src/i18n/lang/en-US"
 
-const message = {}
+const message = {
+  home: {
+    table: {
+      index: "Index",
+      task_type: "Task Type",
+      run_rule: "Rule",
+      task_status: "Status",
+      next_execution_time: "Next Execution Time",
+      create_user: "Creator",
+      update_time: "Update time",
+    },
+    case: {
+      index: "Ranking",
+      case_name: "Case Name",
+      case_type: "Case Type",
+      test_plan: "Test Plan",
+      failure_times: "Failure times",
+    }
+  }
+}
 export default {
   ...el,
   ...fu,
--- test-track/frontend/src/i18n/lang/zh-CN.js
+++ test-track/frontend/src/i18n/lang/zh-CN.js
@@ -2,7 +2,26 @@ import el from "element-ui/lib/locale/lang/zh-CN"; // 加载element的内容
 import fu from "fit2cloud-ui/src/locale/lang/zh-CN"; // 加载fit2cloud的内容
 import mf from "metersphere-frontend/src/i18n/lang/zh-CN"
 
-const message = {}
+const message = {
+  home: {
+    table: {
+      index: "序号",
+      task_type: "任务类型",
+      run_rule: "运行规则",
+      task_status: "任务状态",
+      next_execution_time: "下次执行时间",
+      create_user: "创建人",
+      update_time: "更新时间",
+    },
+    case: {
+      index: "排名",
+      case_name: "用例名称",
+      case_type: "用例类型",
+      test_plan: "所属测试计划",
+      failure_times: "失败次数",
+    }
+  }
+}
 
 export default {
   ...el,
