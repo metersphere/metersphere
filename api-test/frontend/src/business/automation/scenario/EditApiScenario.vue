cbafdca6812d76c9b1d244b2b646c88c88ebd748
fit2-zhao <yong.zhao@fit2cloud.com>
2022-10-24 12:33:02 +0800

    fix(接口测试): 修复调试偶发session被清除无法停止问题
    --bug=1017936 --user=赵勇 场景中添加自定义SQL请求-调试执行成功，但是显示“停止”（偶现） https://www.tapd.cn/55049933/s/1274008

--- api-test/frontend/src/business/automation/scenario/EditApiScenario.vue
+++ api-test/frontend/src/business/automation/scenario/EditApiScenario.vue
@@ -1034,6 +1034,7 @@ export default {
         this.debugLoading = false;
         this.message = "stop";
         this.stopDebug = "stop";
+        this.messageWebSocket.close();
         this.reload();
       }
     },
