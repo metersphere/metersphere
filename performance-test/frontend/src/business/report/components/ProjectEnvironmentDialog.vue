a5cde494dc6fe88591e3ba915aaa177fd8bc0a02
song-tianyang <tianyang.song@fit2cloud.com>
2022-10-24 18:32:08 +0800

    fix(性能设置): 对性能报告的环境弹窗设置高优先级z-index来保证弹窗不会被遮挡
    --bug=1018704 --user=宋天阳 【性能测试】使用环境组执行测试-任务中心查看-点击...-不展示所有环境
    https://www.tapd.cn/55049933/s/1275090

--- performance-test/frontend/src/business/report/components/ProjectEnvironmentDialog.vue
+++ performance-test/frontend/src/business/report/components/ProjectEnvironmentDialog.vue
@@ -5,6 +5,7 @@
     width="30%"
     :append-to-body="true"
     :destroy-on-close="true"
+    z-index="99998"
     :before-close="handleClose">
     <div>
       <div v-if="projectEnvMap" type="flex">
@@ -40,6 +41,7 @@ export default {
       this.dialogVisible = true;
       this.projectEnvMap = projectEnvMap;
     }
+
   }
 }
 </script>
