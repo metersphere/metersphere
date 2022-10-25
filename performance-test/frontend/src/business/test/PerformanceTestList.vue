466e04879d7bffdfa7999470c476aeaf058f0f15
CaptainB <bin@fit2cloud.com>
2022-10-25 10:28:18 +0800

    fix(性能测试): 修复删除测试选择当前版本失败的问题
    --bug=1018799 --user=刘瑞斌 【性能测试】性能测试-用例有多个版本，用例列表中删除该用例当前版本失败 https://www.tapd.cn/55049933/s/1275838

--- performance-test/frontend/src/business/test/PerformanceTestList.vue
+++ performance-test/frontend/src/business/test/PerformanceTestList.vue
@@ -342,7 +342,7 @@ export default {
     },
     _handleDelete(test, deleteCurrentVersion) {
       if (deleteCurrentVersion) {
-        deleteCurrentVersionTest()
+        deleteCurrentVersionTest(test)
           .then(() => {
             this.$success(this.$t('commons.delete_success'));
             this.initTableData();
