875fb55c9d715aaf029ecc36ab9adc251e20b3dc
song-cc-rock <changchang.song@fit2cloud.com>
2022-10-25 12:47:37 +0800

    fix(测试跟踪): 用例关联性能测试弹窗无版本信息
    --bug=1018793 --user=宋昌昌 【测试跟踪】功能用例-编辑用例时关联性能用例弹框没有版本信息 https://www.tapd.cn/55049933/s/1275969

--- performance-test/backend/src/main/java/io/metersphere/track/service/PerformanceTestCaseService.java
+++ performance-test/backend/src/main/java/io/metersphere/track/service/PerformanceTestCaseService.java
@@ -1,14 +1,19 @@
 package io.metersphere.track.service;
 
+import io.metersphere.base.domain.ProjectVersion;
 import io.metersphere.base.mapper.ext.ExtLoadTestMapper;
+import io.metersphere.commons.utils.CommonBeanFactory;
 import io.metersphere.dto.LoadTestDTO;
 import io.metersphere.plan.request.LoadCaseRequest;
 import io.metersphere.service.ServiceUtils;
+import io.metersphere.xpack.version.service.ProjectVersionService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(rollbackFor = Exception.class)
@@ -19,7 +24,17 @@ public class PerformanceTestCaseService {
 
     public List<LoadTestDTO> getRelevanceLoadList(LoadCaseRequest request) {
         List<LoadTestDTO> loadTestDTOList = extLoadTestMapper.relevanceLoadList(request);
-        ServiceUtils.buildVersionInfo(loadTestDTOList);
+        buildVersionInfo(loadTestDTOList);
         return loadTestDTOList;
+    }
+
+    public void buildVersionInfo(List<LoadTestDTO> loadTests) {
+        List<String> versionIds = loadTests.stream().map(LoadTestDTO::getVersionId).collect(Collectors.toList());
+        ProjectVersionService projectVersionService = CommonBeanFactory.getBean(ProjectVersionService.class);
+        Map<String, String> projectVersionMap = projectVersionService.getProjectVersionByIds(versionIds).stream()
+                .collect(Collectors.toMap(ProjectVersion::getId, ProjectVersion::getName));
+        loadTests.forEach(loadTest -> {
+            loadTest.setVersionName(projectVersionMap.get(loadTest.getVersionId()));
+        });
     }
 }
