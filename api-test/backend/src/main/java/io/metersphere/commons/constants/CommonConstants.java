3a36c4facc153eb4e4b419bc312c496d30c879bf
fit2-zhao <yong.zhao@fit2cloud.com>
2022-10-25 14:44:19 +0800

    fix(接口测试): 并发执行完成后报告状态统计不准确问题修复
    --bug=1018829 --user=赵勇 【接口测试】场景批量执行-集合报告，报告状态是error了，点开查看报告详情，还有好多pending https://www.tapd.cn/55049933/s/1276340

--- api-test/backend/src/main/java/io/metersphere/commons/constants/CommonConstants.java
+++ api-test/backend/src/main/java/io/metersphere/commons/constants/CommonConstants.java
@@ -3,4 +3,8 @@ package io.metersphere.commons.constants;
 public class CommonConstants {
     public static final String TrashStatus = "Trash";
     public static final String PROJECT_TEMPLATE = "PROJECT_TEMPLATE";
+    public static final String LOCAL_STATUS_KEY = "LOCAL_STATUS_KEY";
+    public static final String REPORT_STATUS = "REPORT_STATUS";
+    public static final String TRIGGER_MODE = "trigger_mode";
+
 }
