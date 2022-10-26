e4b0565d93204a3b265a69bc37d1e116fd0b72f9
shiziyuan9527 <yuhao.li@fit2cloud.com>
2022-10-26 19:21:34 +0800

    fix(系统设置): 登录日志未记录
    --bug=1018659 --user=李玉号 【系统设置】操作日志-只有一条登入日志-其他登录系统日志未记录
    https://www.tapd.cn/55049933/s/1279067

--- framework/gateway/src/main/java/io/metersphere/gateway/controller/SSOController.java
+++ framework/gateway/src/main/java/io/metersphere/gateway/controller/SSOController.java
@@ -4,13 +4,15 @@ import io.metersphere.commons.constants.OperLogConstants;
 import io.metersphere.commons.constants.OperLogModule;
 import io.metersphere.commons.user.SessionUser;
 import io.metersphere.commons.utils.CodingUtil;
+import io.metersphere.gateway.log.annotation.MsAuditLog;
 import io.metersphere.gateway.service.SSOService;
-import io.metersphere.log.annotation.MsAuditLog;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.result.view.Rendering;
 import org.springframework.web.server.WebSession;
-import reactor.core.publisher.Mono;
 
 import javax.annotation.Resource;
 import java.util.Locale;
