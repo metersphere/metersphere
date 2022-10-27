7d15321213077155b867f099b1c8983685adbc32
nathan.liu <yao.liu@fit2cloud.com>
2022-10-27 15:08:03 +0800

    fix(测试跟踪): 修复UI 用例统计分析饼图展示
    --bug=1018848 --user=刘瑶 【测试跟踪】报告-UI 用例统计分析-未执行和失败tab页未显示数据且点击执行结果未显示报告 https://www.tapd.cn/55049933/s/1280207

--- test-track/frontend/src/business/plan/view/comonents/report/detail/TestPlanUiReport.vue
+++ test-track/frontend/src/business/plan/view/comonents/report/detail/TestPlanUiReport.vue
@@ -21,7 +21,7 @@
         <ui-scenario-result :is-db="isDb" :share-id="shareId" :is-share="isShare"
                             :report="report" :is-template="isTemplate" :plan-id="planId"
                             :ui-all-cases="uiAllCases"
-                            :filter-status="['unexecute', 'STOP', 'PENDING']"
+                            :filter-status="['unexecute', 'STOP', 'PENDING', 'STOPPED']"
                             @setSize="setUnExecuteSize"/>
       </el-tab-pane>
 
