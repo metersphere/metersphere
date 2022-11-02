59c049559ecf6dfb593c45917f581c12300e556f
wxg0103 <727495428@qq.com>
2022-11-02 16:34:04 +0800

    fix(接口测试): 修复环境配置全局认证不生效的缺陷
    --bug=1019285 --user=王孝刚
    [接口测试]github#19367接口测试在环境配置中设置了全局认证信息后执行时无法生效，但在单个接口中设置了同样信息的认证配置可以生效
    https://www.tapd.cn/55049933/s/1286281

--- api-test/backend/src/main/java/io/metersphere/api/dto/scenario/environment/EnvAuthManager.java
+++ api-test/backend/src/main/java/io/metersphere/api/dto/scenario/environment/EnvAuthManager.java
@@ -1,9 +1,8 @@
 package io.metersphere.api.dto.scenario.environment;
 
-import io.metersphere.api.dto.definition.request.auth.MsAuthManager;
 import lombok.Data;
 
 @Data
 public class EnvAuthManager {
-    private EnvAuthManager authManager;
+    private ItemAuthManager authManager;
 }
