44f112d143bd00f6d26179f3fccd5fc10b653b32
guoyuqi <xiaomeinvG@126.com>
2022-11-02 17:34:19 +0800

    fix(接口测试): 修复场景导入引用用例id与生成的用例ID不一致问题
    https://www.tapd.cn/55049933/bugtrace/bugs/view/1155049933001018447
    https://www.tapd.cn/55049933/bugtrace/bugs/view/1155049933001018451
    --user=郭雨琦

---
+++ backend/src/main/java/io/metersphere/api/dto/automation/ApiScenarioParamDto.java
@@ -0,0 +1,21 @@
+package io.metersphere.api.dto.automation;
+
+import io.metersphere.base.domain.ApiDefinition;
+import io.metersphere.base.mapper.ApiDefinitionMapper;
+import io.metersphere.base.mapper.ApiScenarioMapper;
+import io.metersphere.base.mapper.ApiTestCaseMapper;
+import io.metersphere.base.mapper.ext.ExtApiScenarioMapper;
+import lombok.Data;
+
+import java.util.Map;
+import java.util.Set;
+
+@Data
+public class ApiScenarioParamDto {
+   private ApiScenarioMapper batchMapper;
+   private ExtApiScenarioMapper extApiScenarioMapper;
+   private ApiTestCaseMapper apiTestCaseMapper;
+   private ApiDefinitionMapper apiDefinitionMapper;
+   private Map<String, ApiDefinition> definitionMap;
+   private Map<String, Set<String>> apiIdCaseNameMap;
+}
