1186418eef4d3bdae71a7b48557ab1cf0c649ac3
guoyuqi <xiaomeinvG@126.com>
2022-11-08 13:51:16 +0800

    fix(工作台): 修复执行失败就进入工作台待更新列表问题
    https://www.tapd.cn/55049933/bugtrace/bugs/view/1155049933001019490
    https://www.tapd.cn/55049933/bugtrace/bugs/view/1155049933001018365
    
    --user=郭雨琦

--- api-test/backend/src/main/java/io/metersphere/service/definition/ApiDefinitionExecResultService.java
+++ api-test/backend/src/main/java/io/metersphere/service/definition/ApiDefinitionExecResultService.java
@@ -237,11 +237,6 @@ public class ApiDefinitionExecResultService {
                     // 更新用例最后执行结果
                     caseWithBLOBs.setLastResultId(reportId);
                     caseWithBLOBs.setStatus(status);
-                    if (status.equals(ApiReportStatus.ERROR.name())) {
-                        caseWithBLOBs.setToBeUpdated(true);
-                    } else {
-                        caseWithBLOBs.setToBeUpdated(false);
-                    }
                     caseWithBLOBs.setUpdateTime(System.currentTimeMillis());
                     apiTestCaseMapper.updateByPrimaryKey(caseWithBLOBs);
 
