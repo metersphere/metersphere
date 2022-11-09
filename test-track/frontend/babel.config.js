1eb7b3b5af375cb27401f00a255b71de046b45b9
chenjianxing <jianxing.chen@fit2cloud.com>
2022-11-09 11:29:49 +0800

    fix(测试跟踪): 解决69版本chrome兼容性问题
    --bug=1019481 --user=陈建星 【测试跟踪】2.3版本的测试跟踪模块，使用69版本的chrome无法访问，使用80或更高版本的chrom可以正常访问，希望兼容69版本 #19519 https://www.tapd.cn/55049933/s/1292771

--- test-track/frontend/babel.config.js
+++ test-track/frontend/babel.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
+  presets: ["@vue/cli-plugin-babel/preset"],
+  // 解析可选链语法插件
+  plugins: ["@babel/plugin-proposal-optional-chaining"]
 };
