8f0498f70ea2168f5ccecab596ffce38f676d158
song-tianyang <tianyang.song@fit2cloud.com>
2022-11-11 18:47:02 +0800

    feat(接口测试): 添加接口测试的2.4数据库升级文件
    添加接口测试的2.4数据库升级文件

---
+++ api-test/backend/src/main/resources/db/migration/V2__2.4.0_release.sql
@@ -0,0 +1,13 @@
+ALTER TABLE api_execution_info ADD project_id VARCHAR(50) NULL;
+ALTER TABLE api_execution_info ADD execute_type VARCHAR(50) NULL;
+ALTER TABLE api_case_execution_info ADD project_id VARCHAR(50) NULL;
+ALTER TABLE api_case_execution_info ADD execute_type VARCHAR(50) NULL;
+ALTER TABLE scenario_execution_info ADD project_id VARCHAR(50) NULL;
+ALTER TABLE scenario_execution_info ADD execute_type VARCHAR(50) NULL;
+ALTER TABLE  api_execution_info ADD INDEX idx_project_id(project_id);
+ALTER TABLE  api_execution_info ADD INDEX idx_execute_type(execute_type);
+ALTER TABLE  api_case_execution_info ADD INDEX idx_project_id(project_id);
+ALTER TABLE  api_case_execution_info ADD INDEX idx_execute_type(execute_type);
+ALTER TABLE  scenario_execution_info ADD INDEX idx_project_id(project_id);
+ALTER TABLE  scenario_execution_info ADD INDEX idx_execute_type(execute_type);
+
