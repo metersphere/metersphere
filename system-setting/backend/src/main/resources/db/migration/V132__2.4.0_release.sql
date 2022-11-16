5864c5eac360723e262d8ab7378ad0ba0ea54a7b
CaptainB <bin@fit2cloud.com>
2022-11-16 15:01:36 +0800

    build: v2.4 flyway

--- system-setting/backend/src/main/resources/db/migration/V132__2.4.0_release.sql
+++ system-setting/backend/src/main/resources/db/migration/V132__2.4.0_release.sql
@@ -1,1 +1,16 @@
 -- v2.4.0 init
+--
+-- 通知表时间加索引
+CREATE INDEX notification_create_time_index
+    ON notification (create_time);
+
+-- v2-4-init-permission
+-- 	创建人 liyuhao
+INSERT INTO user_group_permission (id, group_id, permission_id, module_id)
+SELECT UUID(), id, 'PROJECT_TRACK_PLAN:READ+BATCH_DELETE', 'PROJECT_TRACK_PLAN'
+FROM `group`
+WHERE type = 'PROJECT'
+  AND `group`.id IN
+      (SELECT group_id FROM user_group_permission WHERE permission_id = 'PROJECT_TRACK_PLAN:READ');
+
+
