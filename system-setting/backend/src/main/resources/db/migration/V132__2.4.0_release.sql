8772efb00b2d38614169f03a242c72db4ce93d9c
CaptainB <bin@fit2cloud.com>
2022-11-16 15:33:22 +0800

    build: v2.4 plugin flyway

--- system-setting/backend/src/main/resources/db/migration/V132__2.4.0_release.sql
+++ system-setting/backend/src/main/resources/db/migration/V132__2.4.0_release.sql
@@ -14,3 +14,9 @@ WHERE type = 'PROJECT'
       (SELECT group_id FROM user_group_permission WHERE permission_id = 'PROJECT_TRACK_PLAN:READ');
 
 
+-- platform_plugin
+-- 创建人 jianxing
+-- 是否是企业版插件
+ALTER TABLE plugin ADD xpack TINYINT(1) NULL COMMENT 'Is xpack plugin';
+-- 插件的使用场景
+ALTER TABLE plugin ADD scenario varchar(50) NOT NULL COMMENT 'Plugin usage scenarios';
