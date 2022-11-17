f60fcb9d213ac7e8b1f9bdf6fa6409da52379e2a
fit2-zhao <yong.zhao@fit2cloud.com>
2022-11-17 15:16:40 +0800

    refactor(接口测试): 禁用本地执行授权校验优化

--- framework/sdk-parent/xpack-interface/src/main/java/io/metersphere/xpack/api/service/ApiPoolDebugService.java
+++ framework/sdk-parent/xpack-interface/src/main/java/io/metersphere/xpack/api/service/ApiPoolDebugService.java
@@ -2,9 +2,12 @@ package io.metersphere.xpack.api.service;
 
 import io.metersphere.base.domain.TestResource;
 import io.metersphere.dto.JmeterRunRequestDTO;
+import io.metersphere.dto.RunModeConfigDTO;
 
 import java.util.List;
 
 public interface ApiPoolDebugService {
     public void run(JmeterRunRequestDTO request, List<TestResource> resources);
+
+    public void verifyPool(String projectId, RunModeConfigDTO runConfig);
 }
