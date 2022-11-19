982b6106d58869ffbfdaaa5a5d2886b0dac183dc
guoyuqi <xiaomeinvG@126.com>
2022-11-19 12:46:00 +0800

    refactor(系统设置): 修改类名
    --user=郭雨琦

--- framework/sdk-parent/sdk/src/main/java/io/metersphere/service/remote/BaseSystemSettingService.java
+++ framework/sdk-parent/sdk/src/main/java/io/metersphere/service/remote/BaseSystemSettingService.java
@@ -5,8 +5,8 @@ import io.metersphere.service.RemoteService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SystemSettingService extends RemoteService {
-    public SystemSettingService() {
+public class BaseSystemSettingService extends RemoteService {
+    public BaseSystemSettingService() {
         super(MicroServiceName.SYSTEM_SETTING);
     }
 }
