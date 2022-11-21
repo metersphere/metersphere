bbf27d14d4a6d9e808102ea3977b2494b00fae16
fit2-zhao <yong.zhao@fit2cloud.com>
2022-11-21 11:00:23 +0800

    fix(接口测试): 场景复制接口版本显示错误问题
    --bug=1019921 --user=赵勇 【接口测试】场景复制的api，版本显示成了项目名称 https://www.tapd.cn/55049933/s/1299112

--- api-test/backend/src/main/java/io/metersphere/listener/ApiAppStartListener.java
+++ api-test/backend/src/main/java/io/metersphere/listener/ApiAppStartListener.java
@@ -18,8 +18,6 @@ import org.python.util.PythonInterpreter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
