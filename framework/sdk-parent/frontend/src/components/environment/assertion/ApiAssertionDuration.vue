0d721c2aa3290ec59345b792dee12cc2a37ea0d3
song-tianyang <tianyang.song@fit2cloud.com>
2022-11-23 20:36:05 +0800

    fix(接口测试): 修复环境变量添加不上断言时间的问题
    --bug=1020176 --user=宋天阳 【接口测试】项目环境-全局变量，添加响应时间断言添加不上
    https://www.tapd.cn/55049933/s/1303290

--- framework/sdk-parent/frontend/src/components/environment/assertion/ApiAssertionDuration.vue
+++ framework/sdk-parent/frontend/src/components/environment/assertion/ApiAssertionDuration.vue
@@ -2,21 +2,51 @@
   <div>
     <el-row :gutter="10" type="flex" justify="space-between" align="middle">
       <el-col>
-        <el-input :disabled="isReadOnly" :value="value" v-bind="$attrs" step="100" size="small" type="number"
-                  @change="change" @input="input" :min="1"
-                  :placeholder="$t('api_test.request.assertions.response_in_time')"
-                  onKeypress="return (/[\d]/.test(String.fromCharCode(event.keyCode)))"
+        <el-input
+          :disabled="isReadOnly"
+          :value="value"
+          v-bind="$attrs"
+          step="100"
+          size="small"
+          type="number"
+          @change="change"
+          @input="input"
+          :min="1"
+          :placeholder="$t('api_test.request.assertions.response_in_time')"
+          onKeypress="return (/[\d]/.test(String.fromCharCode(event.keyCode)))"
         />
       </el-col>
       <el-col class="assertion-btn">
-        <el-tooltip :content="$t('test_resource_pool.enable_disable')" placement="top" v-if="edit">
-          <el-switch v-model="duration.enable" class="enable-switch" size="mini" :disabled="isReadOnly"
-                     style="width: 30px;margin-right: 10px"/>
+        <el-tooltip
+          :content="$t('test_resource_pool.enable_disable')"
+          placement="top"
+          v-if="edit"
+        >
+          <el-switch
+            v-model="duration.enable"
+            class="enable-switch"
+            size="mini"
+            :disabled="isReadOnly"
+            style="width: 30px; margin-right: 10px"
+          />
         </el-tooltip>
-        <el-button :disabled="isReadOnly" type="danger" size="mini" icon="el-icon-delete" circle @click="remove"
-                   v-if="edit"/>
-        <el-button :disabled="isReadOnly" type="primary" size="mini" @click="add" v-else>
-          {{ $t('api_test.request.assertions.add') }}
+        <el-button
+          :disabled="isReadOnly"
+          type="danger"
+          size="mini"
+          icon="el-icon-delete"
+          circle
+          @click="remove"
+          v-if="edit"
+        />
+        <el-button
+          :disabled="isReadOnly"
+          type="primary"
+          size="mini"
+          @click="add"
+          v-else
+        >
+          {{ $t("api_test.request.assertions.add") }}
         </el-button>
       </el-col>
     </el-row>
@@ -24,7 +54,6 @@
 </template>
 
 <script>
-
 export default {
   name: "MsApiAssertionDuration",
 
@@ -35,43 +64,41 @@ export default {
     callback: Function,
     isReadOnly: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
   },
 
   methods: {
     add() {
       if (this.validate()) {
-        this.duration.value = this.value;
-        this.duration.enable = true;
+        this.$set(this.duration, "value", this.value);
+        this.$set(this.duration, "enable", true);
         this.callback();
       }
     },
     remove() {
       this.duration.value = undefined;
-
     },
     change(value) {
       if (this.validate()) {
-        this.$emit('change', value);
+        this.$emit("change", value);
       }
     },
     input(value) {
-      this.$emit('input', value);
+      this.$emit("input", value);
     },
     validate() {
-      if (this.value === '' || Number(this.value) <= 0) {
-        this.$warning(this.$t('commons.response_time_warning'));
+      if (this.value === "" || Number(this.value) <= 0) {
+        this.$warning(this.$t("commons.response_time_warning"));
         return false;
       }
       return true;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 .assertion-btn {
   text-align: right;
   width: 180px;
