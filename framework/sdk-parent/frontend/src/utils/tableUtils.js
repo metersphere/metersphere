1bf53e0e0ee71c15b2f11257215484c29e425454
song-cc-rock <changchang.song@fit2cloud.com>
2022-11-23 16:09:27 +0800

    fix(测试跟踪): 缺陷管理Azure平台自定义字段同步值显示有误
    --bug=1019711 --user=宋昌昌 【测试跟踪】添加azure自定义字段，同步缺陷，字段的值没同步过来 https://www.tapd.cn/55049933/s/1302949

--- framework/sdk-parent/frontend/src/utils/tableUtils.js
+++ framework/sdk-parent/frontend/src/utils/tableUtils.js
@@ -483,7 +483,7 @@ export function getCustomFieldValue(row, field, members) {
           if (field.options) {
             for (let j = 0; j < field.options.length; j++) {
               let option = field.options[j];
-              if (option.value === item.value) {
+              if (option.value == item.value) {
                 return field.system ? i18n.t(option.text) : option.text;
               }
             }
