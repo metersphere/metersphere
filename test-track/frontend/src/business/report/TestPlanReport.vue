c5f3767fc9d2e19beddbfa8c2ad8006fa519446f
chenjianxing <jianxing.chen@fit2cloud.com>
2022-11-25 15:24:55 +0800

    fix(测试跟踪): 数据太多时，测试计划报告列表内存溢出
    --bug=1020253 --user=陈建星 【测试跟踪】访问报告页面，服务会挂 https://www.tapd.cn/55049933/s/1305503

--- test-track/frontend/src/business/report/TestPlanReport.vue
+++ test-track/frontend/src/business/report/TestPlanReport.vue
@@ -12,30 +12,10 @@
 import TestPlanReportList from './components/TestPlanReportList';
 import MsContainer from "metersphere-frontend/src/components/MsContainer";
 import MsMainContainer from "metersphere-frontend/src/components/MsMainContainer";
-import {TEST_PLAN_REPORT_CONFIGS} from "metersphere-frontend/src/components/search/search-components";
 
 export default {
   name: "TestPlanReport",
   components: {MsMainContainer, MsContainer, TestPlanReportList},
-  data() {
-    return {}
-  },
-  activated() {
-    this.refreshTestPlanList();
-  },
-  mounted() {
-    this.refreshTestPlanList();
-  },
-  watch: {
-    '$route'(to, from) {
-    }
-  },
-  methods: {
-    refreshTestPlanList() {
-      this.$refs.testPlanReportList.condition = {components: TEST_PLAN_REPORT_CONFIGS};
-      this.$refs.testPlanReportList.initTableData();
-    }
-  }
 }
 </script>
 
