0d6fbb2df5c8bc08dfb06b7ea7735647dd60c9c5
chenjianxing <jianxing.chen@fit2cloud.com>
2022-12-22 17:02:21 +0800

    fix(测试跟踪): 功能用例高级搜索，用例等级条件不准确
    【【测试跟踪】github #20669，测试跟踪中功能测试用例模块，通过批量编辑修改用例等级，修改后在高级搜索按用例等级进行查询，无法查询到正确的结果。】https://www.tapd.cn/55049933/bugtrace/bugs/view?bug_id=1155049933001020972

--- test-track/frontend/src/business/case/test-case.js
+++ test-track/frontend/src/business/case/test-case.js
@@ -6,16 +6,19 @@ export function initTestCaseConditionComponents(condition, customFields, trashEn
   conditionComponents = conditionComponents.filter(item => item.custom !== true);
   let comp = getAdvSearchCustomField(condition, customFields);
   let statusOption = null;
+  let priorityOption = null;
   // 系统字段国际化处理
   comp = comp.filter(element => {
     if (element.label === '责任人') {
-      element.label = i18n.t('custom_field.case_maintainer')
+      element.label = i18n.t('custom_field.case_maintainer');
     }
     if (element.label === '用例等级') {
-      element.label = i18n.t('custom_field.case_priority')
+      element.label = i18n.t('custom_field.case_priority');
+      priorityOption = element.options;
+      return false;
     }
     if (element.label === '用例状态') {
-      element.label = i18n.t('custom_field.case_status')
+      element.label = i18n.t('custom_field.case_status');
       // 回收站TAB页处理高级搜索用例状态字段
       if (trashEnable) {
         element.operator.options = [OPERATORS.IN];
@@ -29,13 +32,15 @@ export function initTestCaseConditionComponents(condition, customFields, trashEn
     }
     return true;
   });
-  if (statusOption) {
-    conditionComponents.forEach((item) => {
-      if (item.key === 'status' || item.label === '用例状态') {
-        item .options = statusOption;
-      }
-    });
-  } else {
+
+  conditionComponents.forEach((item) => {
+    if ((item.key === 'status' || item.label === '用例状态') && statusOption) {
+      item.options = statusOption;
+    } else if (item.key === 'priority') {
+      item.options = priorityOption;
+    }
+  });
+  if (!statusOption) {
     // statusOption 为空，则去掉状态选项
     conditionComponents = conditionComponents.filter(item => item.key !== 'status' && item.label !== '用例状态');
   }
