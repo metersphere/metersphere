895f971e657adce7614ba26204c965d373cabc51
wxg0103 <727495428@qq.com>
2022-12-26 14:17:31 +0800

    fix(接口测试): 修复新建case保存环境失败的缺陷
    --bug=1021225 --user=王孝刚 【接口测试】新建接口用例，环境保存失败
    https://www.tapd.cn/55049933/s/1319278

--- api-test/frontend/src/business/definition/components/case/ApiCaseItem.vue
+++ api-test/frontend/src/business/definition/components/case/ApiCaseItem.vue
@@ -736,7 +736,7 @@ export default {
           if (!hideAlert) {
             this.$emit('refresh');
           }
-          this.$emit('refreshCaseList');
+          this.$emit('refreshCaseList',row.id);
         },
         (error) => {
           this.isSave = false;
