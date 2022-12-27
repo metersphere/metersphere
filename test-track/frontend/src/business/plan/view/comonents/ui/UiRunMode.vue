3cbc6778e7329e5a72b2830acd047f745630612f
zhangdahai112 <dahai.zhang@fit2cloud.com>
2022-12-28 00:19:40 +0800

    revert: 回退环境管理代码

--- test-track/frontend/src/business/plan/view/comonents/ui/UiRunMode.vue
+++ test-track/frontend/src/business/plan/view/comonents/ui/UiRunMode.vue
@@ -8,21 +8,6 @@
   >
     <div class="mode-container">
 
-      <div>
-        <div>{{ $t("commons.environment") }}：</div>
-        <env-select-popover :project-ids="projectIds"
-                            :project-list="projectList"
-                            :project-env-map="projectEnvListMap"
-                            :environment-type="'JSON'"
-                            :has-option-group="false"
-                            :show-env-group="false"
-                            :group-id="runConfig.environmentGroupId"
-                            @setProjectEnvMap="setProjectEnvMap"
-                            ref="envSelectPopover"
-                            class="mode-row"
-        ></env-select-popover>
-      </div>
-
       <!-- 浏览器 -->
       <div class="browser-row wrap">
         <div class="title">{{ $t("ui.browser") }}：</div>
@@ -282,7 +267,6 @@ export default {
       };
       this.runModeVisible = true;
       this.getWsProjects();
-      this.showPopover();
     },
     changeMode() {
       this.runConfig.runWithinResourcePool = false;
