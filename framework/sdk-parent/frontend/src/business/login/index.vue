e24e0a11a907d4de05bc866b282a7b304e9f8759
guoyuqi <xiaomeinvG@126.com>
2023-01-11 14:50:46 +0800

    fix(系统设置): 修复logo显示问题
    --bug=1021870 --user=郭雨琦
    https://www.tapd.cn/55049933/bugtrace/bugs/view/1155049933001021870

--- framework/sdk-parent/frontend/src/business/login/index.vue
+++ framework/sdk-parent/frontend/src/business/login/index.vue
@@ -170,6 +170,9 @@ export default {
             if (title) {
               document.title = title;
             }
+            if (response.data[0].paramValue) {
+              this.shortcutIcon();
+            }
           })
         getModuleList().then(response => {
           let modules = {};
@@ -195,6 +198,7 @@ export default {
           this.rules = this.getDefaultRules();
         }
       });
+
   },
   created: function () {
     document.addEventListener("keydown", this.watchEnter);
@@ -294,6 +298,13 @@ export default {
             localStorage.setItem(DEFAULT_LANGUAGE, language);
           });
       }
+    },
+    shortcutIcon() {
+      let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+      link.type = 'image/x-icon';
+      link.rel = 'shortcut icon';
+      link.href = '/display/file/logo';
+      document.getElementsByTagName('head')[0].appendChild(link);
     },
     redirectAuth(authId) {
       if (authId === 'LDAP' || authId === 'LOCAL') {
