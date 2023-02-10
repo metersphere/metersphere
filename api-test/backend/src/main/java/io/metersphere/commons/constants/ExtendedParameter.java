ced2fc21a1ebeb3fee7a45952b33ce83c58ed4f2
wxg0103 <727495428@qq.com>
2023-02-10 14:21:49 +0800

    refactor(接口定义): 修改传参方式
    --story=1011108 --user=王孝刚 资源池加载插件直接从MinIO获取
    https://www.tapd.cn/55049933/s/1331634

--- api-test/backend/src/main/java/io/metersphere/commons/constants/ExtendedParameter.java
+++ api-test/backend/src/main/java/io/metersphere/commons/constants/ExtendedParameter.java
@@ -3,7 +3,5 @@ package io.metersphere.commons.constants;
 public class ExtendedParameter {
     public static final String SYNC_STATUS = "SYN_RES";
     public static final String SAVE_RESULT = "SAVE_RESULT";
-    public static final String PROJECT_ID = "projectId";
     public static final String TEST_END = "TEST_END";
-    public static final String PROJECT_JAR_MAP = "PROJECT_JAR_MAP";
 }
