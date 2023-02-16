dca06c2c87080e8408c457065dfa8a7b485552eb
song-tianyang <tianyang.song@fit2cloud.com>
2023-02-16 18:02:51 +0800

    fix(项目管理): 修复git文件关联详情不显示关联用例信息的问题
    --bug=1023101 --user=宋天阳 【项目管理】文件管理，接口中关联第三方存储库的文件，文件详情里不显示
    https://www.tapd.cn/55049933/s/1336764

--- project-management/frontend/src/business/menu/file/dialog/FileMetadataDialog.vue
+++ project-management/frontend/src/business/menu/file/dialog/FileMetadataDialog.vue
@@ -1,35 +1,57 @@
 <template>
-  <el-dialog :title="dialogTitle"
-             :visible.sync="dialogFormVisible"
-             :before-close="close"
-             width="600px">
-    <el-form :model="metadataForm" :rules="metadataRule" ref="form" label-width="80px" @submit.native.prevent>
+  <el-dialog
+    :title="dialogTitle"
+    :visible.sync="dialogFormVisible"
+    :before-close="close"
+    width="600px"
+  >
+    <el-form
+      :model="metadataForm"
+      :rules="metadataRule"
+      ref="form"
+      label-width="80px"
+      @submit.native.prevent
+    >
       <el-form-item
         :label="$t('project.project_file.file.branch')"
-        prop="repositoryBranch">
+        prop="repositoryBranch"
+      >
         <el-input v-model="metadataForm.repositoryBranch"></el-input>
       </el-form-item>
       <el-form-item
         :label="$t('project.project_file.file.path')"
-        prop="repositoryPath">
+        prop="repositoryPath"
+      >
         <el-input v-model="metadataForm.repositoryPath"></el-input>
       </el-form-item>
     </el-form>
 
     <template v-slot:footer>
-      <el-button @click="close">{{ $t('commons.cancel') }}</el-button>
-      <el-button v-xpack v-prevent-re-click type="primary" :loading="isSaveBtnLoading" @click="saveFileMetadata"
-                 @keydown.enter.native.prevent>{{ $t('commons.confirm') }}
+      <el-button @click="close">{{ $t("commons.cancel") }}</el-button>
+      <el-button
+        v-prevent-re-click
+        type="primary"
+        :loading="isSaveBtnLoading"
+        @click="saveFileMetadata"
+        @keydown.enter.native.prevent
+        >{{ $t("commons.confirm") }}
       </el-button>
     </template>
   </el-dialog>
 </template>
 
 <script>
-import {getCurrentProjectID, getCurrentUserId} from "metersphere-frontend/src/utils/token";
-import {getUUID, listenGoBack, removeGoBackListener} from "metersphere-frontend/src/utils";
+import {
+  getCurrentProjectID,
+  getCurrentUserId,
+} from "metersphere-frontend/src/utils/token";
+import {
+  getUUID,
+  listenGoBack,
+  removeGoBackListener,
+} from "metersphere-frontend/src/utils";
 import MsDialogFooter from "metersphere-frontend/src/components/MsDialogFooter";
-import {createFileMeta, editFileMeta} from "@/api/file";
+import { createFileMeta, editFileMeta } from "@/api/file";
 
 export default {
   name: "FileMetadataDialog",
@@ -39,23 +61,33 @@ export default {
   data() {
     return {
       dialogFormVisible: false,
-      dialogTitle: '',
-      operationType: '',
+      dialogTitle: "",
+      operationType: "",
       isSaveBtnLoading: false,
       projectId: getCurrentProjectID(),
       metadataForm: {
-        repositoryBranch: '',
-        repositoryPath: '',
+        repositoryBranch: "",
+        repositoryPath: "",
       },
       metadataRule: {
         repositoryBranch: [
-          {required: true, message: this.$t('project.project_file.validation.input_file_branch'), trigger: 'blur'},
+          {
+            required: true,
+            message: this.$t(
+              "project.project_file.validation.input_file_branch"
+            ),
+            trigger: "blur",
+          },
         ],
         repositoryPath: [
-          {required: true, message: this.$t('project.project_file.validation.input_file_path'), trigger: 'blur'},
+          {
+            required: true,
+            message: this.$t("project.project_file.validation.input_file_path"),
+            trigger: "blur",
+          },
         ],
       },
-    }
+    };
   },
   watch: {},
   props: {
@@ -64,45 +96,47 @@ export default {
   computed: {},
   methods: {
     saveFileMetadata() {
-      this.$refs.form.validate(valid => {
-          if (valid) {
-            let url = '';
-            let param = JSON.parse(JSON.stringify(this.metadataForm));
-            param.storage = 'GIT';
-            this.isSaveBtnLoading = true;
-            if (param.id) {
-              editFileMeta(null, param).then(() => {
-                this.$success(this.$t('commons.save_success'));
-                this.$emit('refresh');
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          let url = "";
+          let param = JSON.parse(JSON.stringify(this.metadataForm));
+          param.storage = "GIT";
+          this.isSaveBtnLoading = true;
+          if (param.id) {
+            editFileMeta(null, param)
+              .then(() => {
+                this.$success(this.$t("commons.save_success"));
+                this.$emit("refresh");
                 this.isSaveBtnLoading = false;
                 this.close();
-              }).catch(() => {
-                this.$emit('refresh');
+              })
+              .catch(() => {
+                this.$emit("refresh");
                 this.isSaveBtnLoading = false;
               });
-            } else {
-              param.id = getUUID();
-              param.createUser = getCurrentUserId();
-              param.updateUser = getCurrentUserId();
-              param.projectId = this.projectId;
-              param.moduleId = this.moduleId;
-
-              createFileMeta(null, param).then(() => {
-                this.$success(this.$t('commons.save_success'));
-                this.$emit('refresh');
-                this.isSaveBtnLoading = false;
-                this.close();
-              }).catch(() => {
-                this.$emit('refresh');
-                this.isSaveBtnLoading = false;
-              });
-            }
           } else {
-            return false;
+            param.id = getUUID();
+            param.createUser = getCurrentUserId();
+            param.updateUser = getCurrentUserId();
+            param.projectId = this.projectId;
+            param.moduleId = this.moduleId;
+
+            createFileMeta(null, param)
+              .then(() => {
+                this.$success(this.$t("commons.save_success"));
+                this.$emit("refresh");
+                this.isSaveBtnLoading = false;
+                this.close();
+              })
+              .catch(() => {
+                this.$emit("refresh");
+                this.isSaveBtnLoading = false;
+              });
           }
+        } else {
+          return false;
         }
-      )
-      ;
+      });
     },
     open(operationType, param) {
       this.isSaveBtnLoading = false;
@@ -112,42 +146,37 @@ export default {
         this.metadataForm = JSON.parse(JSON.stringify(param));
       } else {
         this.metadataForm = {
-          repositoryBranch: '',
-          repositoryPath: '',
+          repositoryBranch: "",
+          repositoryPath: "",
         };
       }
       this.dialogFormVisible = true;
-    }
-    ,
+    },
     close() {
       this.isSaveBtnLoading = false;
       removeGoBackListener(this.close);
       this.$refs.form.resetFields();
       this.dialogFormVisible = false;
-    }
-    ,
+    },
     initDialog(operationType) {
       this.operationType = operationType;
       //初始化弹窗数据-title
-      if (operationType === 'create') {
-        this.dialogTitle = this.$t('commons.create');
-      } else if (operationType === 'edit') {
-        this.dialogTitle = this.$t('commons.edit');
+      if (operationType === "create") {
+        this.dialogTitle = this.$t("commons.create");
+      } else if (operationType === "edit") {
+        this.dialogTitle = this.$t("commons.edit");
       } else {
-        this.dialogTitle = '';
+        this.dialogTitle = "";
       }
-    }
-    ,
+    },
     clearForm() {
       this.metadataForm = {
-        repositoryBranch: '',
-        repositoryPath: '',
+        repositoryBranch: "",
+        repositoryPath: "",
       };
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
