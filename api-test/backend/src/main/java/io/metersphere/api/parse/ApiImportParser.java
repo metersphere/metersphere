4f46efacdb4c69b9dcd48139685c5e6cce9921b7
fit2-zhao <yong.zhao@fit2cloud.com>
2023-02-17 14:21:55 +0800

    fix(接口测试): 修复POI和TIKA冲突问题
    --bug=1023164 --user=赵勇 【接口定义】github#21820，请求体中使用word文件，点击调试后，没有内容返回 https://www.tapd.cn/55049933/s/1337167

--- api-test/backend/src/main/java/io/metersphere/api/parse/ApiImportParser.java
+++ api-test/backend/src/main/java/io/metersphere/api/parse/ApiImportParser.java
@@ -1,11 +1,10 @@
 package io.metersphere.api.parse;
 
 import io.metersphere.api.dto.ApiTestImportRequest;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 public interface ApiImportParser<T> {
-    T parse(InputStream source, ApiTestImportRequest request) throws IOException, InvalidFormatException, Exception;
+    T parse(InputStream source, ApiTestImportRequest request) throws IOException, Exception;
 }
