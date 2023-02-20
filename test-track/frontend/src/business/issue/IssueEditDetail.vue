cb6d7ae8cecd1d92da4b4868380dcf1ae8fcf627
shiziyuan9527 <yuhao.li@fit2cloud.com>
2023-02-20 16:44:07 +0800

    fix(测试跟踪): 编辑缺陷时没显示tapd处理人
    --bug=1023054 --user=李玉号 【测试跟踪】缺陷管理，关联tapd平台，编辑缺陷时没显示tapd处理人
    https://www.tapd.cn/55049933/s/1338472

--- test-track/frontend/src/business/issue/IssueEditDetail.vue
+++ test-track/frontend/src/business/issue/IssueEditDetail.vue
@@ -207,7 +207,7 @@ import {
   saveFollow,
   getFollow,
   getComments,
-  getTapdUser, getPlatformTransitions, getPlatformFormOption
+  getTapdUser, getPlatformTransitions, getPlatformFormOption, getTapdCurrentOwner
 } from "@/api/issue";
 import {
   uploadIssueAttachment,
@@ -401,6 +401,13 @@ export default {
         });
       });
     },
+    getTapdCurrentOwner() {
+      getTapdCurrentOwner(this.form.id).then(res => {
+        if (res && res.data && res.data[0]) {
+          this.form.tapdUsers = res.data[0].split(';');
+        }
+      })
+    },
     getDataInfoAsync(data) {
       if (data && data.id) {
         getFollow(data.id).then((response) => {
@@ -450,6 +457,7 @@ export default {
       }
       if (platform === 'Tapd') {
         this.hasTapdId = true;
+        this.getTapdCurrentOwner();
         getTapdUser(data)
           .then((response) => {
             this.tapdUsers = response.data;
