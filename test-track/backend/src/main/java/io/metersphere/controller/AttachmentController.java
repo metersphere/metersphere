e413951b9f118a4fed2737746065f3da6a7f5185
guoyuqi <xiaomeinvG@126.com>
2023-02-22 17:59:20 +0800

    refactor(测试跟踪): 修复功能用例上传附件没有记录到操作日志问题
    --bug=1023259 --user=郭雨琦
    https://www.tapd.cn/55049933/bugtrace/bugs/view/1155049933001023259

--- test-track/backend/src/main/java/io/metersphere/controller/AttachmentController.java
+++ test-track/backend/src/main/java/io/metersphere/controller/AttachmentController.java
@@ -1,6 +1,9 @@
 package io.metersphere.controller;
 
 import io.metersphere.base.domain.FileAttachmentMetadata;
+import io.metersphere.commons.constants.OperLogConstants;
+import io.metersphere.commons.constants.OperLogModule;
+import io.metersphere.log.annotation.MsAuditLog;
 import io.metersphere.metadata.service.FileMetadataService;
 import io.metersphere.request.attachment.AttachmentDumpRequest;
 import io.metersphere.xpack.track.dto.AttachmentRequest;
@@ -27,6 +30,7 @@ public class AttachmentController {
     @Resource
     private FileMetadataService fileMetadataService;
 
+    @MsAuditLog(module = OperLogModule.TRACK_TEST_CASE, type = OperLogConstants.UPDATE, title = "#request.belongType", content = "#msClass.getLogDetails(#request.belongId, #request.belongType, #file.getOriginalFilename(), false)", msClass = AttachmentService.class)
     @PostMapping(value = "/upload", consumes = {"multipart/form-data"})
     public void uploadAttachment(@RequestPart("request") AttachmentRequest request, @RequestPart(value = "file", required = false) MultipartFile file) {
         attachmentService.uploadAttachment(request, file);
@@ -57,6 +61,7 @@ public class AttachmentController {
         }
     }
 
+    @MsAuditLog(module = OperLogModule.TRACK_TEST_CASE, type = OperLogConstants.UPDATE, beforeEvent = "#msClass.getLogDetails(#attachmentId, #attachmentType)", title = "#request.belongType", msClass = AttachmentService.class)
     @GetMapping("/delete/{attachmentType}/{attachmentId}")
     public void deleteAttachment(@PathVariable String attachmentId, @PathVariable String attachmentType) {
         attachmentService.deleteAttachment(attachmentId, attachmentType);
