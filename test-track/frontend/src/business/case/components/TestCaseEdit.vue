6358d8930b754f923c1bf16432ef21789860ae4a
chenjianxing <jianxing.chen@fit2cloud.com>
2023-02-22 20:19:24 +0800

    fix(测试跟踪): 复制用例不添加到用例库
    --bug=1023690 --user=陈建星 【测试跟踪】功能用例-公共用例库列表-操作-复制/查看详情-复制-复制的用例保持后自动添加到公共用例库了 https://www.tapd.cn/55049933/s/1341304

--- test-track/frontend/src/business/case/components/TestCaseEdit.vue
+++ test-track/frontend/src/business/case/components/TestCaseEdit.vue
@@ -611,9 +611,6 @@ export default {
     isCopy() {
       return this.editType == 'copy';
     },
-    publicEnable() {
-      return this.editType == 'public';
-    },
     showPublic() {
       return this.isPublic && this.isXpack;
     },
@@ -1070,6 +1067,7 @@ export default {
             item.isEdit = false;
           });
           this.form.id = null;
+          testCase.casePublic = false;
         }
         this.currentTestCaseInfo = testCase;
         this.setFormData(testCase);
@@ -1229,14 +1227,8 @@ export default {
       param.steps = JSON.stringify(this.form.steps);
       param.nodeId = this.form.module;
       param.copyCaseId = this.caseId;
-      if (!this.publicEnable) {
-        if (this.projectId) {
-          param.projectId = this.projectId;
-        }
-      }
-      if (this.publicEnable) {
-        this.casePublic = true;
-      }
+      param.projectId = this.projectId;
+
       param.name = param.name.trim();
       if (this.form.tags instanceof Array) {
         param.tags = JSON.stringify(this.form.tags);
