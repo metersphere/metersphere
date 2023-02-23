e1c0f69432ef47a44fb77139e5e2c5fa6c05b586
song-tianyang <tianyang.song@fit2cloud.com>
2023-02-23 15:01:24 +0800

    fix(项目报告): 修复项目报告的权限设置，功能开源
    修复项目报告的权限设置，功能开源

--- report-stat/frontend/src/business/header/ReportStatisticsHeaderMenus.vue
+++ report-stat/frontend/src/business/header/ReportStatisticsHeaderMenus.vue
@@ -1,49 +1,66 @@
 <template>
   <div id="menu-bar" v-if="isRouterAlive">
     <el-row type="flex">
-      <project-switch :project-name="currentProject"/>
+      <project-switch :project-name="currentProject" />
       <el-col :span="14">
-        <el-menu class="header-menu" :unique-opened="true" mode="horizontal" router :default-active='$route.path'>
+        <el-menu
+          class="header-menu"
+          :unique-opened="true"
+          mode="horizontal"
+          router
+          :default-active="$route.path"
+        >
           <el-menu-item
             :index="'/report/project-statistics'"
-            v-permission="['PROJECT_REPORT_ANALYSIS:READ', 'PROJECT_REPORT_ANALYSIS:READ+EXPORT',
-            'PROJECT_REPORT_ANALYSIS:READ+UPDATE', 'PROJECT_REPORT_ANALYSIS:READ+CREATE']">
+            v-permission="[
+              'PROJECT_REPORT_ANALYSIS:READ',
+              'PROJECT_REPORT_ANALYSIS:READ+EXPORT',
+              'PROJECT_REPORT_ANALYSIS:READ+UPDATE',
+              'PROJECT_REPORT_ANALYSIS:READ+CREATE',
+            ]"
+          >
             {{ $t("commons.report_statistics.menu.project_statistics") }}
           </el-menu-item>
           <el-menu-item
-            :index="'/report/project-report'" v-xpack
-            v-permission="['PROJECT_ENTERPRISE_REPORT:READ+EXPORT', 'PROJECT_ENTERPRISE_REPORT:READ+CREATE',
-            'PROJECT_ENTERPRISE_REPORT:READ+DELETE', 'PROJECT_ENTERPRISE_REPORT:READ+COPY',
-            'PROJECT_ENTERPRISE_REPORT:READ+SCHEDULE', 'PROJECT_ENTERPRISE_REPORT:READ+EDIT']">
+            :index="'/report/project-report'"
+            v-permission="[
+              'PROJECT_ENTERPRISE_REPORT:READ+EXPORT',
+              'PROJECT_ENTERPRISE_REPORT:READ+CREATE',
+              'PROJECT_ENTERPRISE_REPORT:READ+DELETE',
+              'PROJECT_ENTERPRISE_REPORT:READ+COPY',
+              'PROJECT_ENTERPRISE_REPORT:READ+SCHEDULE',
+              'PROJECT_ENTERPRISE_REPORT:READ+EDIT',
+            ]"
+          >
             {{ $t("commons.report_statistics.menu.project_report") }}
           </el-menu-item>
-
         </el-menu>
       </el-col>
       <el-col :span="10">
-        <ms-header-right-menus/>
+        <ms-header-right-menus />
       </el-col>
     </el-row>
   </div>
-
 </template>
 
 <script>
-
 import ProjectSwitch from "metersphere-frontend/src/components/head/ProjectSwitch";
-import {hasLicense, hasPermission} from "metersphere-frontend/src/utils/permission";
+import {
+  hasLicense,
+  hasPermission,
+} from "metersphere-frontend/src/utils/permission";
 import MsHeaderRightMenus from "metersphere-frontend/src/components/layout/HeaderRightMenus";
-import {PROJECT_NAME} from "metersphere-frontend/src/utils/constants";
+import { PROJECT_NAME } from "metersphere-frontend/src/utils/constants";
 
 export default {
   name: "ReportStatisticsHeaderMenus",
-  components: {ProjectSwitch, MsHeaderRightMenus},
+  components: { ProjectSwitch, MsHeaderRightMenus },
   data() {
     return {
       licenseCheck: false,
       isProjectActivation: true,
       isRouterAlive: true,
-      apiTestProjectPath: '',
+      apiTestProjectPath: "",
       currentProject: sessionStorage.getItem(PROJECT_NAME),
     };
   },
@@ -57,21 +74,19 @@ export default {
       });
     },
   },
-  mounted() {
-  },
+  mounted() {},
   created() {
     if (this.hasLicense()) {
       this.licenseCheck = true;
     }
-  }
+  },
 };
-
 </script>
 
 <style scoped>
 #menu-bar {
-  border-bottom: 1px solid #E6E6E6;
-  background-color: #FFF;
+  border-bottom: 1px solid #e6e6e6;
+  background-color: #fff;
 }
 
 .menu-divider {
@@ -82,7 +97,7 @@ export default {
   display: none;
 }
 
-.deactivation :deep( .el-submenu__title ) {
+.deactivation :deep(.el-submenu__title) {
   border-bottom: white !important;
 }
 
