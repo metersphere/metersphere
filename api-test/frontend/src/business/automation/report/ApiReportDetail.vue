f340302f0954a42303d3639d99fdae9a80fa9dfa
wxg0103 <727495428@qq.com>
2023-02-23 15:28:36 +0800

    refactor(接口定义): 开源失败重跑

--- api-test/frontend/src/business/automation/report/ApiReportDetail.vue
+++ api-test/frontend/src/business/automation/report/ApiReportDetail.vue
@@ -235,7 +235,7 @@ export default {
       }
     },
     rerunVerify() {
-      if (hasLicense() && this.fullTreeNodes && this.fullTreeNodes.length > 0 && !this.isShare) {
+      if (this.fullTreeNodes && this.fullTreeNodes.length > 0 && !this.isShare) {
         this.fullTreeNodes.forEach((item) => {
           item.redirect = true;
           if (
