889ee996bf226fb3c24b606f6e0a57f6f69cb37f
chenjianxing <jianxing.chen@fit2cloud.com>
2023-03-06 15:41:54 +0800

    fix(测试跟踪): 测试用例详情页以及测试计划评论，没有进行Markdown渲染
    --bug=1023963 --user=陈建星 [测试跟踪] github#22399测试计划中用例展示的评论内容没有进行Markdown渲染 https://www.tapd.cn/55049933/s/1345517
    --bug=1023962 --user=陈建星 [测试跟踪] github#22393编辑用例页面, 前置条件和备注没有进行Markdown渲染, 显示的是源码 https://www.tapd.cn/55049933/s/1345518

--- test-track/frontend/src/business/review/commom/ReviewCommentItem.vue
+++ test-track/frontend/src/business/review/commom/ReviewCommentItem.vue
@@ -18,20 +18,11 @@
         <el-link icon="el-icon-close" v-prevent-link-re-click="1300" @click="deleteComment" :disabled="readOnly"/>
       </span>
       <br/>
-
-      <div v-if="!isImage" class="comment-desc" style="font-size: 10px;color: #303133">
-        <pre>{{ comment.description }}</pre>
-      </div>
-      <div v-if="isImage" class="demo-image__preview">
-        <pre>{{ imgDescription }}</pre>
-        <el-image
-          :z-index="imageIndex"
-          style="width: 100px; height: 100px;"
-          fit="contain"
-          :src="src"
-          :preview-src-list="srcList">
-        </el-image>
-      </div>
+      <ms-mark-down-text
+        class="rich-text"
+        prop="description"
+        :disabled="true"
+        :data="comment"/>
     </div>
 
     <el-dialog :visible.sync="visible"
@@ -80,11 +71,9 @@ export default {
     return {
       visible: false,
       imgDescription: '',
-      imageIndex: 99999,
       originDesc: '',
       src: "",
       srcList: [],
-      imgNameList: [],
       description: "",
       imageMatchPattern: "(\\!\\[)\\S+]\\(\\S+\\)",
       toolbars: {
@@ -124,21 +113,11 @@ export default {
       }
     }
   },
-  computed: {
-    isImage() {
-      return this.checkImage(this.comment.description);
-    }
-  },
   methods: {
     deleteComment() {
       if (getCurrentUser().id !== this.comment.author) {
         this.$warning(this.$t('test_track.comment.cannot_delete'));
         return;
-      }
-      if (this.imgNameList.length > 0) {
-        this.imgNameList.forEach(imgName => {
-          deleteMarkDownImgByName(imgName);
-        });
       }
       this.$get(this.apiUrl + "/comment/delete/" + this.comment.id)
         .then(() => {
@@ -166,107 +145,6 @@ export default {
     },
     handleClose() {
       this.comment.description = this.originDesc;
-    },
-    checkImage() {
-      this.srcList = [];
-      let param = this.comment.description;
-      let returnFlag = false;
-      if (param) {
-        let message = param + "";
-        let matchIndex = message.indexOf("](/resource/md/get");
-        if (matchIndex > 0) {
-          let messageSplitArr = message.split("](/resource/md/get");
-          for (let itemIndex = 0; itemIndex < messageSplitArr.length; itemIndex++) {
-            let itemStr = messageSplitArr[itemIndex];
-            let picNameIndex = itemStr.indexOf("![");
-            if (picNameIndex < 0) {
-              let endUrlIndex = itemStr.indexOf(")");
-              if (endUrlIndex > 0) {
-                let itemStrArr = itemStr.substr(0, endUrlIndex);
-                //if(imgNameList.)
-                if (this.imgNameList.indexOf(itemStrArr) < 0) {
-                  this.imgNameList.push(itemStrArr);
-                }
-
-                let imgUrl = "/resource/md/get" + itemStrArr;
-                this.src = imgUrl;
-                if (this.srcList.indexOf(itemStrArr) < 0) {
-                  this.srcList.push(imgUrl);
-                }
-                if (endUrlIndex !== itemStr.length - 1) {
-                  let inputStr = itemStr.substr(endUrlIndex + 1, itemStr.length - 1);
-                  if (this.imgDescription === "") {
-                    this.imgDescription = inputStr;
-                  } else {
-                    this.imgDescription = "\n" + inputStr;
-                  }
-                }
-                if (endUrlIndex !== itemStr.length - 1) {
-                  let inputStr = itemStr.substr(endUrlIndex + 1, itemStr.length - 1);
-                  if (this.imgDescription === "") {
-                    this.imgDescription = inputStr;
-                  } else {
-                    this.imgDescription = "\n" + inputStr;
-                  }
-                }
-              }
-            } else {
-              let inputStr = itemStr.substr(0, picNameIndex);
-              if (this.imgDescription === "") {
-                this.imgDescription = inputStr;
-              } else {
-                this.imgDescription = "\n" + inputStr;
-              }
-            }
-          }
-        } else {
-          let imgUrlIndex = message.indexOf("](http");
-          if (imgUrlIndex > 0) {
-            let imgUrlSplitArr = message.split("](http");
-            for (let itemIndex = 0; itemIndex < imgUrlSplitArr.length; itemIndex++) {
-              let itemStr = imgUrlSplitArr[itemIndex];
-              let picNameIndex = itemStr.indexOf("![");
-              if (picNameIndex < 0) {
-                let endUrlIndex = itemStr.indexOf(")");
-                if (endUrlIndex > 0) {
-                  let itemStrArr = itemStr.substr(0, endUrlIndex);
-                  //if(imgNameList.)
-                  if (this.imgNameList.indexOf(itemStrArr) < 0) {
-                    this.imgNameList.push(itemStrArr);
-                  }
-                  let imgUrl = "http" + itemStrArr;
-                  this.src = imgUrl;
-                  if (this.srcList.indexOf(itemStrArr) < 0) {
-                    this.srcList.push(imgUrl);
-                  }
-                }
-              } else {
-                let inputStr = itemStr.substr(0, picNameIndex);
-                if (this.imgDescription === "") {
-                  this.imgDescription = inputStr;
-                } else {
-                  this.imgDescription = "\n" + inputStr;
-                }
-              }
-            }
-          }
-        }
-        if (this.srcList.length > 0) {
-          returnFlag = true;
-        }
-      }
-      return returnFlag;
-    },
-    checkByUrls(url) {
-      let checkResultFlag = false;
-      if (this.imgNameList.length > 0) {
-        this.imgNameList.forEach(imgName => {
-          if (imgName === url) {
-            checkResultFlag = true;
-          }
-        });
-      }
-      return checkResultFlag;
     }
   }
 }
@@ -302,13 +180,6 @@ export default {
   text-align: center;
   border-radius: 30px;
   font-size: 14px;
-}
-
-
-.comment-desc {
-  overflow-wrap: break-word;
-  word-break: break-all;
-  border-bottom: 1px solid #ced3de;
 }
 
 pre {
