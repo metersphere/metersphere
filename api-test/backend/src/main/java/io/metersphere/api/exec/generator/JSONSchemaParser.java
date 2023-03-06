27f3d521a2f16d1c4498b697244a9ec739b88e37
fit2-zhao <yong.zhao@fit2cloud.com>
2023-03-06 11:15:52 +0800

    fix(接口测试): 修复JSON-Schema请求参数对数字类型使用变量变成字符问题
    --bug=1019328 --user=赵勇 [接口测试]github#19415用场景变量和前置返回的变量 int类型的值 都会转成string，也设置成 number 和 integer 都不行 https://www.tapd.cn/55049933/s/1345270

--- api-test/backend/src/main/java/io/metersphere/api/exec/generator/JSONSchemaParser.java
+++ api-test/backend/src/main/java/io/metersphere/api/exec/generator/JSONSchemaParser.java
@@ -5,32 +5,35 @@ import io.metersphere.commons.constants.PropertyConstant;
 import io.metersphere.commons.utils.EnumPropertyUtil;
 import io.metersphere.commons.utils.JSONUtil;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
 public class JSONSchemaParser {
 
-    private static void analyzeSchema(String json, JSONObject rootObj) {
+    private static void analyzeSchema(String json, JSONObject rootObj, Map<String, String> processMap) {
         Gson gson = new Gson();
         JsonElement element = gson.fromJson(json, JsonElement.class);
         JsonObject rootElement = element.getAsJsonObject();
-        analyzeRootSchemaElement(rootElement, rootObj);
+        analyzeRootSchemaElement(rootElement, rootObj, processMap);
     }
 
-    private static void analyzeRootSchemaElement(JsonObject rootElement, JSONObject rootObj) {
+    private static void analyzeRootSchemaElement(JsonObject rootElement, JSONObject rootObj, Map<String, String> processMap) {
         if (rootElement.has(PropertyConstant.TYPE) || rootElement.has(PropertyConstant.ALL_OF)) {
-            analyzeObject(rootElement, rootObj);
+            analyzeObject(rootElement, rootObj, processMap);
         }
         if (rootElement.has("definitions")) {
-            analyzeDefinitions(rootElement);
+            analyzeDefinitions(rootElement, processMap);
         }
     }
 
-    private static void analyzeObject(JsonObject object, JSONObject rootObj) {
+    private static void analyzeObject(JsonObject object, JSONObject rootObj, Map<String, String> processMap) {
         if (object != null && object.has(PropertyConstant.ALL_OF)) {
             JsonArray allOfArray = object.get(PropertyConstant.ALL_OF).getAsJsonArray();
             for (JsonElement allOfElement : allOfArray) {
@@ -38,29 +41,32 @@ public class JSONSchemaParser {
                 if (!allOfElementObj.has(PropertyConstant.PROPERTIES)) {
                     continue;
                 }
-                formatObject(allOfElementObj, rootObj);
+                formatObject(allOfElementObj, rootObj, processMap);
             }
         } else if (object.has(PropertyConstant.PROPERTIES)) {
-            formatObject(object, rootObj);
+            formatObject(object, rootObj, processMap);
         } else if (object.has(PropertyConstant.ADDITIONAL_PROPERTIES)) {
-            analyzeProperty(rootObj, PropertyConstant.ADDITIONAL_PROPERTIES, object.get(PropertyConstant.ADDITIONAL_PROPERTIES).getAsJsonObject());
-        } else if (object.has(PropertyConstant.TYPE) && object.get(PropertyConstant.TYPE).getAsString().equals(PropertyConstant.ARRAY)) {
-            analyzeProperty(rootObj, PropertyConstant.MS_OBJECT, object);
-        } else if (object.has(PropertyConstant.TYPE) && !object.get(PropertyConstant.TYPE).getAsString().equals(PropertyConstant.OBJECT)) {
-            analyzeProperty(rootObj, object.getAsString(), object);
+            analyzeProperty(rootObj, PropertyConstant.ADDITIONAL_PROPERTIES,
+                    object.get(PropertyConstant.ADDITIONAL_PROPERTIES).getAsJsonObject(), processMap);
+        } else if (object.has(PropertyConstant.TYPE)
+                && object.get(PropertyConstant.TYPE).getAsString().equals(PropertyConstant.ARRAY)) {
+            analyzeProperty(rootObj, PropertyConstant.MS_OBJECT, object, processMap);
+        } else if (object.has(PropertyConstant.TYPE)
+                && !object.get(PropertyConstant.TYPE).getAsString().equals(PropertyConstant.OBJECT)) {
+            analyzeProperty(rootObj, object.getAsString(), object, processMap);
         }
     }
 
-    private static void formatObject(JsonObject object, JSONObject rootObj) {
+    private static void formatObject(JsonObject object, JSONObject rootObj, Map<String, String> processMap) {
         JsonObject propertiesObj = object.get(PropertyConstant.PROPERTIES).getAsJsonObject();
         for (Entry<String, JsonElement> entry : propertiesObj.entrySet()) {
             String propertyKey = entry.getKey();
             JsonObject propertyObj = propertiesObj.get(propertyKey).getAsJsonObject();
-            analyzeProperty(rootObj, propertyKey, propertyObj);
+            analyzeProperty(rootObj, propertyKey, propertyObj, processMap);
         }
     }
 
-    private static void analyzeProperty(JSONObject concept, String propertyName, JsonObject object) {
+    private static void analyzeProperty(JSONObject concept, String propertyName, JsonObject object, Map<String, String> processMap) {
         if (!object.has(PropertyConstant.TYPE)) {
             return;
         }
@@ -105,7 +111,7 @@ public class JSONSchemaParser {
                     concept.put(propertyName, value);
                 } else {
                     String value = FormatterUtil.getStrValue(object);
-                    concept.put(propertyName, value);
+                    JSONSchemaBuilder.processValue(concept, processMap, propertyName, value);
                 }
             }
         } else if (StringUtils.equals(type, PropertyConstant.NUMBER)) {
@@ -123,7 +129,7 @@ public class JSONSchemaParser {
                         concept.put(propertyName, value.floatValue());
                     }
                 } else {
-                    concept.put(propertyName, FormatterUtil.getStrValue(object));
+                    JSONSchemaBuilder.processValue(concept, processMap, propertyName, FormatterUtil.getStrValue(object));
                 }
             }
         } else if (StringUtils.equals(type, PropertyConstant.BOOLEAN)) {
@@ -142,11 +148,11 @@ public class JSONSchemaParser {
                 }
             }
         } else if (StringUtils.equals(type, PropertyConstant.ARRAY)) {
-            analyzeArray(concept, propertyName, object);
+            analyzeArray(concept, propertyName, object, processMap);
         } else if (StringUtils.equals(type, PropertyConstant.OBJECT)) {
             JSONObject obj = JSONUtil.createJsonObject(true);
             concept.put(propertyName, obj);
-            analyzeObject(object, obj);
+            analyzeObject(object, obj, processMap);
         } else if (StringUtils.equalsIgnoreCase(type, "null")) {
             concept.put(propertyName, JSONObject.NULL);
         } else {
@@ -154,12 +160,12 @@ public class JSONSchemaParser {
         }
     }
 
-    private static void analyzeArray(JSONObject concept, String propertyName, JsonObject object) {
+    private static void analyzeArray(JSONObject concept, String propertyName, JsonObject object, Map<String, String> processMap) {
         JSONArray array = new JSONArray();
         if (object.has(PropertyConstant.ITEMS)) {
             if (object.get(PropertyConstant.ITEMS).isJsonArray()) {
                 JsonArray jsonArray = object.get(PropertyConstant.ITEMS).getAsJsonArray();
-                loopArray(array, jsonArray, object, propertyName);
+                loopArray(array, jsonArray, object, propertyName, processMap);
             } else {
                 JsonObject itemsObject = object.get(PropertyConstant.ITEMS).getAsJsonObject();
                 array.put(itemsObject);
@@ -168,7 +174,7 @@ public class JSONSchemaParser {
         concept.put(propertyName, array);
     }
 
-    private static void loopArray(JSONArray array, JsonArray jsonArray, JsonObject object, String propertyName) {
+    private static void loopArray(JSONArray array, JsonArray jsonArray, JsonObject object, String propertyName, Map<String, String> processMap) {
         jsonArray.forEach(element -> {
             JsonObject jsonObject = element.getAsJsonObject();
             if (object.has(PropertyConstant.ITEMS)) {
@@ -200,12 +206,12 @@ public class JSONSchemaParser {
                     }
                 } else if (jsonObject.has(PropertyConstant.PROPERTIES)) {
                     JSONObject propertyConcept = JSONUtil.createJsonObject(true);
-                    formatObject(jsonObject, propertyConcept);
+                    formatObject(jsonObject, propertyConcept, processMap);
                     array.put(propertyConcept);
 
                 } else if (jsonObject.has(PropertyConstant.TYPE) && jsonObject.get(PropertyConstant.TYPE) instanceof JsonPrimitive) {
                     JSONObject newJsonObj = JSONUtil.createJsonObject(true);
-                    analyzeProperty(newJsonObj, propertyName + PropertyConstant.ITEM, jsonObject);
+                    analyzeProperty(newJsonObj, propertyName + PropertyConstant.ITEM, jsonObject, processMap);
                     array.put(newJsonObj.get(propertyName + PropertyConstant.ITEM));
                 }
             } else if (object.has(PropertyConstant.ITEMS) && object.get(PropertyConstant.ITEMS).isJsonArray()) {
@@ -215,24 +221,33 @@ public class JSONSchemaParser {
         });
     }
 
-    private static void analyzeDefinitions(JsonObject object) {
+    private static void analyzeDefinitions(JsonObject object, Map<String, String> processMap) {
         JsonObject definitionsObj = object.get("definitions").getAsJsonObject();
         for (Entry<String, JsonElement> entry : definitionsObj.entrySet()) {
             String definitionKey = entry.getKey();
             JsonObject definitionObj = definitionsObj.get(definitionKey).getAsJsonObject();
             JSONObject obj = JSONUtil.createJsonObject(true);
-            analyzeRootSchemaElement(definitionObj, obj);
+            analyzeRootSchemaElement(definitionObj, obj, processMap);
         }
     }
 
     private static String formerJson(String jsonSchema) {
         JSONObject root = JSONUtil.createJsonObject(true);
-        analyzeSchema(jsonSchema, root);
+        Map<String, String> processMap = new HashMap<>();
+        String json;
+        analyzeSchema(jsonSchema, root, processMap);
         // 格式化返回
         if (root.opt(PropertyConstant.MS_OBJECT) != null) {
-            return root.get(PropertyConstant.MS_OBJECT).toString();
+            json = root.get(PropertyConstant.MS_OBJECT).toString();
+        } else {
+            json = root.toString();
         }
-        return root.toString();
+        if (MapUtils.isNotEmpty(processMap)) {
+            for (String str : processMap.keySet()) {
+                json = json.replace(str, processMap.get(str));
+            }
+        }
+        return json;
     }
 
     public static String schemaToJson(String jsonSchema) {
