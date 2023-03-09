c2fecfd35eb550b6f7557b93948743f836107eab
fit2-zhao <yong.zhao@fit2cloud.com>
2023-03-09 16:04:51 +0800

    refactor(接口测试): 调试结果优化
    Signed-off-by: fit2-zhao <yong.zhao@fit2cloud.com>

--- api-test/backend/src/main/java/io/metersphere/api/jmeter/ResourcePoolCalculation.java
+++ api-test/backend/src/main/java/io/metersphere/api/jmeter/ResourcePoolCalculation.java
@@ -6,17 +6,11 @@ import io.metersphere.base.domain.TestResourcePool;
 import io.metersphere.base.domain.TestResourcePoolExample;
 import io.metersphere.base.mapper.TestResourceMapper;
 import io.metersphere.base.mapper.TestResourcePoolMapper;
-import io.metersphere.commons.utils.BeanUtils;
-import io.metersphere.commons.utils.JSON;
-import io.metersphere.dto.JvmInfoDTO;
-import io.metersphere.dto.NodeDTO;
-import io.metersphere.dto.TestResourceDTO;
+import jakarta.annotation.Resource;
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
 
-import jakarta.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -28,18 +22,6 @@ public class ResourcePoolCalculation {
     TestResourcePoolMapper testResourcePoolMapper;
     @Resource
     TestResourceMapper testResourceMapper;
-    @Resource
-    private RestTemplate restTemplate;
-
-    private static final String BASE_URL = "http://%s:%d";
-
-    private JvmInfoDTO getNodeJvmInfo(String uri) {
-        try {
-            return restTemplate.getForObject(uri, JvmInfoDTO.class);
-        } catch (Exception e) {
-            return null;
-        }
-    }
 
     public List<TestResource> getResourcePools(String resourcePoolId) {
         TestResourcePoolExample example = new TestResourcePoolExample();
@@ -54,27 +36,5 @@ public class ResourcePoolCalculation {
             return testResources;
         }
         return new ArrayList<>();
-    }
-
-    public List<JvmInfoDTO> getPools(String resourcePoolId) {
-        // 按照NODE节点的可用内存空间大小排序
-        List<JvmInfoDTO> availableNodes = new ArrayList<>();
-        List<TestResource> testResources = getResourcePools(resourcePoolId);
-        for (TestResource testResource : testResources) {
-            String configuration = testResource.getConfiguration();
-            NodeDTO node = JSON.parseObject(configuration, NodeDTO.class);
-            String nodeIp = node.getIp();
-            Integer port = node.getPort();
-            String uri = String.format(BASE_URL + "/jmeter/get-jvm-info", nodeIp, port);
-            JvmInfoDTO nodeJvm = this.getNodeJvmInfo(uri);
-            if (nodeJvm == null) {
-                continue;
-            }
-            TestResourceDTO dto = new TestResourceDTO();
-            BeanUtils.copyBean(dto, testResource);
-            nodeJvm.setTestResource(dto);
-            availableNodes.add(nodeJvm);
-        }
-        return availableNodes;
     }
 }
