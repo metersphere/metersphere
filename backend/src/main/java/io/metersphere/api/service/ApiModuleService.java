3397ea0942d91fa73a992ad2b77ae4bed05f2d48
guoyuqi <xiaomeinvG@126.com>
2023-03-10 14:05:10 +0800

    refactor(接口测试): 首次导入时文件存下重复接口的导入优化
    --story=1011301 --user=郭雨琦
    https://www.tapd.cn/55049933/bugtrace/bugs/view/1155049933001011301

--- backend/src/main/java/io/metersphere/api/service/ApiModuleService.java
+++ backend/src/main/java/io/metersphere/api/service/ApiModuleService.java
@@ -985,12 +985,18 @@ public class ApiModuleService extends NodeTreeService<ApiModuleDTO> {
             }
             /*apiTestCaseWithBLOBs.add(apiToCase(optionData,apiId));*/
             //取出当前操作接口应该导入的case用于对比名字重复加序号
+            List<String> nameList = new ArrayList<>();
             List<ApiTestCaseWithBLOBs> importCaseList = importCaseMap.get(apiId);
-            List<String> nameList = importCaseList.stream().map(ApiTestCaseWithBLOBs::getName).collect(Collectors.toList());
+            if (CollectionUtils.isNotEmpty(nameList)) {
+                nameList = importCaseList.stream().map(ApiTestCaseWithBLOBs::getName).collect(Collectors.toList());
+            }
             for (int i = 0; i < apiTestCaseWithBLOBs.size(); i++) {
                 ApiTestCaseWithBLOBs apiTestCaseWithBLOBs1 = apiTestCaseWithBLOBs.get(i);
                 if (nameList.contains(apiTestCaseWithBLOBs1.getName())) {
                     apiTestCaseWithBLOBs1.setName(apiTestCaseWithBLOBs1.getName() + "0" + i);
+                    nameList.add(apiTestCaseWithBLOBs1.getName() + "0" + i);
+                } else {
+                    nameList.add(apiTestCaseWithBLOBs1.getName());
                 }
             }
             optionDataCases.addAll(apiTestCaseWithBLOBs);
