8a6121e097794748341fcf465947d76f9ae3b508
song-tianyang <tianyang.song@fit2cloud.com>
2023-03-16 18:26:40 +0800

    fix(报表统计): 优化饼状图的展示方式以及导出细节
    --bug=1021171 --user=宋天阳 【报表统计】github#20870，测试用例统计图，报表展示数值重叠，导出后缺少图表
    https://www.tapd.cn/55049933/s/1351691

---
+++ report-stat/backend/src/main/java/io/metersphere/reportstatistics/utils/DiscoveryUtil.java
@@ -0,0 +1,30 @@
+package io.metersphere.reportstatistics.utils;
+
+import io.metersphere.commons.utils.CommonBeanFactory;
+import io.metersphere.dto.ServiceDTO;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class DiscoveryUtil {
+
+    public static boolean hasService(String serviceId) {
+        return getServiceIdSet().contains(serviceId);
+    }
+
+    public static Set<String> getServiceIdSet() {
+        DiscoveryClient discoveryClient = CommonBeanFactory.getBean(DiscoveryClient.class);
+        return discoveryClient.getServices()
+                .stream()
+                .collect(Collectors.toSet());
+    }
+
+    public static List<ServiceDTO> getServices() {
+        DiscoveryClient discoveryClient = CommonBeanFactory.getBean(DiscoveryClient.class);
+        return discoveryClient.getServices().stream()
+                .map(service -> new ServiceDTO(service, discoveryClient.getInstances(service).get(0).getPort()))
+                .collect(Collectors.toList());
+    }
+}
