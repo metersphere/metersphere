9f6eb212a746a8592e4034450c58eb2b445c8be4
wxg0103 <727495428@qq.com>
2023-03-17 18:14:22 +0800

    fix(接口测试): 修复tcp除groovy协议都展示了报文处理的缺陷
    --bug=1023982 --user=王孝刚 [接口测试] github#22422 TCP 协议前置脚本-报文头添加长度脚本只使用于
    groovy，其他类型脚本报错，建议修改脚本模板，或者隐藏其他脚本的生成入口
    https://www.tapd.cn/55049933/s/1352443

--- frontend/src/business/components/project/menu/function/ScriptNavMenu.vue
+++ frontend/src/business/components/project/menu/function/ScriptNavMenu.vue
@@ -15,7 +15,7 @@
       </span>
 
       <el-collapse-transition>
-        <div v-if="menu.open">
+        <div v-if="menu.open && !menu.hideScript">
           <div v-for="(child, key) in menu.children" :key="key" class="func-div">
             <el-link :disabled="child.disabled" @click="handleClick(child)" class="func-link nav-font">
               {{ child.title }}
