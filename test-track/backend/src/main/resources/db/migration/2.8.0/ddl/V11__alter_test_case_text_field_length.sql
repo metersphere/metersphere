cfea469036ccf402780b2813de61736b458de13b
chenjianxing <jianxing.chen@fit2cloud.com>
2023-03-20 15:42:50 +0800

    refactor(测试跟踪): 自定义字段，文本类型值改为LONGTEXT
    【【测试跟踪】功能用例新建/编辑-文本描述步骤超长保sql异常-提示不明确】https://www.tapd.cn/55049933/bugtrace/bugs/view?bug_id=1155049933001024584

--- test-track/backend/src/main/resources/db/migration/2.8.0/ddl/V11__alter_test_case_text_field_length.sql
+++ test-track/backend/src/main/resources/db/migration/2.8.0/ddl/V11__alter_test_case_text_field_length.sql
@@ -7,4 +7,7 @@ ALTER TABLE test_case MODIFY COLUMN steps LONGTEXT NULL COMMENT 'Test case steps
 ALTER TABLE test_case MODIFY COLUMN step_description LONGTEXT NULL;
 ALTER TABLE test_case MODIFY COLUMN expected_result LONGTEXT NULL;
 
+ALTER TABLE custom_field_issues MODIFY COLUMN text_value LONGTEXT NULL;
+ALTER TABLE custom_field_test_case MODIFY COLUMN text_value LONGTEXT NULL;
+
 SET SESSION innodb_lock_wait_timeout = DEFAULT;
