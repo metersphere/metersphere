b97ec6dadf49018092943bd1d687b521b896801f
CaptainB <bin@fit2cloud.com>
2023-03-20 13:35:08 +0800

    chore: 规范group_id名称

--- system-setting/backend/src/main/java/io/metersphere/listener/PlatformPluginListener.java
+++ system-setting/backend/src/main/java/io/metersphere/listener/PlatformPluginListener.java
@@ -18,7 +18,7 @@ public class PlatformPluginListener {
     private PlatformPluginService platformPluginService;
 
     // groupId 必须是每个实例唯一
-    @KafkaListener(id = TEST_TRACK_PLATFORM_PLUGIN, topics = KafkaTopicConstants.PLATFORM_PLUGIN, groupId = TEST_TRACK_PLATFORM_PLUGIN + "_" + "#{T(java.util.UUID).randomUUID()})")
+    @KafkaListener(id = TEST_TRACK_PLATFORM_PLUGIN, topics = KafkaTopicConstants.PLATFORM_PLUGIN, groupId = TEST_TRACK_PLATFORM_PLUGIN + "_" + "${random.uuid}")
     public void handlePluginChange(ConsumerRecord<?, String> record) {
         LogUtil.info("track service consume platform_plugin add message, plugin id: " + record);
         String[] info = record.value().split(":");
