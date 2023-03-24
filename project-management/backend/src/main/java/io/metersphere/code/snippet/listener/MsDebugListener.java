573ebdbc9da4d90fb9e75216a8fc52bca780d9b4
fit2-zhao <yong.zhao@fit2cloud.com>
2023-03-24 12:52:04 +0800

    refactor(项目设置): 代码片段执行日志隔离处理
    --bug=1024856 --user=赵勇 【测试跟踪】自定义代码片段控制台日志未隔离 https://www.tapd.cn/55049933/s/1355517
    
    Signed-off-by: fit2-zhao <yong.zhao@fit2cloud.com>

--- project-management/backend/src/main/java/io/metersphere/code/snippet/listener/MsDebugListener.java
+++ project-management/backend/src/main/java/io/metersphere/code/snippet/listener/MsDebugListener.java
@@ -155,7 +155,7 @@ public class MsDebugListener extends AbstractListenerElement implements SampleLi
                 dto.setReportId("send." + this.getName());
                 dto.setToReport(this.getName());
 
-                String console = FixedCapacityUtils.getJmeterLogger(this.getName(), false);
+                String console = FixedCapacityUtils.getJmeterLogger(this.getName());
                 if (StringUtils.isNotEmpty(requestResult.getName()) && requestResult.getName().startsWith("Transaction=")) {
                     requestResult.getSubRequestResults().forEach(transactionResult -> {
                         transactionResult.getResponseResult().setConsole(console);//解析误报内容
