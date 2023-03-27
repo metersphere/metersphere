926beaccfb23185acc0275b2f21db268cbf81ae5
wxg0103 <727495428@qq.com>
2023-03-27 09:48:31 +0800

    fix(接口测试): 修复swagger定时同步值为null的缺陷
    --bug=1024805 --user=王孝刚 【接口测试】swagger定时导入-后端报错-空值显示为null
    https://www.tapd.cn/55049933/s/1355712

--- api-test/backend/src/main/java/io/metersphere/commons/utils/FakeErrorParse.java
+++ api-test/backend/src/main/java/io/metersphere/commons/utils/FakeErrorParse.java
@@ -28,57 +28,59 @@ public class FakeErrorParse {
         if (StringUtils.isNotBlank(result.getFakeErrorMessage())) {
             FakeError errorReportDTO = JsonUtils.parseObject(result.getFakeErrorMessage(), FakeError.class);
             ErrorReportLibraryService service = CommonBeanFactory.getBean(ErrorReportLibraryService.class);
-            ErrorReportLibraryExample example = new ErrorReportLibraryExample();
-            example.createCriteria().andProjectIdEqualTo(errorReportDTO.getProjectId()).andStatusEqualTo(true);
-            List<ErrorReportLibraryWithBLOBs> bloBs = service.selectByExampleWithBLOBs(example);
-            List<MsRegexDTO> regexList = new ArrayList<>();
-            bloBs.forEach(item -> {
-                if (StringUtils.isNotEmpty(item.getContent())) {
-                    try {
-                        Map<String, Object> assertionMap = JSON.parseObject(item.getContent(), Map.class);
-                        if (assertionMap != null) {
-                            MsRegexDTO regexConfig = JSON.parseObject(JSONUtil.toJSONString(assertionMap.get("regexConfig")), MsRegexDTO.class);
-                            regexConfig.setErrorCode(item.getErrorCode());
-                            regexList.add(regexConfig);
+            if (service != null) {
+                ErrorReportLibraryExample example = new ErrorReportLibraryExample();
+                example.createCriteria().andProjectIdEqualTo(errorReportDTO.getProjectId()).andStatusEqualTo(true);
+                List<ErrorReportLibraryWithBLOBs> bloBs = service.selectByExampleWithBLOBs(example);
+                List<MsRegexDTO> regexList = new ArrayList<>();
+                bloBs.forEach(item -> {
+                    if (StringUtils.isNotEmpty(item.getContent())) {
+                        try {
+                            Map<String, Object> assertionMap = JSON.parseObject(item.getContent(), Map.class);
+                            if (assertionMap != null) {
+                                MsRegexDTO regexConfig = JSON.parseObject(JSONUtil.toJSONString(assertionMap.get("regexConfig")), MsRegexDTO.class);
+                                regexConfig.setErrorCode(item.getErrorCode());
+                                regexList.add(regexConfig);
+                            }
+                        } catch (Exception e) {
+                            LogUtil.error(e);
                         }
-                    } catch (Exception e) {
-                        LogUtil.error(e);
                     }
-                }
-            });
-            //根据配置来筛选断言、获取误报编码、获取接口状态是否是误报
-            List<String> errorCodeList = new ArrayList<>();
-            regexList.forEach(item -> {
-                if (StringUtils.isNotEmpty(item.getSubject())) {
-                    switch (item.getSubject()) {
-                        case "Response Code" ->
-                                item.setPass(parseResponseCode(result.getResponseResult().getResponseCode(), item.getValue(), item.getCondition()));
+                });
+                //根据配置来筛选断言、获取误报编码、获取接口状态是否是误报
+                List<String> errorCodeList = new ArrayList<>();
+                regexList.forEach(item -> {
+                    if (StringUtils.isNotEmpty(item.getSubject())) {
+                        switch (item.getSubject()) {
+                            case "Response Code" ->
+                                    item.setPass(parseResponseCode(result.getResponseResult().getResponseCode(), item.getValue(), item.getCondition()));
 
-                        case "Response Headers" ->
-                                item.setPass(parseResponseCode(result.getResponseResult().getHeaders(), item.getValue(), item.getCondition()));
+                            case "Response Headers" ->
+                                    item.setPass(parseResponseCode(result.getResponseResult().getHeaders(), item.getValue(), item.getCondition()));
 
-                        case "Response Data" ->
-                                item.setPass(parseResponseCode(result.getResponseResult().getBody(), item.getValue(), item.getCondition()));
-                        default -> item.setPass(false);
+                            case "Response Data" ->
+                                    item.setPass(parseResponseCode(result.getResponseResult().getBody(), item.getValue(), item.getCondition()));
+                            default -> item.setPass(false);
+                        }
                     }
+                    if (item.isPass()) {
+                        errorCodeList.add(item.getErrorCode());
+                    }
+                });
+                boolean higherThanError = errorReportDTO.isHigherThanError();
+                boolean higherThanSuccess = errorReportDTO.isHigherThanSuccess();
+                if (CollectionUtils.isNotEmpty(errorCodeList)) {
+                    if ((higherThanError && !result.isSuccess()) || (higherThanSuccess && result.isSuccess())) {
+                        fakeError.setRequestStatus(ApiReportStatus.FAKE_ERROR.name());
+                    }
+                    fakeError.setErrorCodeList(errorCodeList);
                 }
-                if (item.isPass()) {
-                    errorCodeList.add(item.getErrorCode());
-                }
-            });
-            boolean higherThanError = errorReportDTO.isHigherThanError();
-            boolean higherThanSuccess = errorReportDTO.isHigherThanSuccess();
-            if (CollectionUtils.isNotEmpty(errorCodeList)) {
-                if ((higherThanError && !result.isSuccess()) || (higherThanSuccess && result.isSuccess())) {
-                    fakeError.setRequestStatus(ApiReportStatus.FAKE_ERROR.name());
-                }
-                fakeError.setErrorCodeList(errorCodeList);
+                LogUtil.info(" FAKE_ERROR result:  config-higherThanError:" + higherThanError
+                        + ", config-higherThanSuccess:" + higherThanSuccess
+                        + ", resultIsSuccess: " + result.isSuccess()
+                        + ", isFakeError: " + ((higherThanError && !result.isSuccess()) || (higherThanSuccess && result.isSuccess()))
+                        + "; status:" + fakeError.getRequestStatus());
             }
-            LogUtil.info(" FAKE_ERROR result:  config-higherThanError:" + higherThanError
-                    + ", config-higherThanSuccess:" + higherThanSuccess
-                    + ", resultIsSuccess: " + result.isSuccess()
-                    + ", isFakeError: " + ((higherThanError && !result.isSuccess()) || (higherThanSuccess && result.isSuccess()))
-                    + "; status:" + fakeError.getRequestStatus());
         }
         fakeError.setResult(result);
         return fakeError;
