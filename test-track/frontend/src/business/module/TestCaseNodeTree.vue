7e5d3def6a5d70a5263aa604b3bf4d40fb3c719a
song-cc-rock <changchang.song@fit2cloud.com>
2023-03-29 20:18:17 +0800

    fix(测试跟踪): 左侧模块树节点click刷新树回退

--- test-track/frontend/src/business/module/TestCaseNodeTree.vue
+++ test-track/frontend/src/business/module/TestCaseNodeTree.vue
@@ -281,9 +281,8 @@ export default {
       this.currentNode = node;
 
       this.$emit("nodeSelectEvent", node, node.data.id === 'root' ? [] : nodeIds, pNodes);
-
-      // 刷新模块树
-      this.nohupReloadTree(node.data.id);
+      // 只在TAB页切换时才刷新树
+      // this.nohupReloadTree(node.data.id);
     },
     nohupReloadTree(selectNodeId) {
       if (this.projectId) {
