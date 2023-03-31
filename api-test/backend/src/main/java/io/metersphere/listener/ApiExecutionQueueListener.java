96f7817a6655591bfa05f52ce4f99b3db2973aeb
fit2-zhao <yong.zhao@fit2cloud.com>
2023-03-31 18:49:52 +0800

    fix(接口测试): 修复等待控制器时间过长导致WebSocket连接中断问题
    Signed-off-by: fit2-zhao <yong.zhao@fit2cloud.com>

--- api-test/backend/src/main/java/io/metersphere/listener/ApiExecutionQueueListener.java
+++ api-test/backend/src/main/java/io/metersphere/listener/ApiExecutionQueueListener.java
@@ -1,15 +1,15 @@
 package io.metersphere.listener;
 
+import com.fit2cloud.quartz.anno.QuartzScheduled;
 import io.metersphere.commons.utils.CommonBeanFactory;
 import io.metersphere.service.ApiExecutionQueueService;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ApiExecutionQueueListener {
     private ApiExecutionQueueService queueService;
 
-    @Scheduled(cron = "0 0/10 0/1 * * ?")
+    @QuartzScheduled(cron = "0 0/10 0/1 * * ?")
     public void execute() {
         if (queueService == null) {
             queueService = CommonBeanFactory.getBean(ApiExecutionQueueService.class);
