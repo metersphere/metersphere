4059d09424c6e88c9bda5d559e30abb286e40205
wxg0103 <727495428@qq.com>
2023-04-03 17:07:39 +0800

    perf(接口测试): 增加接口和场景的索引
    --bug=1025028 --user=王孝刚 【接口测试】场景-删除模块-模块下只有一个场景-耗时20秒左右
    https://www.tapd.cn/55049933/s/1358738

---
+++ api-test/backend/src/main/resources/db/migration/2.8.0/ddl/V5__add_api_scenario_index.sql
@@ -0,0 +1,42 @@
+SET SESSION innodb_lock_wait_timeout = 7200;
+
+-- 增加场景模块id索引
+SELECT IF(EXISTS(SELECT DISTINCT index_name
+                 FROM information_schema.statistics
+                 WHERE table_schema = DATABASE()
+                   AND table_name = 'api_scenario'
+                   AND index_name LIKE 'api_scenario_module_id_index'),
+          'select 1',
+          'ALTER TABLE api_scenario ADD INDEX api_scenario_module_id_index (api_scenario_module_id)')
+INTO @a;
+PREPARE stmt1 FROM @a;
+EXECUTE stmt1;
+DEALLOCATE PREPARE stmt1;
+
+-- 增加接口定义协议索引
+SELECT IF(EXISTS(SELECT DISTINCT index_name
+                 FROM information_schema.statistics
+                 WHERE table_schema = DATABASE()
+                   AND table_name = 'api_definition'
+                   AND index_name LIKE 'api_definition_protocol_index'),
+          'select 1',
+          'ALTER TABLE api_definition ADD INDEX api_definition_protocol_index (protocol)')
+INTO @a;
+PREPARE stmt1 FROM @a;
+EXECUTE stmt1;
+DEALLOCATE PREPARE stmt1;
+
+-- 增加case项目id索引
+SELECT IF(EXISTS(SELECT DISTINCT index_name
+                 FROM information_schema.statistics
+                 WHERE table_schema = DATABASE()
+                   AND table_name = 'api_test_case'
+                   AND index_name LIKE 'api_test_case_project_id_index'),
+          'select 1',
+          'ALTER TABLE api_test_case ADD INDEX api_test_case_project_id_index (project_id)')
+INTO @a;
+PREPARE stmt1 FROM @a;
+EXECUTE stmt1;
+DEALLOCATE PREPARE stmt1;
+
+SET SESSION innodb_lock_wait_timeout = DEFAULT;
