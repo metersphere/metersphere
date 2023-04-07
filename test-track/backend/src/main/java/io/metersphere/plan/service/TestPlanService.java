dba2c2c594c7a48f90e528dcdcb9eecee141723d
song-tianyang <tianyang.song@fit2cloud.com>
2023-04-07 17:53:58 +0800

    style(测试跟踪): 优化测试计划报告开始结束时间的展示方式，增加对v1数据的支持
    优化测试计划报告开始结束时间的展示方式，增加对v1数据的支持

--- test-track/backend/src/main/java/io/metersphere/plan/service/TestPlanService.java
+++ test-track/backend/src/main/java/io/metersphere/plan/service/TestPlanService.java
@@ -1477,6 +1477,8 @@ public class TestPlanService {
                         testPlan, testPlanExecuteReportDTO);
             } else {
                 //针对已经保存过的数据结构，增加对旧版本数据的支持
+                testPlanReportStruct.setStartTime(testPlanReportContentWithBLOBs.getStartTime());
+                testPlanReportStruct.setEndTime(testPlanReportContentWithBLOBs.getEndTime());
                 this.dealOldVersionData(testPlanReportStruct);
             }
             //查找运行环境
