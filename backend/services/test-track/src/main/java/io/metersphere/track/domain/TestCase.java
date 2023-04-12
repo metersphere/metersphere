9441b6b2aaad724ca72287bcda28342c209ce27d
CaptainB <bin@fit2cloud.com>
2023-04-12 10:19:57 +0800

    build: 初始化各个模块

---
+++ backend/services/test-track/src/main/java/io/metersphere/track/domain/TestCase.java
@@ -0,0 +1,72 @@
+package io.metersphere.track.domain;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class TestCase implements Serializable {
+    private String id;
+
+    private String nodeId;
+
+    private String testId;
+
+    private String nodePath;
+
+    private String projectId;
+
+    private String name;
+
+    private String type;
+
+    private String maintainer;
+
+    private String priority;
+
+    private String method;
+
+    private Long createTime;
+
+    private Long updateTime;
+
+    private Integer sort;
+
+    private Integer num;
+
+    private String reviewStatus;
+
+    private String tags;
+
+    private String demandId;
+
+    private String demandName;
+
+    private String status;
+
+    private String stepModel;
+
+    private String customNum;
+
+    private String createUser;
+
+    private String originalStatus;
+
+    private Long deleteTime;
+
+    private String deleteUserId;
+
+    private Long order;
+
+    private Boolean casePublic;
+
+    private String versionId;
+
+    private String refId;
+
+    private Boolean latest;
+
+    private String lastExecuteResult;
+
+    private static final long serialVersionUID = 1L;
+}
