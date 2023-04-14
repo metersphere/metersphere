8d84282ae271c1c78111a4f21fbc731f64954d5e
建国 <tianyang.song@fit2cloud.com>
2023-04-14 19:32:07 +0800

    fix(工作台): 没有项目的用户登录我的待办我的关注和我的创建不显示数据
    --bug=1024743 --user=宋天阳 【工作台】github#22990，工作台-我的待办、我的关注、我的创建
    https://www.tapd.cn/55049933/s/1363078

--- workstation/frontend/src/business/upcoming/Upcoming.vue
+++ workstation/frontend/src/business/upcoming/Upcoming.vue
@@ -1,28 +1,34 @@
 <template>
-  <workstation-detail :is-upcoming=true :current-todo-name="currentTodo"></workstation-detail>
+  <workstation-detail
+    :is-upcoming="true"
+    :current-todo-name="currentTodo"
+    v-if="projectId !== '' && projectId !== 'no_such_project'"
+  ></workstation-detail>
 </template>
 <script>
 import WorkstationDetail from "@/business/detail/WorkstationDetail";
+import { getCurrentProjectID } from "metersphere-frontend/src/utils/token";
 
 export default {
-  name: 'Upcoming',
+  name: "Upcoming",
   components: {
-    WorkstationDetail
+    WorkstationDetail,
   },
   data() {
     return {
-      currentTodo:'',
-    }
+      currentTodo: "",
+      projectId: getCurrentProjectID(),
+    };
   },
   methods: {},
   created() {
     if (this.$route.query.name) {
-      this.currentTodo = this.$route.query.name
+      this.currentTodo = this.$route.query.name;
     } else {
-      this.currentTodo = 'track_case'
+      this.currentTodo = "track_case";
     }
-  }
-}
+  },
+};
 </script>
 <style scoped>
 :deep(.el-main) {
