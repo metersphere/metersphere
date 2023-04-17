6fc1763bc30308ca6bcc83025c0e2448dab55847
song-cc-rock <changchang.song@fit2cloud.com>
2023-04-17 15:04:37 +0800

    fix(测试跟踪): 关联UI及场景用例弹窗左侧模块树筛选问题
    --bug=1025458 --user=宋昌昌 【测试跟踪】-测试用例-编辑用例-关联UI/场景用例-筛选用例-模块树数量显示不变 https://www.tapd.cn/55049933/s/1363349

--- test-track/frontend/src/api/remote/ui/ui-scenario-module.js
+++ test-track/frontend/src/api/remote/ui/ui-scenario-module.js
@@ -5,3 +5,7 @@ const BASE_URL = '/ui/scenario/module/';
 export function uiScenarioModuleProjectList(projectId) {
   return get(BASE_URL + `list/${projectId}`);
 }
+
+export function getUiScenarioModuleListByCondition(projectId, type, params) {
+  return post(BASE_URL + `list/${projectId}/${type}`, params);
+}
