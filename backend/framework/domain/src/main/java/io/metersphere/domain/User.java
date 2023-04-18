6fcf254a05e4c0655730d818fbaba7ab24ac91cb
CaptainB <bin@fit2cloud.com>
2023-04-18 19:39:40 +0800

    build:  remove class

--- backend/framework/domain/src/main/java/io/metersphere/domain/User.java
+++ backend/framework/domain/src/main/java/io/metersphere/domain/User.java
@@ -1,7 +1,9 @@
 package io.metersphere.domain;
 
 import com.baomidou.mybatisplus.annotation.TableName;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -12,12 +14,13 @@ public class User implements Serializable {
     @NotBlank(message = "id不能为空")
     private String id;
     @NotBlank(message = "用户名不能为空")
+    @Size(min = 1, max = 50, message = "用户名长度必须在1-50之间")
     private String name;
-
+    @Email
     private String email;
 
     private String password;
-
+    @NotBlank(message = "状态不能为空")
     private String status;
 
     private Long createTime;
