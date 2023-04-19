6760686c4b4dfe744fa965e2e097007ae8eaa303
CaptainB <bin@fit2cloud.com>
2023-04-19 11:47:11 +0800

    fix: 禁用spring默认的datasource，强制使用系统内自定义的datasource

--- project-management/backend/src/main/java/io/metersphere/ProjectApplication.java
+++ project-management/backend/src/main/java/io/metersphere/ProjectApplication.java
@@ -3,6 +3,7 @@ package io.metersphere;
 import io.metersphere.config.MinioProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.ldap.LdapAutoConfiguration;
 import org.springframework.boot.autoconfigure.neo4j.Neo4jAutoConfiguration;
 import org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration;
@@ -11,6 +12,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
         QuartzAutoConfiguration.class,
         LdapAutoConfiguration.class,
         Neo4jAutoConfiguration.class
