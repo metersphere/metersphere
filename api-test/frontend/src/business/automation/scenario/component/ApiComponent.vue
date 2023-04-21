2dfcc4d7dcc89d5fc46a08a9c474dcb814081662
wxg0103 <727495428@qq.com>
2023-04-21 10:07:47 +0800

    fix(接口测试): 点击切换api和cae的tab页，模块树没有更新的缺陷
    --bug=1025641 --user=王孝刚
    【接口测试】接口定义-API-输入关键字筛选-切换到CASE-再切换到API页面-搜索条件清空-模块树未数量未更新
    https://www.tapd.cn/55049933/s/1365039

--- api-test/frontend/src/business/automation/scenario/component/ApiComponent.vue
+++ api-test/frontend/src/business/automation/scenario/component/ApiComponent.vue
@@ -273,11 +273,11 @@ export default {
       this.request.projectId = getCurrentProjectID();
     }
     this.request.customizeReq = this.isCustomizeReq;
-    if (this.request.customizeReq) {
+   if (this.request.customizeReq) {
       if (this.node.parent && this.node.parent.data && this.node.parent.data.length > 1) {
         this.request.projectId = getCurrentProjectID();
       }else {
-        this.request.projectId = this.node.parent.data.projectId;
+        this.request.projectId = this.node.parent.data[0].projectId;
       }
     }
     if (this.currentScenario) {
