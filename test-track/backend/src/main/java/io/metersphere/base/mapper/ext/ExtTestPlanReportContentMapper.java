f20411ed9cfa10331b190b7a6ac8de1badfcbd21
song-tianyang <tianyang.song@fit2cloud.com>
2023-04-26 20:05:17 +0800

    fix(测试跟踪): 测试计划删除报告时级联删除生成的自动化用例报告
    --bug=1025762 --user=宋天阳
    【测试跟踪】测试计划-查看历史报告的接口和场景用例内容为空
    https://www.tapd.cn/55049933/s/1367408

--- test-track/backend/src/main/java/io/metersphere/base/mapper/ext/ExtTestPlanReportContentMapper.java
+++ test-track/backend/src/main/java/io/metersphere/base/mapper/ext/ExtTestPlanReportContentMapper.java
@@ -16,4 +16,23 @@ public interface ExtTestPlanReportContentMapper {
     boolean hasRunningReportByPlanIds(@Param("planIds") List<String> planIds);
 
     boolean isApiBasicCountIsNull(String testPlanReportId);
+    
+    List<String> selectScenarioReportByTestPlanReportIds(@Param("testPlanReportIds") List<String> testPlanReportIdList);
+
+    List<String> selectUiReportByTestPlanReportIds(@Param("testPlanReportIds") List<String> testPlanReportIdList);
+
+    int deleteApiReportByTestPlanReportList(@Param("testPlanReportIds") List<String> testPlanReportIdList);
+
+    void deleteScenarioReportByIds(@Param("testPlanReportIds") List<String> scenarioReportIds);
+
+    void deleteScenarioReportResultByIds(@Param("testPlanReportIds") List<String> scenarioReportIds);
+
+    void deleteScenarioReportStructureByIds(@Param("testPlanReportIds") List<String> scenarioReportIds);
+
+    void deleteUiReportByIds(@Param("testPlanReportIds") List<String> scenarioReportIds);
+
+    void deleteUiReportResultByIds(@Param("testPlanReportIds") List<String> scenarioReportIds);
+
+    void deleteUiReportStructureByIds(@Param("testPlanReportIds") List<String> scenarioReportIds);
+
 }
