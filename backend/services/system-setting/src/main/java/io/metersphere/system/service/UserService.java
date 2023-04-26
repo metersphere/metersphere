e11f60406aed0dcf9a34b6b0a1d445b45ef4dc54
CaptainB <bin@fit2cloud.com>
2023-04-26 18:41:31 +0800

    chore: 测试覆盖率统计的github action

---
+++ backend/services/system-setting/src/main/java/io/metersphere/system/service/UserService.java
@@ -0,0 +1,11 @@
+package io.metersphere.system.service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import io.metersphere.domain.User;
+import io.metersphere.sdk.mapper.UserMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService extends ServiceImpl<UserMapper, User> {
+
+}
---
+++ backend/services/system-setting/src/test/java/io/metersphere/system/controller/UserControllerTests.java
@@ -0,0 +1,31 @@
+package io.metersphere.system.controller;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTests {
+    @Resource
+    private MockMvc mockMvc;
+
+    @Test
+    public void testSelectAll() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/user/list-all"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(jsonPath("$.person.name").value("Jason"))
+                .andDo(print());
+    }
+
+}
