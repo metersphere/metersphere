785408f6778442627ab8a7d60de8d1bea429ab4c
liqiang-fit2cloud <liqiang@fit2cloud.com>
2023-04-28 22:20:00 +0800

    Revert "refactor(gateway): 重写is-login方法，不用响应式，防止阻塞redisson线程"
    This reverts commit 166cde0b114b4e9e80558eebbe7d5921bc6bbca5.

--- framework/gateway/src/main/java/io/metersphere/gateway/config/SessionConfig.java
+++ framework/gateway/src/main/java/io/metersphere/gateway/config/SessionConfig.java
@@ -1,19 +1,13 @@
 package io.metersphere.gateway.config;
 
 import io.metersphere.commons.constants.SessionConstants;
-import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.session.data.redis.RedisSessionRepository;
 import org.springframework.web.server.session.HeaderWebSessionIdResolver;
 import org.springframework.web.server.session.WebSessionIdResolver;
 
 @Configuration
 public class SessionConfig {
-
-    @Resource
-    private RedisTemplate<String, Object> redisTemplate;
 
     @Bean
     public WebSessionIdResolver webSessionIdResolver() {
@@ -21,10 +15,4 @@ public class SessionConfig {
         sessionIdResolver.setHeaderName(SessionConstants.HEADER_TOKEN);        // Define Session Header Name
         return sessionIdResolver;
     }
-
-    @Bean
-    public RedisSessionRepository redisSessionRepository() {
-        return new RedisSessionRepository(redisTemplate);
-    }
 }
-
