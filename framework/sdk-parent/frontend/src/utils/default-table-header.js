17ba5ad8bda2ecb9d1c72a2e04e7a8dec0bb0465
guoyuqi <xiaomeinvG@126.com>
2023-05-15 13:48:41 +0800

    fix(UI测试): 修复元素列表所属模块相关问题
    --bug=1026143 --user=郭雨琦
    https://www.tapd.cn/55049933/bugtrace/bugs/view/1155049933001026143

--- framework/sdk-parent/frontend/src/utils/default-table-header.js
+++ framework/sdk-parent/frontend/src/utils/default-table-header.js
@@ -160,14 +160,15 @@ export let CUSTOM_TABLE_HEADER = {
 
   //缺陷列表
   ELEMENT_LIST: [
-    { id: "num", key: "1", label: "ID" },
-    { id: "name", key: "2", label: "元素名称" },
-    { id: "locationType", key: "3", label: "定位类型" },
-    { id: "location", key: "4", label: "元素定位" },
-    { id: "createUser", key: "5", label: "创建人" },
-    { id: "createTime", key: "6", label: "commons.create_time" },
-    { id: "updateUser", key: "7", label: "更新人" },
-    { id: "updateTime", key: "8", label: "commons.update_time" },
+    {id: "num", key: "1", label: "ID"},
+    {id: "name", key: "2", label: "元素名称"},
+    {id: "locationType", key: "3", label: "定位类型"},
+    {id: "location", key: "4", label: "元素定位"},
+    {id: 'modulePath', key: 'a', label: 'test_track.case.module'},
+    {id: "createUser", key: "5", label: "创建人"},
+    {id: "createTime", key: "6", label: "commons.create_time"},
+    {id: "updateUser", key: "7", label: "更新人"},
+    {id: "updateTime", key: "8", label: "commons.update_time"},
   ],
 
   //空间配额
