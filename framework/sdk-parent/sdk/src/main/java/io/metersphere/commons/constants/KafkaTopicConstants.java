9711e672f911a96132d5caf915eea09ad0d3beae
guoyuqi <xiaomeinvG@126.com>
2023-05-17 16:23:08 +0800

    feat(UI测试): ui执行增加kafka广播
    --story=1012014 --user=郭雨琦
    https://www.tapd.cn/55049933/prong/stories/view/1155049933001012014

--- framework/sdk-parent/sdk/src/main/java/io/metersphere/commons/constants/KafkaTopicConstants.java
+++ framework/sdk-parent/sdk/src/main/java/io/metersphere/commons/constants/KafkaTopicConstants.java
@@ -8,6 +8,7 @@ public interface KafkaTopicConstants {
     // 原接口执行结果TOPIC
     String API_REPORT_TOPIC = "ms-api-exec-topic";
     String DEBUG_TOPICS = "MS-API-DEBUG-TOPIC";
+    String UI_DEBUG_TOPICS = "MS-UI-DEBUG-TOPIC";
     // 测试计划接收执行结果TOPIC
     String TEST_PLAN_REPORT_TOPIC = "TEST_PLAN_REPORT_TOPIC";
     // 上传插件后，通知各服务从 minio 加载插件, 删除插件后卸载插件
