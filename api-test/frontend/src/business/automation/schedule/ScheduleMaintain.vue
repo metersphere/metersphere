4ca567c8e6debd7e8aa4084d2a48277b43a068e7
fit2-zhao <yong.zhao@fit2cloud.com>
2023-05-18 17:33:42 +0800

    fix(接口测试): 修复配额限制后批量资源池反显问题
    --bug=1026274 --user=赵勇 【接口测试】修改配额资源池和项目接口资源池不一致-接口用例/场景批量执行-资源池显示错误 https://www.tapd.cn/55049933/s/1372931
    
    Signed-off-by: fit2-zhao <yong.zhao@fit2cloud.com>

--- api-test/frontend/src/business/automation/schedule/ScheduleMaintain.vue
+++ api-test/frontend/src/business/automation/schedule/ScheduleMaintain.vue
@@ -194,19 +194,26 @@ export default {
     };
   },
   methods: {
-    getProjectApplication() {
+    async checkPool(){
       let hasPool = false;
       this.resourcePools.forEach(item => {
         if (item.id === this.runConfig.resourcePoolId) {
           hasPool = true;
-          return;
         }
       });
+      return hasPool;
+    },
+    async getProjectApplication() {
+      let hasPool = await this.checkPool();
       if (!hasPool) {
         this.runConfig.resourcePoolId = null;
-        getProjectConfig(getCurrentProjectID(), "").then((res) => {
+        getProjectConfig(getCurrentProjectID(), "").then(async (res) => {
           if (res.data && res.data.poolEnable && res.data.resourcePoolId) {
             this.runConfig.resourcePoolId = res.data.resourcePoolId;
+          }
+          hasPool = await this.checkPool();
+          if (!hasPool) {
+            this.runConfig.resourcePoolId = undefined;
           }
         });
       }
@@ -281,7 +288,7 @@ export default {
       param.testId = this.testId;
       return param;
     },
-    open(row) {
+    async open(row) {
       //测试计划页面跳转来的
       let paramTestId = '';
       this.paramRow = row;
@@ -296,7 +303,7 @@ export default {
         this.scheduleTaskType = 'API_SCENARIO_TEST';
       }
       this.testId = paramTestId;
-      this.findSchedule(paramTestId);
+      await this.findSchedule(paramTestId);
       this.initUserList();
       this.dialogVisible = true;
       this.form.cronValue = this.schedule.value;
@@ -306,7 +313,7 @@ export default {
       this.getWsProjects();
       this.runConfig.environmentType = ENV_TYPE.JSON;
     },
-    findSchedule() {
+    async findSchedule() {
       let scheduleResourceID = this.testId;
       let taskType = this.scheduleTaskType;
       this.result = getScheduleByIdAndType(scheduleResourceID, taskType).then((response) => {
