9659198a463eee54a8617e2deb201bec3655ac73
WangXu10 <xu.wang@fit2cloud.com>
2023-05-19 15:54:00 +0800

    fix(测试跟踪)：修复测试计划关联缺陷状态显示错误
    --bug=1026334 --user=王旭 【测试跟踪】项目集成local-测试计划-功能用例-关联缺陷-添加缺陷/关联缺陷后-状态显示错误 https://www.tapd.cn/55049933/s/1373230

--- test-track/frontend/src/api/issue.js
+++ test-track/frontend/src/api/issue.js
@@ -78,6 +78,7 @@ export function getIssuesByCaseId(refType, caseId, page) {
       .then((response) => {
         page.data = response.data;
         buildIssues(page);
+        parseFields(page);
       });
   }
 }
