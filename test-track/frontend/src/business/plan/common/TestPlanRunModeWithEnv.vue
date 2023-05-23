91fdace459d61fa71d7754be9cf74bb013432c71
song-cc-rock <changchang.song@fit2cloud.com>
2023-05-23 10:57:05 +0800

    fix(接口测试): 测试计划批量执行接口弹窗样式问题
    --bug=1026380 --user=宋昌昌 【测试跟踪】测试计划中批量执行接口测试-选择资源池运行样式显示异常 https://www.tapd.cn/55049933/s/1374266

--- test-track/frontend/src/business/plan/common/TestPlanRunModeWithEnv.vue
+++ test-track/frontend/src/business/plan/common/TestPlanRunModeWithEnv.vue
@@ -7,7 +7,7 @@
     @close="close"
     :visible.sync="runModeVisible"
   >
-    <div class="env-container" style="max-height: 400px; overflow: auto">
+    <div class="env-container" style="max-height: 400px">
       <div>
         <div>{{ $t("commons.environment") }}：</div>
         <env-select-popover
