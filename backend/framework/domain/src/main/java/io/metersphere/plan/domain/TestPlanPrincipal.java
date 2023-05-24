ddce02410997f5ec8f6946be7f6f14d1a033d00e
song-tianyang <tianyang.song@fit2cloud.com>
2023-05-24 16:42:13 +0800

    fix(测试计划): 去除测试计划domain联合组件的Id注解
    去除测试计划domain联合组件的Id注解

--- backend/framework/domain/src/main/java/io/metersphere/plan/domain/TestPlanPrincipal.java
+++ backend/framework/domain/src/main/java/io/metersphere/plan/domain/TestPlanPrincipal.java
@@ -5,7 +5,6 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.io.Serializable;
@@ -15,12 +14,10 @@ import java.io.Serializable;
 @Data
 public class TestPlanPrincipal implements Serializable {
     private static final long serialVersionUID = 1L;
-    @Id
     @NotBlank(message = "{test_plan_principal.test_plan_id.not_blank}", groups = {Updated.class})
     @ApiModelProperty(name = "测试计划ID", required = true, allowableValues = "range[1, 50]")
     private String testPlanId;
 
-    @Id
     @NotBlank(message = "{test_plan_principal.user_id.not_blank}", groups = {Updated.class})
     @ApiModelProperty(name = "用户ID", required = true, allowableValues = "range[1, 50]")
     private String userId;
