4f8131fba6d97b77d145490e5d54c4ed6f08396f
CaptainB <bin@fit2cloud.com>
2023-05-24 12:17:17 +0800

    build: 批量插入

--- backend/services/system-setting/src/main/java/io/metersphere/system/service/UserService.java
+++ backend/services/system-setting/src/main/java/io/metersphere/system/service/UserService.java
@@ -5,9 +5,11 @@ import io.metersphere.sdk.mapper.UserMapper;
 import io.metersphere.sdk.util.BeanUtils;
 import io.metersphere.system.domain.User;
 import io.metersphere.system.domain.UserExtend;
+import io.metersphere.system.util.BatchSaveUtils;
 import jakarta.annotation.Resource;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,7 +23,7 @@ public class UserService {
     @Resource
     private JdbcAggregateTemplate jdbcAggregateTemplate;
     @Resource
-    private JdbcTemplate jdbcTemplate;
+    private SqlSessionFactory sqlSessionFactory;
 
     public boolean save(UserDTO entity) {
         User user = new User();
@@ -41,4 +43,60 @@ public class UserService {
     public List<User> list() {
         return userMapper.findAll();
     }
+
+    public boolean batchSave(List<User> users) {
+        long start = System.currentTimeMillis();
+        BatchSaveUtils.batchSave(users);
+        System.out.println("batch save cost: " + (System.currentTimeMillis() - start) + "ms");
+        return true;
+    }
+
+    public boolean batchSave2(List<User> users) {
+        long start = System.currentTimeMillis();
+
+        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            for (int i = 0, size = users.size(); i < size; i++) {
+                mapper.insert(users.get(i));
+                if (i % 100 == 0) {
+                    sqlSession.flushStatements();
+                }
+            }
+            sqlSession.flushStatements();
+        }
+        System.out.println("batch save cost: " + (System.currentTimeMillis() - start) + "ms");
+        return true;
+    }
+
+    public boolean batchSave3(List<User> users) {
+
+        long start = System.currentTimeMillis();
+        int batchSize = 100;
+        int size = users.size();
+        int pageSize = size / batchSize;
+        if (pageSize == 0) {
+            userMapper.batchSave(users);
+            System.out.println("batch save cost: " + (System.currentTimeMillis() - start) + "ms");
+            return true;
+        }
+
+        for (int i = 0; i < pageSize; i++) {
+            int startIndex = i * batchSize;
+            List<User> sub = users.subList(startIndex, startIndex + batchSize);
+            userMapper.batchSave(sub);
+        }
+
+        if (size % batchSize != 0) {
+            int startIndex = pageSize * batchSize;
+            List<User> sub = users.subList(startIndex, size);
+            userMapper.batchSave(sub);
+        }
+        System.out.println("batch save cost: " + (System.currentTimeMillis() - start) + "ms");
+        return true;
+    }
+
+    public long count() {
+        return jdbcAggregateTemplate.count(User.class);
+    }
+
 }
