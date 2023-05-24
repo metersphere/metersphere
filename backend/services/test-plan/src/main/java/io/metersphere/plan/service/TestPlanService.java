0e3adc71257caac162c0276840ff14a68fb0aaef
song-tianyang <tianyang.song@fit2cloud.com>
2023-05-24 15:50:09 +0800

    feat(测试计划): 测试计划相关代码更改
    测试计划相关代码更改

---
+++ backend/services/test-plan/src/main/java/io/metersphere/plan/service/TestPlanService.java
@@ -0,0 +1,14 @@
+package io.metersphere.plan.service;
+
+import io.metersphere.plan.domain.TestPlan;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class TestPlanService {
+
+    public boolean add(TestPlan testPlan) {
+        return true;
+    }
+}
---
+++ backend/services/test-plan/src/test/java/io/metersphere/system/controller/TestPlanControllerTests.java
@@ -0,0 +1,58 @@
+package io.metersphere.system.controller;
+
+import io.metersphere.plan.domain.TestPlan;
+import io.metersphere.sdk.util.JSON;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class TestPlanControllerTests {
+    @Resource
+    private MockMvc mockMvc;
+
+    @Test
+    @Order(1)
+    public void testAddUserTrue() throws Exception {
+        TestPlan testPlan = new TestPlan();
+        testPlan.setName("test");
+        testPlan.setProjectId("1");
+        testPlan.setParentId("1");
+        testPlan.setCreateUser("JianGuo");
+        testPlan.setStage("Smock");
+        testPlan.setStatus("PREPARE");
+
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/test-plan/add")
+                        .content(JSON.toJSONString(testPlan))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @Order(2)
+    public void testAddUserFalse() throws Exception {
+        TestPlan testPlan = new TestPlan();
+        testPlan.setName("test");
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/test-plan/add")
+                        .content(JSON.toJSONString(testPlan))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+}
