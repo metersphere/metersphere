c64f283ef7683d1a8b0893454574b689649564e4
fit2-zhao <yong.zhao@fit2cloud.com>
2023-05-26 19:02:18 +0800

    build: add api test template

---
+++ backend/services/api-test/src/main/java/io/metersphere/api/dto/ApiDefinitionDTO.java
@@ -0,0 +1,7 @@
+package io.metersphere.api.dto;
+
+import io.metersphere.api.domain.ApiDefinition;
+
+public class ApiDefinitionDTO extends ApiDefinition {
+
+}
---
+++ backend/services/api-test/src/main/test/java/io/metersphere/api/Application.java
@@ -0,0 +1,23 @@
+package io.metersphere.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.ldap.LdapAutoConfiguration;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jAutoConfiguration;
+import org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(exclude = {
+        QuartzAutoConfiguration.class,
+        LdapAutoConfiguration.class,
+        Neo4jAutoConfiguration.class
+})
+@ServletComponentScan
+@ComponentScan(basePackages = {"io.metersphere.sdk", "io.metersphere.api"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
