e97705aaa43e03fea61fb366b48b2b9d3bb2fbe0
CaptainB <bin@fit2cloud.com>
2023-05-26 18:47:37 +0800

    build: 单元测试有提取结果参数

--- backend/services/project-management/src/main/java/io/metersphere/project/controller/ProjectController.java
+++ backend/services/project-management/src/main/java/io/metersphere/project/controller/ProjectController.java
@@ -2,10 +2,11 @@ package io.metersphere.project.controller;
 
 import io.metersphere.project.domain.Project;
 import io.metersphere.project.service.ProjectService;
+import io.metersphere.validation.groups.Created;
+import io.metersphere.validation.groups.Updated;
 import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +19,15 @@ public class ProjectController {
     @GetMapping("list-all")
     public List<Project> selectAll() {
         return projectService.list();
+    }
+
+    @PostMapping("add")
+    public Project add(@Validated({Created.class}) @RequestBody Project project) {
+        return projectService.add(project);
+    }
+
+    @PostMapping("edit")
+    public Project edit(@Validated({Updated.class}) @RequestBody Project project) {
+        return projectService.edit(project);
     }
 }
