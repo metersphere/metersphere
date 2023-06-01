fa0d4e079b5d3b48fd1403940fd5d1fbf7c0066d
chenjianxing <jianxing.chen@fit2cloud.com>
2023-06-01 12:03:14 +0800

    build: issue test demo

---
+++ backend/services/issue-management/src/test/java/io/metersphere/issue/Application.java
@@ -0,0 +1,22 @@
+package io.metersphere.issue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.ldap.LdapAutoConfiguration;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jAutoConfiguration;
+import org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(exclude = {
+        QuartzAutoConfiguration.class,
+        LdapAutoConfiguration.class,
+        Neo4jAutoConfiguration.class
+})
+@ServletComponentScan
+@ComponentScan(basePackages = {"io.metersphere.sdk", "io.metersphere.issue"})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
