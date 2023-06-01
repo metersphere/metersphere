d62ac3ca3a7aa9d80cc9745f5aac66dc31fe4774
CaptainB <bin@fit2cloud.com>
2023-06-01 10:33:37 +0800

    chore: 去掉不用的参数

--- backend/framework/sdk/src/main/java/io/metersphere/sdk/dto/UserDTO.java
+++ backend/framework/sdk/src/main/java/io/metersphere/sdk/dto/UserDTO.java
@@ -9,9 +9,9 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = false)
 public class UserDTO extends User {
 
-    @Schema(title = "其他平台对接信息", requiredMode = Schema.RequiredMode.NOT_REQUIRED, allowableValues = "range[1, 2000]")
+    @Schema(title = "其他平台对接信息", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private byte[] platformInfo;
 
-    @Schema(title = "UI本地调试地址", requiredMode = Schema.RequiredMode.NOT_REQUIRED, allowableValues = "range[1, 255]")
+    @Schema(title = "UI本地调试地址", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private String seleniumServer;
 }
