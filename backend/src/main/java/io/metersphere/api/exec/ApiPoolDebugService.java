056ae9ed7007c8ce4748f1523fabe479ec1bff50
wxg0103 <727495428@qq.com>
2023-06-01 18:01:25 +0800

    refactor(接口测试): local资源池被禁后重新选择资源池

--- backend/src/main/java/io/metersphere/api/exec/ApiPoolDebugService.java
+++ backend/src/main/java/io/metersphere/api/exec/ApiPoolDebugService.java
@@ -108,11 +108,14 @@ public class ApiPoolDebugService {
 
             if (!contains) {
                 List<TestResourcePoolDTO> pools = poolList.stream().filter(pool ->
-                        StringUtils.equals(pool.getName(), "LOCAL")).collect(Collectors.toList());
+                        StringUtils.equals(pool.getName(), "LOCAL") &&
+                                !StringUtils.equals(config.getResourcePoolId(),pool.getId())).collect(Collectors.toList());
                 if (CollectionUtils.isNotEmpty(pools)) {
                     config.setResourcePoolId(pools.get(0).getId());
                 } else {
-                    config.setResourcePoolId(poolList.get(0).getId());
+                    List<TestResourcePoolDTO> other = poolList.stream().filter(pool ->
+                            !StringUtils.equals(config.getResourcePoolId() ,pool.getId())).collect(Collectors.toList());
+                    config.setResourcePoolId(other.get(0).getId());
                 }
             }
             runConfig = runConfig == null ? new RunModeConfigDTO() : runConfig;
