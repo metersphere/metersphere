8f5b09b309fa58ff7f85d7cd46bd94873889d5a2
CaptainB <bin@fit2cloud.com>
2023-06-02 12:59:26 +0800

    build: shiro api key config

---
+++ backend/framework/sdk/src/main/java/io/metersphere/sdk/service/UserKeyService.java
@@ -0,0 +1,85 @@
+package io.metersphere.sdk.service;
+
+import io.metersphere.sdk.constants.ApiKeyConstants;
+import io.metersphere.sdk.exception.MSException;
+import io.metersphere.sdk.util.Translator;
+import io.metersphere.system.domain.UserKey;
+import io.metersphere.system.domain.UserKeyExample;
+import io.metersphere.system.mapper.UserKeyMapper;
+import jakarta.annotation.Resource;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class UserKeyService {
+
+    @Resource
+    private UserKeyMapper userKeyMapper;
+
+    @Resource
+    private BaseUserService baseUserService;
+
+    public List<UserKey> getUserKeysInfo(String userId) {
+        UserKeyExample userKeysExample = new UserKeyExample();
+        userKeysExample.createCriteria().andCreateUserEqualTo(userId);
+        userKeysExample.setOrderByClause("create_time");
+        return userKeyMapper.selectByExample(userKeysExample);
+    }
+
+    public UserKey generateUserKey(String userId) {
+        if (baseUserService.getUserDTO(userId) == null) {
+            MSException.throwException(Translator.get("user_not_exist") + userId);
+        }
+        UserKeyExample userKeysExample = new UserKeyExample();
+        userKeysExample.createCriteria().andCreateUserEqualTo(userId);
+        List<UserKey> userKeysList = userKeyMapper.selectByExample(userKeysExample);
+
+        if (!CollectionUtils.isEmpty(userKeysList) && userKeysList.size() >= 5) {
+            MSException.throwException(Translator.get("user_apikey_limit"));
+        }
+
+        UserKey userKeys = new UserKey();
+        userKeys.setId(UUID.randomUUID().toString());
+        userKeys.setCreateUser(userId);
+        userKeys.setStatus(ApiKeyConstants.ACTIVE.name());
+        userKeys.setAccessKey(RandomStringUtils.randomAlphanumeric(16));
+        userKeys.setSecretKey(RandomStringUtils.randomAlphanumeric(16));
+        userKeys.setCreateTime(System.currentTimeMillis());
+        userKeyMapper.insert(userKeys);
+        return userKeyMapper.selectByPrimaryKey(userKeys.getId());
+    }
+
+    public void deleteUserKey(String id) {
+        userKeyMapper.deleteByPrimaryKey(id);
+    }
+
+    public void activeUserKey(String id) {
+        UserKey userKeys = new UserKey();
+        userKeys.setId(id);
+        userKeys.setStatus(ApiKeyConstants.ACTIVE.name());
+        userKeyMapper.updateByPrimaryKeySelective(userKeys);
+    }
+
+    public void disableUserKey(String id) {
+        UserKey userKeys = new UserKey();
+        userKeys.setId(id);
+        userKeys.setStatus(ApiKeyConstants.DISABLED.name());
+        userKeyMapper.updateByPrimaryKeySelective(userKeys);
+    }
+
+    public UserKey getUserKey(String accessKey) {
+        UserKeyExample userKeyExample = new UserKeyExample();
+        userKeyExample.createCriteria().andAccessKeyEqualTo(accessKey).andStatusEqualTo(ApiKeyConstants.ACTIVE.name());
+        List<UserKey> userKeysList = userKeyMapper.selectByExample(userKeyExample);
+        if (!CollectionUtils.isEmpty(userKeysList)) {
+            return userKeysList.get(0);
+        }
+        return null;
+    }
+}
