651ccb4639cbfd03ca5b1366ef6a523fa868b076
baiqi <qi.bai@fit2cloud.com>
2023-06-07 14:22:13 +0800

    feat: 国际化语言切换时切换字体

--- frontend/src/locale/helper.ts
+++ frontend/src/locale/helper.ts
@@ -2,6 +2,13 @@ import type { LocaleType } from '#/global';
 
 export function setHtmlPageLang(locale: LocaleType) {
   document.querySelector('html')?.setAttribute('lang', locale);
+  let fontFamily = '';
+  if (locale === 'en-US') {
+    fontFamily = 'Helvetica Neue, Arial';
+  } else {
+    fontFamily = 'PingFang SC, Microsoft YaHei';
+  }
+  document.body.style.fontFamily = fontFamily;
 }
 
 export const loadLocalePool: LocaleType[] = [];
