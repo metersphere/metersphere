9cf5df3635c7e8105479f8bfeb666ba91d8a0a00
RubyLiu <liulubingjava@163.com>
2023-06-12 15:31:28 +0800

    feat(系统设置): 用户组静态页面

---
+++ frontend/src/components/bussiness/usergroup/type.ts
@@ -0,0 +1,5 @@
+export interface UserGroupListItem {
+  name: string;
+  id: number;
+  title?: string;
+}
---
+++ frontend/src/views/system/locale/zh-CN.ts
@@ -0,0 +1,13 @@
+export default {
+  system: {
+    userGroup: {
+      searchHolder: '请输入用户组名称',
+      inSystem: '系统内置',
+      customUserGroup: '自定义用户组',
+      addSystemUserGroup: '添加系统用户组',
+      addSystemUser: '添加系统用户',
+      addUser: '添加成员',
+      emptyUserGroup: '暂无自定义用户组，请点击上方创建或 “+” 创建用户组',
+    },
+  },
+};
