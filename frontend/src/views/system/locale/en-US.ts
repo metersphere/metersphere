9cec62ee4b797325180facd10d1211e000606676
RubyLiu <liulubingjava@163.com>
2023-06-12 15:31:28 +0800

    feat(系统设置): 用户组静态页面

---
+++ frontend/src/views/system/locale/en-US.ts
@@ -0,0 +1,14 @@
+export default {
+  system: {
+    userGroup: {
+      searchHolder: 'Please input user group name',
+      inSystem: 'In system',
+      customUserGroup: 'Custom user group',
+      addSystemUserGroup: 'Add system user group',
+      addSystemUser: 'Add system user',
+      addUser: 'Add user',
+      emptyUserGroup:
+        'There are currently no custom user groups available. Please click "Create" or "+" above to create a user group',
+    },
+  },
+};
