6d1832ec1c8e22d66e6d76fd9b40bc1452285125
CaptainB <bin@fit2cloud.com>
2023-06-14 16:17:13 +0800

    ci: spring session serial json

--- backend/framework/sdk/src/main/java/io/metersphere/sdk/config/SessionConfig.java
+++ backend/framework/sdk/src/main/java/io/metersphere/sdk/config/SessionConfig.java
@@ -1,15 +1,28 @@
 package io.metersphere.sdk.config;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.metersphere.sdk.constants.SessionConstants;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
+
 @Configuration
 public class SessionConfig {
 
     @Bean
     public HeaderHttpSessionIdResolver sessionIdResolver() {
         return new HeaderHttpSessionIdResolver(SessionConstants.HEADER_TOKEN);
+    }
+
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+        return new GenericJackson2JsonRedisSerializer(objectMapper());
+    }
+
+    private ObjectMapper objectMapper() {
+        return new ObjectMapper();
     }
 }
