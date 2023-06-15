3935032ed8568a06c8d006a7f8d79673bf4bdba3
fit2-zhao <yong.zhao@fit2cloud.com>
2023-06-15 14:29:02 +0800

    refactor: 提供MinIO流式处理方式逐块地下载文件

--- backend/framework/sdk/src/main/java/io/metersphere/sdk/file/FileRepository.java
+++ backend/framework/sdk/src/main/java/io/metersphere/sdk/file/FileRepository.java
@@ -32,12 +32,30 @@ public interface FileRepository {
     public void delete(FileRequest request) throws Exception;
 
     /**
-     * 获取文件字节内容
+     * 删除文件夹
+     *
+     * @param request
+     * @throws Exception
+     */
+    public void deleteFolder(FileRequest request) throws Exception;
+
+
+    /**
+     * 获取文件字节内容，大文件不建议使用
      *
      * @param request
      * @return
      * @throws Exception
      */
     public byte[] getFile(FileRequest request) throws Exception;
+
+    /**
+     * 流式处理方式，通过逐块地下载文件
+     *
+     * @param request
+     * @param localPath
+     * @throws Exception
+     */
+    public void downloadFile(FileRequest request, String localPath) throws Exception;
 
 }
