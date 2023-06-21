75dc26c92e837c065a391a37229cd57b384ac5fa
jianxing <jianxing.chen.ag@gmail.com>
2023-06-21 14:27:41 +0800

    fix(测试跟踪): 修改缺陷评论和用例附件表字符集

--- backend/src/main/resources/db/migration/V30__test_case_file.sql
+++ backend/src/main/resources/db/migration/V30__test_case_file.sql
@@ -4,4 +4,5 @@ create table if not exists test_case_file
     file_id varchar(64) null,
     constraint test_case_file_unique_key
         unique (case_id, file_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
