ab6b9efb31de0d7731dcc1566cdbee7f7b8c1100
jianxing <jianxing.chen.ag@gmail.com>
2023-06-21 14:31:20 +0800

    fix(测试跟踪): 修改缺陷评论和用例附件表字符集

--- system-setting/backend/src/main/resources/db/migration/V30__test_case_file.sql
+++ system-setting/backend/src/main/resources/db/migration/V30__test_case_file.sql
@@ -1,10 +1,12 @@
 SET SESSION innodb_lock_wait_timeout = 7200;
+
 create table if not exists test_case_file
 (
     case_id varchar(64) null,
     file_id varchar(64) null,
     constraint test_case_file_unique_key
         unique (case_id, file_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB
+    DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 
 SET SESSION innodb_lock_wait_timeout = DEFAULT;
