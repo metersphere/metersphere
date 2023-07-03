6d2fe94a80211f7ea5887253816e2b83b144fb90
wxg0103 <727495428@qq.com>
2023-07-03 18:22:19 +0800

    feat(系统设置): 项目功能开发
    --task=1012263 --user=王孝刚 组织与项目-后台 https://www.tapd.cn/55049933/s/1388987

---
+++ backend/services/system-setting/src/test/java/io/metersphere/system/job/CleanProjectJobTests.java
@@ -0,0 +1,28 @@
+package io.metersphere.system.job;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class CleanProjectJobTests {
+    @Resource
+    private CleanProjectJob cleanProjectJob;
+
+
+    @Test
+    @Sql(scripts = {"/dml/init_clean_project.sql"},
+            config = @SqlConfig(encoding = "utf-8", transactionMode = SqlConfig.TransactionMode.ISOLATED),
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    public void cleanupProject() throws Exception{
+        //TODO
+        cleanProjectJob.cleanupProject();
+    }
+}
