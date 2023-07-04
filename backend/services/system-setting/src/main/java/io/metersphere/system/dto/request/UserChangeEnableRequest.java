d524761fb225838bccf81627d389909ca4de4dec
CaptainB <bin@fit2cloud.com>
2023-07-04 12:35:37 +0800

    chore: add equals and hashcode anno

--- backend/services/system-setting/src/main/java/io/metersphere/system/dto/request/UserChangeEnableRequest.java
+++ backend/services/system-setting/src/main/java/io/metersphere/system/dto/request/UserChangeEnableRequest.java
@@ -1,8 +1,10 @@
 package io.metersphere.system.dto.request;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class UserChangeEnableRequest extends UserBatchProcessRequest {
     boolean enable;
 }
--- backend/services/system-setting/src/main/java/io/metersphere/system/dto/response/UserTableResponse.java
+++ backend/services/system-setting/src/main/java/io/metersphere/system/dto/response/UserTableResponse.java
@@ -5,11 +5,13 @@ import io.metersphere.system.domain.User;
 import io.metersphere.system.domain.UserRole;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class UserTableResponse extends User {
     @Schema(title = "用户所属组织")
     private List<Organization> organizationList = new ArrayList<>();
