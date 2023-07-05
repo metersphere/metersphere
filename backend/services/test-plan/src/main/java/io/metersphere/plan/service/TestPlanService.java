db8626ac0e262a9998968faa281051d1e8584f54
CaptainB <bin@fit2cloud.com>
2023-07-05 17:37:40 +0800

    build: swagger deps move to api module

--- backend/services/test-plan/src/main/java/io/metersphere/plan/service/TestPlanService.java
+++ backend/services/test-plan/src/main/java/io/metersphere/plan/service/TestPlanService.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
