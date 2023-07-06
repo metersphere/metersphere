cf00ad8e20ef0e80a991d4e5d41a313bd4351ce8
baiqi <qi.bai@fit2cloud.com>
2023-07-06 09:36:05 +0800

    feat: useVisit-判断用户是否首次访问某个功能

---
+++ frontend/src/hooks/useVisit.ts
@@ -0,0 +1,21 @@
+import { useUserStore, useVisitStore } from '@/store';
+
+/**
+ * 判断账户是否第一次访问
+ * @param key 自定义 key
+ * @param needTimeStamp  是否需要加上时间戳
+ */
+export default function useVisit(key: string, needTimeStamp = false) {
+  const userStore = useUserStore();
+  const visitStore = useVisitStore();
+  const localKey = `${userStore.accountId}-${key}-${needTimeStamp ? new Date().getTime() : ''}`;
+  const addVisited = () => {
+    visitStore.addVisitedKey(localKey);
+  };
+  const getIsVisited = () => visitStore.getIsVisited(localKey);
+
+  return {
+    addVisited,
+    getIsVisited,
+  };
+}
