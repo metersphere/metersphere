3d595c384f1b0eaa7347f2ec5c7d4a977cdad937
xinxin.wu <xinxin.wu@fit2cloud.com>
2023-07-11 13:20:52 +0800

    fix: 修改cr_问题

--- frontend/src/components/pure/ms-alert/type.ts
+++
@@ -1,5 +0,0 @@
-export interface SlotContent {
-  title: string;
-  slotName: string;
-}
-export type SlotContentlist = SlotContent[];
--- frontend/src/views/system/pluginManager/index.vue
+++ frontend/src/views/system/pluginManager/index.vue
@@ -1,15 +1,13 @@
 /* stylelint-disable order/properties-order */
 <template>
   <div>
-    <MSAlert :closable="true" type="primary" :banner="false">
-      <template #content>
-        <div>
-          MeterSphereV2.10LTS版本支持DevOps、API导入、请求、项目管理、协议类型的插件，具体支持插件请
-          <a class="mx-1" href="javascript:;">查看表格</a>更多开源插件，请在此下载
-          <a class="mx-1" href="javascript:;">去下载 </a>
-        </div>
-      </template>
-    </MSAlert>
+    <a-alert type="info" :closable="true">
+      <div>
+        MeterSphereV2.10LTS版本支持DevOps、API导入、请求、项目管理、协议类型的插件，具体支持插件请
+        <a class="mx-1" href="javascript:;">查看表格</a>更多开源插件，请在此下载
+        <a class="mx-1" href="javascript:;">去下载 </a>
+      </div>
+    </a-alert>
     <div class="mt-4">
       <pluginTable />
     </div>
@@ -17,7 +15,6 @@
 </template>
 
 <script setup lang="ts">
-  import MSAlert from '@/components/pure/ms-alert/index.vue';
   import pluginTable from './components/pluginTable.vue';
 </script>
 
