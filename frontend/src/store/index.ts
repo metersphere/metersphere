50352468519e039fdab7547e70716ee57e955735
baiqi <qi.bai@fit2cloud.com>
2023-07-11 11:46:52 +0800

    feat: useVisit-export

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,11 +1,12 @@
 import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import useAppStore from './modules/app';
+import useVisitStore from './modules/app/visit';
 import useUserStore from './modules/user';
 import useTabBarStore from './modules/tab-bar';
 import { debouncePlugin } from './plugins';
 
 const pinia = createPinia().use(debouncePlugin).use(piniaPluginPersistedstate);
 
-export { useAppStore, useUserStore, useTabBarStore };
+export { useAppStore, useUserStore, useTabBarStore, useVisitStore };
 export default pinia;
