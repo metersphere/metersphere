1e750461e63c2a4cde8387cdc2cd6a84b43946f3
xinxin.wu <xinxin.wu@fit2cloud.com>
2023-07-12 16:32:53 +0800

    style(系统设置): 系统_插件管理_弹窗_全局样式

--- frontend/src/views/system/pluginManager/components/updatePluginModal.vue
+++ frontend/src/views/system/pluginManager/components/updatePluginModal.vue
@@ -3,7 +3,7 @@
     v-model:visible="updateVisible"
     class="ms-modal-form ms-modal-small"
     title-align="start"
-    width="540px"
+    width="480px"
     @ok="handleOk"
     @cancel="handleCancel"
   >
