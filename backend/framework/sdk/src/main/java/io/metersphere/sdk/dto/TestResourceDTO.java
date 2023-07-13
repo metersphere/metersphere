2b792c94d07971f7e293b78931ada848aa4617ae
guoyuqi <xiaomeinvG@126.com>
2023-07-13 18:22:29 +0800

    feat: add dto note

--- backend/framework/sdk/src/main/java/io/metersphere/sdk/dto/TestResourceDTO.java
+++ backend/framework/sdk/src/main/java/io/metersphere/sdk/dto/TestResourceDTO.java
@@ -1,5 +1,6 @@
 package io.metersphere.sdk.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,62 +16,75 @@ public class TestResourceDTO {
     /**
      * type为 node时, 性能测试的镜像
      */
+    @Schema(title = "type为node时, 性能测试的镜像")
     private String loadTestImage;
 
     /**
      * type为 node时, 性能测试jvm配置
      */
+    @Schema(title = "type为node时, 性能测试jvm配置")
     private String loadTestHeap;
 
     /**
      * type为 node时, 接口测试 性能测试 node 节点配置
      */
+    @Schema(title = "type为node时, 接口测试,性能测试node节点配置")
     private List<TestResourceNodeDTO> nodesList;
 
     /**
      *  type为 k8s 时，接口测试，性能测试的ip
      */
+    @Schema(title = "type为k8s时，接口测试，性能测试的ip")
     private String ip;
     /**
      *  type为 k8s 时，接口测试，性能测试的token
      */
+    @Schema(title = "type为k8s时，接口测试，性能测试的token")
     private String token;
     /**
      *  type为 k8s 时，接口测试，性能测试的命名空间
      */
+    @Schema(title = "type为k8s时，接口测试，性能测试的命名空间")
     private String nameSpaces;
     /**
      *  type为 k8s 时，接口测试，性能测试，UI测试的最大并发数
      */
+    @Schema(title = "type为k8s时，接口测试，性能测试，UI测试的最大并发数")
     private Integer concurrentNumber;
 
     /**
      *  type为 k8s 时，接口测试，性能测试的单pod 最大线程数
      */
+    @Schema(title = "type为k8s时，接口测试，性能测试的单pod最大线程数")
     private Integer podThreads;
 
     /**
      *  type为 k8s 时，性能测试自定义JOB模版 string
      */
+    @Schema(title = " type为k8s时，性能测试自定义JOB模版string")
     private String jobDefinition;
     /**
      *  type为 k8s 时，接口测试镜像
      */
+    @Schema(title = "type为k8s时，接口测试镜像")
     private String apiTestImage;
 
     /**
      *  type为 k8s 时，接口测试deployName
      */
+    @Schema(title = "type为k8s时，接口测试deployName")
     private String deployName;
 
     /**
      * UI测试的grid配置
      */
+    @Schema(title = "UI测试的grid配置")
     private String uiGrid;
 
     /**
      * 关联的组织id集合
      */
+    @Schema(title = "关联的组织id集合")
     private List<String>orgIds;
 
 
--- backend/framework/sdk/src/main/java/io/metersphere/sdk/dto/TestResourceNodeDTO.java
+++ backend/framework/sdk/src/main/java/io/metersphere/sdk/dto/TestResourceNodeDTO.java
@@ -1,5 +1,6 @@
 package io.metersphere.sdk.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,21 +11,25 @@ public class TestResourceNodeDTO {
     /**
      * 接口测试 性能测试 node节点ip
      */
+    @Schema(title = "接口测试性能测试 node节点ip")
     private String ip;
 
     /**
      * 接口测试 性能测试 node节点端口
      */
+    @Schema(title = "接口测试性能测试 node节点端口")
     private String port;
 
     /**
      * 性能测试 node节点监控器
      */
+    @Schema(title = "性能测试 node节点监控器")
     private String monitor;
 
     /**
      * 接口测试 性能测试 最大并发数
      */
+    @Schema(title = "接口测试,性能测试最大并发数")
     private Integer concurrentNumber;
 
 }
