21e36a1639cafede14cf1383a69d8ef52b51d921
baiqi <qi.bai@fit2cloud.com>
2023-07-13 15:49:24 +0800

    feat: ms-icon组件，集成了设计稿的iconfont字体图标

---
+++ frontend/src/components/pure/ms-icon-font/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <icon-font
+    :type="props.type"
+    :size="props.size || 14"
+    :class="props.color ? `text-[${props.color}]` : 'text-[var(--color-text-4)]'"
+  />
+</template>
+
+<script lang="ts" setup>
+  import { Icon } from '@arco-design/web-vue';
+  import '@/assets/icon-font/iconfont';
+
+  const props = defineProps<{
+    type: string;
+    size?: string | number;
+    rotate?: number;
+    spin?: boolean;
+    color?: string;
+  }>();
+
+  const IconFont = Icon.addFromIconFontCn({
+    extraProps: { ...props },
+  });
+</script>
