63894ee4c855c0cc7dc7e53086ebcb8315cd7e6b
guoyuqi <xiaomeinvG@126.com>
2023-07-17 11:26:09 +0800

    refactor: add resource pool dto note

--- backend/framework/sdk/src/main/java/io/metersphere/sdk/dto/TestResourcePoolDTO.java
+++ backend/framework/sdk/src/main/java/io/metersphere/sdk/dto/TestResourcePoolDTO.java
@@ -1,12 +1,14 @@
 package io.metersphere.sdk.dto;
 
 import io.metersphere.system.domain.TestResourcePool;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class TestResourcePoolDTO extends TestResourcePool {
     private TestResourceDTO testResourceDTO;
 
+    @Schema(title = "资源池是否在使用中")
     private Boolean inUsed;
 
 }
