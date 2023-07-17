d424ef28bc3559fa05368f854e377651d9141d01
song-cc-rock <changchang.song@fit2cloud.com>
2023-07-17 14:45:13 +0800

    fix(测试跟踪): 功能用例默认模块多次创建的问题

--- test-track/backend/src/main/java/io/metersphere/listener/ProjectCreatedListener.java
+++ test-track/backend/src/main/java/io/metersphere/listener/ProjectCreatedListener.java
@@ -1,20 +1,15 @@
 package io.metersphere.listener;
 
-import io.metersphere.base.domain.ModuleNode;
 import io.metersphere.base.domain.Project;
-import io.metersphere.base.domain.TestCaseNodeExample;
 import io.metersphere.base.mapper.ProjectMapper;
 import io.metersphere.base.mapper.ext.ExtModuleNodeMapper;
 import io.metersphere.commons.constants.KafkaTopicConstants;
-import io.metersphere.commons.constants.ProjectModuleDefaultNodeEnum;
 import io.metersphere.commons.utils.LogUtil;
+import io.metersphere.service.TestCaseNodeService;
+import jakarta.annotation.Resource;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-
-import jakarta.annotation.Resource;
-import java.util.List;
-import java.util.UUID;
 
 @Component
 public class ProjectCreatedListener {
@@ -24,6 +19,8 @@ public class ProjectCreatedListener {
     private ExtModuleNodeMapper extModuleNodeMapper;
     @Resource
     private ProjectMapper projectMapper;
+    @Resource
+    private TestCaseNodeService testCaseNodeService;
 
     @KafkaListener(id = CONSUME_ID, topics = KafkaTopicConstants.PROJECT_CREATED_TOPIC, groupId = "${spring.application.name}")
     public void consume(ConsumerRecord<?, String> record) {
@@ -37,23 +34,7 @@ public class ProjectCreatedListener {
         if (project == null) {
             return;
         }
-
-        // 防止重复创建功能用例默认节点
-        TestCaseNodeExample example = new TestCaseNodeExample();
-        example.createCriteria()
-                .andProjectIdEqualTo(projectId).andNameEqualTo(ProjectModuleDefaultNodeEnum.TEST_CASE_DEFAULT_NODE.getNodeName());
-        List<ModuleNode> moduleNodes = extModuleNodeMapper.selectByExample(ProjectModuleDefaultNodeEnum.TEST_CASE_DEFAULT_NODE.getTableName(), example);
-        if (moduleNodes.size() == 0) {
-            ModuleNode record = new ModuleNode();
-            record.setId(UUID.randomUUID().toString());
-            record.setCreateUser(project.getCreateUser());
-            record.setPos(1.0);
-            record.setLevel(1);
-            record.setCreateTime(System.currentTimeMillis());
-            record.setUpdateTime(System.currentTimeMillis());
-            record.setProjectId(projectId);
-            record.setName(ProjectModuleDefaultNodeEnum.TEST_CASE_DEFAULT_NODE.getNodeName());
-            extModuleNodeMapper.insert(ProjectModuleDefaultNodeEnum.TEST_CASE_DEFAULT_NODE.getTableName(), record);
-        }
+        // 创建功能用例默认模块
+        testCaseNodeService.createDefaultNode(projectId);
     }
 }
