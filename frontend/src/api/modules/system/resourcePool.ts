6fc92f17e06cc9a736c341a8278966448b0b12e3
baiqi <qi.bai@fit2cloud.com>
2023-07-18 09:48:05 +0800

    feat: 获取主题变量 hook &资源池model

--- frontend/src/api/modules/system/resourcePool.ts
+++ frontend/src/api/modules/system/resourcePool.ts
@@ -1,6 +1,7 @@
 import MSR from '@/api/http/index';
 import { PoolListUrl, UpdatePoolUrl, AddPoolUrl, DetailPoolUrl } from '@/api/requrls/system/resourcePool';
-import type { ResourcePoolItem, ResourcePoolInfo } from '@/models/system/resourcePool';
+
+import type { ResourcePoolItem, AddResourcePoolParams } from '@/models/system/resourcePool';
 import type { TableQueryParams } from '@/models/common';
 
 // 获取资源池列表
@@ -14,7 +15,7 @@ export function updatePoolInfo(data: ResourcePoolItem) {
 }
 
 // 添加资源池
-export function addPool(data: ResourcePoolInfo) {
+export function addPool(data: AddResourcePoolParams) {
   return MSR.post({ url: AddPoolUrl, data });
 }
 
