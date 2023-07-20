d0a93b7d5c1609503954926338e9220e7bc0d673
xinxin.wu <xinxin.wu@fit2cloud.com>
2023-07-20 21:35:12 +0800

    feat: 提取批量添加用户&项目&修改upload

---
+++ frontend/src/components/bussiness/ms-batch-modal/locale/en-US.ts
@@ -0,0 +1,8 @@
+export default {
+  'msbatchmodal.batchModalSubTitle': '(Selected {count} users)',
+  'msbatchmodal.batchModalTip': 'Add project member usergroup as member by default',
+  'msbatchmodal.batchOptional': 'Optional',
+  'msbatchmodal.batchChosen': 'Chosen',
+  'msbatchmodal.batchModalCancel': 'Cancel',
+  'msbatchmodal.batchModalConfirm': 'Add',
+};
