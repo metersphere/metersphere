0b6210d9e446a603add7ef487f777ba3b0da86d1
jianxing <jianxing.chen.ag@gmail.com>
2023-07-27 10:57:48 +0800

    fix(测试跟踪): 功能用例导入失败后依然创建了模块
    --bug=1028167 --user=陈建星 【测试跟踪】功能用例导入-excel导入-导入失败-却新建了excel表里的路径文件夹 https://www.tapd.cn/55049933/s/1398321

--- framework/sdk-parent/sdk/src/main/java/io/metersphere/commons/exception/MSException.java
+++ framework/sdk-parent/sdk/src/main/java/io/metersphere/commons/exception/MSException.java
@@ -2,16 +2,32 @@ package io.metersphere.commons.exception;
 
 public class MSException extends RuntimeException {
 
+    private Object detail;
+
     private MSException(String message) {
         super(message);
+    }
+
+    private MSException(String message, Object detail) {
+        super(message);
+        this.detail = detail;
     }
 
     private MSException(Throwable t) {
         super(t);
     }
 
+    private MSException(Throwable t, Object detail) {
+        super(t);
+        this.detail = detail;
+    }
+
     public static void throwException(String message) {
         throw new MSException(message);
+    }
+
+    public static void throwException(String message, Object detail) {
+        throw new MSException(message, detail);
     }
 
     public static MSException getException(String message) {
@@ -20,5 +36,13 @@ public class MSException extends RuntimeException {
 
     public static void throwException(Throwable t) {
         throw new MSException(t);
+    }
+
+    public static void throwException(Throwable t, Object detail) {
+        throw new MSException(t, detail);
+    }
+
+    public Object getDetail() {
+        return detail;
     }
 }
