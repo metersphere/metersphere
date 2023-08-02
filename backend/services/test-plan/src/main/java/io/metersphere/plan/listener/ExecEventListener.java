fb01646f2353dce81267b7c54f4f84062ff3ebb3
fit2-zhao <yong.zhao@fit2cloud.com>
2023-08-02 18:15:33 +0800

    refactor: 修改公共触发监听

--- backend/services/test-plan/src/main/java/io/metersphere/plan/listener/ExecEventListener.java
+++ backend/services/test-plan/src/main/java/io/metersphere/plan/listener/ExecEventListener.java
@@ -10,6 +10,6 @@ public class ExecEventListener implements EventListener<Event> {
     @Override
     public void onEvent(Event event) {
         // todo: 测试计划事件处理逻辑
-        LogUtils.info("ExecEventListener: " + event.getModule() + "：" + event.getMessage());
+        LogUtils.info("ExecEventListener: " + event.module() + "：" + event.message());
     }
 }
