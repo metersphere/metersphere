5cda4295356a7552ac2ebaabcaefe2be08270543
song-tianyang <tianyang.song@fit2cloud.com>
2023-08-08 15:50:18 +0800

    style(性能测试): 优化性能测试报告中环境的展示效果
    --bug=1028215 --user=宋天阳 【性能测试】-报告的运行环境弹窗内容显示有点挤，建议增加行高
    https://www.tapd.cn/55049933/s/1401694

--- performance-test/frontend/src/business/report/components/ProjectEnvironmentDialog.vue
+++ performance-test/frontend/src/business/report/components/ProjectEnvironmentDialog.vue
@@ -1,15 +1,15 @@
 <template>
   <el-dialog
-    :title="$t('commons.environment')"
-    :visible.sync="dialogVisible"
-    width="30%"
-    :append-to-body="true"
-    :destroy-on-close="true"
-    z-index="99998"
-    :before-close="handleClose">
+      :title="$t('commons.environment')"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :append-to-body="true"
+      :destroy-on-close="true"
+      z-index="99998"
+      :before-close="handleClose">
     <div>
       <div v-if="projectEnvMap" type="flex">
-        <div v-for="(values,key) in projectEnvMap" :key="key" style="margin-right: 10px">
+        <div v-for="(values,key) in projectEnvMap" :key="key" style="margin-right: 10px;margin-bottom: 10px">
           {{ key + ":" }}
           <ms-tag v-for="(item,index) in values" :key="index" type="success" :content="item"
                   style="margin-left: 2px"/>
