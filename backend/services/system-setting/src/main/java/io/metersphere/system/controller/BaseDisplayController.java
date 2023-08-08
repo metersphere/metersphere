3a254f964fd645d4512f84db6d82e39f0555e625
WangXu10 <xu.wang@fit2cloud.com>
2023-08-08 18:40:54 +0800

    feat(系统设置): 首页图片资源

---
+++ backend/services/system-setting/src/main/java/io/metersphere/system/controller/BaseDisplayController.java
@@ -0,0 +1,47 @@
+package io.metersphere.system.controller;
+
+import io.metersphere.system.service.BaseDisplayService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@Tag(name = "首页图片")
+@RestController
+@RequestMapping(value = "/base-display")
+public class BaseDisplayController {
+
+    @Resource
+    private BaseDisplayService displayService;
+
+
+    @Operation(summary = "获取icon图片")
+    @GetMapping("/get/icon")
+    public ResponseEntity<byte[]> getIcon() throws IOException {
+        return displayService.getFile("icon");
+    }
+
+
+    @Operation(summary = "获取loginImage图片")
+    @GetMapping("/get/login-image")
+    public ResponseEntity<byte[]> getLoginImage() throws IOException {
+        return displayService.getFile("loginImage");
+    }
+
+    @Operation(summary = "获取loginLogo图片")
+    @GetMapping("/get/login-logo")
+    public ResponseEntity<byte[]> getLoginLogo() throws IOException {
+        return displayService.getFile("loginLogo");
+    }
+
+    @Operation(summary = "获取logoPlatform图片")
+    @GetMapping("/get/logo-platform")
+    public ResponseEntity<byte[]> getLogoPlatform() throws IOException {
+        return displayService.getFile("logoPlatform");
+    }
+}
---
+++ backend/services/system-setting/src/test/java/io/metersphere/system/controller/BaseDisplayControllerTests.java
@@ -0,0 +1,128 @@
+package io.metersphere.system.controller;
+
+import io.metersphere.sdk.base.BaseTest;
+import io.metersphere.sdk.constants.SessionConstants;
+import io.metersphere.sdk.file.FileRequest;
+import io.metersphere.sdk.file.MinioRepository;
+import io.metersphere.sdk.log.constants.OperationLogModule;
+import jakarta.annotation.Resource;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class BaseDisplayControllerTests extends BaseTest {
+
+    public static final String DISPLAY_ICON = "/base-display/get/icon";
+    public static final String DISPLAY_LOGINIMAGE = "/base-display/get/login-image";
+    public static final String DISPLAY_LOGINLOGO = "/base-display/get/login-logo";
+    public static final String DISPLAY_LOGOPLATFORM = "/base-display/get/logo-platform";
+
+    //默认图片
+    @Resource
+    private MinioRepository repository;
+
+    @Test
+    @Order(1)
+    public void getIconDefault() throws Exception {
+        this.requestGet(DISPLAY_ICON);
+    }
+
+    @Test
+    @Order(2)
+    public void getLoginImageDefault() throws Exception {
+        this.requestGet(DISPLAY_LOGINIMAGE);
+    }
+
+    @Test
+    @Order(3)
+    public void getLoginLogoDefault() throws Exception {
+        this.requestGet(DISPLAY_LOGINLOGO);
+    }
+
+    @Test
+    @Order(4)
+    public void getLogoPlatformDefault() throws Exception {
+        this.requestGet(DISPLAY_LOGOPLATFORM);
+    }
+
+    //文件先放入minio
+    @Test
+    @Order(6)
+    public void saveFile() throws Exception {
+
+        saveImages("/file/favicon.ico","favicon","favicon.ico","ui.icon:favicon.ico");
+        saveImages("/file/login-banner.jpg","login-banner","login-banner.jpg","ui.loginImage:login-banner.jpg");
+        saveImages("/file/login-logo.svg","login-logo","login-logo.svg","ui.loginLogo:login-logo.svg");
+        saveImages("/file/MS-full-logo.svg","MS-full-logo","MS-full-logo.svg","ui.logoPlatform:MS-full-logo.svg");
+
+    }
+
+    private void saveImages(String url, String name, String originalFileName, String fileName) throws Exception{
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver(getClass().getClassLoader());
+        byte[] bytes = IOUtils.toByteArray(resolver.getResource(url).getInputStream());
+        MockMultipartFile mockFile = new MockMultipartFile(name, originalFileName, "application/octet-stream", bytes);
+        FileRequest request = new FileRequest();
+        request.setFileName(fileName);
+        request.setProjectId("system");
+        request.setResourceId(OperationLogModule.SYSTEM_PARAMETER_SETTING);
+        repository.saveFile(mockFile, request);
+    }
+
+
+    //取minio中的图片
+    @Test
+    @Order(5)
+    @Sql(scripts = {"/dml/init_display_test.sql"},
+            config = @SqlConfig(encoding = "utf-8", transactionMode = SqlConfig.TransactionMode.ISOLATED),
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    public void getIconError() throws Exception {
+        this.requestGet(DISPLAY_ICON);
+    }
+
+    //取minio中的图片
+    @Test
+    @Order(7)
+    public void getIcon() throws Exception {
+        this.requestGet(DISPLAY_ICON);
+    }
+
+    @Test
+    @Order(7)
+    public void getLoginImage() throws Exception {
+        this.requestGet(DISPLAY_LOGINIMAGE);
+    }
+
+    @Test
+    @Order(7)
+    public void getLoginLogo() throws Exception {
+        this.requestGet(DISPLAY_LOGINLOGO);
+    }
+
+    @Test
+    @Order(8)
+    public void getLogoPlatform() throws Exception {
+        this.requestGet(DISPLAY_LOGOPLATFORM);
+    }
+
+    private MvcResult requestGet(String url) throws Exception {
+        return mockMvc.perform(MockMvcRequestBuilders.get(url)
+                        .header(SessionConstants.HEADER_TOKEN, sessionId)
+                        .header(SessionConstants.CSRF_TOKEN, csrfToken))
+                .andDo(print()).andReturn();
+    }
+}
