8b3c30efe2983e32217f659ed1a3ec2afc930d3a
guoyuqi <xiaomeinvG@126.com>
2023-08-08 13:12:26 +0800

    fix: change permission to or

--- backend/services/system-setting/src/main/java/io/metersphere/system/controller/OrganizationController.java
+++ backend/services/system-setting/src/main/java/io/metersphere/system/controller/OrganizationController.java
@@ -18,6 +18,7 @@ import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
+import org.apache.shiro.authz.annotation.Logical;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,7 +40,7 @@ public class OrganizationController {
 
     @PostMapping("/member/list")
     @Operation(summary = "组织级别获取组织成员")
-    @RequiresPermissions(value = {PermissionConstants.ORGANIZATION_MEMBER_READ, PermissionConstants.SYSTEM_USER_READ})
+    @RequiresPermissions(value = {PermissionConstants.ORGANIZATION_MEMBER_READ, PermissionConstants.SYSTEM_USER_READ}, logical = Logical.OR)
     public Pager<List<OrgUserExtend>> getMemberList(@Validated @RequestBody OrganizationRequest organizationRequest) {
         Page<Object> page = PageHelper.startPage(organizationRequest.getCurrent(), organizationRequest.getPageSize());
         return PageUtils.setPageInfo(page, organizationService.getMemberListByOrg(organizationRequest));
@@ -61,7 +62,7 @@ public class OrganizationController {
 
     @PostMapping("/update-member")
     @Operation(summary = "更新用户")
-    @RequiresPermissions(value = {PermissionConstants.ORGANIZATION_MEMBER_UPDATE, PermissionConstants.PROJECT_USER_READ_ADD,  PermissionConstants.PROJECT_USER_READ_DELETE})
+    @RequiresPermissions(value = {PermissionConstants.ORGANIZATION_MEMBER_UPDATE, PermissionConstants.PROJECT_USER_READ_ADD, PermissionConstants.PROJECT_USER_READ_DELETE}, logical = Logical.OR)
     public void updateMember(@Validated @RequestBody OrganizationMemberUpdateRequest organizationMemberExtendRequest) {
         organizationService.updateMember(organizationMemberExtendRequest, SessionUtils.getUserId());
     }
