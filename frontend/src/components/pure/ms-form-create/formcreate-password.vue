8f1a2a4a2cad604311b9496b4040112e241af7d6
xinxin.wu <xinxin.wu@fit2cloud.com>
2023-08-09 14:25:09 +0800

    feat(系统设置): 服务集成页面搭建

---
+++ frontend/src/components/pure/ms-form-create/formcreate-password.vue
@@ -0,0 +1,46 @@
+<template>
+  <a-input
+    v-model="inputValue"
+    :max-length="250"
+    :type="isShowPassword ? 'password' : 'text'"
+    :placeholder="placeholder"
+    style="width: 100%"
+    autocomplete="username"
+  >
+    <template #suffix>
+      <span v-if="!isShowPassword" @click="togglePasswordVisibility">
+        <icon-eye />
+      </span>
+      <span v-else>
+        <icon-eye-invisible @click="togglePasswordVisibility" />
+      </span>
+    </template>
+  </a-input>
+</template>
+
+<script setup lang="ts">
+  import { ref, watch } from 'vue';
+
+  defineProps<{
+    placeholder?: string;
+    value?: string;
+  }>();
+  const inputValue = ref('');
+  const emits = defineEmits<{
+    (event: 'update:modelValue', value: string): void;
+  }>();
+
+  watch(
+    () => inputValue.value,
+    (val: string) => {
+      emits('update:modelValue', val);
+    }
+  );
+
+  const isShowPassword = ref<boolean>(true);
+  const togglePasswordVisibility = () => {
+    isShowPassword.value = !isShowPassword.value;
+  };
+</script>
+
+<style scoped></style>
