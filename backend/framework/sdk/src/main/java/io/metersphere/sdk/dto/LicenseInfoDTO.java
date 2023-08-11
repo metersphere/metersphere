c4b5d2dd55e3e2cbbc4c4faa73cfb176f3485f8b
wxg0103 <727495428@qq.com>
2023-08-11 13:59:35 +0800

    refactor(系统设置): 授权管理返回值增加swagger注解

--- backend/framework/sdk/src/main/java/io/metersphere/sdk/dto/LicenseInfoDTO.java
+++ backend/framework/sdk/src/main/java/io/metersphere/sdk/dto/LicenseInfoDTO.java
@@ -1,5 +1,6 @@
 package io.metersphere.sdk.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -9,25 +10,31 @@ public class LicenseInfoDTO implements Serializable {
     /**
      * 客户名称
      */
+    @Schema(description = "客户名称")
     private String corporation;
     /**
      * 授权截止时间
      */
+    @Schema(description = "授权截止时间")
     private String expired;
     /**
      * 产品名称
      */
+    @Schema(description = "产品名称")
     private String product;
     /**
      * 产品版本
      */
+    @Schema(description = "产品版本")
     private String edition;
     /**
      * icense版本
      */
+    @Schema(description = "license版本")
     private String licenseVersion;
     /**
      * 授权数量
      */
+    @Schema(description = "授权数量")
     private int count;
 }
