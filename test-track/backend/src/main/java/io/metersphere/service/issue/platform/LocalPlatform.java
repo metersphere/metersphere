f580d8e2a404dd149305daa8e623a2d9c841a557
song-cc-rock <changchang.song@fit2cloud.com>
2023-08-15 14:34:58 +0800

    fix(测试跟踪): 缺陷复制后保存创建人有误
    --bug=1028663 --user=宋昌昌 【测试跟踪】github#26143，【测试跟踪-缺陷管理-复制】B用户基于A提交的缺陷复制并修改内容后，B用户提交的缺陷“创建人”显示的是A用户 https://www.tapd.cn/55049933/s/1403791

--- test-track/backend/src/main/java/io/metersphere/service/issue/platform/LocalPlatform.java
+++ test-track/backend/src/main/java/io/metersphere/service/issue/platform/LocalPlatform.java
@@ -39,12 +39,11 @@ public class LocalPlatform extends LocalAbstractPlatform {
             List<CustomFieldItemDTO> customFields = issuesRequest.getRequestFields();
             String customFieldStr = JSON.toJSONString(customFields);
             List<TestCaseBatchRequest.CustomFiledRequest> fields = JSON.parseArray(customFieldStr, TestCaseBatchRequest.CustomFiledRequest.class);
-            for (TestCaseBatchRequest.CustomFiledRequest field : fields) {
+            for (TestCaseBatchRequest.CustomFiledRequest field : fields)
                 if (StringUtils.equals("状态", field.getName())) {
                     issueStatus = (String) field.getValue();
                     break;
                 }
-            }
         }
         SessionUser user = SessionUtils.getUser();
         String id = UUID.randomUUID().toString();
@@ -58,6 +57,7 @@ public class LocalPlatform extends LocalAbstractPlatform {
         issues.setUpdateTime(System.currentTimeMillis());
         issues.setPlatform(IssuesManagePlatform.Local.toString());
         issues.setNum(getNextNum(issuesRequest.getProjectId()));
+        issues.setCreator(SessionUtils.getUserId());
         issuesMapper.insert(issues);
 
         issuesRequest.setId(id);
