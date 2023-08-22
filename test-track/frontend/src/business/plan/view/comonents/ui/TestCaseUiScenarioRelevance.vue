e9e2f1e5ce906d8e4d40eef2a5db40394641ccda
guoyuqi <xiaomeinvG@126.com>
2023-08-22 11:37:09 +0800

    fix(接口测试): 修复测试计划全选UI用例环境数据丢失问题
    --bug=1029146 --user=郭雨琦
    https://www.tapd.cn/55049933/bugtrace/bugs/view/1155049933001029146

--- test-track/frontend/src/business/plan/view/comonents/ui/TestCaseUiScenarioRelevance.vue
+++ test-track/frontend/src/business/plan/view/comonents/ui/TestCaseUiScenarioRelevance.vue
@@ -38,7 +38,6 @@ import {strMapToObj} from "metersphere-frontend/src/utils";
 import {ENV_TYPE} from "metersphere-frontend/src/utils/constants";
 import RelevanceUiScenarioList from "@/business/plan/view/comonents/ui/RelevanceUiScenarioList";
 import {testPlanAutoCheck} from "@/api/remote/plan/test-plan";
-import {testPlanUiScenarioRelevanceListIds} from "@/api/remote/ui/test-plan-ui-scenario-case";
 import UiScenarioModule from "@/business/plan/view/comonents/ui/UiScenarioModule";
 import {uiAutomationRelevance} from "@/api/remote/ui/api-scenario";
 
@@ -139,14 +138,8 @@ export default {
         this.$refs.baseRelevance.close();
       });
     },
-    getAllId(param) {
-      return new Promise((resolve) => {
-        testPlanUiScenarioRelevanceListIds(param).then((r) => {
-          resolve(r.data);
-        });
-      });
-    },
-    async saveCaseRelevance() {
+
+    saveCaseRelevance() {
       this.isSaving = true;
       let selectIds = [];
       let selectRows = this.$refs.apiScenarioList.selectRows;
@@ -154,9 +147,14 @@ export default {
       let map = this.$refs.apiScenarioList.map;
       let envGroupId = this.$refs.apiScenarioList.envGroupId;
 
-      selectRows.forEach((row) => {
-        selectIds.push(row.id);
-      });
+      if (!this.$refs.apiScenarioList.selectAll) {
+        selectRows.forEach((row) => {
+          selectIds.push(row.id);
+        });
+      } else {
+        selectIds = this.$refs.apiScenarioList.selectAllIds;
+      }
+
       if (selectIds.length < 1) {
         this.isSaving = false;
         this.$warning(this.$t("test_track.plan_view.please_choose_test_case"));
@@ -169,13 +167,6 @@ export default {
       param.envMap = strMapToObj(envMap);
       param.envGroupId = envGroupId;
       param.selectIds = selectIds;
-
-      //查找所有数据
-      let params = this.$refs.apiScenarioList.condition;
-      if (params.selectAll) {
-        let result = await this.getAllId(params);
-        param.selectIds = result;
-      }
       this.loading = true;
       uiAutomationRelevance(param)
           .then(() => {
