3ed57ed9d9955aec050dc279459e2075264a3375
baiqi <qi.bai@fit2cloud.com>
2023-08-25 15:06:20 +0800

    fix(测试跟踪): 测试报告-接口用例-执行结果pending状态后多显示了.
    --bug=1028817 --user=白奇 【测试跟踪】测试报告-接口用例-执行结果pending状态后多显示了. https://www.tapd.cn/55049933/s/1408430

--- test-track/frontend/src/business/plan/view/comonents/api/TestPlanApiStatus.vue
+++ test-track/frontend/src/business/plan/view/comonents/api/TestPlanApiStatus.vue
@@ -1,16 +1,29 @@
 <template>
-  <div style="display: inline">
-    <el-tag size="mini" type="primary" effect="plain" v-if="getStatus(status) === 'running'">
+  <div>
+    <el-tag
+      size="mini"
+      type="primary"
+      effect="plain"
+      v-if="getStatus(status) === 'running'"
+    >
       {{ showStatus(status) }}
     </el-tag>
-    <el-tag size="mini" type="success" v-else-if="getStatus(status) === 'success'">
+    <el-tag
+      size="mini"
+      type="success"
+      v-else-if="getStatus(status) === 'success'"
+    >
       {{ showStatus(status) }}
     </el-tag>
     <el-tag size="mini" type="danger" v-else-if="getStatus(status) === 'error'">
       {{ showStatus(status) }}
     </el-tag>
-    <el-tag size="mini" type="danger" style="background-color: #F6972A; color: #FFFFFF"
-            v-else-if="getStatus(status) === 'fake_error'">
+    <el-tag
+      size="mini"
+      type="danger"
+      style="background-color: #f6972a; color: #ffffff"
+      v-else-if="getStatus(status) === 'fake_error'"
+    >
       FakeError
     </el-tag>
     <el-tag v-else size="mini" type="info">
@@ -24,7 +37,7 @@ export default {
   name: "MsTestPlanApiStatus",
 
   props: {
-    status: String
+    status: String,
   },
   methods: {
     getStatus(status) {
@@ -35,14 +48,14 @@ export default {
     },
     showStatus(status) {
       if (!status) {
-        status = 'PENDING';
+        status = "PENDING";
       }
-      return status.toLowerCase()[0].toUpperCase() + status.toLowerCase().substr(1);
-    }
-  }
-}
+      return (
+        status.toLowerCase()[0].toUpperCase() + status.toLowerCase().substr(1)
+      );
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
