9cc14d7bb8f98619246507a75b5c01335b5d5016
WangXu10 <xu.wang@fit2cloud.com>
2023-09-05 10:23:24 +0800

    fix(系统设置): 修复keyword为空问题

--- backend/services/system-setting/src/main/java/io/metersphere/system/service/UserService.java
+++ backend/services/system-setting/src/main/java/io/metersphere/system/service/UserService.java
@@ -304,7 +304,7 @@ public class UserService {
     }
 
     public List<User> getUserList(String keyword) {
-        return extUserMapper.selectUserList(keyword.trim());
+        return extUserMapper.selectUserList(keyword);
     }
 
     /**
