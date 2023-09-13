a63f56dc0ff732bd60320d5d270da5ed5aa41bbe
guoyuqi <xiaomeinvG@126.com>
2023-09-13 16:16:05 +0800

    feat(消息管理): 增加新增项目时同步新增其他资源功能

--- backend/services/system-setting/src/test/java/io/metersphere/system/controller/CreateResourceTests.java
+++ backend/services/system-setting/src/test/java/io/metersphere/system/controller/CreateResourceTests.java
@@ -1,7 +1,6 @@
 package io.metersphere.system.controller;
 
 import io.metersphere.sdk.invoker.ProjectServiceInvoker;
-import io.metersphere.system.mock.CleanupTestResourceService;
 import io.metersphere.system.mock.CreateTestResourceService;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.MethodOrderer;
