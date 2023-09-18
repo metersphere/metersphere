02caba2de0a6c87c0a6216ad0e30025b300b0d14
WangXu10 <xu.wang@fit2cloud.com>
2023-09-18 17:02:02 +0800

    refactor(项目管理): 项目与权限-菜单管理-功能优化

--- backend/services/project-management/src/main/java/io/metersphere/project/service/ProjectApplicationService.java
+++ backend/services/project-management/src/main/java/io/metersphere/project/service/ProjectApplicationService.java
@@ -8,22 +8,21 @@ import io.metersphere.project.mapper.ExtProjectTestResourcePoolMapper;
 import io.metersphere.project.mapper.ExtProjectUserRoleMapper;
 import io.metersphere.project.mapper.ProjectApplicationMapper;
 import io.metersphere.project.request.ProjectApplicationRequest;
-import io.metersphere.sdk.constants.OperationLogConstants;
-import io.metersphere.sdk.constants.ProjectApplicationType;
-import io.metersphere.sdk.constants.ScheduleType;
+import io.metersphere.sdk.constants.*;
 import io.metersphere.sdk.dto.LogDTO;
 import io.metersphere.sdk.dto.OptionDTO;
-import io.metersphere.system.log.constants.OperationLogModule;
-import io.metersphere.system.log.constants.OperationLogType;
-import io.metersphere.system.sechedule.BaseScheduleService;
-import io.metersphere.system.service.PluginLoadService;
+import io.metersphere.sdk.dto.SessionUser;
 import io.metersphere.sdk.util.JSON;
-import io.metersphere.system.utils.ServiceUtils;
 import io.metersphere.sdk.util.SessionUtils;
 import io.metersphere.system.domain.*;
+import io.metersphere.system.log.constants.OperationLogModule;
+import io.metersphere.system.log.constants.OperationLogType;
 import io.metersphere.system.mapper.ExtPluginMapper;
 import io.metersphere.system.mapper.PluginMapper;
 import io.metersphere.system.mapper.ServiceIntegrationMapper;
+import io.metersphere.system.sechedule.BaseScheduleService;
+import io.metersphere.system.service.PluginLoadService;
+import io.metersphere.system.utils.ServiceUtils;
 import jakarta.annotation.Resource;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.BooleanUtils;
@@ -32,6 +31,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -65,15 +65,16 @@ public class ProjectApplicationService {
     /**
      * 更新配置信息
      *
-     * @param application
+     * @param applications
      * @return
      */
-    public ProjectApplication update(ProjectApplication application) {
-        //定时任务配置，检查是否存在定时任务配置，存在则更新，不存在则新增
-        this.doBeforeUpdate(application);
-        //配置信息入库
-        this.createOrUpdateConfig(application);
-        return application;
+    public void update(List<ProjectApplication> applications) {
+        applications.forEach(application -> {
+            //定时任务配置，检查是否存在定时任务配置，存在则更新，不存在则新增
+            this.doBeforeUpdate(application);
+            //配置信息入库
+            this.createOrUpdateConfig(application);
+        });
     }
 
     private void createOrUpdateConfig(ProjectApplication application) {
@@ -93,10 +94,10 @@ public class ProjectApplicationService {
     private void doBeforeUpdate(ProjectApplication application) {
         String type = application.getType();
         //TODO 自定义id配置 &其他配置
-        if (StringUtils.equals(type, ProjectApplicationType.APPLICATION_CLEAN_TEST_PLAN_REPORT.name())
-                || StringUtils.equals(type, ProjectApplicationType.APPLICATION_CLEAN_UI_REPORT.name())
-                || StringUtils.equals(type, ProjectApplicationType.APPLICATION_CLEAN_PERFORMANCE_TEST_REPORT.name())
-                || StringUtils.equals(type, ProjectApplicationType.APPLICATION_CLEAN_API_REPORT.name())) {
+        if (StringUtils.equals(type, ProjectApplicationType.TEST_PLAN.TEST_PLAN_CLEAN_REPORT.name())
+                || StringUtils.equals(type, ProjectApplicationType.UI.UI_CLEAN_REPORT.name())
+                || StringUtils.equals(type, ProjectApplicationType.PERFORMANCE_TEST.PERFORMANCE_TEST_CLEAN_REPORT.name())
+                || StringUtils.equals(type, ProjectApplicationType.API.API_CLEAN_REPORT.name())) {
             //清除 测试计划/UI测试/性能测试/接口测试 报告 定时任务
             this.doHandleSchedule(application);
         }
@@ -143,9 +144,9 @@ public class ProjectApplicationService {
      * @param request
      * @return
      */
-    public List<ProjectApplication> get(ProjectApplicationRequest request) {
+    public List<ProjectApplication> get(ProjectApplicationRequest request, List<String> types) {
         ProjectApplicationExample projectApplicationExample = new ProjectApplicationExample();
-        projectApplicationExample.createCriteria().andProjectIdEqualTo(request.getProjectId()).andTypeIn(request.getTypes());
+        projectApplicationExample.createCriteria().andProjectIdEqualTo(request.getProjectId()).andTypeIn(types);
         List<ProjectApplication> applicationList = projectApplicationMapper.selectByExample(projectApplicationExample);
         if (CollectionUtils.isNotEmpty(applicationList)) {
             return applicationList;
@@ -186,7 +187,7 @@ public class ProjectApplicationService {
         example.createCriteria().andOrganizationIdEqualTo(organizationId).andEnableEqualTo(true);
         List<ServiceIntegration> serviceIntegrations = serviceIntegrationMapper.selectByExample(example);
         List<OptionDTO> options = new ArrayList<>();
-        if(CollectionUtils.isNotEmpty(serviceIntegrations)){
+        if (CollectionUtils.isNotEmpty(serviceIntegrations)) {
             List<String> pluginIds = serviceIntegrations.stream().map(ServiceIntegration::getPluginId).collect(Collectors.toList());
             options = extPluginMapper.selectPluginOptions(pluginIds);
             return options;
@@ -206,71 +207,152 @@ public class ProjectApplicationService {
 
 
     /**
-     * 测试计划 日志
+     * 获取所有配置信息
      *
-     * @param application
+     * @param user
      * @return
      */
-    public LogDTO updateTestPlanLog(ProjectApplication application) {
-        return delLog(application, OperationLogModule.PROJECT_PROJECT_MANAGER, "测试计划配置");
+    public List<ProjectApplication> getAllConfigs(SessionUser user, String projectId) {
+        List<ProjectApplication> list = new ArrayList<>();
+        Boolean flag = checkAdmin(user);
+        ProjectApplicationExample example = new ProjectApplicationExample();
+        ProjectApplicationExample.Criteria criteria = example.createCriteria();
+        criteria.andProjectIdEqualTo(projectId);
+        if (flag) {
+            list = projectApplicationMapper.selectByExample(example);
+        }
+        List<String> types = checkPermission(user);
+        if (CollectionUtils.isNotEmpty(types)) {
+            criteria.andTypeIn(types);
+            list = projectApplicationMapper.selectByExample(example);
+        }
+        return list;
+    }
+
+    private List<String> checkPermission(SessionUser user) {
+        List<UserRolePermission> permissions = new ArrayList<>();
+        user.getUserRolePermissions().forEach(g -> {
+            permissions.addAll(g.getUserRolePermissions());
+        });
+        List<String> permissionIds = permissions.stream().map(UserRolePermission::getPermissionId).collect(Collectors.toList());
+
+        List<String> types = new ArrayList<>();
+        permissionIds.forEach(permissionId -> {
+            switch (permissionId) {
+                case PermissionConstants.PROJECT_APPLICATION_WORKSTATION_READ ->
+                        types.addAll(Arrays.asList(ProjectApplicationType.WORKSTATION.values()).stream().map(ProjectApplicationType.WORKSTATION::name).collect(Collectors.toList()));
+                case PermissionConstants.PROJECT_APPLICATION_TEST_PLAN_READ ->
+                        types.addAll(Arrays.asList(ProjectApplicationType.TEST_PLAN.values()).stream().map(ProjectApplicationType.TEST_PLAN::name).collect(Collectors.toList()));
+                case PermissionConstants.PROJECT_APPLICATION_ISSUE_READ ->
+                        types.addAll(Arrays.asList(ProjectApplicationType.ISSUE.values()).stream().map(ProjectApplicationType.ISSUE::name).collect(Collectors.toList()));
+                case PermissionConstants.PROJECT_APPLICATION_CASE_READ ->
+                        types.addAll(Arrays.asList(ProjectApplicationType.CASE.values()).stream().map(ProjectApplicationType.CASE::name).collect(Collectors.toList()));
+                case PermissionConstants.PROJECT_APPLICATION_API_READ ->
+                        types.addAll(Arrays.asList(ProjectApplicationType.API.values()).stream().map(ProjectApplicationType.API::name).collect(Collectors.toList()));
+                case PermissionConstants.PROJECT_APPLICATION_UI_READ ->
+                        types.addAll(Arrays.asList(ProjectApplicationType.UI.values()).stream().map(ProjectApplicationType.UI::name).collect(Collectors.toList()));
+                case PermissionConstants.PROJECT_APPLICATION_PERFORMANCE_TEST_READ ->
+                        types.addAll(Arrays.asList(ProjectApplicationType.PERFORMANCE_TEST.values()).stream().map(ProjectApplicationType.PERFORMANCE_TEST::name).collect(Collectors.toList()));
+                default -> {
+                }
+            }
+        });
+        return types;
+    }
+
+    private Boolean checkAdmin(SessionUser user) {
+        long count = user.getUserRoles()
+                .stream()
+                .filter(g -> StringUtils.equalsIgnoreCase(g.getId(), InternalUserRole.ADMIN.getValue()))
+                .count();
+
+        if (count > 0) {
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * 测试计划 日志
+     *
+     * @param applications
+     * @return
+     */
+    public List<LogDTO> updateTestPlanLog(List<ProjectApplication> applications) {
+        return delLog(applications, OperationLogModule.PROJECT_PROJECT_MANAGER, "测试计划配置");
     }
 
 
     /**
      * UI 日志
      *
-     * @param application
+     * @param applications
      * @return
      */
-    public LogDTO updateUiLog(ProjectApplication application) {
-        return delLog(application, OperationLogModule.PROJECT_PROJECT_MANAGER, "UI配置");
+    public List<LogDTO> updateUiLog(List<ProjectApplication> applications) {
+        return delLog(applications, OperationLogModule.PROJECT_PROJECT_MANAGER, "UI配置");
     }
 
     /**
      * 性能测试 日志
      *
-     * @param application
+     * @param applications
      * @return
      */
-    public LogDTO updatePerformanceLog(ProjectApplication application) {
-        return delLog(application, OperationLogModule.PROJECT_PROJECT_MANAGER, "性能测试配置");
+    public List<LogDTO> updatePerformanceLog(List<ProjectApplication> applications) {
+        return delLog(applications, OperationLogModule.PROJECT_PROJECT_MANAGER, "性能测试配置");
     }
 
     /**
      * 接口测试 日志
      *
-     * @param application
+     * @param applications
      * @return
      */
-    public LogDTO updateApiLog(ProjectApplication application) {
-        return delLog(application, OperationLogModule.PROJECT_PROJECT_MANAGER, "接口测试配置");
+    public List<LogDTO> updateApiLog(List<ProjectApplication> applications) {
+        return delLog(applications, OperationLogModule.PROJECT_PROJECT_MANAGER, "接口测试配置");
     }
 
 
     /**
      * 用例管理 日志
      *
-     * @param application
+     * @param applications
      * @return
      */
-    public LogDTO updateCaseLog(ProjectApplication application) {
-        return delLog(application, OperationLogModule.PROJECT_PROJECT_MANAGER, "用例管理配置");
+    public List<LogDTO> updateCaseLog(List<ProjectApplication> applications) {
+        return delLog(applications, OperationLogModule.PROJECT_PROJECT_MANAGER, "用例管理配置");
     }
 
-    private LogDTO delLog(ProjectApplication application, String module, String content) {
-        ProjectApplicationExample example = new ProjectApplicationExample();
-        example.createCriteria().andTypeEqualTo(application.getType()).andProjectIdEqualTo(application.getProjectId());
-        List<ProjectApplication> list = projectApplicationMapper.selectByExample(example);
-        LogDTO dto = new LogDTO(
-                application.getProjectId(),
-                "",
-                OperationLogConstants.SYSTEM,
-                null,
-                OperationLogType.UPDATE.name(),
-                module,
-                content);
-        dto.setOriginalValue(JSON.toJSONBytes(list));
-        return dto;
+    /**
+     * 工作台 日志
+     *
+     * @param applications
+     * @return
+     */
+    public List<LogDTO> updateWorkstationLog(List<ProjectApplication> applications) {
+        return delLog(applications, OperationLogModule.PROJECT_PROJECT_MANAGER, "工作台配置");
+    }
+
+    private List<LogDTO> delLog(List<ProjectApplication> applications, String module, String content) {
+        List<LogDTO> logs = new ArrayList<>();
+        applications.forEach(application -> {
+            ProjectApplicationExample example = new ProjectApplicationExample();
+            example.createCriteria().andTypeEqualTo(application.getType()).andProjectIdEqualTo(application.getProjectId());
+            List<ProjectApplication> list = projectApplicationMapper.selectByExample(example);
+            LogDTO dto = new LogDTO(
+                    application.getProjectId(),
+                    "",
+                    OperationLogConstants.SYSTEM,
+                    null,
+                    OperationLogType.UPDATE.name(),
+                    module,
+                    content);
+            dto.setOriginalValue(JSON.toJSONBytes(list));
+            logs.add(dto);
+        });
+        return logs;
     }
 
 }
