b98a9c47ac3589f0e31046346558c7975f5d2293
RubyLiu <liulubingjava@163.com>
2023-09-25 11:46:02 +0800

    feat: useTable 导出清空选中方法

--- frontend/src/views/bug-management/index.vue
+++ frontend/src/views/bug-management/index.vue
@@ -2,6 +2,16 @@
   <MsCard simple>
     <h1>BugManagement is waiting for development </h1>
     <ms-pagination size="small" :total="100000" show-total show-jumper show-page-size />
+
+    <a-range-picker
+      style="margin: 0 24px 24px 0; width: 360px"
+      show-time
+      :time-picker-props="{ defaultValue: ['00:00:00', '09:09:06'] }"
+      format="YYYY-MM-DD HH:mm"
+      @change="onChange"
+      @select="onSelect"
+      @ok="onOk"
+    />
   </MsCard>
 </template>
 
