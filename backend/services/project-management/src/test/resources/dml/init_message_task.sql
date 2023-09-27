44a3233aaed5a1604d21e0d06e4a6f57a79a9f2d
guoyuqi <xiaomeinvG@126.com>
2023-09-27 11:05:26 +0800

    refactor(消息管理): 补充消息管理接口内容

--- backend/services/project-management/src/test/resources/dml/init_message_task.sql
+++ backend/services/project-management/src/test/resources/dml/init_message_task.sql
@@ -9,7 +9,7 @@ replace INTO project_robot(id, project_id, name, platform, webhook, type, app_ke
 
 
 replace INTO message_task(id, event, receiver, project_robot_id, task_type, test_id, project_id, enable, create_user, create_time, update_user, update_time, use_default_template, use_default_subject, subject)
-VALUES ('message_task_id1', 'CASE_EXECUTE_FAKE_ERROR', 'FOLLOW_PEOPLE', '测试机器人1', 'API_DEFINITION_TASK', 'NONE',  'test', true, 'admin', unix_timestamp() * 1000,'admin',  unix_timestamp() * 1000, true, true, 'message.title.api_definition_task_case_delete');
+VALUES ('message_task_id1', 'CASE_EXECUTE_FAKE_ERROR', 'FOLLOW_PEOPLE', 'message_task_robot1', 'API_DEFINITION_TASK', 'NONE',  'test', true, 'admin', unix_timestamp() * 1000,'admin',  unix_timestamp() * 1000, true, true, 'message.title.api_definition_task_case_delete');
 
 replace INTO message_task_blob(id, template) VALUES ('message_task_id1', 'message.api_definition_task_case_execute');
 
