93c1ea7131c8d678d98f178ee03764d30bd94a0e
song-cc-rock <changchang.song@fit2cloud.com>
2023-10-18 16:03:57 +0800

    feat(系统设置): 添加组织列表名称修改接口

---
+++ backend/services/system-setting/src/main/java/io/metersphere/system/request/OrganizationNameEditRequest.java
@@ -0,0 +1,27 @@
+package io.metersphere.system.request;
+
+import io.metersphere.validation.groups.Created;
+import io.metersphere.validation.groups.Updated;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+
+/**
+ * @author song-cc-rock
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class OrganizationNameEditRequest implements Serializable {
+
+    @Schema(description =  "组织ID", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String id;
+
+    @Schema(description =  "组织名称", requiredMode = Schema.RequiredMode.REQUIRED)
+    @NotBlank(message = "{organization.name.not_blank}", groups = {Created.class, Updated.class})
+    @Size(min = 1, max = 100, message = "{organization.name.length_range}", groups = {Created.class, Updated.class})
+    private String name;
+}
