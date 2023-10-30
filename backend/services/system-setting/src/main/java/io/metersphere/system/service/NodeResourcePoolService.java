a15e36ea01f9baa360bed71bf95864c0800827a8
CaptainB <bin@fit2cloud.com>
2023-10-30 18:20:44 +0800

    build: 发送到task-runner的请求都从task-runner-client中发出

--- backend/services/system-setting/src/main/java/io/metersphere/system/service/NodeResourcePoolService.java
+++ backend/services/system-setting/src/main/java/io/metersphere/system/service/NodeResourcePoolService.java
@@ -1,45 +1,27 @@
 package io.metersphere.system.service;
 
 
-import com.bastiaanjansen.otp.TOTPGenerator;
-import io.metersphere.sdk.constants.MsHttpHeaders;
-import io.metersphere.system.controller.handler.ResultHolder;
-import io.metersphere.system.dto.pool.TestResourceDTO;
-import io.metersphere.system.dto.pool.TestResourceNodeDTO;
 import io.metersphere.sdk.exception.MSException;
 import io.metersphere.sdk.util.LogUtils;
 import io.metersphere.sdk.util.Translator;
-import jakarta.annotation.Resource;
+import io.metersphere.system.controller.handler.ResultHolder;
+import io.metersphere.system.dto.pool.TestResourceDTO;
+import io.metersphere.system.dto.pool.TestResourceNodeDTO;
+import io.metersphere.system.utils.TaskRunnerClient;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class NodeResourcePoolService {
-    @Resource
-    private TOTPGenerator totpGenerator;
 
     private final static String nodeControllerUrl = "http://%s:%s/status";
 
-    private static final RestTemplate restTemplateWithTimeOut = new RestTemplate();
-
-    static {
-        HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
-        httpRequestFactory.setConnectionRequestTimeout(2000);
-        httpRequestFactory.setConnectTimeout(2000);
-        restTemplateWithTimeOut.setRequestFactory(httpRequestFactory);
-    }
 
     public boolean validate(TestResourceDTO testResourceDTO, Boolean usedApiType) {
         List<TestResourceNodeDTO> nodesList = testResourceDTO.getNodesList();
@@ -82,12 +64,7 @@ public class NodeResourcePoolService {
 
     private boolean validateNode(TestResourceNodeDTO node) {
         try {
-            String token = totpGenerator.now();
-            HttpHeaders headers = new HttpHeaders();
-            headers.add(MsHttpHeaders.OTP_TOKEN, token);
-            HttpEntity<String> httpEntity = new HttpEntity<>(headers);
-            ResponseEntity<ResultHolder> entity = restTemplateWithTimeOut.exchange(String.format(nodeControllerUrl, node.getIp(), node.getPort()), HttpMethod.GET, httpEntity, ResultHolder.class);
-            ResultHolder body = entity.getBody();
+            ResultHolder body = TaskRunnerClient.get(String.format(nodeControllerUrl, node.getIp(), node.getPort()));
             if (body == null) {
                 return false;
             }
