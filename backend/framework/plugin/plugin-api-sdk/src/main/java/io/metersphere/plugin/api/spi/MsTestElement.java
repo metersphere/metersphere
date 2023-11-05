0f6501aa337b5f812f3fe752b700cb3c10926d96
AgAngle <1323481023@qq.com>
2023-11-05 12:24:15 +0800

    chore: 定义接口组件和转换器接口

---
+++ backend/framework/plugin/plugin-api-sdk/src/main/java/io/metersphere/plugin/api/spi/MsTestElement.java
@@ -0,0 +1,17 @@
+package io.metersphere.plugin.api.spi;
+
+import org.pf4j.ExtensionPoint;
+
+/**
+ *  MS 中的测试组件
+ *  即场景中的每个步骤组件
+ *  或者接口定义对应的协议组件
+ */
+public interface MsTestElement extends ExtensionPoint {
+
+}
+
+
+
+
+
---
+++ backend/services/api-test/src/main/java/io/metersphere/api/dto/request/ParameterConfig.java
@@ -0,0 +1,10 @@
+package io.metersphere.api.dto.request;
+
+import io.metersphere.plugin.api.spi.MsParameter;
+
+/**
+ * @Author: jianxing
+ * @CreateTime: 2023-10-27  17:30
+ */
+public class ParameterConfig extends MsParameter {
+}
