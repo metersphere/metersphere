349764359bcf6b3d229d9954b616fc78f993aa63
fit2-zhao <yong.zhao@fit2cloud.com>
2023-11-10 15:24:22 +0800

    refactor: API插件解析器微调

--- backend/framework/plugin/plugin-api-sdk/src/main/java/io/metersphere/plugin/api/spi/AbstractJmeterElementConverter.java
+++ backend/framework/plugin/plugin-api-sdk/src/main/java/io/metersphere/plugin/api/spi/AbstractJmeterElementConverter.java
@@ -15,58 +15,44 @@ import java.util.Map;
  */
 public abstract class AbstractJmeterElementConverter<T extends MsTestElement> {
 
-    private Class<? extends MsTestElement> msTestElementClass;
     /**
      * 解析器集合
      * key 为 MsTestElement 实现类的 Class
      * value 为对应的转换器
      */
-    private static final Map<Class<? extends MsTestElement>, AbstractJmeterElementConverter> parserMap = new HashMap();
+    private static final Map<MsTestElement, AbstractJmeterElementConverter<?>> parserMap = new HashMap<>();
 
     public AbstractJmeterElementConverter() {
         Type genericSuperclass = getClass().getGenericSuperclass();
-        if (genericSuperclass instanceof ParameterizedType) {
-            ParameterizedType parameterizedType = (ParameterizedType) genericSuperclass;
+        if (genericSuperclass instanceof ParameterizedType parameterizedType) {
             // 获取泛型的具体类型，即 MsTestElement 的具体实现类
-            msTestElementClass = (Class<? extends MsTestElement>) parameterizedType.getActualTypeArguments()[0];
+            MsTestElement baseClass = (MsTestElement) parameterizedType.getActualTypeArguments()[0];
             // 注册到解析器集合中
-            parserMap.put(msTestElementClass, this);
+            parserMap.put(baseClass, this);
         }
     }
 
 
     /**
      * 将 MsTestElement 具体实现类转换为 HashTree
-     *
-     * @param tree
-     * @param msTestElement
-     * @param config
      */
-    public abstract void toHashTree(HashTree tree, T msTestElement, MsParameter config);
+    public abstract void toHashTree(HashTree tree, T element, MsParameter config);
 
     /**
      * 解析 MsTestElement 的子节点
-     *
-     * @param tree
-     * @param msTestElement
-     * @param config
      */
-    public void parseChild(HashTree tree, AbstractMsTestElement msTestElement, MsParameter config) {
-        if (msTestElement != null && msTestElement.getChildren() != null) {
-            for (MsTestElement testElement : msTestElement.getChildren()) {
-                AbstractJmeterElementConverter converter = getConverter(testElement);
-                converter.toHashTree(tree, msTestElement, config);
-            }
+    public void parseChild(HashTree tree, AbstractMsTestElement element, MsParameter config) {
+        if (element != null && element.getChildren() != null) {
+            element.getChildren().forEach(child -> {
+                getConverter(child).toHashTree(tree, element, config);
+            });
         }
     }
 
     /**
      * 获取对应组件的转换器
-     *
-     * @param msTestElement
-     * @return
      */
-    public static AbstractJmeterElementConverter getConverter(MsTestElement msTestElement) {
-        return parserMap.get(msTestElement.getClass());
+    public static AbstractJmeterElementConverter getConverter(MsTestElement element) {
+        return parserMap.get(element);
     }
 }
