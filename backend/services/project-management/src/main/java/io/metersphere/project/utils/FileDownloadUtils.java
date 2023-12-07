0ff439f776ed066ba69c141c2e310a84283dfdc3
song-tianyang <tianyang.song@fit2cloud.com>
2023-12-07 17:26:30 +0800

    refactor(项目管理): 优化文件管理批量下载

--- backend/services/project-management/src/main/java/io/metersphere/project/utils/FileDownloadUtils.java
+++ backend/services/project-management/src/main/java/io/metersphere/project/utils/FileDownloadUtils.java
@@ -1,12 +1,46 @@
 package io.metersphere.project.utils;
 
+import io.metersphere.sdk.util.LogUtils;
+
+import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class FileDownloadUtils {
-    public static byte[] listBytesToZip(Map<String, byte[]> mapReport) {
+
+    public static byte[] listBytesToZip(Map<String, File> fileMap) {
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+             ZipOutputStream zipOutputStream = new ZipOutputStream(byteArrayOutputStream)) {
+            for (Map.Entry<String, File> fileEntry : fileMap.entrySet()) {
+                String fileName = fileEntry.getKey();
+                File file = fileEntry.getValue();
+                if (file.exists()) {
+                    try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file))) {
+                        ZipEntry zipEntry = new ZipEntry(fileName);
+                        zipOutputStream.putNextEntry(zipEntry);
+                        byte[] buffer = new byte[1024];
+                        int num;
+                        while ((num = bis.read(buffer)) > 0) {
+                            zipOutputStream.write(buffer, 0, num);
+                        }
+                        zipOutputStream.closeEntry();
+                    } catch (Exception ignore) {
+                    }
+                }
+            }
+            zipOutputStream.close();
+            return byteArrayOutputStream.toByteArray();
+        } catch (Exception e) {
+            LogUtils.error(e);
+            return new byte[0];
+        }
+    }
+
+    public static byte[] listFileBytesToZip(Map<String, byte[]> mapReport) {
         try {
             if (!mapReport.isEmpty()) {
                 ByteArrayOutputStream baos = new ByteArrayOutputStream();
