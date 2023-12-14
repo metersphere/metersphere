6270c45d0a9cfa64863c730059cfaa93e3a5793f
CaptainB <bin@fit2cloud.com>
2023-12-14 16:39:35 +0800

    refactor: 检查资源和组织关系

--- backend/services/system-setting/src/main/java/io/metersphere/system/mapper/ExtCheckOwnerMapper.java
+++ backend/services/system-setting/src/main/java/io/metersphere/system/mapper/ExtCheckOwnerMapper.java
@@ -6,4 +6,6 @@ import java.util.List;
 
 public interface ExtCheckOwnerMapper {
     boolean checkoutOwner(@Param("table") String resourceType, @Param("projectId") String projectId, @Param("ids") List<String> ids);
+
+    boolean checkoutOrganizationOwner(@Param("table") String resourceType, @Param("organizationId") String organizationId, @Param("ids") List<String> ids);
 }
