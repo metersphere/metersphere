65e9c45897d1ecd2503bfcb9de5f1b33bcb5d26c
CaptainB <bin@fit2cloud.com>
2023-12-15 10:04:41 +0800

    ci: 配置 virtual thread

--- backend/services/system-setting/src/main/java/io/metersphere/system/config/AsyncConfig.java
+++ backend/services/system-setting/src/main/java/io/metersphere/system/config/AsyncConfig.java
@@ -1,21 +1,10 @@
 package io.metersphere.system.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import java.util.concurrent.ThreadPoolExecutor;
 
 @EnableAsync(proxyTargetClass = true)
+@Configuration
 public class AsyncConfig {
 
-    @Bean
-    public AsyncTaskExecutor asyncTaskExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setThreadNamePrefix("Async-Executor-");
-        executor.setCorePoolSize(5);
-        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-        return executor;
-    }
 }
