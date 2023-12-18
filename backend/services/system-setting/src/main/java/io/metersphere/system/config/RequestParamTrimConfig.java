864f9a2d522fcdca981a06298c8d1cbb5e3678ea
CaptainB <bin@fit2cloud.com>
2023-12-18 13:41:47 +0800

    ci: 请求参数统一去除前后空格

---
+++ backend/services/system-setting/src/main/java/io/metersphere/system/config/RequestParamTrimConfig.java
@@ -0,0 +1,31 @@
+package io.metersphere.system.config;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdScalarDeserializer;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+@Configuration
+public class RequestParamTrimConfig {
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return jacksonObjectMapperBuilder -> {
+            // 为 String 类型自定义反序列化操作
+            jacksonObjectMapperBuilder
+                    .deserializerByType(String.class, new StdScalarDeserializer<String>(String.class) {
+                        @Override
+                        public String deserialize(JsonParser jsonParser, DeserializationContext ctx)
+                                throws IOException {
+                            // 去除前后空格
+                            return StringUtils.trim(jsonParser.getValueAsString());
+                        }
+                    });
+        };
+    }
+}
