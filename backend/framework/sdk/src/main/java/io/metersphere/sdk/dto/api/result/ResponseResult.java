6fff1c5f2046200f65d7651f94c3e2128ec6929e
fit2-zhao <yong.zhao@fit2cloud.com>
2023-12-18 15:52:13 +0800

    feat(接口测试): 响应内容详情扩展

--- backend/framework/sdk/src/main/java/io/metersphere/sdk/dto/api/result/ResponseResult.java
+++ backend/framework/sdk/src/main/java/io/metersphere/sdk/dto/api/result/ResponseResult.java
@@ -15,10 +15,12 @@ public class ResponseResult {
 
     private String responseMessage;
 
+    // 总响应时间
     private long responseTime;
 
     private long latency;
 
+    // 总响应内容大小
     private long responseSize;
 
     private String headers;
@@ -32,6 +34,30 @@ public class ResponseResult {
     private String contentType;
 
     private byte[] imageUrl;
+
+    // Socket Initialization（Socket 初始化时间）
+    private long socketInitTime = 0;
+
+    // DNS Lookup（DNS 查询时间）
+    private long dnsLookupTime = 0;
+
+    // TCP Handshake（TCP 握手时间）
+    private long tcpHandshakeTime = 0;
+
+    //SSL Handshake（SSL 握手时间）
+    private long sslHandshakeTime = 0;
+
+    //Transfer Start（传输开始时间）
+    private long transferStartTime = 0;
+
+    //Download（下载时间）
+    private long downloadTime = 0;
+
+    // Body size
+    private long bodySize = 0;
+
+    // header size
+    private long headerSize = 0;
 
     private final List<ResponseAssertionResult> assertions = new ArrayList<>();
 
