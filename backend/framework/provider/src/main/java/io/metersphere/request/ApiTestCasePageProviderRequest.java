afc2921eb4a06d16db7e0b8432e8e8c26be23600
CaptainB <bin@fit2cloud.com>
2023-12-27 14:26:12 +0800

    ci: provider pom changed

--- backend/framework/provider/src/main/java/io/metersphere/request/ApiTestCasePageProviderRequest.java
+++ backend/framework/provider/src/main/java/io/metersphere/request/ApiTestCasePageProviderRequest.java
@@ -1,23 +1,25 @@
 package io.metersphere.request;
 
+import com.google.common.base.CaseFormat;
 import io.metersphere.sdk.constants.ModuleConstants;
-import io.metersphere.system.dto.sdk.BasePageRequest;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author guoyuqi
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
-public class ApiTestCasePageProviderRequest extends BasePageRequest implements Serializable {
+public class ApiTestCasePageProviderRequest implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
@@ -49,4 +51,43 @@ public class ApiTestCasePageProviderRequest extends BasePageRequest implements S
     @Schema(description = "版本来源")
     private String refId;
 
+    @Min(value = 1, message = "当前页码必须大于0")
+    @Schema(description =  "当前页码")
+    private int current;
+
+    @Min(value = 5, message = "每页显示条数必须不小于5")
+    @Max(value = 500, message = "每页显示条数不能大于500")
+    @Schema(description =  "每页显示条数")
+    private int pageSize;
+
+    @Schema(description =  "排序字段（model中的字段 : asc/desc）")
+    private Map<@Valid @Pattern(regexp = "^[A-Za-z]+$") String, @Valid @NotBlank String> sort;
+
+
+    public String getSortString() {
+        if (sort == null || sort.isEmpty()) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, String> entry : sort.entrySet()) {
+            String column = CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, entry.getKey());
+            sb.append(column)
+                    .append(StringUtils.SPACE)
+                    .append(StringUtils.equalsIgnoreCase(entry.getValue(), "DESC") ? "DESC" : "ASC")
+                    .append(",");
+        }
+        return sb.substring(0, sb.length() - 1);
+    }
+
+    @Schema(description =  "关键字")
+    private String keyword;
+
+    @Schema(description = "匹配模式 所有/任一", allowableValues = {"AND", "OR"})
+    private String searchMode = "AND";
+
+    @Schema(description =  "过滤字段")
+    private Map<String, List<String>> filter;
+
+    @Schema(description =  "高级搜索")
+    private Map<String, Object> combine;
 }
