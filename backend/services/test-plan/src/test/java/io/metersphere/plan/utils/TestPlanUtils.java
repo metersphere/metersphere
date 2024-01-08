a700c00a4aa262d54c214b4d286bd95cce28682a
song-tianyang <tianyang.song@fit2cloud.com>
2024-01-08 19:12:59 +0800

    feat(测试计划): 测试计划模块树开发

---
+++ backend/services/test-plan/src/test/java/io/metersphere/plan/utils/TestPlanUtils.java
@@ -0,0 +1,32 @@
+package io.metersphere.plan.utils;
+
+import io.metersphere.system.dto.sdk.BaseTreeNode;
+import org.apache.commons.collections4.CollectionUtils;
+import org.testcontainers.shaded.org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+
+public class TestPlanUtils {
+    public static BaseTreeNode getNodeByName(List<BaseTreeNode> preliminaryTreeNodes, String nodeName) {
+        for (BaseTreeNode firstLevelNode : preliminaryTreeNodes) {
+            if (StringUtils.equals(firstLevelNode.getName(), nodeName)) {
+                return firstLevelNode;
+            }
+            if (CollectionUtils.isNotEmpty(firstLevelNode.getChildren())) {
+                for (BaseTreeNode secondLevelNode : firstLevelNode.getChildren()) {
+                    if (StringUtils.equals(secondLevelNode.getName(), nodeName)) {
+                        return secondLevelNode;
+                    }
+                    if (CollectionUtils.isNotEmpty(secondLevelNode.getChildren())) {
+                        for (BaseTreeNode thirdLevelNode : secondLevelNode.getChildren()) {
+                            if (StringUtils.equals(thirdLevelNode.getName(), nodeName)) {
+                                return thirdLevelNode;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
+}
