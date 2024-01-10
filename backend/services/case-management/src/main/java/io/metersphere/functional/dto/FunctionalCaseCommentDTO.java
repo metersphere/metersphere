edc4bcc010dee0e6b635dee136d2f16602106b19
guoyuqi <xiaomeinvG@126.com>
2024-01-10 16:54:26 +0800

    refactor(功能用例): 优化功能用例返回的评论结构

--- backend/services/case-management/src/main/java/io/metersphere/functional/dto/FunctionalCaseCommentDTO.java
+++ backend/services/case-management/src/main/java/io/metersphere/functional/dto/FunctionalCaseCommentDTO.java
@@ -1,6 +1,7 @@
 package io.metersphere.functional.dto;
 
 import io.metersphere.functional.domain.FunctionalCaseComment;
+import io.metersphere.system.dto.CommentUserInfo;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -9,17 +10,8 @@ import java.util.List;
 @Data
 public class FunctionalCaseCommentDTO extends FunctionalCaseComment {
 
-    @Schema(description =  "评论的人名")
-    private String userName;
-
-    @Schema(description =  "被回复的人名")
-    private String replyUserName;
-
-    @Schema(description =  "被回复的人头像")
-    private String replyUserLogo;
-
-    @Schema(description =  "评论的人头像")
-    private String userLogo;
+    @Schema(description = "评论相关用户信息")
+    private List<CommentUserInfo> commentUserInfos;
 
     @Schema(description =  "该条评论下的所有回复数据")
     private List<FunctionalCaseCommentDTO> childComments;
