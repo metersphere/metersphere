d14c30cdb2a8e21313dca35c5e0376947f501517
WangXu10 <xu.wang@fit2cloud.com>
2024-01-10 16:02:49 +0800

    refactor(功能用例): 功能用例关联缺陷列表返回字段新增属性

--- backend/framework/provider/src/main/java/io/metersphere/dto/BugProviderDTO.java
+++ backend/framework/provider/src/main/java/io/metersphere/dto/BugProviderDTO.java
@@ -15,8 +15,11 @@ public class BugProviderDTO implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    @Schema(description = "缺陷id")
+    @Schema(description = "id")
     private String id;
+
+    @Schema(description = "bugId")
+    private String bugId;
 
     @Schema(description = "缺陷名称")
     private String name;
@@ -35,4 +38,13 @@ public class BugProviderDTO implements Serializable {
 
     @Schema(description = "创建时间")
     private Long createTime;
+
+    @Schema(description = "计划名称")
+    private String testPlanName;
+
+    @Schema(description = "计划id")
+    private String testPlanId;
+
+    @Schema(description = "来源")
+    private String source;
 }
