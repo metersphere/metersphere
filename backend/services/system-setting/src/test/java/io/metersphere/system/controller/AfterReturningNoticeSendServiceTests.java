2b413415ac143e4b034892b48e3e2dbbd4bb0499
guoyuqi <xiaomeinvG@126.com>
2024-01-16 14:50:40 +0800

    feat(消息通知): 测试钉钉发送消息

--- backend/services/system-setting/src/test/java/io/metersphere/system/controller/AfterReturningNoticeSendServiceTests.java
+++ backend/services/system-setting/src/test/java/io/metersphere/system/controller/AfterReturningNoticeSendServiceTests.java
@@ -14,17 +14,24 @@ import io.metersphere.functional.mapper.FunctionalCaseMapper;
 import io.metersphere.load.domain.LoadTest;
 import io.metersphere.plan.domain.TestPlan;
 import io.metersphere.plan.mapper.TestPlanMapper;
+import io.metersphere.project.domain.Project;
+import io.metersphere.project.mapper.ProjectMapper;
 import io.metersphere.sdk.util.BeanUtils;
 import io.metersphere.sdk.util.JSON;
 import io.metersphere.system.base.BaseTest;
 import io.metersphere.system.controller.dto.FunctionalCaseDTO;
 import io.metersphere.system.domain.CustomField;
+import io.metersphere.system.dto.sdk.BaseSystemConfigDTO;
 import io.metersphere.system.dto.sdk.OptionDTO;
 import io.metersphere.system.dto.sdk.SessionUser;
 import io.metersphere.system.dto.user.UserDTO;
 import io.metersphere.system.mapper.CustomFieldMapper;
+import io.metersphere.system.notice.NoticeModel;
 import io.metersphere.system.notice.constants.NoticeConstants;
 import io.metersphere.system.notice.sender.AfterReturningNoticeSendService;
+import io.metersphere.system.notice.utils.MessageTemplateUtils;
+import io.metersphere.system.service.NoticeSendService;
+import io.metersphere.system.service.SystemParameterService;
 import jakarta.annotation.Resource;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.MethodOrderer;
@@ -36,9 +43,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -63,7 +68,12 @@ public class AfterReturningNoticeSendServiceTests extends BaseTest {
     private TestPlanMapper testPlanMapper;
     @Resource
     private CaseReviewMapper caseReviewMapper;
-
+    @Resource
+    private NoticeSendService noticeSendService;
+    @Resource
+    private ProjectMapper projectMapper;
+    @Resource
+    private SystemParameterService systemParameterService;
 
     private ThreadLocal<String> source = new ThreadLocal<>();
 
@@ -119,7 +129,69 @@ public class AfterReturningNoticeSendServiceTests extends BaseTest {
         for (String event : eventList) {
             afterReturningNoticeSendService.sendNotice(functionalCaseTask, event,resources, user, "100001100001");
         }
+        source = new ThreadLocal<>();
+        FunctionalCase functionalCaseTwo = functionalCaseMapper.selectByPrimaryKey("aspect_gyq_three");
+        functionalCaseDTO = new FunctionalCaseDTO();
+        BeanUtils.copyBean(functionalCaseDTO,functionalCaseTwo);
+        jsonObject = JSON.toJSONString(functionalCaseDTO);
+        if (!StringUtils.equals("{}", jsonObject) && !StringUtils.equals("[]", jsonObject)) {
+            source.set(JSON.toJSONString(functionalCaseDTO));
+        }
 
+        resources = new ArrayList<>();
+        v = source.get();
+        if (StringUtils.isNotBlank(v)) {
+            // array
+            if (StringUtils.startsWith(v, "[")) {
+                resources.addAll(JSON.parseArray(v, Map.class));
+            }
+            // map
+            else {
+                Map<?, ?> value = JSON.parseObject(v, Map.class);
+                resources.add(value);
+            }
+        }
+        BaseSystemConfigDTO baseSystemConfigDTO = systemParameterService.getBaseInfo();
+        for (Map resource : resources) {
+            Map paramMap = new HashMap<>();
+            paramMap.put("url", baseSystemConfigDTO.getUrl());
+            paramMap.put(NoticeConstants.RelatedUser.OPERATOR, user.getName());
+            paramMap.putAll(resource);
+            paramMap.putIfAbsent("projectId", "aspect_gyq_project_one");
+            //TODO: 加来源处理
+
+            // 占位符
+            paramMap.put("planShareUrl", StringUtils.EMPTY);
+
+            Project project = projectMapper.selectByPrimaryKey("aspect_gyq_project_one");
+            Map<String, String> defaultTemplateMap = MessageTemplateUtils.getDefaultTemplateMap();
+            String context = defaultTemplateMap.get(functionalCaseTask + "_UPDATE");
+            Map<String, String> defaultTemplateTitleMap = MessageTemplateUtils.getDefaultTemplateSubjectMap();
+            String subject = defaultTemplateTitleMap.get(functionalCaseTask + "_UPDATE");
+
+            List<String> relatedUsers = getRelatedUsers(resource.get("relatedUsers"));
+
+            NoticeModel noticeModel = NoticeModel.builder()
+                    .operator(user.getId())
+                    .context(context)
+                    .subject(subject)
+                    .paramMap(paramMap)
+                    .event("UPDATE")
+                    .status((String) paramMap.get("status"))
+                    .excludeSelf(true)
+                    .relatedUsers(relatedUsers)
+                    .build();
+            noticeSendService.send(project,functionalCaseTask,noticeModel);
+        }
+    }
+
+    private List<String> getRelatedUsers(Object relatedUsers) {
+        String relatedUser = (String) relatedUsers;
+        List<String> relatedUserList = new ArrayList<>();
+        if (StringUtils.isNotBlank(relatedUser)) {
+            relatedUserList = Arrays.asList(relatedUser.split(";"));
+        }
+        return relatedUserList;
     }
 
     @Test
--- backend/services/system-setting/src/test/resources/dml/init_aspect.sql
+++ backend/services/system-setting/src/test/resources/dml/init_aspect.sql
@@ -40,14 +40,35 @@ VALUES ('aspect_gyq_api_one', 'api_test','HTTP', 'POST','api/test','test-api-sta
 INSERT INTO api_scenario(id, name, priority, status, last_report_status, last_report_id, num, pos, version_id, ref_id, project_id, module_id, description, tags, create_user, create_time, delete_time, delete_user, update_user, update_time)
 VALUES ('aspect_gyq_api_scenario_one', 'api_scenario', 'p1', 'test-api-status',  null, null,1000001, 1,'v1.10', 'aspect_gyq_api_scenario_one','100001100001', 'test_module', null,null,'admin',  UNIX_TIMESTAMP() * 1000,null,null,'admin', UNIX_TIMESTAMP() * 1000);
 
-INSERT INTO test_plan(id, project_id, group_id, module_id, name, status, type, tags, create_time, create_user,
-                      update_time, update_user, planned_start_time, planned_end_time, actual_start_time,
-                      actual_end_time, description)
-VALUES ('aspect_gyq_test_plan_one', '100001100001', 'none', 'root', 'test_plan', 'PREPARED', 'TEST_PLAN', null,
-        UNIX_TIMESTAMP() * 1000, 'admin', UNIX_TIMESTAMP() * 1000, 'admin', UNIX_TIMESTAMP() * 2000,
+INSERT INTO test_plan(id, num, project_id, group_id, module_id, name, status, type, tags, create_time, create_user, update_time, update_user, planned_start_time, planned_end_time, actual_start_time, actual_end_time, description)
+VALUES ('aspect_gyq_test_plan_one', 5000,'100001100001','NONE','test_plan_module_id','test_plan', 'PREPARED','a', null, UNIX_TIMESTAMP() * 1000, 'admin', UNIX_TIMESTAMP() * 1000,'admin',  UNIX_TIMESTAMP() * 2000,
         UNIX_TIMESTAMP() * 3000, UNIX_TIMESTAMP() * 2000, UNIX_TIMESTAMP() * 3000, null);
+
 
 INSERT INTO case_review(id, num, name, module_id, project_id, status, review_pass_rule, pos, start_time, end_time, tags, description, create_time, create_user, update_time, update_user)
 VALUES ('aspect_gyq_case_review_one','10001','case_review','module_id', '100001100001','PREPARED','SINGLE','0', null, null, null, null, UNIX_TIMESTAMP() * 1000, 'admin', UNIX_TIMESTAMP() * 1000, 'admin');
+
+INSERT INTO project (id, num, organization_id, name, description, create_user, update_user, create_time, update_time) VALUE
+    ('aspect_gyq_project_one', 100, '100001', '测试项目(消息)', '系统默认创建的项目(缺陷)', 'admin', 'admin', UNIX_TIMESTAMP() * 1000, UNIX_TIMESTAMP() * 1000);
+
+INSERT INTO functional_case(id, num, module_id, project_id, template_id, name, review_status, tags, case_edit_type, pos,
+                            version_id, ref_id, last_execute_result, deleted, public_case, latest, create_user,
+                            update_user, delete_user, create_time, update_time, delete_time)
+VALUES ('aspect_gyq_three', 1000001, 'test_guo', 'aspect_gyq_project_one', 'test_guo', 'gyq_test_two', 'UN_REVIEWED', null, 'text',
+        10001, '111', 'aspect_gyq_two', 'success', false, false, true, 'gyq', 'gyq', null, 1698058347559, 1698058347559,
+        null);
+
+Insert into project_robot(id, project_id, name, platform, webhook, type, app_key, app_secret, enable, create_user, create_time, update_user, update_time, description)
+VALUES ('robot_ding_custom_id', 'aspect_gyq_project_one', 'robot_in_ding_custom', 'DING_CUSTOM_ROBOT', 'https://oapi.dingtalk.com/robot/send?access_token=fd963136a4d7eebaaa68de261223089148e62d7519fbaf426626fe3157725b8a', 'CUSTOM', null, null, true, 'admin', unix_timestamp() * 1000,'admin',  unix_timestamp() * 1000, 'robot_in_site_description');
+Insert into project_robot(id, project_id, name, platform, webhook, type, app_key, app_secret, enable, create_user, create_time, update_user, update_time, description)
+VALUES ('robot_ding_enterprise_id', 'aspect_gyq_project_one', 'robot_in_ding_enterprise', 'DING_ENTERPRISE_ROBOT', 'https://oapi.dingtalk.com/robot/send?access_token=5243ba0bdee4a20f920c169fcad352e3f5c00c62e34c4352194729b6e571c936', 'ENTERPRISE', 'dingxwd71o7kj4qoixo7', 'szmOD9bjGgKtfYk09-Xx2rPdX-xkW4R8Iic0eig_k1D3k95nG4TLKRSpUKUD_f0G', true, 'admin', unix_timestamp() * 1000,'admin',  unix_timestamp() * 1000, 'robot_in_site_description');
+
+
+Insert into message_task(id, event, receiver, project_robot_id, task_type, test_id, project_id, enable, create_user, create_time, update_user, update_time, use_default_template, use_default_subject, subject) VALUES ('message_robot_ding_id', 'UPDATE', 'CREATE_USER', 'robot_ding_custom_id', 'FUNCTIONAL_CASE_TASK', 'aspect_gyq_two', 'aspect_gyq_project_one', true, 'admin', unix_timestamp() * 1000, 'admin',  unix_timestamp() * 1000, true, true, 'message.title.functional_case_task_update');
+INSERT INTO message_task_blob(id, template) VALUES ('message_robot_ding_id', 'message.functional_case_task_update');
+
+Insert into message_task(id, event, receiver, project_robot_id, task_type, test_id, project_id, enable, create_user, create_time, update_user, update_time, use_default_template, use_default_subject, subject) VALUES ('message_robot_ding_en_id', 'UPDATE', 'CREATE_USER', 'robot_ding_enterprise_id', 'FUNCTIONAL_CASE_TASK', 'aspect_gyq_two', 'aspect_gyq_project_one', true, 'admin', unix_timestamp() * 1000, 'admin',  unix_timestamp() * 1000, true, true, 'message.title.functional_case_task_update');
+INSERT INTO message_task_blob(id, template) VALUES ('message_robot_ding_en_id', 'message.functional_case_task_update');
+
 
 
