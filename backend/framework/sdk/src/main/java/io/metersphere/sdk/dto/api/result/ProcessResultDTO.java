71e466810cb0d7e7956874f1be509de781c498c9
fit2-zhao <yong.zhao@fit2cloud.com>
2024-01-19 14:45:20 +0800

    refactor(接口测试): 调整报告指标计算

--- backend/framework/sdk/src/main/java/io/metersphere/sdk/dto/api/result/ProcessResultDTO.java
+++ backend/framework/sdk/src/main/java/io/metersphere/sdk/dto/api/result/ProcessResultDTO.java
@@ -19,16 +19,15 @@ public class ProcessResultDTO {
 
     // TODO 根据初始步骤大小，在执行过程中进行标记； 初始大小 - 过程标记步骤
     private long pendingCount;
+
     // 断言总数
     private long assertionCount;
+
     // 成功断言数总数
     private long successAssertionCount;
-    // 请求执行率
-    private String requestExecutionRate;
-    // 请求通过率
-    private String requestPassRate;
-    // 断言通过率
-    private String assertionPassRate;
+
+    // 请求总时长
+    private long requestDuration;
 
     public void computerTotal() {
         this.total = this.pendingCount + this.errorCount + this.successCount;
@@ -54,35 +53,8 @@ public class ProcessResultDTO {
         this.successAssertionCount += successAssertion;
     }
 
-    /**
-     * 整体执行完成后调用计算执行率
-     * (成功+失败 ) /总量* 100
-     */
-    public void computerRequestExecutionRate() {
-        long count = this.successCount + this.errorCount;
-        if (this.total > 0 && count > 0) {
-            double executionRate = (double) count / this.total * 100;
-            this.requestExecutionRate = String.format("%.2f", executionRate);
-        }
-    }
-
-    /**
-     * 整体完成后调用计算通过率
-     * 成功总数 / 总量 * 100
-     */
-    public void computerRequestPassRate() {
-        if (this.total > 0 && this.successCount > 0) {
-            this.requestPassRate = String.format("%.2f", (double) this.successCount / this.total * 100);
-        }
-    }
-
-    /**
-     * 整体完成后调用计算断言通过率
-     */
-    public void computerAssertionPassRate() {
-        if (this.assertionCount > 0 && successAssertionCount > 0) {
-            this.assertionPassRate = String.format("%.2f", (double) successAssertionCount / this.assertionCount * 100);
-        }
+    public void computerRequestDuration(long requestDuration) {
+        this.requestDuration += requestDuration;
     }
 
 }
