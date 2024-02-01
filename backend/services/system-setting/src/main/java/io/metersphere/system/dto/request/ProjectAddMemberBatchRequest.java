cf40902d9972ebc3f1ed50fe2c740628c6824fd8
wxg0103 <727495428@qq.com>
2024-02-01 20:23:57 +0800

    fix(系统设置): 修复项目添加成员报错的缺陷

--- backend/services/system-setting/src/main/java/io/metersphere/system/dto/request/ProjectAddMemberBatchRequest.java
+++ backend/services/system-setting/src/main/java/io/metersphere/system/dto/request/ProjectAddMemberBatchRequest.java
@@ -3,16 +3,14 @@ package io.metersphere.system.dto.request;
 import io.metersphere.validation.groups.Created;
 import io.metersphere.validation.groups.Updated;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
-public class ProjectAddMemberBatchRequest extends ProjectAddMemberRequest{
-    @Schema(description =  "项目ID集合", requiredMode = Schema.RequiredMode.REQUIRED)
-    @Valid
+public class ProjectAddMemberBatchRequest extends ProjectAddMemberRequest {
+    @Schema(description = "项目ID集合", requiredMode = Schema.RequiredMode.REQUIRED)
     private List<
             @NotBlank(message = "{project.id.not_blank}", groups = {Created.class, Updated.class})
                     String> projectIds;
