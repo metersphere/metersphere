6a82fdf2ec60c34cae00e855df98e663b83eea53
guoyuqi <xiaomeinvG@126.com>
2024-02-05 15:13:13 +0800

    fix(项目管理): 修复发送通知内容不符问题

--- backend/services/system-setting/src/main/java/io/metersphere/system/notice/sender/impl/WeComNoticeSender.java
+++ backend/services/system-setting/src/main/java/io/metersphere/system/notice/sender/impl/WeComNoticeSender.java
@@ -1,13 +1,12 @@
 package io.metersphere.system.notice.sender.impl;
 
+import io.metersphere.sdk.util.LogUtils;
+import io.metersphere.system.domain.User;
 import io.metersphere.system.notice.MessageDetail;
 import io.metersphere.system.notice.NoticeModel;
 import io.metersphere.system.notice.Receiver;
 import io.metersphere.system.notice.sender.AbstractNoticeSender;
 import io.metersphere.system.notice.utils.WeComClient;
-import io.metersphere.sdk.util.LogUtils;
-import io.metersphere.system.domain.User;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -24,8 +23,7 @@ public class WeComNoticeSender extends AbstractNoticeSender {
         List<User> users = super.getUsers(userIds);
         List<String> mobileList = users.stream().map(User::getPhone).toList();
         LogUtils.info("企业微信收件人: {}", userIds);
-        context += StringUtils.join(mobileList, StringUtils.SPACE);
-        WeComClient.send(messageDetail.getWebhook(), "消息通知: \n" + context, mobileList);
+        WeComClient.send(messageDetail.getWebhook(), messageDetail.getSubject()+": \n" + context, mobileList);
     }
 
     @Override
