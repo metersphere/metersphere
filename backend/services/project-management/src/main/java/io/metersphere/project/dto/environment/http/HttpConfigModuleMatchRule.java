174d2f00d22d928b3af309772825e5fb8e0ac5a0
AgAngle <1323481023@qq.com>
2024-02-05 20:24:05 +0800

    refactor(项目设置): 优化环境http配置参数

---
+++ backend/services/project-management/src/main/java/io/metersphere/project/dto/environment/http/HttpConfigModuleMatchRule.java
@@ -0,0 +1,32 @@
+package io.metersphere.project.dto.environment.http;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: jianxing
+ * @CreateTime: 2024-02-05  18:53
+ */
+@Data
+public class HttpConfigModuleMatchRule  implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Schema(description = "选中模块")
+    @Valid
+    private List<SelectModule> modules = new ArrayList<>(0);
+
+    @Data
+    public class SelectModule {
+        @Schema(description = "模块ID")
+        private String moduleId;
+        @Schema(description = "是否包含新增子模块")
+        private Boolean containChildModule = false;
+    }
+}
