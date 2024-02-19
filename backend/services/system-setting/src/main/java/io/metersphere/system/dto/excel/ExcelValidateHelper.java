ecbf7b49b68b0b7c476ba2b295a9a3ec119dc451
WangXu10 <xu.wang@fit2cloud.com>
2024-02-19 18:02:24 +0800

    fix(用例管理): 用例导入校验，用例等级不区分大小写
    --bug=1035534 --user=王旭 【用例管理】建议不区分大小写，并且给出具体不能为空的字段名称 https://www.tapd.cn/55049933/s/1462436

--- backend/services/system-setting/src/main/java/io/metersphere/system/dto/excel/ExcelValidateHelper.java
+++ backend/services/system-setting/src/main/java/io/metersphere/system/dto/excel/ExcelValidateHelper.java
@@ -1,5 +1,6 @@
 package io.metersphere.system.dto.excel;
 
+import com.alibaba.excel.annotation.ExcelProperty;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.Resource;
 import jakarta.validation.ConstraintViolation;
@@ -7,6 +8,7 @@ import jakarta.validation.Validator;
 import jakarta.validation.groups.Default;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Field;
 import java.util.Set;
 
 @Component
@@ -22,7 +24,10 @@ public class ExcelValidateHelper {
         Set<ConstraintViolation<T>> set = excelValidateHelper.validator.validate(obj, Default.class);
         if (set != null && !set.isEmpty()) {
             for (ConstraintViolation<T> cv : set) {
-                result.append(cv.getMessage()).append("; ");
+                Field declaredField = obj.getClass().getDeclaredField(cv.getPropertyPath().toString());
+                ExcelProperty annotation = declaredField.getAnnotation(ExcelProperty.class);
+                //拼接错误信息，包含当前出错数据的标题名字+错误信息
+                result.append(annotation.value()[0] + cv.getMessage()).append("; ");
             }
         }
         return result.toString();
