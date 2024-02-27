95c685693f90948f9fa18313cb394c11656e674c
wxg0103 <727495428@qq.com>
2024-02-27 20:31:41 +0800

    refactor(项目管理): 优化获取环境列表接口

--- backend/services/project-management/src/main/java/io/metersphere/project/controller/EnvironmentController.java
+++ backend/services/project-management/src/main/java/io/metersphere/project/controller/EnvironmentController.java
@@ -133,6 +133,7 @@ public class EnvironmentController {
     @GetMapping("/get-options/{projectId}")
     @Operation(summary = "项目管理-环境-环境目录-列表")
     @RequiresPermissions(PermissionConstants.PROJECT_ENVIRONMENT_READ)
+    @CheckOwner(resourceId = "#projectId", resourceType = "project")
     public List<EnvironmentOptionsDTO> list(@PathVariable String projectId) {
         return environmentService.listOption(projectId);
     }
