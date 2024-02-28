7c0a38ed0b5a5757678ec20f9745b845fc581fb6
guoyuqi <xiaomeinvG@126.com>
2024-02-28 16:09:20 +0800

    fix(用例管理): 修复评审评论缺少内容问题

--- backend/services/case-management/src/main/java/io/metersphere/functional/dto/CaseReviewHistoryDTO.java
+++ backend/services/case-management/src/main/java/io/metersphere/functional/dto/CaseReviewHistoryDTO.java
@@ -13,7 +13,13 @@ public class CaseReviewHistoryDTO extends CaseReviewHistory {
     @Schema(description =  "评审人名")
     private String userName;
 
+    @Schema(description =  "评审人邮箱")
+    private String email;
+
     @Schema(description =  "评审解析内容")
     private String contentText;
+
+    @Schema(description =  "评审名称")
+    private String reviewName;
 
 }
