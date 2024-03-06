9b5b850d8fcbbe1b2c832be7bf1678a9446baf6c
song-tianyang <tianyang.song@fit2cloud.com>
2024-03-06 18:28:30 +0800

    fix(缺陷管理): 修复缺陷富文本框粘贴图片报错的问题
    --bug=1036806 --user=宋天阳 【缺陷管理】创建缺陷-不支持粘贴图片-粘贴图片报错 https://www.tapd.cn/55049933/s/1470469

--- frontend/src/api/requrls/bug-management.ts
+++ frontend/src/api/requrls/bug-management.ts
@@ -44,7 +44,7 @@ export const deleteFileOrCancelAssociationUrl = '/bug/attachment/delete';
 // 获取附件列表
 export const getAttachmentListUrl = '/bug/attachment/list/';
 // 富文本编辑器上传图片
-export const editorUploadFileUrl = '/bug/attachment/upload/editor';
+export const editorUploadFileUrl = '/bug/attachment/upload/md/file';
 
 // 获取回收站列表
 export const getRecycleListUrl = '/bug/trash/page';
