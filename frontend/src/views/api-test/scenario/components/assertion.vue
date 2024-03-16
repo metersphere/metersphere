056a972a718f189d844753cb7a21413153cd026a
WangXu10 <xu.wang@fit2cloud.com>
2024-03-16 19:22:51 +0800

    feat(接口测试): 场景前后置&断言&设置

--- frontend/src/views/api-test/scenario/components/assertion.vue
+++ frontend/src/views/api-test/scenario/components/assertion.vue
@@ -1,7 +1,15 @@
 <template>
-  <div> assertion </div>
+  <assertion v-model:params="assertions" :is-definition="false" :assertion-config="assertionConfig" />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import assertion from '@/components/business/ms-assertion/index.vue';
+
+  const assertions = ref([]);
+  const assertionConfig = ref({
+    enableGlobal: false,
+    assertions: [],
+  });
+</script>
 
 <style lang="less" scoped></style>
--- frontend/src/views/api-test/scenario/components/prePost.vue
+++ frontend/src/views/api-test/scenario/components/prePost.vue
@@ -1,7 +1,35 @@
 <template>
-  <div> prePost </div>
+  <div class="condition">
+    <div>
+      <precondition v-model:config="preProcessorConfig" :is-definition="false" />
+    </div>
+    <a-divider class="my-[8px]" type="dashed" />
+    <div>
+      <postcondition v-model:config="postProcessorConfig" :is-definition="false" :layout="activeLayout" />
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import postcondition from '@/views/api-test/components/requestComposition/postcondition.vue';
+  import precondition from '@/views/api-test/components/requestComposition/precondition.vue';
 
-<style lang="less" scoped></style>
+  const activeLayout = ref<'horizontal' | 'vertical'>('vertical');
+  const preProcessorConfig = ref({
+    enableGlobal: false,
+    processors: [],
+  });
+  const postProcessorConfig = ref({
+    enableGlobal: false,
+    processors: [],
+  });
+</script>
+
+<style lang="less" scoped>
+  .condition {
+    flex-shrink: 0;
+    width: 100%;
+    height: 700px;
+    overflow: overlay;
+  }
+</style>
