8d8bdca8fc687bc06ede060e19a34bb7df830963
guoyuqi <xiaomeinvG@126.com>
2024-03-19 10:14:00 +0800

    fix(用例管理): 修复用例关联缺陷列表不显示标签的问题
    --bug=1037263 --user=郭雨琦 https://www.tapd.cn/55049933/bugtrace/bugs/view/1155049933001037263

--- backend/services/bug-management/src/test/resources/dml/init_bug_relation_case.sql
+++ backend/services/bug-management/src/test/resources/dml/init_bug_relation_case.sql
@@ -1,7 +1,7 @@
 INSERT INTO bug (id, num, title, handle_users, handle_user, create_user, create_time,
                  update_user, update_time, delete_user, delete_time, project_id, template_id, platform, status, tags, platform_bug_id, deleted, pos) VALUES
-    ('bug_id_1', 100000, 'default-bug', 'oasis', 'oasis', 'admin', UNIX_TIMESTAMP() * 1000, 'admin', UNIX_TIMESTAMP() * 1000, 'admin', UNIX_TIMESTAMP() * 1000, 'project_wx_associate_test"', 'bug-template-id', 'Local', 'open', '["default-tag"]', null, 0, 5000),
-    ('bug_id_2', 100001, 'default-bug', 'oasis', 'oasis', 'admin', UNIX_TIMESTAMP() * 1000, 'admin', UNIX_TIMESTAMP() * 1000, 'admin', UNIX_TIMESTAMP() * 1000, 'project_wx_associate_test"', 'bug-template-id', 'Local', 'open', '["default-tag"]', null, 0, 10000);
+    ('bug_id_1', 100000, 'default-bug', 'oasis', 'oasis', 'admin', UNIX_TIMESTAMP() * 1000, 'admin', UNIX_TIMESTAMP() * 1000, 'admin', UNIX_TIMESTAMP() * 1000, 'project_wx_associate_test', 'bug-template-id', 'Local', 'open', '["default-tag"]', null, 0, 5000),
+    ('bug_id_2', 100001, 'default-bug', 'oasis', 'oasis', 'admin', UNIX_TIMESTAMP() * 1000, 'admin', UNIX_TIMESTAMP() * 1000, 'admin', UNIX_TIMESTAMP() * 1000, 'project_wx_associate_test', 'bug-template-id', 'Local', 'open', '["default-tag"]', null, 0, 10000);
 
 INSERT INTO bug_relation_case(id, case_id, bug_id, case_type, test_plan_id, test_plan_case_id, create_user, create_time, update_time)
 VALUES ('wx_test_id_1', 'wx_1', 'bug_id_1', 'FUNCTIONAL', null, null, 'admin', UNIX_TIMESTAMP() * 1000, UNIX_TIMESTAMP() * 1000),
