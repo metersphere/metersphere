af988df1b83a85a1f28e2d490e69568972ecf291
wxg0103 <727495428@qq.com>
2024-03-22 16:42:22 +0800

    refactor(接口测试): 优化接口分享报告

--- backend/services/api-test/src/main/java/io/metersphere/api/controller/ApiReportShareController.java
+++ backend/services/api-test/src/main/java/io/metersphere/api/controller/ApiReportShareController.java
@@ -1,8 +1,9 @@
 package io.metersphere.api.controller;
 
+import io.metersphere.api.dto.share.ApiReportShareDTO;
+import io.metersphere.api.dto.share.ApiReportShareRequest;
 import io.metersphere.api.dto.share.ShareInfoDTO;
 import io.metersphere.api.service.ApiReportShareService;
-import io.metersphere.sdk.domain.ShareInfo;
 import io.metersphere.system.utils.SessionUtils;
 import io.metersphere.validation.groups.Created;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -21,12 +22,12 @@ public class ApiReportShareController {
     private ApiReportShareService apiReportShareService;
 
     @PostMapping("/gen")
-    public ShareInfoDTO generateShareInfo(@Validated(Created.class) @RequestBody ShareInfo request) {
+    public ShareInfoDTO generateShareInfo(@Validated(Created.class) @RequestBody ApiReportShareRequest request) {
         return apiReportShareService.gen(request, Objects.requireNonNull(SessionUtils.getUser()));
     }
 
     @GetMapping("/get/{id}")
-    public ShareInfo get(@PathVariable String id) {
+    public ApiReportShareDTO get(@PathVariable String id) {
         return apiReportShareService.get(id);
     }
 
