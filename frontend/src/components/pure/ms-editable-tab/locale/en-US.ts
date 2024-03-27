89e41c025386aebb12ae5db74e70543d0a9150ec
song-tianyang <tianyang.song@fit2cloud.com>
2024-03-27 11:23:27 +0800

    style(接口测试): 接口测试一揽子文案修改
    https://www.tapd.cn/55049933/s/1481593；https://www.tapd.cn/55049933/s/1481600；

--- frontend/src/components/pure/ms-editable-tab/locale/en-US.ts
+++ frontend/src/components/pure/ms-editable-tab/locale/en-US.ts
@@ -2,10 +2,8 @@ export default {
   'ms.editableTab.arrivedLeft': 'Already reached the far left~',
   'ms.editableTab.arrivedRight': 'Already reached the far right~',
   'ms.editableTab.limitTip': 'Up to {max} tabs can currently be open',
-  'ms.editableTab.closeTabTip':
-    'The modified content of this tab has not been saved. The unsaved content will be lost after closing. Are you sure you want to close?',
-  'ms.editableTab.batchCloseTabTip':
-    'The content of some tabs has not been saved. The unsaved content will be lost after closing. Are you sure you want to close?',
+  'ms.editableTab.closeTabTip': 'The current tab has not been saved. Do you want to close still?',
+  'ms.editableTab.batchCloseTabTip': 'Some tabs has not been saved. Do you want to close still?',
   'ms.editableTab.closeAll': 'Close all',
   'ms.editableTab.closeOther': 'Close other',
 };
