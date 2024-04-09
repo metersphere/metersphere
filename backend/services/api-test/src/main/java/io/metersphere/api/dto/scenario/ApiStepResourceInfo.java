4408222206d33f205a85c3b325f9aead327e5a7c
AgAngle <1323481023@qq.com>
2024-04-09 12:50:52 +0800

    fix(接口测试): 新增场景步骤所属资源查询接口
    --bug=1038780 --user=陈建星 【接口测试】场景-添加步骤-导入系统请求-导入后修改原请求的名称-hover图标查看导入系统请求名称-名称显示undefined https://www.tapd.cn/55049933/s/1491298

---
+++ backend/services/api-test/src/main/java/io/metersphere/api/dto/scenario/ApiStepResourceInfo.java
@@ -0,0 +1,18 @@
+package io.metersphere.api.dto.scenario;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
+public class ApiStepResourceInfo {
+    @Schema(description = "资源ID")
+    private String id;
+    @Schema(description = "资源编号")
+    private Long num;
+    @Schema(description = "资源名称")
+    private String name;
+    @Schema(description = "所属项目ID")
+    private String projectId;
+    @Schema(description = "所属项目名称")
+    private String projectName;
+}
