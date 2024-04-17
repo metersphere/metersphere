b0f4c5bcb6e47f1bf999a0df080560cf5f8482f1
xinxin.wu <xinxin.wu@fit2cloud.com>
2024-04-17 14:54:40 +0800

    fix(项目管理): 修复环境管理离开页面时统一弹窗&弹窗两次

--- frontend/src/hooks/useLeaveUnSaveTip.ts
+++ frontend/src/hooks/useLeaveUnSaveTip.ts
@@ -1,12 +1,26 @@
 import { onBeforeRouteLeave } from 'vue-router';
 
 import { useI18n } from '@/hooks/useI18n';
+import type { ModalType } from '@/hooks/useModal';
 import useModal from '@/hooks/useModal';
 
+export interface LeaveProps {
+  leaveTitle: string;
+  leaveContent: string;
+  tipType: ModalType;
+}
+
+const leaveProps: LeaveProps = {
+  leaveTitle: 'common.unSaveLeaveTitle',
+  leaveContent: 'common.unSaveLeaveContent',
+  tipType: 'error',
+};
+
 // 离开页面确认提示
-export default function useLeaveUnSaveTip() {
+export default function useLeaveUnSaveTip(leaveProp = leaveProps) {
   const { openModal } = useModal();
   const { t } = useI18n();
+  const { leaveTitle, leaveContent, tipType } = leaveProp;
 
   const isSave = ref(true);
 
@@ -21,11 +35,11 @@ export default function useLeaveUnSaveTip() {
 
     if (!isSave.value) {
       openModal({
-        type: 'error',
-        title: t('common.unSaveLeaveTitle'),
-        content: t('common.unSaveLeaveContent'),
+        type: tipType,
+        title: t(leaveTitle),
+        content: t(leaveContent),
         okText: t('common.leave'),
-        cancelText: t('common.cancel'),
+        cancelText: t('common.stay'),
         okButtonProps: {
           status: 'normal',
         },
