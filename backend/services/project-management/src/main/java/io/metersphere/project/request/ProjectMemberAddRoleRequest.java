1e355322dab4f10696cd1240dfd3b9a9fcfe4863
song-cc-rock <changchang.song@fit2cloud.com>
2024-04-18 18:10:39 +0800

    fix(项目管理): 项目与权限批量操作问题
    --bug=1039710 --user=宋昌昌 【项目管理】项目与权限-成员-全选所有页-切换用户组/搜索框搜索-所选择数量错误 https://www.tapd.cn/55049933/s/1501228

---
+++ backend/services/project-management/src/main/java/io/metersphere/project/request/ProjectMemberAddRoleRequest.java
@@ -0,0 +1,27 @@
+package io.metersphere.project.request;
+
+import io.metersphere.system.dto.table.TableBatchProcessDTO;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author song-cc-rock
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class ProjectMemberAddRoleRequest extends TableBatchProcessDTO implements Serializable {
+
+	@Schema(description =  "项目ID", requiredMode = Schema.RequiredMode.REQUIRED)
+	@NotBlank(message = "{project.id.not_blank}")
+	private String projectId;
+
+	@Schema(description =  "用户组ID集合", requiredMode = Schema.RequiredMode.REQUIRED)
+	@NotEmpty(message = "{user_role.id.not_blank}")
+	private List<String> roleIds;
+}
