15de85655dd6b7f28148d612d2bd45a3966e8c4d
guoyuqi <xiaomeinvG@126.com>
2024-04-22 17:12:41 +0800

    fix(用例管理): 修复用例管理模块树按钮依据权限显示问题
    --bug=1039850 --user=郭雨琦 https://www.tapd.cn/55049933/bugtrace/bugs/view/1155049933001039850

--- backend/services/case-management/src/main/java/io/metersphere/functional/service/CaseReviewFunctionalCaseService.java
+++ backend/services/case-management/src/main/java/io/metersphere/functional/service/CaseReviewFunctionalCaseService.java
@@ -35,7 +35,6 @@ import io.metersphere.system.mapper.UserRoleRelationMapper;
 import io.metersphere.system.notice.constants.NoticeConstants;
 import io.metersphere.system.service.BaseCustomFieldOptionService;
 import io.metersphere.system.service.BaseCustomFieldService;
-import io.metersphere.system.service.UserLoginService;
 import io.metersphere.system.uid.IDGenerator;
 import io.metersphere.system.utils.ServiceUtils;
 import jakarta.annotation.Resource;
@@ -111,8 +110,6 @@ public class CaseReviewFunctionalCaseService {
     private BaseCustomFieldService baseCustomFieldService;
     @Resource
     private BaseCustomFieldOptionService baseCustomFieldOptionService;
-    @Resource
-    private UserLoginService userLoginService;
 
 
     private static final String CASE_MODULE_COUNT_ALL = "all";
