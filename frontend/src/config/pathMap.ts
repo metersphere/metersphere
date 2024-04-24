f1c206c3c991cbb65d7eca3b65ff737d6fbfd6f9
song-cc-rock <changchang.song@fit2cloud.com>
2024-04-24 11:34:32 +0800

    fix(日志管理): 日志操作对象缺陷管理下级模块显示有误

--- frontend/src/config/pathMap.ts
+++ frontend/src/config/pathMap.ts
@@ -184,13 +184,6 @@ export const pathMap: PathMapItem[] = [
         permission: [],
         level: MENU_LEVEL[2],
       },
-      {
-        key: 'BUG_MANAGEMENT_BUG_DETAIL', // 缺陷管理-回收站
-        locale: 'menu.bugManagement.bugDetail',
-        route: RouteEnum.BUG_MANAGEMENT_DETAIL,
-        permission: [],
-        level: MENU_LEVEL[2],
-      },
     ],
   },
   {
