aa68631b175ccd9af3eb4a98453fb05823731275
xinxin.wu <xinxin.wu@fit2cloud.com>
2024-04-26 14:17:03 +0800

    style: 调整授权到期导致页面滚动条调整和高度调整

--- frontend/src/layout/page-layout.vue
+++ frontend/src/layout/page-layout.vue
@@ -2,7 +2,13 @@
   <router-view v-slot="{ Component, route }">
     <transition name="fade" mode="out-in" appear>
       <!-- transition内必须有且只有一个根元素，不然会导致二级路由的组件无法渲染 -->
-      <div v-show="true" class="page-content">
+      <div
+        v-show="true"
+        class="page-content"
+        :class="[
+          licenseStore.expiredDuring && route.path.includes('/setting/system') ? 'h-[calc(100%-48px)]' : 'h-full',
+        ]"
+      >
         <!-- TODO 实验性组件，以后优化 -->
         <Suspense v-if="!route.meta.isCache">
           <component :is="Component" :key="route.fullPath" />
@@ -15,12 +21,16 @@
   </router-view>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import useLicenseStore from '@/store/modules/setting/license';
+
+  const licenseStore = useLicenseStore();
+</script>
 
 <style lang="less" scoped>
   .page-content {
-    @apply h-full overflow-y-auto;
-
     min-height: 500px;
+    @apply overflow-y-auto;
+    .ms-scroll-bar();
   }
 </style>
