e126bca529a31f625e15a6b341fec24344f91b5c
teukkk <mengyuan.lv@fit2cloud.com>
2024-05-08 10:57:25 +0800

    feat(测试计划): 创建测试计划抽屉

---
+++ frontend/src/components/pure/ms-more-setting-collapse/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <a-collapse v-model:active-key="moreSettingActive" :bordered="false" :show-expand-icon="false">
+    <a-collapse-item :key="1">
+      <template #header>
+        <MsButton
+          type="text"
+          @click="() => (moreSettingActive.length > 0 ? (moreSettingActive = []) : (moreSettingActive = [1]))"
+        >
+          {{ t('common.moreSetting') }}
+          <icon-down v-if="moreSettingActive.length > 0" class="text-rgb(var(--primary-5))" />
+          <icon-right v-else class="text-rgb(var(--primary-5))" />
+        </MsButton>
+      </template>
+      <div class="mt-[24px]">
+        <slot name="content"></slot>
+      </div>
+    </a-collapse-item>
+  </a-collapse>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  import MsButton from '@/components/pure/ms-button/index.vue';
+
+  import { useI18n } from '@/hooks/useI18n';
+
+  const { t } = useI18n();
+
+  const moreSettingActive = ref<number[]>([]);
+  function clearMoreSettingActive() {
+    moreSettingActive.value = [];
+  }
+
+  defineExpose({
+    clearMoreSettingActive,
+  });
+</script>
--- frontend/src/models/testPlan/testPlan.ts
+++ frontend/src/models/testPlan/testPlan.ts
@@ -1,3 +1,5 @@
+import { BatchApiParams } from '../common';
+
 // 计划分页
 export interface TestPlanItem {
   id?: string;
@@ -22,6 +24,33 @@ export interface ResourcesItem {
   name: string;
   cpuRate: string;
   status: boolean;
+}
+
+export interface AssociateCaseRequest extends BatchApiParams {
+  functionalSelectIds?: string[];
+  apiSelectIds?: string[];
+  apiCaseSelectIds?: string[];
+  apiScenarioSelectIds?: string[];
+}
+
+export interface AddTestPlanParams {
+  id?: string;
+  name: string;
+  projectId: string;
+  groupId?: string;
+  moduleId: string;
+  cycle?: number[];
+  plannedStartTime?: number;
+  plannedEndTime?: number;
+  tags: string[];
+  description?: string;
+  testPlanning: boolean; // 是否开启测试规划
+  automaticStatusUpdate: boolean; // 是否自定更新功能用例状态
+  repeatCase: boolean; // 是否允许重复添加用例
+  passThreshold: number;
+  type: string;
+  baseAssociateCaseRequest: AssociateCaseRequest;
+  groupOption?: boolean;
 }
 
 export default {};
