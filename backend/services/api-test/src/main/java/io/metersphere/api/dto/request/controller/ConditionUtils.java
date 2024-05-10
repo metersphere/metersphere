c65713794dc77cbda66c112982eac9e6a606b9fa
AgAngle <1323481023@qq.com>
2024-05-10 18:24:08 +0800

    fix(接口测试): 条件控制器数值变量判断有误
    --bug=1040138 --user=陈建星 【接口测试】【场景】在条件控制器做数值判断正确的情况下，未执行控制器里的请求步骤 https://www.tapd.cn/55049933/s/1514594

--- backend/services/api-test/src/main/java/io/metersphere/api/dto/request/controller/ConditionUtils.java
+++ backend/services/api-test/src/main/java/io/metersphere/api/dto/request/controller/ConditionUtils.java
@@ -1,31 +1,35 @@
 package io.metersphere.api.dto.request.controller;
 
 import io.metersphere.sdk.constants.MsAssertionCondition;
-import org.apache.commons.lang3.StringUtils;
 
 public class ConditionUtils {
 
 
     public String getConditionValue(String variable, String condition, String value) {
-        variable = "\"" + variable + "\"";
+        String wrappedVariable = "\"" + variable + "\"";
+        String wrappedValue = "\"" + value + "\"";
         MsAssertionCondition msAssertionCondition = MsAssertionCondition.valueOf(condition);
         return switch (msAssertionCondition) {
-            case EMPTY -> String.format("(%s==\"\"|| empty(%s))", variable, variable);
-            case NOT_EMPTY ->
-                    String.format("(%s!=\"\"&& !empty(%s))", variable, variable);
-            case GT -> StringUtils.isNumeric(value) ? variable + ">" + value : variable + ">\"" + value + "\"";
-            case LT -> StringUtils.isNumeric(value) ? variable + "<" + value : variable + "<\"" + value + "\"";
-            case LT_OR_EQUALS ->
-                    StringUtils.isNumeric(value) ? variable + "<=" + value : variable + "<=\"" + value + "\"";
-            case GT_OR_EQUALS ->
-                    StringUtils.isNumeric(value) ? variable + ">=" + value : variable + ">=\"" + value + "\"";
-            case CONTAINS -> String.format("%s.contains(%s)", variable,  "\"" + value + "\"");
-            case NOT_CONTAINS -> String.format("!%s.contains(%s)", variable,  "\"" + value + "\"");
-            case EQUALS ->
-                    StringUtils.isNumeric(value) ? variable + "==" + value : variable + "==" + "\"" + value + "\"";
-            case NOT_EQUALS ->
-                    StringUtils.isNumeric(value) ? variable + "!=" + value : variable + "!=" + "\"" + value + "\"";
-            default -> "\"" + value + "\"";
+            case EMPTY -> String.format("(%s==\"\"|| empty(%s))", wrappedVariable, wrappedVariable);
+            case NOT_EMPTY -> String.format("(%s!=\"\"&& !empty(%s))", wrappedVariable, wrappedVariable);
+            case GT -> variable + ">" + value;
+            case LT -> variable + "<" + value;
+            case LT_OR_EQUALS -> variable + "<=" + value;
+            case GT_OR_EQUALS -> variable + ">=" + value;
+            case CONTAINS -> String.format("%s.contains(%s)", wrappedVariable,  wrappedValue);
+            case NOT_CONTAINS -> String.format("!%s.contains(%s)", wrappedVariable,  wrappedValue);
+            case EQUALS -> isNumeric(value) ? wrappedVariable + "==" + value : wrappedVariable + "==" + wrappedValue;
+            case NOT_EQUALS -> isNumeric(value) ? wrappedVariable + "!=" + value : wrappedVariable + "!=" + wrappedValue;
+            default -> wrappedValue;
         };
+    }
+
+    public boolean isNumeric(String strNum) {
+        try {
+            Double.parseDouble(strNum);
+            return true;
+        } catch (NumberFormatException e2) {
+            return false;
+        }
     }
 }
