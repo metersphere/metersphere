2b5bc5499e277954f03db8fc442f8a431b141371
guoyuqi <xiaomeinvG@126.com>
2024-05-10 18:59:37 +0800

    feat(项目管理): 增加测试计划跳转

--- frontend/src/hooks/useOpenNewPage.ts
+++ frontend/src/hooks/useOpenNewPage.ts
@@ -26,7 +26,27 @@ export default function useOpenNewPage() {
     );
   }
 
+  function openNewPageWidthSingleParam(
+    name: RouteRecordName | undefined,
+    param: string,
+    query: Record<string, any> = {}
+  ) {
+    const pId = query.pId || appStore.currentProjectId;
+    if (pId) {
+      // 如果传入参数指定了项目 id，则使用传入的项目 id
+      delete query.pId;
+    }
+    const queryParams = new URLSearchParams(query).toString();
+    window.open(
+      `${window.location.origin}#${router.resolve({ name }).fullPath}/${param}?orgId=${
+        appStore.currentOrgId
+      }&pId=${pId}&${queryParams}`,
+      '_blank'
+    );
+  }
+
   return {
     openNewPage,
+    openNewPageWidthSingleParam,
   };
 }
