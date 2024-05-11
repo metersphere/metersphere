6d6592d7974b4a11f28bca8dfa4ad24464f1a061
guoyuqi <xiaomeinvG@126.com>
2024-05-11 14:03:29 +0800

    refactor(功能用例): 调整功能用例步骤结构的DTO

--- backend/services/case-management/src/main/java/io/metersphere/functional/dto/FunctionalCaseStepDTO.java
+++ backend/services/case-management/src/main/java/io/metersphere/functional/dto/FunctionalCaseStepDTO.java
@@ -1,15 +1,27 @@
 package io.metersphere.functional.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class FunctionalCaseStepDTO {
 
+    @Schema(description = "步骤id")
     private String id;
 
+    @Schema(description = "步骤序号")
     private Integer num;
 
+    @Schema(description = "步骤描述")
     private String desc;
 
+    @Schema(description = "步骤结果")
     private String result;
+
+    @Schema(description = "实际结果")
+    private String actualResult;
+
+    @Schema(description = "执行结果（失败/成功/阻塞）")
+    private String executeResult;
+
 }
