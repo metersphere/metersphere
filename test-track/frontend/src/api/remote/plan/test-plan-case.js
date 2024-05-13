1dde28a80bc23cc0ae8affcbfea893abc8bf436f
AgAngle <1323481023@qq.com>
2024-05-13 14:46:31 +0800

    fix(测试跟踪): 测试计划脑图保存报错
    --bug=1040625 --user=陈建星 【测试跟踪】测试计划-脑图-编辑脑图后保存失败 https://www.tapd.cn/55049933/s/1515280

--- test-track/frontend/src/api/remote/plan/test-plan-case.js
+++ test-track/frontend/src/api/remote/plan/test-plan-case.js
@@ -2,6 +2,6 @@ import {post, get} from "@/business/utils/sdk-utils";
 
 const BASE_URL = '/test/plan/case/';
 
-export function testPlanCaseMinderEdit(param) {
-  return post(BASE_URL + 'minder/edit', param);
+export function testPlanCaseMinderEdit(planId, param) {
+  return post(BASE_URL + 'minder/edit/' + planId, param);
 }
