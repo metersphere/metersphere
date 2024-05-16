1a3dc117843a0cef303f7a05a214dce787b10fa9
fit2-zhao <yong.zhao@fit2cloud.com>
2024-05-16 14:33:27 +0800

    chore: 报告状态统一

---
+++ backend/framework/sdk/src/main/java/io/metersphere/sdk/constants/ReportExecStatus.java
@@ -0,0 +1,27 @@
+package io.metersphere.sdk.constants;
+
+/**
+ * 报告执行状态
+ */
+public enum ReportExecStatus {
+    /**
+     * 未执行
+     */
+    PENDING,
+    /**
+     * 执行中
+     */
+    RUNNING,
+    /**
+     * 重新执行
+     */
+    RERUNNING,
+    /**
+     * 已停止
+     */
+    STOPPED,
+    /**
+     * 已完成
+     */
+    COMPLETED
+}
---
+++ backend/framework/sdk/src/main/java/io/metersphere/sdk/constants/ReportStatus.java
@@ -0,0 +1,21 @@
+package io.metersphere.sdk.constants;
+
+/**
+ * 报告状态
+ */
+public enum ReportStatus {
+    /**
+     * 成功
+     */
+    SUCCESS,
+
+    /**
+     * 错误
+     */
+    ERROR,
+
+    /**
+     * 误报
+     */
+    FAKE_ERROR
+}
