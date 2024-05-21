685815e62e73f1dc0f48b6d2c9deaa04c92d925f
guoyuqi <xiaomeinvG@126.com>
2024-05-21 14:01:45 +0800

    refactor(用例管理): 调整功能用例关联其他用例校验问题

--- backend/services/case-management/src/main/java/io/metersphere/functional/request/DisassociateOtherCaseRequest.java
+++ backend/services/case-management/src/main/java/io/metersphere/functional/request/DisassociateOtherCaseRequest.java
@@ -8,7 +8,7 @@ import lombok.Data;
 @Data
 public class DisassociateOtherCaseRequest extends BaseFunctionalCaseBatchDTO {
 
-    @Schema(description = "功能用例选择的项目id", requiredMode = Schema.RequiredMode.REQUIRED)
+    @Schema(description = "功能用例id", requiredMode = Schema.RequiredMode.REQUIRED)
     @NotBlank(message = "{functional_case.id.not_blank}")
     private String caseId;
 
