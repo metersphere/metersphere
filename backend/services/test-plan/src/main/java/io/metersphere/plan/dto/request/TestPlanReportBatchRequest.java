12fb7ecd598fc94d551fd0bbb0a59dedd8fafa61
song-cc-rock <changchang.song@fit2cloud.com>
2024-05-23 10:48:59 +0800

    fix(测试计划): 报告列表筛选问题
    --bug=1040954 --user=宋昌昌 【测试计划】报告-表头字段筛选/搜索框搜索后-全选所有页-批量删除-把所有报告都给删除了 https://www.tapd.cn/55049933/s/1518926

--- backend/services/test-plan/src/main/java/io/metersphere/plan/dto/request/TestPlanReportBatchRequest.java
+++ backend/services/test-plan/src/main/java/io/metersphere/plan/dto/request/TestPlanReportBatchRequest.java
@@ -1,6 +1,6 @@
 package io.metersphere.plan.dto.request;
 
-import io.metersphere.system.dto.request.TableBatchRequest;
+import io.metersphere.system.dto.table.TableBatchProcessDTO;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -8,7 +8,7 @@ import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
-public class TestPlanReportBatchRequest extends TableBatchRequest {
+public class TestPlanReportBatchRequest extends TableBatchProcessDTO {
 
     @Schema(description = "项目ID", requiredMode = Schema.RequiredMode.REQUIRED)
     @NotBlank(message = "{test_plan.project_id.not_blank}")
