4c48bae74f6fa0a3c00157d4f6908a89df13ec15
CaptainB <bin@fit2cloud.com>
2024-06-04 17:05:36 +0800

    fix: 修复jenkins执行接口场景不能正常查看结果的问题

--- framework/sdk-parent/sdk/src/main/java/io/metersphere/base/mapper/ext/ExtCheckOwnerMapper.java
+++ framework/sdk-parent/sdk/src/main/java/io/metersphere/base/mapper/ext/ExtCheckOwnerMapper.java
@@ -5,5 +5,5 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 public interface ExtCheckOwnerMapper {
-    boolean checkoutOwner(@Param("table") String resourceType, @Param("projectId") String projectId, @Param("ids") List ids);
+    boolean checkoutOwner(@Param("table") String resourceType, @Param("userId") String userId, @Param("ids") List ids);
 }
