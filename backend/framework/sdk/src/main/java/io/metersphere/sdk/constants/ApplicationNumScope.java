3560b615a41ffff5a7e05f439bb436c33c0b9ddc
fit2-zhao <yong.zhao@fit2cloud.com>
2024-06-11 10:02:14 +0800

    chore: 初始化SQL调整

--- backend/framework/sdk/src/main/java/io/metersphere/sdk/constants/ApplicationNumScope.java
+++ backend/framework/sdk/src/main/java/io/metersphere/sdk/constants/ApplicationNumScope.java
@@ -1,25 +1,23 @@
 package io.metersphere.sdk.constants;
 
 public enum ApplicationNumScope {
+    // API
     API_DEFINITION,
     API_TEST_CASE,
     API_MOCK,
     API_SCENARIO,
     API_IMPORT,
 
-    UI_SCENARIO,
-    UI_ELEMENT,
-    UI_CUSTOM_COMMAND,
-
-    LOAD_TEST,
-
+    // Test Plan
     TEST_PLAN,
     TEST_PLAN_FUNCTION_CASE,
     TEST_PLAN_API_CASE,
     TEST_PLAN_API_SCENARIO,
 
+    // Bug
     BUG_MANAGEMENT,
 
+    // Test Case
     CASE_MANAGEMENT,
     REVIEW_CASE_MANAGEMENT
 }
