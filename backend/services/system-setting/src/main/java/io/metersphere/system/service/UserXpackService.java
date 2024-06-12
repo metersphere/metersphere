251f923b43a09a81534b13cbb53ff92e8842b820
Jianguo-Genius <herman.song.yang@gmail.com>
2024-06-12 18:44:08 +0800

    feat(系统设置): 优化用户批量开启的方法

--- backend/services/system-setting/src/main/java/io/metersphere/system/service/UserXpackService.java
+++ backend/services/system-setting/src/main/java/io/metersphere/system/service/UserXpackService.java
@@ -4,6 +4,8 @@ import io.metersphere.system.domain.UserInvite;
 import io.metersphere.system.dto.request.UserRegisterRequest;
 import io.metersphere.system.dto.user.request.UserBatchCreateRequest;
 
+import java.util.List;
+
 /**
  * 系统用户相关接口
  */
@@ -12,4 +14,6 @@ public interface UserXpackService {
     int guessWhatHowToAddUser(UserBatchCreateRequest userCreateDTO, String source, String operator);
 
     int guessWhatHowToAddUser(UserRegisterRequest registerRequest, UserInvite userInvite) throws Exception;
+
+    int guessWhatHowToChangeUser(List<String> userIds, boolean enable, String operator);
 }
