12cda645a44a88a6147938580cc447b3f78007a0
Jianguo-Genius <herman.song.yang@gmail.com>
2024-06-17 10:26:17 +0800

    fix(测试计划): 优化测试计划执行的入参

--- backend/services/test-plan/src/main/java/io/metersphere/plan/dto/request/TestPlanBatchExecuteRequest.java
+++ backend/services/test-plan/src/main/java/io/metersphere/plan/dto/request/TestPlanBatchExecuteRequest.java
@@ -1,7 +1,6 @@
 package io.metersphere.plan.dto.request;
 
 import io.metersphere.sdk.constants.ApiBatchRunMode;
-import io.metersphere.sdk.constants.ApiExecuteRunMode;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
@@ -22,8 +21,5 @@ public class TestPlanBatchExecuteRequest {
 
     @Schema(description = "执行模式", allowableValues = {"SERIAL", "PARALLEL"}, requiredMode = Schema.RequiredMode.REQUIRED)
     private String runMode = ApiBatchRunMode.SERIAL.name();
-
-    @Schema(description = "执行来源", allowableValues = {"JENKINS", "SCENARIO", "RUN"}, requiredMode = Schema.RequiredMode.REQUIRED)
-    private String executionSource = ApiExecuteRunMode.RUN.name();
 
 }
