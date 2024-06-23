4b218f2baf73dc7a4e1eed7a3180715f87f6cba6
song-tianyang <tianyang.song@fit2cloud.com>
2024-06-23 18:23:50 +0800

    fix(测试计划): 修复日志中对测试计划组或测试计划对应所属类型判断错误的问题

--- backend/services/test-plan/src/main/java/io/metersphere/plan/job/TestPlanScheduleJob.java
+++ backend/services/test-plan/src/main/java/io/metersphere/plan/job/TestPlanScheduleJob.java
@@ -6,6 +6,7 @@ import io.metersphere.sdk.constants.ApiBatchRunMode;
 import io.metersphere.sdk.constants.ApiExecuteRunMode;
 import io.metersphere.sdk.util.CommonBeanFactory;
 import io.metersphere.sdk.util.JSON;
+import io.metersphere.sdk.util.LogUtils;
 import io.metersphere.system.schedule.BaseScheduleJob;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobKey;
@@ -21,13 +22,14 @@ public class TestPlanScheduleJob extends BaseScheduleJob {
         assert testPlanExecuteService != null;
         Map<String, String> runConfig = JSON.parseObject(context.getJobDetail().getJobDataMap().get("config").toString(), Map.class);
         String runMode = runConfig.containsKey("runMode") ? runConfig.get("runMode") : ApiBatchRunMode.SERIAL.name();
-        Thread.startVirtualThread(() -> {
+        LogUtils.info("开始执行测试计划的定时任务. ID：" + resourceId);
+        Thread.startVirtualThread(() ->
             testPlanExecuteService.singleExecuteTestPlan(new TestPlanExecuteRequest() {{
                 this.setExecuteId(resourceId);
                 this.setRunMode(runMode);
                 this.setExecutionSource(ApiExecuteRunMode.SCHEDULE.name());
-            }}, userId);
-        });
+            }}, userId)
+        );
     }
 
 
