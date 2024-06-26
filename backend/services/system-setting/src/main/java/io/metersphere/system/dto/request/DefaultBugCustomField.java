4a1ec61dc472cfd967bd1fae2c2efeb4aa43e5c7
song-cc-rock <changchang.song@fit2cloud.com>
2024-06-26 17:36:38 +0800

    fix(缺陷模板): 严重程度字段选项值初始化有误

--- backend/services/system-setting/src/main/java/io/metersphere/system/dto/request/DefaultBugCustomField.java
+++ backend/services/system-setting/src/main/java/io/metersphere/system/dto/request/DefaultBugCustomField.java
@@ -2,7 +2,6 @@ package io.metersphere.system.dto.request;
 
 import io.metersphere.sdk.constants.CustomFieldType;
 import io.metersphere.system.domain.CustomFieldOption;
-import io.metersphere.system.uid.IDGenerator;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,10 +17,10 @@ public enum DefaultBugCustomField {
      */
     DEGREE("bug_degree", CustomFieldType.SELECT,
             Arrays.asList(
-                    getNewOption(IDGenerator.nextStr(), "提示", 1),
-                    getNewOption(IDGenerator.nextStr(), "一般", 2),
-                    getNewOption(IDGenerator.nextStr(), "严重", 3),
-                    getNewOption(IDGenerator.nextStr(), "致命", 4)
+                    getNewOption("suggestive", "提示", 1),
+                    getNewOption("general", "一般", 2),
+                    getNewOption("severity", "严重", 3),
+                    getNewOption("deadly", "致命", 4)
             )
     );
 
