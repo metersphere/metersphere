add903346cf36319b37c6fda7b1cb3e08315c8b5
guoyuqi <xiaomeinvG@126.com>
2024-07-02 16:05:18 +0800

    feat(功能用例): 飞书扫码后端配置页面

--- frontend/src/api/modules/setting/qrCode.ts
+++ frontend/src/api/modules/setting/qrCode.ts
@@ -1,19 +1,34 @@
 import MSR from '@/api/http/index';
 import {
   GetDingTalkInfoUrl,
+  GetLarkInfoUrl,
+  GetLarkSuiteInfoUrl,
   GetPlatformInfoUrl,
   GetWeComInfoUrl,
   PostDingTalkEnableUrl,
   PostDingTalkSaveUrl,
   PostDingTalkValidateFalseUrl,
+  PostLarkEnableUrl,
+  PostLarkSaveUrl,
+  PostLarkSuiteEnableUrl,
+  PostLarkSuiteSaveUrl,
+  PostLarkSuiteValidateFalseUrl,
+  PostLarkValidateFalseUrl,
   PostValidateDingTalkUrl,
+  PostValidateLarkSuiteUrl,
+  PostValidateLarkUrl,
   PostValidateWeComUrl,
   PostWeComEnableUrl,
   PostWeComSaveUrl,
   PostWeComValidateFalseUrl,
 } from '@/api/requrls/setting/qrCode';
 
-import { DingTalkInfo, EnableEditorRequest, PlatformSourceList, WeComInfo } from '@/models/setting/qrCode';
+import { DingTalkInfo, EnableEditorRequest, LarkInfo, PlatformSourceList, WeComInfo } from '@/models/setting/qrCode';
+
+// 获取所有平台配置基础信息
+export function getPlatformSourceList() {
+  return MSR.get<PlatformSourceList>({ url: GetPlatformInfoUrl });
+}
 
 // 获取企业微信配置
 export function getWeComInfo() {
@@ -23,6 +38,16 @@ export function getWeComInfo() {
 // 获取钉钉配置
 export function getDingInfo() {
   return MSR.get<DingTalkInfo>({ url: GetDingTalkInfoUrl });
+}
+
+// 获取飞书配置
+export function getLarkInfo() {
+  return MSR.get<LarkInfo>({ url: GetLarkInfoUrl });
+}
+
+// 获取国际飞书配置
+export function getLarkSuiteInfo() {
+  return MSR.get<LarkInfo>({ url: GetLarkSuiteInfoUrl });
 }
 
 // 保存企业微信登陆配置
@@ -35,6 +60,16 @@ export function saveDingTalkConfig(data: DingTalkInfo) {
   return MSR.post({ url: PostDingTalkSaveUrl, data });
 }
 
+// 保存飞书登陆配置
+export function saveLarkConfig(data: LarkInfo) {
+  return MSR.post({ url: PostLarkSaveUrl, data });
+}
+
+// 保存国际飞书登陆配置
+export function saveLarkSuiteConfig(data: LarkInfo) {
+  return MSR.post({ url: PostLarkSuiteSaveUrl, data });
+}
+
 // 校验企业微信外链接
 export function validateWeComConfig(data: WeComInfo) {
   return MSR.post({ url: PostValidateWeComUrl, data });
@@ -43,6 +78,16 @@ export function validateWeComConfig(data: WeComInfo) {
 // 校验钉钉外链接
 export function validateDingTalkConfig(data: DingTalkInfo) {
   return MSR.post({ url: PostValidateDingTalkUrl, data });
+}
+
+// 校验飞书外链接
+export function validateLarkConfig(data: LarkInfo) {
+  return MSR.post({ url: PostValidateLarkUrl, data });
+}
+
+// 校验国际飞书外链接
+export function validateLarkSuiteConfig(data: LarkInfo) {
+  return MSR.post({ url: PostValidateLarkSuiteUrl, data });
 }
 
 // 开启企业微信登陆
@@ -55,9 +100,14 @@ export function enableDingTalk(data: EnableEditorRequest) {
   return MSR.post({ url: PostDingTalkEnableUrl, data });
 }
 
-// 获取所有平台配置基础信息
-export function getPlatformSourceList() {
-  return MSR.get<PlatformSourceList>({ url: GetPlatformInfoUrl });
+// 开启飞书登陆
+export function enableLark(data: EnableEditorRequest) {
+  return MSR.post({ url: PostLarkEnableUrl, data });
+}
+
+// 开启国际飞书登陆
+export function enableLarkSuite(data: EnableEditorRequest) {
+  return MSR.post({ url: PostLarkSuiteEnableUrl, data });
 }
 
 // 开启企业微信登陆
@@ -68,4 +118,14 @@ export function closeValidateWeCom() {
 // 开启钉钉登陆
 export function closeValidateDingTalk() {
   return MSR.post({ url: PostDingTalkValidateFalseUrl });
+}
+
+// 开启飞书登陆
+export function closeValidateLark() {
+  return MSR.post({ url: PostLarkValidateFalseUrl });
+}
+
+// 开启国际飞书登陆
+export function closeValidateLarkSuite() {
+  return MSR.post({ url: PostLarkSuiteValidateFalseUrl });
 }
--- frontend/src/api/requrls/setting/qrCode.ts
+++ frontend/src/api/requrls/setting/qrCode.ts
@@ -1,11 +1,24 @@
+export const GetPlatformInfoUrl = '/setting/get/platform/info';
 export const PostValidateWeComUrl = '/we_com/validate';
 export const GetWeComInfoUrl = '/we_com/info/with_detail';
 export const PostWeComSaveUrl = '/we_com/save';
 export const PostWeComEnableUrl = '/we_com/enable';
+export const PostWeComValidateFalseUrl = '/we_com/change/validate';
+
 export const PostValidateDingTalkUrl = '/ding_talk/validate';
 export const GetDingTalkInfoUrl = '/ding_talk/info/with_detail';
 export const PostDingTalkSaveUrl = '/ding_talk/save';
 export const PostDingTalkEnableUrl = '/ding_talk/enable';
-export const GetPlatformInfoUrl = '/setting/get/platform/info';
-export const PostWeComValidateFalseUrl = '/we_com/change/validate';
 export const PostDingTalkValidateFalseUrl = '/ding_talk/change/validate';
+
+export const PostValidateLarkUrl = '/lark/validate';
+export const GetLarkInfoUrl = '/lark/info/with_detail';
+export const PostLarkSaveUrl = '/lark/save';
+export const PostLarkEnableUrl = '/lark/enable';
+export const PostLarkValidateFalseUrl = '/lark/change/validate';
+
+export const PostValidateLarkSuiteUrl = '/lark_suite/validate';
+export const GetLarkSuiteInfoUrl = '/lark_suite/info/with_detail';
+export const PostLarkSuiteSaveUrl = '/lark_suite/save';
+export const PostLarkSuiteEnableUrl = '/lark_suite/enable';
+export const PostLarkSuiteValidateFalseUrl = '/lark_suite/change/validate';
