34ebcd8371f12420aeb8881fc19a0b94198c210b
teukkk <mengyuan.lv@fit2cloud.com>
2024-07-03 15:12:31 +0800

    refactor(功能用例): 优化用例评审脑图查看

--- frontend/src/components/pure/ms-minder-editor/main/header.vue
+++ frontend/src/components/pure/ms-minder-editor/main/header.vue
@@ -14,14 +14,7 @@
         <MsIcon type="icon-icon_full_screen_one" class="text-[var(--color-text-4)]" />
       </MsButton>
     </a-tooltip>
-    <a-button
-      v-if="props.showSaveButton"
-      type="outline"
-      :disabled="props.disabled"
-      class="px-[8px] py-[2px] text-[12px]"
-      size="small"
-      @click="save"
-    >
+    <a-button v-if="!props.disabled" type="outline" class="px-[8px] py-[2px] text-[12px]" size="small" @click="save">
       {{ t('minder.main.main.save') }}
     </a-button>
   </div>
@@ -39,7 +32,6 @@
   const props = defineProps<{
     iconButtons?: MinderIconButtonItem[];
     disabled?: boolean;
-    showSaveButton?: boolean;
   }>();
   const emit = defineEmits<{
     (e: 'click', eventTag: string): void;
