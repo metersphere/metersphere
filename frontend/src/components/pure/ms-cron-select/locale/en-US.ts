217acf8a0db582c09b2c9b6a52ffb4b212fc9a16
baiqi <qi.bai@fit2cloud.com>
2024-07-04 16:31:06 +0800

    feat(cron): ms-cron-select组件

---
+++ frontend/src/components/pure/ms-cron-select/locale/en-US.ts
@@ -0,0 +1,6 @@
+export default {
+  'ms.cron.select.timeTaskHour': '(Every hour)',
+  'ms.cron.select.timeTaskSixHour': '(Every 6 hours)',
+  'ms.cron.select.timeTaskTwelveHour': '(Every 12 hours)',
+  'ms.cron.select.timeTaskDay': '(Every day)',
+};
