cb37911b9f754b14d14f6e01ec19971a6ac56ff7
guoyuqi <xiaomeinvG@126.com>
2024-07-11 18:10:17 +0800

    refactor(系统设置): 去掉飞书扫码登陆遗留的url的参数

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -696,6 +696,16 @@ export function getQueryVariable(variable: string) {
   }
 }
 
+export function getUrlParameterWidthRegExp(name: string) {
+  const url = window.location.href;
+  name = name.replace(/[[\]]/g, '\\$&');
+  const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`);
+  const results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
 let lastTimestamp = 0;
 let sequence = 0;
 
