80f9c598e3a71edbe32b8690972e8a9b3c1456a8
xinxin.wu <xinxin.wu@fit2cloud.com>
2024-07-12 18:38:53 +0800

    fix(测试计划): 去掉多余的

--- frontend/src/views/test-plan/report/detail/component/config.vue
+++ frontend/src/views/test-plan/report/detail/component/config.vue
@@ -278,8 +278,8 @@
   watch(
     [() => configList.value, () => cardItemList.value],
     () => {
-      const configValue = resetConfigEditList(cloneDeep(configList.value));
-      const cardItemValue = resetConfigEditList(cloneDeep(cardItemList.value));
+      const configValue = resetConfigEditList(configList.value);
+      const cardItemValue = resetConfigEditList(cardItemList.value);
 
       const isisEqualList = props.isGroup ? cloneDeep(defaultGroupConfig) : cloneDeep(defaultSingleConfig);
       if (!isEqual(configValue, isisEqualList) || (!isEqual(cardItemValue, isisEqualList) && !isInit.value)) {
