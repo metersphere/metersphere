4dbe854cf92c4ec0eb9582b8939910f889ffbc19
teukkk <mengyuan.lv@fit2cloud.com>
2024-07-19 15:52:15 +0800

    style: 富文本@下拉列表超过7个选项就滑动
    --bug=1044182 --user=吕梦园
    https://www.tapd.cn/55049933/bugtrace/bugs/view/1155049933001044182

--- frontend/src/components/pure/ms-rich-text/extensions/mention/MentionList.vue
+++ frontend/src/components/pure/ms-rich-text/extensions/mention/MentionList.vue
@@ -82,14 +82,17 @@
   });
 </script>
 
-<style lang="scss">
+<style lang="less">
   .items {
     position: relative;
-    overflow: hidden;
+    overflow: auto;
     padding: 8px;
+    max-height: 220px;
     border-radius: 4px;
     color: block;
     box-shadow: 0 0 0 1px rgba(0 0 0 / 5%), 0 10px 20px rgba(0 0 0 / 10%);
+    .ms-scroll-bar();
+
     @apply bg-white;
   }
   .item {
