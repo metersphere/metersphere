95917773a13b487b848fa20ec96bdc0859006199
baiqi <qi.bai@fit2cloud.com>
2024-07-23 11:17:56 +0800

    fix(接口测试): 参数表格提取应用时出现重复参数&场景导入系统请求选择项目下拉框文本超长问题修复

--- frontend/src/views/api-test/components/paramTable.vue
+++ frontend/src/views/api-test/components/paramTable.vue
@@ -853,9 +853,9 @@
       const lastLineData = paramsData.value[rowIndex]; // 上一行数据
       const selectColumnKeys = props.columns.filter((e) => e.options).map((e) => e.dataIndex); // 找到下拉框选项的列
       const nextLine = {
+        ...cloneDeep(defaultLineData.value), // 深拷贝，避免有嵌套引用类型，数据隔离
         id,
         enable: true, // 是否勾选
-        ...cloneDeep(defaultLineData.value), // 深拷贝，避免有嵌套引用类型，数据隔离
       } as any;
       selectColumnKeys.forEach((key) => {
         // 如果是更改了下拉框导致添加新的一列，需要将更改后的下拉框的值应用到下一行（产品为了方便统一输入参数类型）
