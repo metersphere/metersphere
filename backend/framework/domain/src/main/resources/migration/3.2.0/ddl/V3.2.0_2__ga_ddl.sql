de429a90fb982ca964ecf051efb0fd27b26dbbe8
song-cc-rock <changchang.song@fit2cloud.com>
2024-08-06 10:13:47 +0800

    refactor(测试计划): 报告生成缺陷明细表字段

--- backend/framework/domain/src/main/resources/migration/3.2.0/ddl/V3.2.0_2__ga_ddl.sql
+++ backend/framework/domain/src/main/resources/migration/3.2.0/ddl/V3.2.0_2__ga_ddl.sql
@@ -4,6 +4,8 @@ SET SESSION innodb_lock_wait_timeout = 7200;
 ALTER TABLE api_test_case ADD api_change BIT(1) DEFAULT 0 NOT NULL COMMENT '接口定义参数变更标识';
 ALTER TABLE api_test_case ADD ignore_api_change BIT(1) DEFAULT 0 NOT NULL COMMENT '忽略接口定义参数变更';
 ALTER TABLE api_test_case ADD ignore_api_diff BIT(1) DEFAULT 0 NOT NULL COMMENT '忽略接口与用例参数不一致';
+ALTER TABLE test_plan_report_bug MODIFY bug_handle_user VARCHAR(255) NULL COMMENT '缺陷处理人';
+
 
 -- set innodb lock wait timeout to default
 SET SESSION innodb_lock_wait_timeout = DEFAULT;
