03b7f9b77bd753defd31a942020c9429a297eab5
fit2-zhao <yong.zhao@fit2cloud.com>
2024-08-19 12:18:39 +0800

    chore: 初始化示例数据评审人

--- backend/framework/domain/src/main/resources/migration/3.2.0/dml/V3.2.0_2_1__data.sql
+++ backend/framework/domain/src/main/resources/migration/3.2.0/dml/V3.2.0_2_1__data.sql
@@ -5,3 +5,18 @@ SET SESSION innodb_lock_wait_timeout = 7200;
 -- 项目管理员、项目成员增加权限
 INSERT INTO user_role_permission (id, role_id, permission_id) VALUES (UUID_SHORT(), 'project_admin', 'FUNCTIONAL_CASE:READ+EXPORT');
 INSERT INTO user_role_permission (id, role_id, permission_id) VALUES (UUID_SHORT(), 'project_member', 'FUNCTIONAL_CASE:READ+EXPORT');
+
+-- 补充示例数据评审人
+INSERT INTO case_review_functional_case_user VALUES ('970662624288768', '999009408442368', 'admin');
+INSERT INTO case_review_functional_case_user VALUES ('974785792892928', '999009408442368', 'admin');
+INSERT INTO case_review_functional_case_user VALUES ('978908961497088', '999009408442368', 'admin');
+INSERT INTO case_review_functional_case_user VALUES ('988271990202368', '999009408442368', 'admin');
+INSERT INTO case_review_functional_case_user VALUES ('995917031989248', '999009408442368', 'admin');
+INSERT INTO case_review_functional_case_user VALUES ('649072653148160', '999009408442368', 'admin');
+INSERT INTO case_review_functional_case_user VALUES ('650996798496768', '999009408442368', 'admin');
+INSERT INTO case_review_functional_case_user VALUES ('655978960560128', '999009408442368', 'admin');
+INSERT INTO case_review_functional_case_user VALUES ('668829502709760', '999009408442368', 'admin');
+INSERT INTO case_review_functional_case_user VALUES ('674739377709056', '999009408442368', 'admin');
+INSERT INTO case_review_functional_case_user VALUES ('680735152054272', '999009408442368', 'admin');
+INSERT INTO case_review_functional_case_user VALUES ('682968535048192', '999009408442368', 'admin');
+INSERT INTO case_review_functional_case_user VALUES ('693894931849216', '999009408442368', 'admin');
