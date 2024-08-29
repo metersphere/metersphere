1e9ecd51ba39a68cd4d2566a23fc1a4ae1454c96
WangXu10 <xu.wang@fit2cloud.com>
2024-08-29 15:28:37 +0800

    feat(测试计划): 测试计划详情用例脑图批量新增缺陷

---
+++ backend/services/test-plan/src/main/java/io/metersphere/plan/controller/TestPlanFunctionalCaseMinderController.java
@@ -0,0 +1,46 @@
+package io.metersphere.plan.controller;
+
+import io.metersphere.bug.domain.Bug;
+import io.metersphere.bug.dto.request.BugEditRequest;
+import io.metersphere.bug.service.BugService;
+import io.metersphere.plan.dto.request.TestPlanCaseMinderBatchAddBugRequest;
+import io.metersphere.plan.service.TestPlanFunctionalCaseMinderService;
+import io.metersphere.sdk.constants.PermissionConstants;
+import io.metersphere.sdk.util.BeanUtils;
+import io.metersphere.system.security.CheckOwner;
+import io.metersphere.system.utils.SessionUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Tag(name = "测试计划-功能用例-脑图-操作")
+@RestController
+@RequestMapping("/test-plan/functional/case/minder")
+public class TestPlanFunctionalCaseMinderController {
+
+    @Resource
+    private BugService bugService;
+    @Resource
+    private TestPlanFunctionalCaseMinderService testPlanFunctionalCaseMinderService;
+
+    @PostMapping("/batch/add-bug")
+    @Operation(summary = "测试计划-功能用例-脑图-批量添加缺陷")
+    @RequiresPermissions(PermissionConstants.TEST_PLAN_READ_EXECUTE)
+    @CheckOwner(resourceId = "#request.getProjectId()", resourceType = "project")
+    public void minderBatchAddBug(@Validated @RequestPart("request") TestPlanCaseMinderBatchAddBugRequest request,
+                                  @RequestPart(value = "files", required = false) List<MultipartFile> files) {
+        BugEditRequest bugEditRequest = new BugEditRequest();
+        BeanUtils.copyBean(bugEditRequest, request);
+        Bug bug = bugService.addOrUpdate(bugEditRequest, files, SessionUtils.getUserId(), SessionUtils.getCurrentOrganizationId(), false);
+        testPlanFunctionalCaseMinderService.minderBatchAssociateBug(request, bug.getId(), SessionUtils.getUserId());
+    }
+}
---
+++ backend/services/test-plan/src/main/java/io/metersphere/plan/dto/request/TestPlanCaseMinderBatchAddBugRequest.java
@@ -0,0 +1,22 @@
+package io.metersphere.plan.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author wx
+ */
+@Data
+public class TestPlanCaseMinderBatchAddBugRequest extends TestPlanCaseBatchAddBugRequest {
+
+    @Schema(description = "脑图选中的模块id集合")
+    private List<String> minderModuleIds;
+
+    @Schema(description = "脑图选中的用例id集合")
+    private List<String> minderCaseIds;
+
+    @Schema(description = "脑图选中的项目id集合")
+    private List<String> minderProjectIds;
+}
