493af86b3e31d610efc3771040951953fd821f0f
WangXu10 <xu.wang@fit2cloud.com>
2024-09-11 18:25:36 +0800

    fix(测试计划): 测试计划搜索问题
    --bug=1045573 --user=王旭 【测试测试】功能用例列表-按执行人筛选后-全选所有页-批量修改执行人报500 https://www.tapd.cn/55049933/s/1577066

--- backend/services/test-plan/src/main/java/io/metersphere/plan/service/TestPlanFunctionalCaseService.java
+++ backend/services/test-plan/src/main/java/io/metersphere/plan/service/TestPlanFunctionalCaseService.java
@@ -243,7 +243,7 @@ public class TestPlanFunctionalCaseService extends TestPlanResourceService {
 
 
     public List<TestPlanCasePageResponse> getFunctionalCasePage(TestPlanCaseRequest request, boolean deleted, String projectId) {
-        filterCaseRequest(request);
+        request.setNullExecutorKey(filterCaseRequest(request.getFilter()));
         List<TestPlanCasePageResponse> functionalCaseLists = extTestPlanFunctionalCaseMapper.getCasePage(request, deleted, request.getSortString());
         if (CollectionUtils.isEmpty(functionalCaseLists)) {
             return new ArrayList<>();
@@ -369,7 +369,7 @@ public class TestPlanFunctionalCaseService extends TestPlanResourceService {
 
 
     public Map<String, Long> moduleCount(TestPlanCaseModuleRequest request) {
-        filterCaseRequest(request);
+        request.setNullExecutorKey(filterCaseRequest(request.getFilter()));
         switch (request.getTreeType()) {
             case TreeTypeEnums.MODULE:
                 return getModuleCount(request);
@@ -464,6 +464,7 @@ public class TestPlanFunctionalCaseService extends TestPlanResourceService {
 
     public List<String> doSelectIds(BasePlanCaseBatchRequest request) {
         if (request.isSelectAll()) {
+            request.setNullExecutorKey(filterCaseRequest(request.getCondition().getFilter()));
             List<String> ids = extTestPlanFunctionalCaseMapper.getIds(request, false);
             if (CollectionUtils.isNotEmpty(request.getExcludeIds())) {
                 ids.removeAll(request.getExcludeIds());
@@ -932,17 +933,16 @@ public class TestPlanFunctionalCaseService extends TestPlanResourceService {
     /**
      * 处理执行人为空过滤参数
      *
-     * @param request 请求参数
+     * @param filter 请求参数
      */
-    protected void filterCaseRequest(TestPlanCaseRequest request) {
-        Map<String, List<String>> filter = request.getFilter();
+    protected boolean filterCaseRequest(Map<String, List<String>> filter) {
         if (filter != null && filter.containsKey(EXECUTOR)) {
             List<String> filterExecutorIds = filter.get(EXECUTOR);
             if (CollectionUtils.isNotEmpty(filterExecutorIds)) {
-                boolean containNullKey = filterExecutorIds.removeIf(id -> StringUtils.equals(id, "-"));
-                request.setNullExecutorKey(containNullKey);
+                return filterExecutorIds.contains("-");
             }
         }
+        return false;
     }
 
     public void batchAssociateBug(TestPlanCaseBatchAddBugRequest request, String bugId, String userId) {
