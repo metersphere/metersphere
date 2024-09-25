6da945949612a487fc8b3315700704b9544fd207
guoyuqi <xiaomeinvG@126.com>
2024-09-25 18:49:12 +0800

    feat(用例管理): 用例评审增加高级搜索

--- backend/services/case-management/src/main/java/io/metersphere/functional/mapper/ExtCaseReviewMapper.java
+++ backend/services/case-management/src/main/java/io/metersphere/functional/mapper/ExtCaseReviewMapper.java
@@ -5,6 +5,7 @@ import io.metersphere.functional.dto.CaseReviewDTO;
 import io.metersphere.functional.request.CaseReviewBatchRequest;
 import io.metersphere.functional.request.CaseReviewPageRequest;
 import io.metersphere.project.dto.ModuleCountDTO;
+import io.metersphere.system.interceptor.BaseConditionFilter;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -18,18 +19,22 @@ public interface ExtCaseReviewMapper {
 
     Long getPos(@Param("projectId") String projectId);
 
+    @BaseConditionFilter
     List<CaseReviewDTO> list(@Param("request") CaseReviewPageRequest request);
 
     Long getPrePos(@Param("projectId") String projectId, @Param("basePos") Long basePos);
 
     Long getLastPos(@Param("projectId") String projectId, @Param("basePos") Long basePos);
 
+    @BaseConditionFilter
     List<String> getIds(@Param("request") CaseReviewBatchRequest request, @Param("projectId") String projectId);
 
     void batchMoveModule(@Param("request") CaseReviewBatchRequest request, @Param("ids") List<String> ids, @Param("userId") String userId);
 
+    @BaseConditionFilter
     List<ModuleCountDTO> countModuleIdByKeywordAndFileType(@Param("request") CaseReviewPageRequest request);
 
+    @BaseConditionFilter
     long caseCount(@Param("request") CaseReviewPageRequest request);
 
 
