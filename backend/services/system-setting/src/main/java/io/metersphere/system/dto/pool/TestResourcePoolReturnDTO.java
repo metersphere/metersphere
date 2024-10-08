1cef362df62abff23ddf768f1fac36dd26d99a99
fit2-zhao <yong.zhao@fit2cloud.com>
2024-10-08 15:51:01 +0800

    chore: 公共 DTO 增加资源池类型

--- backend/services/system-setting/src/main/java/io/metersphere/system/dto/pool/TestResourcePoolReturnDTO.java
+++ backend/services/system-setting/src/main/java/io/metersphere/system/dto/pool/TestResourcePoolReturnDTO.java
@@ -10,6 +10,9 @@ import lombok.EqualsAndHashCode;
 public class TestResourcePoolReturnDTO  extends TestResourcePool {
     private TestResourceReturnDTO testResourceReturnDTO;
 
+    @Schema(description =  "资源池类型, Node/Kubernetes")
+    private String type;
+
     @Schema(description =  "资源池是否在使用中")
     private Boolean inUsed;
 }
