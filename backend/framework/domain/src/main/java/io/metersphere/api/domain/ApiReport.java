0f0cbef09cb6ca7d4d2b6150fe8e08f3226e9eff
WangXu10 <xu.wang@fit2cloud.com>
2024-10-15 19:00:35 +0800

    feat(任务中心): 新增报告与任务关联关系表

--- backend/framework/domain/src/main/java/io/metersphere/api/domain/ApiReport.java
+++ backend/framework/domain/src/main/java/io/metersphere/api/domain/ApiReport.java
@@ -148,9 +148,6 @@ public class ApiReport implements Serializable {
     @NotNull(message = "{api_report.plan.not_blank}", groups = {Created.class})
     private Boolean plan;
 
-    @Schema(description = "关联任务id(集合报告)/任务项id")
-    private String associatedTaskId;
-
     private static final long serialVersionUID = 1L;
 
     public enum Column {
@@ -186,8 +183,7 @@ public class ApiReport implements Serializable {
         assertionPassRate("assertion_pass_rate", "assertionPassRate", "VARCHAR", false),
         scriptIdentifier("script_identifier", "scriptIdentifier", "VARCHAR", false),
         execStatus("exec_status", "execStatus", "VARCHAR", false),
-        plan("plan", "plan", "BIT", true),
-        associatedTaskId("associated_task_id", "associatedTaskId", "VARCHAR", false);
+        plan("plan", "plan", "BIT", true);
 
         private static final String BEGINNING_DELIMITER = "`";
 
--- backend/framework/domain/src/main/java/io/metersphere/api/domain/ApiScenarioReport.java
+++ backend/framework/domain/src/main/java/io/metersphere/api/domain/ApiScenarioReport.java
@@ -151,9 +151,6 @@ public class ApiScenarioReport implements Serializable {
     @NotNull(message = "{api_scenario_report.plan.not_blank}", groups = {Created.class})
     private Boolean plan;
 
-    @Schema(description = "关联任务id(集合报告)/任务项id")
-    private String associatedTaskId;
-
     private static final long serialVersionUID = 1L;
 
     public enum Column {
@@ -190,8 +187,7 @@ public class ApiScenarioReport implements Serializable {
         scriptIdentifier("script_identifier", "scriptIdentifier", "VARCHAR", false),
         waitingTime("waiting_time", "waitingTime", "BIGINT", false),
         execStatus("exec_status", "execStatus", "VARCHAR", false),
-        plan("plan", "plan", "BIT", true),
-        associatedTaskId("associated_task_id", "associatedTaskId", "VARCHAR", false);
+        plan("plan", "plan", "BIT", true);
 
         private static final String BEGINNING_DELIMITER = "`";
 
