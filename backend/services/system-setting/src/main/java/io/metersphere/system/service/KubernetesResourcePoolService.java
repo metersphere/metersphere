620b834789709b67b53f42a5991da8e772849822
guoyuqi <xiaomeinvG@126.com>
2024-10-17 15:32:45 +0800

    refactor(系统管理): 去掉多余文件

--- backend/services/system-setting/src/main/java/io/metersphere/system/service/KubernetesResourcePoolService.java
+++
@@ -1,9 +0,0 @@
-package io.metersphere.system.service;
-
-
-import io.metersphere.system.dto.pool.TestResourceDTO;
-
-public interface KubernetesResourcePoolService {
-
-    boolean validate(TestResourceDTO testResourceDTO);
-}
