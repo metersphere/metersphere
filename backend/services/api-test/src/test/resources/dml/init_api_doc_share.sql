0aaa24ad23e4da8be86a5852b3b7630e177dd759
song-cc-rock <changchang.song@fit2cloud.com>
2024-10-24 14:32:12 +0800

    feat(接口测试): 定义分享详情补充插件脚本接口

---
+++ backend/services/api-test/src/test/resources/dml/init_api_doc_share.sql
@@ -0,0 +1,3 @@
+-- 插入测试数据
+INSERT INTO `api_definition` (`id`, `name`, `protocol`, `method`, `path`, `status`, `num`, `tags`, `pos`, `project_id`, `module_id`, `latest`, `version_id`, `ref_id`, `description`, `create_time`, `create_user`, `update_time`, `update_user`, `delete_user`, `delete_time`, `deleted`) VALUES
+('doc-share-id', 'test1', 'HTTP', 'POST', '/api/admin/1', 'PROCESSING', 1001, '[\"test3\",\"te\"]', 1, '100001100001', 'root', b'1', '100570499574136985', '1001', NULL, 1699500298164, 'admin', 1699500298162, 'admin', NULL, NULL, b'0');
