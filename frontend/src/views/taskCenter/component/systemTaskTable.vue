7f4d6558d573a615139a457e49d487572a1acca9
song-cc-rock <changchang.song@fit2cloud.com>
2024-10-28 11:17:33 +0800

    fix(任务中心): 未展示上次完成时间&&排序有误
    --bug=1047993 --user=宋昌昌 【任务中心】系统后台任务列表-定时任务开启且执行完成-未展示上次完成时间 https://www.tapd.cn/55049933/s/1598986

--- frontend/src/views/taskCenter/component/systemTaskTable.vue
+++ frontend/src/views/taskCenter/component/systemTaskTable.vue
@@ -228,7 +228,7 @@
     },
     {
       title: 'ms.taskCenter.lastFinishTime',
-      dataIndex: 'lastFinishTime',
+      dataIndex: 'lastTime',
       width: 170,
       sortable: {
         sortDirections: ['ascend', 'descend'],
@@ -360,7 +360,7 @@
         ...item,
         runRuleLoading: false,
         createTime: item.createTime ? dayjs(item.createTime).format('YYYY-MM-DD HH:mm:ss') : '-',
-        lastFinishTime: item.lastFinishTime ? dayjs(item.lastFinishTime).format('YYYY-MM-DD HH:mm:ss') : '-',
+        lastTime: item.lastTime && item.lastTime !== -1 ? dayjs(item.lastTime).format('YYYY-MM-DD HH:mm:ss') : '-',
         nextTime: item.nextTime ? dayjs(item.nextTime).format('YYYY-MM-DD HH:mm:ss') : '-',
       };
     }
