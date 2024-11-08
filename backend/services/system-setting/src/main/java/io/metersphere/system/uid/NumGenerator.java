fbffe46a996a77c5ea771c4913213840ae880c27
fit2-zhao <yong.zhao@fit2cloud.com>
2024-11-08 10:13:41 +0800

    fix: 尝试解决任务编号重复问题
    https://github.com/metersphere/metersphere/issues/34056

--- backend/services/system-setting/src/main/java/io/metersphere/system/uid/NumGenerator.java
+++ backend/services/system-setting/src/main/java/io/metersphere/system/uid/NumGenerator.java
@@ -37,14 +37,19 @@ public class NumGenerator {
      */
     public static long nextNum(String prefix, ApplicationNumScope scope) {
         RIdGenerator idGenerator = redisson.getIdGenerator(prefix + "_" + scope.name());
+
         // 二级的用例
         if (SUB_NUM.contains(scope)) {
             // 每次都尝试初始化，容量为1，只有一个线程可以初始化成功
-            idGenerator.tryInit(1, LIMIT);
+            if (!idGenerator.isExists()) {
+                idGenerator.tryInit(1, LIMIT);
+            }
             return Long.parseLong(prefix.split("_")[1] + StringUtils.leftPad(String.valueOf(idGenerator.nextId()), 3, "0"));
         } else {
             // 每次都尝试初始化，容量为1，只有一个线程可以初始化成功
-            idGenerator.tryInit(INIT, LIMIT);
+            if (!idGenerator.isExists()) {
+                idGenerator.tryInit(INIT, LIMIT);
+            }
             return idGenerator.nextId();
         }
     }
