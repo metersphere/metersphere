099a3a8616f337910365bbb6de583e0abecdcfb3
Jianguo-Genius <herman.song.yang@gmail.com>
2024-11-22 10:43:17 +0800

    fix(接口测试): Har导入场景时排除gif路径

--- backend/services/api-test/src/main/java/io/metersphere/api/parser/api/dataimport/HarParserApiScenario.java
+++ backend/services/api-test/src/main/java/io/metersphere/api/parser/api/dataimport/HarParserApiScenario.java
@@ -81,7 +81,8 @@ public class HarParserApiScenario implements ApiScenarioImportParser {
             HarRequest harRequest = entry.request;
             if (harRequest != null) {
                 // css 、 js 略过
-                if (StringUtils.equalsIgnoreCase(harRequest.method, HttpMethodConstants.GET.name()) && StringUtils.endsWithAny(harRequest.url.toLowerCase(), ".svg", ".css", ".js", ".png", ".jpg", ".jpeg")) {
+                if (StringUtils.equalsIgnoreCase(harRequest.method, HttpMethodConstants.GET.name()) &&
+                        StringUtils.endsWithAny(harRequest.url.toLowerCase(), ".svg", ".css", ".js", ".png", ".jpg", ".jpeg", ".gif")) {
                     continue;
                 }
                 String url = harRequest.url;
