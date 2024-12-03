6f0d97629edba925d5fcb2b585398e6dd831f2b8
guoyuqi <xiaomeinvG@126.com>
2024-12-03 15:27:27 +0800

    feat(工作台): 新增测试计划概览接口

--- backend/services/dashboard/src/test/resources/dml/init_dashboard.sql
+++ backend/services/dashboard/src/test/resources/dml/init_dashboard.sql
@@ -129,6 +129,16 @@ INSERT INTO test_plan(id, num, project_id, group_id, module_id, name, status, ty
     VALUE ('dashboard_group-plan', 500, '100001100001', 'NONE', 'case_plan_module', 'test_plan_group_name_three', 'NOT_ARCHIVED', 'GROUP', null, UNIX_TIMESTAMP() * 1000,'admin',
            UNIX_TIMESTAMP() * 1000,'admin',UNIX_TIMESTAMP() * 1000, UNIX_TIMESTAMP() * 1000, UNIX_TIMESTAMP() * 1000, UNIX_TIMESTAMP() * 1000, null);
 
+INSERT INTO test_plan_functional_case(id, test_plan_id, functional_case_id, create_time, create_user, execute_user, last_exec_time, last_exec_result, pos, test_plan_collection_id)
+VALUES ('test_plan_functional_case_dashboard_id', 'dashboard_group-plan', 'dashboard_TEST_FUNCTIONAL_CASE_ID_g', 1669174143999, 'admin', 'admin', 1669174143999, 'SUCCESS', 5000, '123');
+
+INSERT INTO test_plan_api_case(`id`, `test_plan_id`, `api_case_id`, `environment_id`, `last_exec_result`, `last_exec_report_id`, `execute_user`, `create_time`, `create_user`, `pos`, `test_plan_collection_id`) VALUES
+    ('dashboard_plan_api_case_1', 'dashboard_group-plan', 'dashboard_ac_4', '1', 'PASSED', NULL, 'admin', 1716370415311, 'admin', 1, '123');
+
+INSERT INTO test_plan_api_scenario (id, test_plan_id, api_scenario_id, environment_id, execute_user, last_exec_result, last_exec_report_id, create_time, create_user, pos, test_plan_collection_id, grouped) VALUES
+    ('dashboard_plan_api_scenario_1', 'dashboard_group-plan', 'dashboard_scenario_four', '1', 'admin', 'PASSED', NULL, 1716370415311, 'admin', 1, '123', false);
+
+
 INSERT INTO project_application (project_id, type, type_value) VALUES
                                                                    ('100001100001', 'BUG_SYNC_BUG_PLATFORM_CONFIG', '{"jiraKey":"TES","jiraBugTypeId":"10009"}'),
                                                                    ('100001100001', 'BUG_SYNC_PLATFORM_KEY', 'jira'),
