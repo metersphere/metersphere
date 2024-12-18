f78813f3549085517fd6cea7f7cf29a1f9811d5b
AgAngle <1323481023@qq.com>
2024-12-18 14:58:36 +0800

    refactor(接口测试): 避免同一个事务中多次调用获取场景详情方法，导致数据异常
    --bug=1050539 --user=陈建星 【测试计划】github#34649，测试计划执行时，有时候步骤会执行多次 https://www.tapd.cn/55049933/s/1633266

--- backend/services/api-test/src/main/java/io/metersphere/api/mapper/ExtApiScenarioStepMapper.java
+++ backend/services/api-test/src/main/java/io/metersphere/api/mapper/ExtApiScenarioStepMapper.java
@@ -13,6 +13,12 @@ import java.util.List;
 public interface ExtApiScenarioStepMapper {
     List<String> getStepIdsByScenarioId(@Param("scenarioId") String scenarioId);
 
+    /**
+     * 这个 sql 执行时，会清理一级缓存
+     * 避免多次调用后数据异常
+     * @param scenarioIds
+     * @return
+     */
     List<ApiScenarioStepDTO> getStepDTOByScenarioIds(@Param("scenarioIds") List<String> scenarioIds);
 
     List<ApiScenarioCsvStep> getCsvStepByScenarioIds(@Param("scenarioIds") List<String> scenarioId);
