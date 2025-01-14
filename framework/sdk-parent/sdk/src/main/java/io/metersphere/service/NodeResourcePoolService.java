b91904231237f1794579b75fb3f21d70b327f8aa
fit2-zhao <yong.zhao@fit2cloud.com>
2025-01-14 18:12:30 +0800

    perf(系统设置): 资源池验证优化
    https://github.com/metersphere/metersphere/issues/34908

--- framework/sdk-parent/sdk/src/main/java/io/metersphere/service/NodeResourcePoolService.java
+++ framework/sdk-parent/sdk/src/main/java/io/metersphere/service/NodeResourcePoolService.java
@@ -6,25 +6,27 @@ import io.metersphere.base.mapper.TestResourceMapper;
 import io.metersphere.commons.constants.ResourceStatusEnum;
 import io.metersphere.commons.exception.MSException;
 import io.metersphere.commons.utils.JSON;
-import io.metersphere.commons.utils.LogUtil;
-import io.metersphere.controller.handler.ResultHolder;
 import io.metersphere.dto.NodeDTO;
 import io.metersphere.dto.TestResourcePoolDTO;
 import io.metersphere.i18n.Translator;
+import jakarta.annotation.Resource;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
 
-import jakarta.annotation.Resource;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
 @Service
@@ -32,17 +34,10 @@ import java.util.stream.Collectors;
 public class NodeResourcePoolService {
     private final static String nodeControllerUrl = "http://%s:%s/status";
 
-    private static final RestTemplate restTemplateWithTimeOut = new RestTemplate();
 
     @Resource
     private TestResourceMapper testResourceMapper;
 
-    static {
-        HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
-        httpRequestFactory.setConnectionRequestTimeout(2000);
-        httpRequestFactory.setConnectTimeout(2000);
-        restTemplateWithTimeOut.setRequestFactory(httpRequestFactory);
-    }
 
     public boolean validate(TestResourcePoolDTO testResourcePool) {
         if (CollectionUtils.isEmpty(testResourcePool.getResources())) {
@@ -56,7 +51,7 @@ public class NodeResourcePoolService {
                     return new ImmutablePair<>(nodeDTO.getIp(), nodeDTO.getPort());
                 })
                 .distinct()
-                .collect(Collectors.toList());
+                .toList();
         if (ipPort.size() < testResourcePool.getResources().size()) {
             MSException.throwException(Translator.get("duplicate_node_ip_port"));
         }
@@ -69,21 +64,60 @@ public class NodeResourcePoolService {
         deleteTestResources(deletedResources);
 
         testResourcePool.setStatus(ResourceStatusEnum.VALID.name());
-        boolean isValid = true;
-        for (TestResource resource : testResourcePool.getResources()) {
-            NodeDTO nodeDTO = JSON.parseObject(resource.getConfiguration(), NodeDTO.class);
-            boolean isValidate = validateNode(nodeDTO);
-            if (!isValidate) {
-                testResourcePool.setStatus(ResourceStatusEnum.INVALID.name());
-                resource.setStatus(ResourceStatusEnum.INVALID.name());
-                isValid = false;
-            } else {
-                resource.setStatus(ResourceStatusEnum.VALID.name());
+        // 动态线程池验证资源节点
+        return validateResources(testResourcePool);
+    }
+
+    /**
+     *  验证资源节点
+     */
+    public boolean validateResources(TestResourcePoolDTO testResourcePool) {
+        ExecutorService executorService = Executors.newFixedThreadPool(Math.min(testResourcePool.getResources().size(), 10));
+        AtomicBoolean shouldStop = new AtomicBoolean(false); // 共享变量
+        try {
+            List<Future<Boolean>> futures = new ArrayList<>();
+
+            for (TestResource resource : testResourcePool.getResources()) {
+                futures.add(executorService.submit(() -> {
+                    if (shouldStop.get()) { // 检查是否需要提前停止
+                        return false;
+                    }
+                    NodeDTO nodeDTO = JSON.parseObject(resource.getConfiguration(), NodeDTO.class);
+                    boolean isValidate = validateNode(nodeDTO);
+                    if (!isValidate) {
+                        shouldStop.set(true); // 标志所有线程应该停止
+                        testResourcePool.setStatus(ResourceStatusEnum.INVALID.name());
+                        resource.setStatus(ResourceStatusEnum.INVALID.name());
+                    } else {
+                        resource.setStatus(ResourceStatusEnum.VALID.name());
+                    }
+                    resource.setTestResourcePoolId(testResourcePool.getId());
+                    updateTestResource(resource);
+                    return isValidate;
+                }));
             }
-            resource.setTestResourcePoolId(testResourcePool.getId());
-            updateTestResource(resource);
+
+            boolean isValid = true;
+            for (Future<Boolean> future : futures) {
+                try {
+                    if (!future.get()) {
+                        isValid = false;
+                        break;
+                    }
+                } catch (ExecutionException | InterruptedException e) {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if (!isValid) {
+                testResourcePool.setStatus(ResourceStatusEnum.INVALID.name());
+            }
+            return isValid;
+        } finally {
+            executorService.shutdownNow(); // 尝试终止所有线程
         }
-        return isValid;
+
     }
 
     private void deleteTestResources(Collection<String> ids) {
@@ -102,21 +136,18 @@ public class NodeResourcePoolService {
         return testResourceMapper.selectByExample(example);
     }
 
-
     private boolean validateNode(NodeDTO node) {
         try {
-            ResponseEntity<ResultHolder> entity = restTemplateWithTimeOut.getForEntity(String.format(nodeControllerUrl, node.getIp(), node.getPort()), ResultHolder.class);
-            ResultHolder body = entity.getBody();
-            if (body == null) {
-                return false;
-            }
-            if (body.getData() != null && StringUtils.equalsIgnoreCase("OK", body.getData().toString())) {
-                return true;
-            }
+            URL url = new URL(String.format(nodeControllerUrl, node.getIp(), node.getPort()));
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(2000);  // 设置连接超时为 2 秒
+            connection.setReadTimeout(2000);     // 设置读取超时为 2 秒
+            int responseCode = connection.getResponseCode();
+            return responseCode == HttpURLConnection.HTTP_OK;  // 判断是否返回 200 OK
         } catch (Exception e) {
-            LogUtil.error(e.getMessage(), e);
+            return false;
         }
-        return false;
     }
 
     private void updateTestResource(TestResource testResource) {
