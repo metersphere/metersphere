c359c3ddbcd227e9c723fa304d3f47fc26aef15e
Somebody-JIAN <herman.song.yang@gmail.com>
2026-08-05 10:55:01 +0800

    Revert "fix: file preview add user check"
    This reverts commit 62b5f945c61487c330a2a1deb62e6177627cb9d3.

--- backend/services/project-management/src/main/java/io/metersphere/project/controller/FilePreviewController.java
+++ backend/services/project-management/src/main/java/io/metersphere/project/controller/FilePreviewController.java
@@ -6,7 +6,6 @@ import io.metersphere.sdk.constants.PermissionConstants;
 import io.metersphere.sdk.constants.UserRoleType;
 import io.metersphere.sdk.exception.MSException;
 import io.metersphere.system.service.PermissionCheckService;
-import io.metersphere.system.utils.SessionUtils;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
@@ -31,7 +30,6 @@ public class FilePreviewController {
     @GetMapping(value = "/original/{userId}/{fileId}")
     @Operation(summary = "预览原图")
     public ResponseEntity<byte[]> originalImg(@PathVariable String userId, @PathVariable String fileId) throws Exception {
-        checkSessionUser(userId, SessionUtils.getUserId());
         FileInformationResponse fileInformationResponse = fileMetadataService.getFileInformation(fileId);
         if (StringUtils.isEmpty(fileInformationResponse.getId())) {
             throw new MSException("file.not.exist");
@@ -45,7 +43,6 @@ public class FilePreviewController {
     @GetMapping(value = "/compressed/{userId}/{fileId}")
     @Operation(summary = "预览缩略图")
     public ResponseEntity<byte[]> compressedImg(@PathVariable String userId, @PathVariable String fileId) throws Exception {
-        checkSessionUser(userId, SessionUtils.getUserId());
         FileInformationResponse fileInformationResponse = fileMetadataService.getFileInformation(fileId);
         if (StringUtils.isEmpty(fileInformationResponse.getId())) {
             throw new MSException("file.not.exist");
@@ -55,11 +52,5 @@ public class FilePreviewController {
             throw  new MSException("http_result_forbidden");
         }
         return fileMetadataService.downloadPreviewImgById(fileId);
-    }
-
-    private void checkSessionUser(String userId, String sessionUserId) {
-        if (!StringUtils.equals(userId, sessionUserId)) {
-            throw new MSException("file.not.exist");
-        }
     }
 }
